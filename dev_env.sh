689b8fb87a1ee5ecd991f6c5ab967d78f16ce132
Alexander Ford <fordas@uw.edu>
2018-02-15 12:24:09 -0800

    Clarify, fixup dev_env.sh

--- dev_env.sh
+++ dev_env.sh
@@ -5,6 +5,8 @@ set -x
 
 TORCH_TARGET_VERSION="0ef1038"
 
+# Requires an active root conda installation see:
+# https://conda.io/miniconda.html
 conda env update -f environment.yml
 source activate tmol
 
@@ -21,5 +23,7 @@ export NO_CUDA=1
 git clean -xd -i
 git checkout $TORCH_TARGET_VERSION
 python setup.py install
+
+popd
 
 pip install -e .
