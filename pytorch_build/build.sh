e62048091bc0d704c59a269403593f7ced18ceaa
Alex Ford <fordas@uw.edu>
2018-03-20 00:37:00 +0000

    Update to nomkl torch development build, link to prebuild torch wheel.
    Seeing issues with conda 4.4 update, unable to get mkl running property with
    pytorch source install. Updating dev environment to use openblas, rather than
    mkl, torch build and adding link to prebuilt wheel for 16.04.

--- pytorch_build/build.sh
+++ pytorch_build/build.sh
@@ -3,12 +3,12 @@
 set -e
 set -x
 
-TORCH_TARGET_VERSION="0ef1038"
+TORCH_TARGET_VERSION="1936753"
 
 # Requires an active root conda installation see:
 # https://conda.io/miniconda.html
 conda env update -f environment.yml
-source activate tmol
+source activate pytorch_install
 
 mkdir -p src
 if [ ! -d "src/pytorch" ]; then
@@ -17,13 +17,11 @@ fi
 
 pushd src/pytorch
 
-export CMAKE_PREFIX_PATH="$(dirname $(which conda))/../" # [anaconda root directory]
+export CMAKE_PREFIX_PATH="$(conda info | grep 'active env location' | sed 's/.*: //')" # [anaconda root directory]
 export NO_CUDA=1
 
 git clean -xd -i
 git checkout $TORCH_TARGET_VERSION
-python setup.py install
+python setup.py bdist_wheel
 
 popd
-
-pip install -e .
