b46a7daac76041e5bada67e942c413b7e9c4b89c
Alex Ford <fordas@uw.edu>
2018-04-08 15:02:28 -0700

    Add basic test coverage for hbond parameter import.

---
+++ tmol/tests/support/rosetta.py
@@ -0,0 +1,16 @@
+import os
+import toolz
+
+try:
+    import pyrosetta
+except ImportError:
+    pyrosetta = None
+
+if "ROSETTA_DATABASE" in os.environ:
+    normpath = toolz.compose(os.path.abspath, os.path.expanduser,
+                             os.path.expandvars)
+    rosetta_database = normpath(os.environ.get("ROSETTA_DATABASE"))
+elif pyrosetta is not None:
+    rosetta_database = os.path.join(os.path.dirname(pyrosetta.__file__), "database")
+else:
+    rosetta_database = None
---
+++ tmol/tests/support/test_scoring.py
@@ -0,0 +1,17 @@
+import os
+import yaml
+import unittest
+
+from .rosetta import rosetta_database
+
+from tmol.support.scoring.hbond_param_import import RosettaHBParams
+
+
+class TestHBondParamImport(unittest.TestCase):
+    @unittest.skipIf(not rosetta_database, "rosetta database not available")
+    def test_hbond_parm_import(self):
+        params = RosettaHBParams(
+            os.path.join(rosetta_database,
+                         "scoring/score_functions/hbonds/sp2_elec_params"))
+
+        yaml.load(params.to_yaml())
