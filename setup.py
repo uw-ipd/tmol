be875d47495f667609aa6ae56ed5fb6a425e41ce
Alexander Ford <fordas@uw.edu>
2018-04-08 12:02:57 -0700

    Add baseline dev, test and CI configuration. (#2)
    Add baseline dev environment configuration, accessible via `dev_env` setup
    script. Assembles conda-based development environment and setups basic dev repo
    hooks. Partitioning conda environments into baseline `environment.yml` for primary run
    requirements and `dev_environment.yml` for development and experimentation requirements.
    
    Add pytest based test configuration. Currently using unittest fixtures for test
    implementation, but pytest for execution.
    
    Add travis and codecov configuration for CI and coverage analysis. Travis
    configuration utilizes "baseline" conda env, rather than dev env, and caches
    installed packages for each run.

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python
 
-import os
+import sys
 from setuptools import setup
 import subprocess
 import re
 
+
 def git_version():
     git_describe = subprocess.check_output(
-            ["git", "describe", "--long", "--tags", "--match", "[0-9]*"]
-        ).strip().decode()
+        ["git", "describe", "--long", "--tags", "--match", "[0-9]*"]).strip().decode()
 
-    describe_match = re.match(
-        r"(?P<version>[0-9.]+)(-(?P<post_revision>\d+)-g(?P<commit>\w+))?",
-        git_describe)
+    describe_match = re.match(r"(?P<version>[0-9.]+)(-(?P<post_revision>\d+)-g(?P<commit>\w+))?",
+                              git_describe)
     if not describe_match:
         raise ValueError("Invalid version.", git_describe)
     else:
@@ -26,13 +25,15 @@ def git_version():
 
     return version
 
+
+needs_pytest = {'pytest', 'test'}.intersection(sys.argv)
+pytest_runner = ['pytest-runner'] if needs_pytest else []
+
 setup(
     name='tmol',
     version=git_version(),
     packages=['tmol'],
-    install_requires=[
-        l.strip()
-        for l in open("requirements.txt").readlines()
-    ],
-    zip_safe=False
-)
+    setup_requires=pytest_runner,
+    tests_require=[l.strip() for l in open("requirements.tests.txt").readlines()],
+    install_requires=[l.strip() for l in open("requirements.txt").readlines()],
+    zip_safe=False)
