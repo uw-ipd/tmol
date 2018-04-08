209c8be876e10b4078ef2600cddd006ab10b621d
Alexander Ford <fordas@uw.edu>
2018-04-08 12:51:41 -0700

    Add initial residue-based packed atomic system and chemical database. (#3)
    Adds initial "atomic system", which represents a molecular system as a set
    bonded, typed atoms. System packs this representation into a potentially-sparse
    array of atoms, storing an atomic coordinate buffer, an atomic type buffer, and
    a sparse atom bond buffer.
    
    Adds "packed residue" system, which represents the atomic system as a set of
    residues allocated into the system's atom array. Residue objects are
    conceptually similar to atomic systems, containing atomic coordinates, types
    and bonds, with the addition of atomic name metadata and "inter-residue"
    connectivity points. Residue objects are potentially flyweight, and may hold
    *either* their own coordinate buffer or attach to a system and hold view into
    the system's coordinate buffer.
    
    Adds initial chemical database interface and chemical database, containing
    definitions of basic atomic types and canonical L-amino acid residue
    definitions. `tmol.database` contains minimal datatype declarations for
    database information, `tmol.system.residue.restypes` processes the basic data
    declarations into "featured" residue and residue type objects.
    
    Adds initial extension of properties system to support "reactive" property
    definitions. Provides support for cached, on-demand property evaluation and
    dependency-based cache invalidation. Supports a "dataflow like" execution model
    within an objects properties.

---
+++ tmol/tests/properties/test_array.py
@@ -0,0 +1,41 @@
+import unittest
+import numpy
+
+import properties
+
+from tmol.properties.array import Array
+from tmol.properties.shape import spec
+
+class testArrayProperty(unittest.TestCase):
+    def test_prop(self):
+        class TType(properties.HasProperties):
+            coord = Array("a single coord", dtype=float, shape=spec[3])
+            coords = Array("coords", dtype=float, shape=spec[:,3])
+            dcoord = Array(
+                    "a coord with default",
+                    default=list(range(10)),
+                    dtype=int,
+                    shape=spec[10])
+
+        t = TType()
+
+        numpy.testing.assert_allclose(t.dcoord, numpy.arange(10))
+        self.assertEqual(t.dcoord.dtype, numpy.int)
+
+        t.coord = list(range(3))
+        numpy.testing.assert_allclose(t.coord, numpy.arange(3))
+        self.assertEqual(t.coord.dtype, numpy.float)
+
+        t.coords = [list(range(3))]
+        numpy.testing.assert_allclose(t.coords, numpy.arange(3).reshape((1, 3)))
+        self.assertEqual(t.coords.dtype, numpy.float)
+
+        with self.assertRaises(ValueError):
+            t.coords = list(range(3))
+
+        with self.assertRaises(ValueError):
+            class InvalidTType(properties.HasProperties):
+                coord = Array("coord",
+                        default = numpy.empty(10),
+                        dtype = float,
+                        shape = spec[3])
---
+++ tmol/utility/log.py
@@ -0,0 +1,14 @@
+import logging
+
+class LoggerMixin:
+    @property
+    def logger(self):
+        logger = getattr(self, "_logger", None)
+
+        if not logger:
+            logger = logging.getLogger(
+                f"{self.__class__.__module__}.{self.__class__.__name__}"
+            )
+            setattr(self, "_logger", logger)
+
+        return logger
