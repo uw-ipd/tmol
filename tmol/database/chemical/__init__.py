7ca4fd0c3e265e949ab731d4d6724a6c98bf8bb8
Andrew Leaver-Fay <aleaverfay@gmail.com>
2018-04-13 16:15:04 -0400

    Construction of residue-level atom trees seems to be working. Next up: whole structure atom trees. Afterwards, time to start working on segmented-scan-based refolding

--- tmol/database/chemical/__init__.py
+++ tmol/database/chemical/__init__.py
@@ -7,27 +7,38 @@ import os
 import yaml
 import properties
 
-
 @attr.s(auto_attribs=True, frozen=True, slots=True)
 class Atom:
-    name: str
-    atom_type: str
+    name : str
+    atom_type : str
 
+@attr.s(auto_attribs=True, frozen=True, slots=True)
+class Icoor:
+    name : str
+    phi  : float
+    theta: float
+    d    : float
+    parent : str
+    grand_parent : str
+    great_grand_parent : str
 
 @attr.s(auto_attribs=True, frozen=True, slots=True)
 class Residue:
-    name: str
-    name3: str
-    atoms: Tuple[Atom, ...]
-    bonds: Tuple[Tuple[str, str], ...]
-    lower_connect: str
-    upper_connect: str
-
+    name  : str
+    name3 : str
+    atoms : Tuple[Atom, ...]
+    bonds : Tuple[Tuple[str, str], ...]
+    lower_connect : str
+    upper_connect : str
+    icoor : Tuple[Icoor,...]
+    mainchain : Tuple[str,...]
+    cutbond : Tuple[ Tuple[str,str], ... ]
+    chi : Tuple[ Tuple[str,str,str,str], ... ]
 
 @attr.s(auto_attribs=True, frozen=True, slots=True)
 class ChemicalDatabase(properties.HasProperties):
-    atom_types: Tuple[str, ...]
-    residues: Tuple[Residue, ...]
+    atom_types : Tuple[Tuple[str,str], ...]
+    residues : Tuple[Residue, ...]
 
     @classmethod
     def from_file(cls, path):
