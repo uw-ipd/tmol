b973ea64054001ef738ac0e8d0dc49a339ea612e
Alex Ford <fordas@uw.edu>
2018-04-15 15:01:52 -0700

    Fixup pyrosetta test support.

--- tmol/tests/support/rosetta.py
+++ tmol/tests/support/rosetta.py
@@ -5,11 +5,11 @@ import pytest
 pyrosetta_available = True if importlib.util.find_spec("pyrosetta") else False
 
 requires_pyrosetta = pytest.mark.skipif(
-    not rosetta_database_available, "Requires rosetta database."
+    not pyrosetta_available, "Requires pyrosetta"
 )
 
 rosetta_database_available = pyrosetta_available or "ROSETTA_DATABASE" in os.environ
 
 requires_rosetta_database = pytest.mark.skipif(
-    not rosetta_database_available, "Requires rosetta database."
+    not rosetta_database_available, "Requires rosetta database"
 )
