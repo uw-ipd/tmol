bc5ed922800161f39eeb7ff5bfcbbfbbf1bcfcd1
Alex Ford <fordas@uw.edu>
2018-04-16 17:03:05 -0700

    Convert hbond tests to pytest.

--- tmol/tests/score/test_hbond.py
+++ tmol/tests/score/test_hbond.py
@@ -3,7 +3,7 @@ import cattr
 import pandas
 import numpy
 import scipy.spatial
-import unittest
+import pytest
 
 import tmol.system.residue.restypes as restypes
 import tmol.database
@@ -15,138 +15,137 @@ from tmol.score.hbond import HBondElementAnalysis, HBondScoreGraph
 from tmol.tests.data.pdb import data as test_pdbs
 
 
-class TestHBond(unittest.TestCase):
-    def test_bb_identification(self):
-        tsys = read_pdb(test_pdbs["1ubq"])
+def test_bb_identification(bb_hbond_database):
+    tsys = read_pdb(test_pdbs["1ubq"])
 
-        donors = []
-        acceptors = []
+    donors = []
+    acceptors = []
 
-        for ri, r in zip(tsys.start_ind, tsys.residues):
-            if r.residue_type.name3 != "PRO":
-                donors.append({
-                    "d": r.residue_type.atom_to_idx["N"] + ri,
-                    "h": r.residue_type.atom_to_idx["H"] + ri,
-                    "donor_type": "hbdon_PBA",
-                })
-
-            acceptors.append({
-                "a": r.residue_type.atom_to_idx["O"] + ri,
-                "b": r.residue_type.atom_to_idx["C"] + ri,
-                "b0": r.residue_type.atom_to_idx["CA"] + ri,
-                "acceptor_type": "hbacc_PBA",
+    for ri, r in zip(tsys.res_start_ind, tsys.residues):
+        if r.residue_type.name3 != "PRO":
+            donors.append({
+                "d": r.residue_type.atom_to_idx["N"] + ri,
+                "h": r.residue_type.atom_to_idx["H"] + ri,
+                "donor_type": "hbdon_PBA",
             })
 
-        test_params = tmol.score.system_graph_params(tsys, requires_grad=False)
+        acceptors.append({
+            "a": r.residue_type.atom_to_idx["O"] + ri,
+            "b": r.residue_type.atom_to_idx["C"] + ri,
+            "b0": r.residue_type.atom_to_idx["CA"] + ri,
+            "acceptor_type": "hbacc_PBA",
+        })
 
-        hbond_graph = HBondScoreGraph(
-            hbond_database=bb_hbond_database, **test_params
+    test_params = tmol.score.system_graph_params(tsys, requires_grad=False)
+
+    hbond_graph = HBondScoreGraph(
+        hbond_database=bb_hbond_database, **test_params
+    )
+
+    hbe = hbond_graph.hbond_elements
+
+    pandas.testing.assert_frame_equal(
+        pandas.DataFrame.from_records(donors, columns=hbe.donors.dtype.names
+                                      ).sort_values("d"),
+        pandas.DataFrame.from_records(hbe.donors).sort_values("d")
+    )
+
+    pandas.testing.assert_frame_equal(
+        pandas.DataFrame.from_records(
+            acceptors, columns=hbe.sp2_acceptors.dtype.names
+        ).sort_values("a"),
+        pandas.DataFrame.from_records(hbe.sp2_acceptors).sort_values("a")
+    )
+
+
+def test_bb_dummy_score(bb_hbond_database):
+    tsys = read_pdb(test_pdbs["1ubq"])
+    test_params = tmol.score.system_graph_params(tsys, requires_grad=False)
+
+    atom_pair_distances = scipy.spatial.distance.squareform(
+        NaiveInteratomicDistanceGraph(**test_params).atom_pair_dist
+    )
+
+    hbond_graph = HBondScoreGraph(
+        hbond_database=bb_hbond_database,
+        **test_params,
+    )
+    hbond_elements = hbond_graph.hbond_elements
+
+    d_i = hbond_elements.donors["d"].reshape((-1, 1))
+    sp2_i = hbond_elements.sp2_acceptors["a"].reshape((1, -1))
+    sp3_i = hbond_elements.sp3_acceptors["a"].reshape((1, -1))
+    ring_i = hbond_elements.ring_acceptors["a"].reshape((1, -1))
+
+    max_dis = hbond_graph.hbond_database.global_parameters.max_dis
+
+    total_count = (
+        numpy.count_nonzero(atom_pair_distances[d_i, sp2_i] <= max_dis) +
+        numpy.count_nonzero(atom_pair_distances[d_i, sp3_i] <= max_dis) +
+        numpy.count_nonzero(atom_pair_distances[d_i, ring_i] <= max_dis)
+    )
+
+    assert total_count == hbond_graph.total_hbond
+
+
+def test_dummy_score():
+    tsys = read_pdb(test_pdbs["1ubq"])
+    test_params = tmol.score.system_graph_params(tsys, requires_grad=False)
+
+    atom_pair_distances = scipy.spatial.distance.squareform(
+        NaiveInteratomicDistanceGraph(**test_params).atom_pair_dist
+    )
+
+    hbond_graph = HBondScoreGraph(**test_params)
+    hbond_elements = hbond_graph.hbond_elements
+
+    d_i = hbond_elements.donors["d"].reshape((-1, 1))
+    sp2_i = hbond_elements.sp2_acceptors["a"].reshape((1, -1))
+    sp3_i = hbond_elements.sp3_acceptors["a"].reshape((1, -1))
+    ring_i = hbond_elements.ring_acceptors["a"].reshape((1, -1))
+
+    max_dis = hbond_graph.hbond_database.global_parameters.max_dis
+
+    total_count = (
+        numpy.count_nonzero(atom_pair_distances[d_i, sp2_i] <= max_dis) +
+        numpy.count_nonzero(atom_pair_distances[d_i, sp3_i] <= max_dis) +
+        numpy.count_nonzero(atom_pair_distances[d_i, ring_i] <= max_dis)
+    )
+
+    assert total_count == hbond_graph.total_hbond
+
+
+def test_identification_by_ljlk_types():
+    db_res = tmol.database.default.chemical.residues
+    types = [
+        cattr.structure(cattr.unstructure(r), restypes.ResidueType)
+        for r in db_res
+    ]
+    assert len(types) == 21
+
+    lj_types = {
+        t.name: t
+        for t in tmol.database.default.scoring.ljlk.atom_type_parameters
+    }
+
+    for t in types:
+        atom_types = numpy.array([a.atom_type for a in t.atoms])
+        bonds = t.bond_indicies
+
+        hbe = HBondElementAnalysis(atom_types=atom_types, bonds=bonds).setup()
+        identified_donors = set(hbe.donors["d"])
+        identified_acceptors = set(
+            list(hbe.sp2_acceptors["a"]) + list(hbe.sp3_acceptors["a"]) +
+            list(hbe.ring_acceptors["a"])
         )
 
-        hbe = hbond_graph.hbond_elements
-
-        pandas.testing.assert_frame_equal(
-            pandas.DataFrame.from_records(
-                donors, columns=hbe.donors.dtype.names
-            ).sort_values("d"),
-            pandas.DataFrame.from_records(hbe.donors).sort_values("d")
-        )
-
-        pandas.testing.assert_frame_equal(
-            pandas.DataFrame.from_records(
-                acceptors, columns=hbe.sp2_acceptors.dtype.names
-            ).sort_values("a"),
-            pandas.DataFrame.from_records(hbe.sp2_acceptors).sort_values("a")
-        )
-
-    def test_bb_dummy_score(self):
-        tsys = read_pdb(test_pdbs["1ubq"])
-        test_params = tmol.score.system_graph_params(tsys, requires_grad=False)
-
-        atom_pair_distances = scipy.spatial.distance.squareform(
-            NaiveInteratomicDistanceGraph(**test_params).atom_pair_dist
-        )
-
-        hbond_graph = HBondScoreGraph(
-            hbond_database=bb_hbond_database,
-            **test_params,
-        )
-        hbond_elements = hbond_graph.hbond_elements
-
-        d_i = hbond_elements.donors["d"].reshape((-1, 1))
-        sp2_i = hbond_elements.sp2_acceptors["a"].reshape((1, -1))
-        sp3_i = hbond_elements.sp3_acceptors["a"].reshape((1, -1))
-        ring_i = hbond_elements.ring_acceptors["a"].reshape((1, -1))
-
-        max_dis = hbond_graph.hbond_database.global_parameters.max_dis
-
-        total_count = (
-            numpy.count_nonzero(atom_pair_distances[d_i, sp2_i] <= max_dis) +
-            numpy.count_nonzero(atom_pair_distances[d_i, sp3_i] <= max_dis) +
-            numpy.count_nonzero(atom_pair_distances[d_i, ring_i] <= max_dis)
-        )
-
-        assert total_count == hbond_graph.total_hbond
-
-    def test_dummy_score(self):
-        tsys = read_pdb(test_pdbs["1ubq"])
-        test_params = tmol.score.system_graph_params(tsys, requires_grad=False)
-
-        atom_pair_distances = scipy.spatial.distance.squareform(
-            NaiveInteratomicDistanceGraph(**test_params).atom_pair_dist
-        )
-
-        hbond_graph = HBondScoreGraph(**test_params)
-        hbond_elements = hbond_graph.hbond_elements
-
-        d_i = hbond_elements.donors["d"].reshape((-1, 1))
-        sp2_i = hbond_elements.sp2_acceptors["a"].reshape((1, -1))
-        sp3_i = hbond_elements.sp3_acceptors["a"].reshape((1, -1))
-        ring_i = hbond_elements.ring_acceptors["a"].reshape((1, -1))
-
-        max_dis = hbond_graph.hbond_database.global_parameters.max_dis
-
-        total_count = (
-            numpy.count_nonzero(atom_pair_distances[d_i, sp2_i] <= max_dis) +
-            numpy.count_nonzero(atom_pair_distances[d_i, sp3_i] <= max_dis) +
-            numpy.count_nonzero(atom_pair_distances[d_i, ring_i] <= max_dis)
-        )
-
-        assert total_count == hbond_graph.total_hbond
-
-    def test_identification_by_ljlk_types(self):
-        db_res = tmol.database.default.chemical.residues
-        types = [
-            cattr.structure(cattr.unstructure(r), restypes.ResidueType)
-            for r in db_res
-        ]
-        assert len(types) == 21
-
-        lj_types = {
-            t.name: t
-            for t in tmol.database.default.scoring.ljlk.atom_type_parameters
-        }
-
-        for t in types:
-            atom_types = numpy.array([a.atom_type for a in t.atoms])
-            bonds = t.bond_indicies
-
-            hbe = HBondElementAnalysis(
-                atom_types=atom_types, bonds=bonds
-            ).setup()
-            identified_donors = set(hbe.donors["d"])
-            identified_acceptors = set(
-                list(hbe.sp2_acceptors["a"]) + list(hbe.sp3_acceptors["a"]) +
-                list(hbe.ring_acceptors["a"])
-            )
-
-            for ai, at in enumerate(atom_types):
-                if lj_types[at].is_donor:
-                    assert ai in identified_donors, \
-                        f"Unidentified donor. res: {t.name} atom:{t.atoms[ai]}"
-                if lj_types[at].is_acceptor:
-                    assert ai in identified_acceptors, \
-                        f"Unidentified acceptor. res: {t.name} atom:{t.atoms[ai]}"
+        for ai, at in enumerate(atom_types):
+            if lj_types[at].is_donor:
+                assert ai in identified_donors, \
+                    f"Unidentified donor. res: {t.name} atom:{t.atoms[ai]}"
+            if lj_types[at].is_acceptor:
+                assert ai in identified_acceptors, \
+                    f"Unidentified acceptor. res: {t.name} atom:{t.atoms[ai]}"
 
 
 bb_hbond_config = yaml.load(
@@ -240,6 +239,9 @@ bb_hbond_config = yaml.load(
 """
 )
 
-bb_hbond_database = cattr.structure(
-    bb_hbond_config, tmol.database.scoring.HBondDatabase
-)
+
+@pytest.fixture
+def bb_hbond_database():
+    return cattr.structure(
+        bb_hbond_config, tmol.database.scoring.HBondDatabase
+    )
