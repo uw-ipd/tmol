e188b48175c47f7c2879609202512cf025ed1735
Alex Ford <fordas@uw.edu>
2018-04-20 16:06:31 -0700

    Array type annotations and validating decorators. (#18)
    Add `tmol.types` module, intended to support generic typing components.
    
    Add simple decorators to perform run-time time validation and coercion via parameter annotations under `tmol.types.functional`. Intended to support run time type validation as well as verification of array dtype & shape. Decorators support isinstance-based type checking, as well as a single-dispatch registration system to add custom type objects.
    
    Adds simple array dtype/shape validation object, NDArray, under `tmol.types.array`, for use with the above decorators. Currently not compatible with mypy-based type checking.
    
    Move shape specification and validation logic into `tmol.types.shape`, renaming "ShapeSpec" into "Shape".

---
+++ tmol/types/array.py
@@ -0,0 +1,73 @@
+import enum
+import attr
+
+import numpy
+
+from .shape import Shape
+
+from .functional import get_converter, get_validator
+
+
+class Casting(enum.Enum):
+    """Casting specifications for array types, see ndarray.astype."""
+    no = "no"
+    equiv = "equiv"
+    safe = "safe"
+    same_kind = "same_kind"
+    unsafe = "unsafe"
+
+
+@attr.s(slots=True, frozen=True, auto_attribs=True, repr=False)
+class NDArray:
+    dtype: numpy.dtype = attr.ib(converter=numpy.dtype)
+    shape: Shape = Shape.spec[...]
+    casting: Casting = attr.ib(converter=Casting, default=Casting.unsafe)
+
+    def __getitem__(self, shape):
+        if not isinstance(shape, tuple):
+            shape = (shape, )
+
+        shape = Shape(shape)
+
+        return attr.evolve(self, shape=shape)
+
+    def __repr__(self):
+        return f"NDArray({self.dtype!r}){self.shape!s}"
+
+    def _repr_pretty_(self, p, cycle):
+        p.text(str(self))
+
+    def validate(self, value):
+        if not isinstance(value, numpy.ndarray):
+            raise TypeError(
+                f"expected {numpy.ndarray!r}, received {type(value)!r}"
+            )
+        if not value.dtype == self.dtype:
+            raise TypeError(
+                f"expected {self.dtype!r}, received {value.dtype!r}"
+            )
+        self.shape.validate(value.shape)
+
+        return True
+
+    def convert(self, value):
+        if not isinstance(value, numpy.ndarray):
+            value = numpy.array(value, copy=False, dtype=self.dtype)
+        if not value.dtype == self.dtype:
+            value = value.astype(self.dtype, casting=self.casting.value)
+        if value.shape == () and [d.size for d in self.shape.dims] == [1]:
+            value = value.reshape(1)
+
+        self.validate(value)
+
+        return value
+
+
+@get_validator.register(NDArray)
+def validate_ndarray(ndarray_type):
+    return ndarray_type.validate
+
+
+@get_converter.register(NDArray)
+def convert_ndarray(ndarray_type):
+    return ndarray_type.convert
---
+++ tmol/types/functional.py
@@ -0,0 +1,97 @@
+import inspect
+from decorator import decorate
+from functools import singledispatch
+
+import typing
+import toolz
+
+
+@toolz.curry
+def validate_isinstance(type_annotation, value):
+    if not isinstance(value, type_annotation):
+        raise TypeError(
+            f"expected {type_annotation}, received {type(value)!r}"
+        )
+
+
+@singledispatch
+def get_validator(type_annotation):
+    return validate_isinstance(type_annotation)
+
+
+def validate_args(f):
+    f._signature = inspect.signature(f)
+    f._validators = {
+        n: get_validator(v)
+        for n, v in typing.get_type_hints(f).items()
+    }
+
+    def validate_f(f, *args, **kwargs):
+        bound = f._signature.bind(*args, **kwargs)
+        bound.apply_defaults()
+
+        for n, val in bound.arguments.items():
+            validator = f._validators.get(n, None)
+            if validator:
+                try:
+                    validator(val)
+                except Exception as vexec:
+                    raise TypeError(f"Invalid argument: {n}") from vexec
+
+        retval = f(*args, **kwargs)
+
+        validator = f._validators.get("return", None)
+        if validator:
+            try:
+                validator(retval)
+            except Exception as vexec:
+                raise TypeError(f"Invalid return value") from vexec
+
+        return retval
+
+    return decorate(f, validate_f)
+
+
+@toolz.curry
+def constructor_convert(type_annotation, value):
+    if isinstance(value, type_annotation):
+        return value
+    else:
+        return type_annotation(value)
+
+
+@singledispatch
+def get_converter(type_annotation):
+    return constructor_convert(type_annotation)
+
+
+def convert_args(f):
+    f._signature = inspect.signature(f)
+    f._converters = {
+        n: get_converter(v)
+        for n, v in typing.get_type_hints(f).items()
+    }
+
+    def convert_f(f, *args, **kwargs):
+        bound = f._signature.bind(*args, **kwargs)
+        bound.apply_defaults()
+
+        for n, val in bound.arguments.items():
+            converter = f._converters.get(n, None)
+            if converter:
+                try:
+                    bound.arguments[n] = converter(val)
+                except Exception as vexec:
+                    raise TypeError(f"Invalid argument: {n}") from vexec
+
+        retval = f(*bound.args, **bound.kwargs)
+
+        converter = f._converters.get("return", None)
+        if converter:
+            try:
+                retval = converter(retval)
+            except Exception as vexec:
+                raise TypeError(f"Invalid return value") from vexec
+        return retval
+
+    return decorate(f, convert_f)
