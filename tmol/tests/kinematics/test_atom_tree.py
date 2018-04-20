d68c189cce541887b683e5881d01e9c1759d7c54
Andrew Leaver-Fay <aleaverfay@gmail.com>
2018-04-20 10:37:29 -0400

    I'm getting closer to having my jump atoms work; man I am not good at this

--- tmol/tests/kinematics/test_atom_tree.py
+++ tmol/tests/kinematics/test_atom_tree.py
@@ -14,6 +14,17 @@ def print_tree( residues, root, depth = 0 ) :
     for atom in root.children :
         print_tree( residues, atom, depth+1 )
 
+def print_tree_no_names( root, depth = 0 ) :
+    if root.is_jump :
+        print ( " " * depth, root.atomid, \
+                root.xyz, root.rb, root.rot_delta, root.rot )
+    else :
+        print ( " " * depth, root.atomid, \
+                root.xyz, root.phi, root.theta, root.d )
+    for atom in root.children :
+        print_tree_no_names( atom, depth+1 )
+
+
 def print_res1_tree( residues, root, depth = 0 ) :
     if root.atomid.res != 0 :
         return
@@ -279,3 +290,101 @@ class TestAtomTree(unittest.TestCase):
                         atom_records.loc[ kk, [ "x", "y", "z" ]] = res.coords[jj]
         with open( "test_refold2.pdb", "w" ) as fid :
             fid.writelines( pdb_parsing.to_pdb( atom_records ) )
+
+    def test_atomtree_w_jump_atoms( self ) :
+        NATOMS=23
+
+        BOND = 1
+        JUMP = 2
+
+        nodes = [ None ] * NATOMS
+
+        # kintree = numpy.empty( NATOMS, dtype=kintree_node_dtype);
+        nodes[ 0] = atree.BondedAtom(); nodes[ 0].atomid = atree.AtomID( 0, 0 ) # kintree[0]  = ("ORIG", 0, BOND,  0,  (1, 0, 2))
+        nodes[ 1] = atree.BondedAtom(); nodes[ 1].atomid = atree.AtomID( 0, 1 ) # kintree[1]  = (" X  ", 0, BOND,  0,  (1, 0, 2))
+        nodes[ 2] = atree.BondedAtom(); nodes[ 2].atomid = atree.AtomID( 0, 2 ) # kintree[2]  = (" Y  ", 0, BOND,  0,  (2, 0, 1))
+        nodes[0].children.append( nodes[1] ); nodes[1].parent = nodes[0]
+        nodes[1].children.append( nodes[2] ); nodes[2].parent = nodes[1]
+
+        nodes[ 3] = atree.JumpAtom();   nodes[ 3].atomid = atree.AtomID( 1, 0 ) # kintree[3]  = (" N  ", 1, JUMP,  0,  (4, 3, 5))
+        nodes[ 4] = atree.BondedAtom(); nodes[ 4].atomid = atree.AtomID( 1, 1 ) # kintree[4]  = (" CA ", 1, BOND,  3,  (4, 3, 5))
+        nodes[ 5] = atree.BondedAtom(); nodes[ 5].atomid = atree.AtomID( 1, 2 ) # kintree[5]  = (" CB ", 1, BOND,  4,  (5, 4, 3))
+        nodes[ 6] = atree.BondedAtom(); nodes[ 6].atomid = atree.AtomID( 1, 3 ) # kintree[6]  = (" C  ", 1, BOND,  4,  (6, 4, 3))
+        nodes[ 7] = atree.BondedAtom(); nodes[ 7].atomid = atree.AtomID( 1, 4 ) # kintree[7]  = (" O  ", 1, BOND,  6,  (7, 6, 4))
+        nodes[0].children.insert(0,nodes[3]); nodes[3].parent = nodes[0]
+        nodes[3].children.append( nodes[4] ); nodes[4].parent = nodes[3]
+        nodes[4].children.append( nodes[5] ); nodes[5].parent = nodes[4]
+        nodes[4].children.append( nodes[6] ); nodes[6].parent = nodes[4]
+        nodes[6].children.append( nodes[7] ); nodes[7].parent = nodes[6]
+
+        nodes[ 8] = atree.JumpAtom();   nodes[ 8].atomid = atree.AtomID( 2, 0 ) # kintree[8]  = (" N  ", 2, JUMP,  3,  (9, 8, 10))
+        nodes[ 9] = atree.BondedAtom(); nodes[ 9].atomid = atree.AtomID( 2, 1 ) # kintree[9]  = (" CA ", 2, BOND,  8,  (9, 8, 10))
+        nodes[10] = atree.BondedAtom(); nodes[10].atomid = atree.AtomID( 2, 2 ) # kintree[10] = (" CB ", 2, BOND,  9, (10, 9, 8))
+        nodes[11] = atree.BondedAtom(); nodes[11].atomid = atree.AtomID( 2, 3 ) # kintree[11] = (" C  ", 2, BOND,  9, (11, 9, 8))
+        nodes[12] = atree.BondedAtom(); nodes[12].atomid = atree.AtomID( 2, 4 ) # kintree[12] = (" O  ", 2, BOND, 11, (12,11, 9))
+        nodes[3].children.insert(0,nodes[8]); nodes[8].parent = nodes[3]
+        nodes[5+3].children.append( nodes[5+4] ); nodes[5+4].parent = nodes[5+3]
+        nodes[5+4].children.append( nodes[5+5] ); nodes[5+5].parent = nodes[5+4]
+        nodes[5+4].children.append( nodes[5+6] ); nodes[5+6].parent = nodes[5+4]
+        nodes[5+6].children.append( nodes[5+7] ); nodes[5+7].parent = nodes[5+6]
+
+        nodes[13] = atree.JumpAtom();   nodes[13].atomid = atree.AtomID( 3, 0 ) # kintree[13] = (" N  ", 3, JUMP,  3, (14, 13, 15))
+        nodes[14] = atree.BondedAtom(); nodes[14].atomid = atree.AtomID( 3, 1 ) # kintree[14] = (" CA ", 3, BOND, 13, (14, 13, 15))
+        nodes[15] = atree.BondedAtom(); nodes[15].atomid = atree.AtomID( 3, 2 ) # kintree[15] = (" CB ", 3, BOND, 14, (15, 14, 13))
+        nodes[16] = atree.BondedAtom(); nodes[16].atomid = atree.AtomID( 3, 3 ) # kintree[16] = (" C  ", 3, BOND, 14, (16, 14, 13))
+        nodes[17] = atree.BondedAtom(); nodes[17].atomid = atree.AtomID( 3, 4 ) # kintree[17] = (" O  ", 3, BOND, 16, (17, 16, 14))
+        nodes[3].children.insert(1,nodes[13]); nodes[13].parent = nodes[3]
+        nodes[10+3].children.append( nodes[10+4] ); nodes[10+4].parent = nodes[10+3]
+        nodes[10+4].children.append( nodes[10+5] ); nodes[10+5].parent = nodes[10+4]
+        nodes[10+4].children.append( nodes[10+6] ); nodes[10+6].parent = nodes[10+4]
+        nodes[10+6].children.append( nodes[10+7] ); nodes[10+7].parent = nodes[10+6]
+
+        nodes[18] = atree.JumpAtom();   nodes[18].atomid = atree.AtomID( 4, 0 ) # kintree[18] = (" N  ", 4, JUMP,  3, (19, 18, 20))
+        nodes[19] = atree.BondedAtom(); nodes[19].atomid = atree.AtomID( 4, 1 ) # kintree[19] = (" CA ", 4, BOND, 18, (19, 18, 20))
+        nodes[20] = atree.BondedAtom(); nodes[20].atomid = atree.AtomID( 4, 2 ) # kintree[20] = (" CB ", 4, BOND, 19, (20, 19, 18))
+        nodes[21] = atree.BondedAtom(); nodes[21].atomid = atree.AtomID( 4, 3 ) # kintree[21] = (" C  ", 4, BOND, 19, (21, 19, 18))
+        nodes[22] = atree.BondedAtom(); nodes[22].atomid = atree.AtomID( 4, 4 ) # kintree[22] = (" O  ", 4, BOND, 21, (22, 21, 19))
+        nodes[3].children.insert(2,nodes[18]); nodes[18].parent = nodes[3]
+        nodes[15+3].children.append( nodes[15+4] ); nodes[15+4].parent = nodes[15+3]
+        nodes[15+4].children.append( nodes[15+5] ); nodes[15+5].parent = nodes[15+4]
+        nodes[15+4].children.append( nodes[15+6] ); nodes[15+6].parent = nodes[15+4]
+        nodes[15+6].children.append( nodes[15+7] ); nodes[15+7].parent = nodes[15+6]
+
+        coords = numpy.empty( [NATOMS,3] )
+        coords[0,:]  = [0.000 ,  0.000 ,  0.000]
+        coords[1,:]  = [1.000 ,  0.000 ,  0.000]
+        coords[2,:]  = [0.000 ,  1.000 ,  0.000]
+
+        coords[3,:]  = [2.000 ,  2.000 ,  2.000]
+        coords[4,:]  = [3.458 ,  2.000 ,  2.000]
+        coords[5,:]  = [3.988 ,  1.222 ,  0.804]
+        coords[6,:]  = [4.009 ,  3.420 ,  2.000]
+        coords[7,:]  = [3.383 ,  4.339 ,  1.471]
+
+        coords[8,:]  = [5.184 ,  3.594 ,  2.596]
+        coords[9,:]  = [5.821 ,  4.903 ,  2.666]
+        coords[10,:] = [5.331 ,  5.667 ,  3.888]
+        coords[11,:] = [7.339 ,  4.776 ,  2.690]
+        coords[12,:] = [7.881 ,  3.789 ,  3.186]
+
+        coords[13,:]  = [7.601 ,  2.968 ,  5.061] 
+        coords[14,:]  = [6.362 ,  2.242 ,  4.809]
+        coords[15,:]  = [6.431 ,  0.849 ,  5.419]
+        coords[16,:]  = [5.158 ,  3.003 ,  5.349]
+        coords[17,:]  = [5.265 ,  3.736 ,  6.333]
+
+        coords[18,:]  = [4.011 ,  2.824 ,  4.701]
+        coords[19,:]  = [2.785 ,  3.494 ,  5.115]
+        coords[20,:]  = [2.687 ,  4.869 ,  4.470]
+        coords[21,:]  = [1.559 ,  2.657 ,  4.776]
+        coords[22,:]  = [1.561 ,  1.900 ,  3.805]
+
+        for ii, node in enumerate( nodes ) :
+            node.xyz = coords[ii,:]
+
+        nodes[0].update_internal_coords()
+        print_tree_no_names( nodes[0] )
+
+        nodes[0].update_xyz()
+        print_tree_no_names( nodes[0] )
+        
