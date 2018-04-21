6d339eb00be2672a56f9651e95d4ea548d61a283
Alex Ford <fordas@uw.edu>
2018-04-21 15:06:02 -0700

    Add angular value parsing and validation via pint.
    Add generic angular value parsing in `tmol.utility.units` namespace
    using `pint` to parse value expression. Converts all angles to radian
    floats, optionally checking that the value falls within an allowed
    range.
    
    Adds pint as a dependency in both conda environment def and requirements
    file, documenting failure due to conda-forge channel and broken pytorch-cpu
    package.

---
+++ tmol/utility/units.py
@@ -0,0 +1,35 @@
+from typing import Union, Tuple
+
+import pint
+
+ureg = pint.UnitRegistry()
+u = ureg.parse_expression
+
+
+def parse_angle(
+        angle: Union[float, str], lim: Tuple = (u("-2pi rad"), u("2pi rad"))
+) -> float:
+    """Parse an angle and return value in radians."""
+
+    if isinstance(angle, str):
+        val = u(angle)
+    else:
+        val = angle
+
+    if not isinstance(val, pint.unit.Number):
+        # May have parsed to a just a number or a
+        # quantity with units, if so convert to radians
+        val.to(ureg.rad)
+
+    if lim:
+        minv, maxv = lim
+        if minv is not None and val < minv:
+            raise ValueError(
+                f"angle: {angle!r} outside of allowed range: {lim}"
+            )
+        if maxv is not None and val > maxv:
+            raise ValueError(
+                f"angle: {angle!r} outside of allowed range: {lim}"
+            )
+
+    return float(val)
