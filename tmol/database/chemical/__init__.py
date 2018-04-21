b466f84b723e72db22fbc3415596bd58d06e713b
Alex Ford <fordas@uw.edu>
2018-04-21 15:31:59 -0700

    Parse and validate Icoor angles into radians.
    Converts internal angular representation of icoor angles to radians.
    Updates chemical database icoors definition to parse and validate
    ranges for theta and phi. Updates database to indicate angles in
    degrees. Minor formatting fixed in database layer.

--- tmol/database/chemical/__init__.py
+++ tmol/database/chemical/__init__.py
@@ -1,4 +1,6 @@
-from typing import Tuple
+from typing import Tuple, NewType
+from tmol.utility.units import parse_angle, u
+from toolz import curry
 
 import attr
 import cattr
@@ -13,15 +15,30 @@ class Atom:
     name: str
     atom_type: str
 
+
+PhiAngle = NewType("PhiAngle", float)
+ThetaAngle = NewType("ThetaAngle", float)
+
+
 @attr.s(auto_attribs=True, frozen=True, slots=True)
 class Icoor:
-    name : str
-    phi  : float
-    theta: float
-    d    : float
-    parent : str
-    grand_parent : str
-    great_grand_parent : str
+    name: str
+    phi: PhiAngle
+    theta: ThetaAngle
+    d: float
+    parent: str
+    grand_parent: str
+    great_grand_parent: str
+
+
+parse_angle = curry(parse_angle)
+
+parse_phi = parse_angle(lim=(u("-pi rad"), u("pi rad")))
+cattr.register_structure_hook(PhiAngle, lambda v, t: parse_phi(v))
+
+parse_theta = parse_angle(lim=(u("0 rad"), u("pi rad")))
+cattr.register_structure_hook(ThetaAngle, lambda v, t: parse_theta(v))
+
 
 @attr.s(auto_attribs=True, frozen=True, slots=True)
 class Residue:
@@ -31,7 +48,8 @@ class Residue:
     bonds: Tuple[Tuple[str, str], ...]
     lower_connect: str
     upper_connect: str
-    icoor : Tuple[Icoor,...]
+    icoors: Tuple[Icoor, ...]
+
 
 @attr.s(auto_attribs=True, frozen=True, slots=True)
 class ChemicalDatabase(properties.HasProperties):
--- tmol/tests/database/test_chemical.py
+++ tmol/tests/database/test_chemical.py
@@ -6,9 +6,19 @@ class TestChemicalDatabase(unittest.TestCase):
         from tmol.database import default
 
         atom_types = set(default.chemical.atom_types)
-        assert len(default.chemical.atom_types
-                   ) == len(atom_types), "Duplicate atom types."
+        assert len(default.chemical.atom_types) == len(atom_types), \
+            "Duplicate atom types."
 
         for r in default.chemical.residues:
             for a in r.atoms:
                 assert a.atom_type in atom_types, f"Invalid atom type. res: {r.name3} atom: {a}"
+
+            valid_icoor_names = (
+                set(a.name for a in r.atoms).union({"UPPER", "LOWER"})
+            )
+
+            for icoor in r.icoors:
+                assert icoor.name in valid_icoor_names
+                assert icoor.parent in valid_icoor_names
+                assert icoor.grand_parent in valid_icoor_names
+                assert icoor.great_grand_parent in valid_icoor_names
