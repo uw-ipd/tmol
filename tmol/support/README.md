d92672622eef29afac6ffc66aac0665963cb7ba6
Alex Ford <fordas@uw.edu>
2018-04-26 14:17:37 -0700

    Add "support" submodule, managing run-time access to pyrosetta. (#6)
    Adds support submodule, managing access to optional run-time
    dependencies and single-use support code. Adds support for consistent
    pyrosetta initialization. Add test fixures for pyrosetta and rosetta
    database paths.
    
    Add specific test for pyrosetta/pytorch co-import, which may cause
    significant issues if the libraries are compiled on different OS images.

---
+++ tmol/support/README.md
@@ -0,0 +1,5 @@
+# External Support Components
+
+This module provides support components that are *not* runtime
+dependencies. This includes, for example, single-pass parameter import,
+database generation and analysis components for baseline test generation.
--- tmol/tests/conftest.py
+++ tmol/tests/conftest.py
@@ -1,3 +1,36 @@
+import toolz
+import os
+import pytest
+
+
+@pytest.fixture(scope="session")
+def pyrosetta():
+    "Import and initialize pyrosetta."
+    try:
+        from tmol.support.rosetta.init import pyrosetta
+
+        return pyrosetta
+    except ImportError:
+        return None
+
+
+@pytest.fixture(scope="session")
+def rosetta_database(pyrosetta):
+    "Resolve path to rosetta database."
+
+    if "ROSETTA_DATABASE" in os.environ:
+        normpath = toolz.compose(
+            os.path.abspath,
+            os.path.expanduser,
+            os.path.expandvars,
+        )
+        return normpath(os.environ.get("ROSETTA_DATABASE"))
+    elif pyrosetta:
+        return pyrosetta._rosetta_database_from_env()
+    else:
+        return None
+
+
 def pytest_collection_modifyitems(session, config, items):
 
     # Run all linting-tests *after* the functional tests
