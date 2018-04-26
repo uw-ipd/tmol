4b00f29f96820ef7551465a1110a85df997afdb2
Alex Ford <fordas@uw.edu>
2018-04-26 14:20:16 -0700

    Add hbond parameter database from rosetta @v2017.48. (#6)
    Add hbond parameter database and hbond parameter configuration from
    rosetta sp2_elect_params@v2017.48.
    
    Add hbond parameter import support component.
    
    Co-authored-by: Frank DiMaio <dimaio@uw.edu>

--- tmol/database/scoring/hbond.py
+++ tmol/database/scoring/hbond.py
@@ -6,6 +6,15 @@ from typing import Tuple
 
 
 @attr.s(auto_attribs=True, frozen=True, slots=True)
+class GlobalParams:
+    hb_sp2_range_span: float
+    hb_sp2_BAH180_rise: float
+    hb_sp2_outer_width: float
+    hb_sp3_softmax_fade: float
+    threshold_distance: float
+
+
+@attr.s(auto_attribs=True, frozen=True, slots=True)
 class DonorAtoms:
     d: str
     h: str
@@ -53,9 +62,51 @@ class ChemicalTypes:
 
 
 @attr.s(auto_attribs=True, frozen=True, slots=True)
+class HbondWeights:
+    name: str
+    weight: float
+
+
+@attr.s(auto_attribs=True, slots=True, frozen=True)
+class PolynomialParameters:
+    name: str
+    dimension: str
+    xmin: float
+    xmax: float
+    min_val: float
+    max_val: float
+    degree: int
+    c_a: float
+    c_b: float
+    c_c: float
+    c_d: float
+    c_e: float
+    c_f: float
+    c_g: float
+    c_h: float
+    c_i: float
+    c_j: float
+    c_k: float
+
+
+@attr.s(auto_attribs=True, slots=True, frozen=True)
+class PairParameters:
+    don_chem_type: str
+    acc_chem_type: str
+    AHdist: str
+    cosBAH: str
+    cosAHD: str
+
+
+@attr.s(auto_attribs=True, frozen=True, slots=True)
 class HBondDatabase:
+    global_parameters: GlobalParams
     atom_groups: AtomGroups
     chemical_types: ChemicalTypes
+    don_weights: Tuple[HbondWeights, ...]
+    acc_weights: Tuple[HbondWeights, ...]
+    polynomial_parameters: Tuple[PolynomialParameters, ...]
+    pair_parameters: Tuple[PairParameters, ...]
 
     @classmethod
     def from_file(cls, path):
---
+++ tmol/tests/support/test_scoring.py
@@ -0,0 +1,17 @@
+import os
+import yaml
+
+from .rosetta import requires_rosetta_database
+
+from tmol.support.scoring.hbond_param_import import RosettaHBParams
+
+
+@requires_rosetta_database
+def test_hbond_param_import(rosetta_database):
+    params = RosettaHBParams(
+        os.path.join(
+            rosetta_database, "scoring/score_functions/hbonds/sp2_elec_params"
+        )
+    )
+
+    yaml.load(params.to_yaml())
