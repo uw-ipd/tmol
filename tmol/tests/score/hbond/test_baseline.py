74833e9706045327f61f6574ba63459f35b3e194
Alex Ford <fordas@uw.edu>
2018-04-27 18:15:35 -0700

    Report hbond metadata via pairs descriptor object. (#6)

--- tmol/tests/score/hbond/test_baseline.py
+++ tmol/tests/score/hbond/test_baseline.py
@@ -9,7 +9,7 @@ from tmol.score.hbond import HBondScoreGraph
 import tmol.system.residue.packed
 
 
-def hbond_score_comparision(rosetta_baseline):
+def hbond_score_comparison(rosetta_baseline):
     test_system = (
         tmol.system.residue.packed.PackedResidueSystem()
         .from_residues(rosetta_baseline.tmol_residues)
@@ -20,17 +20,23 @@ def hbond_score_comparision(rosetta_baseline):
 
     # Extract list of hbonds from packed system into summary table
     # via atom metadata
-    h_i = hbond_graph.hbond_h_ind
-    a_i = hbond_graph.hbond_acceptor_ind
-    tmol_candidate_hbonds = pandas.DataFrame.from_dict({
-        "h": h_i,
-        "a": a_i,
-        "h_res": test_system.atom_metadata["residue_index"][h_i],
-        "h_atom": test_system.atom_metadata["atom_name"][h_i],
-        "a_res": test_system.atom_metadata["residue_index"][a_i],
-        "a_atom": test_system.atom_metadata["atom_name"][a_i],
-        "score": numpy.nan_to_num(hbond_graph.hbond_scores),
-    }).set_index(["a", "h"])
+    h_i = hbond_graph.hbond_pairs.pairs["h"]
+    a_i = hbond_graph.hbond_pairs.pairs["a"]
+    tmol_candidate_hbonds = pandas.DataFrame.from_dict(
+        toolz.merge(
+            {
+                "h_res": test_system.atom_metadata["residue_index"][h_i],
+                "h_atom": test_system.atom_metadata["atom_name"][h_i],
+                "a_res": test_system.atom_metadata["residue_index"][a_i],
+                "a_atom": test_system.atom_metadata["atom_name"][a_i],
+                "score": numpy.nan_to_num(hbond_graph.hbond_scores),
+            },
+            {
+                n: hbond_graph.hbond_pairs.pairs[n]
+                for n in hbond_graph.hbond_pairs.pairs.dtype.names
+            },
+        )
+    ).set_index(["a", "h"])
     tmol_hbonds = tmol_candidate_hbonds.query("score != 0")
 
     del h_i, a_i
@@ -54,8 +60,8 @@ def hbond_score_comparision(rosetta_baseline):
     )).set_index(["a", "h"])
 
     return pandas.merge(
-        rosetta_hbonds["energy"].to_frame("rosetta_score"),
-        tmol_hbonds["score"].to_frame("tmol_score"),
+        rosetta_hbonds["energy"].to_frame("score_rosetta"),
+        tmol_hbonds.rename(columns={"score": "score_tmol"}),
         left_index=True,
         right_index=True,
         how="outer",
@@ -63,18 +69,25 @@ def hbond_score_comparision(rosetta_baseline):
 
 
 def test_pyrosetta_hbond_comparison(ubq_rosetta_baseline):
-    score_comparision = hbond_score_comparision(ubq_rosetta_baseline)
+    score_comparison = hbond_score_comparison(ubq_rosetta_baseline)
 
-    mismatch = score_comparision.query("tmol_score != rosetta_score")
-    mismatch["abs_delta"] = mismatch.eval("abs(tmol_score - rosetta_score)")
-    mismatch = mismatch.sort_values(by="abs_delta")
+    score_comparison["score_delta"] = score_comparison.eval(
+        "abs(score_tmol - score_rosetta)"
+    )
+    score_comparison["score_rel_delta"] = score_comparison.eval(
+        "abs((score_tmol - score_rosetta) / ((score_tmol + score_rosetta) / 2))"
+    )
+    score_comparison = (
+        score_comparison
+        .sort_values(by="score_rel_delta")
+        .sort_index(axis="columns")
+    ) # yapf: disable
 
-    err_msg = (f"Mismatched bb hbond identification:\n{mismatch}\n\n")
+    err_msg = (f"Mismatched bb hbond identification:\n{score_comparison}\n\n")
 
     numpy.testing.assert_allclose(
-        numpy.nan_to_num(score_comparision["rosetta_score"].values),
-        numpy.nan_to_num(score_comparision["tmol_score"].values),
-        rtol=5e-2,
-        atol=1e-2,
+        numpy.nan_to_num(score_comparison["score_rosetta"].values),
+        numpy.nan_to_num(score_comparison["score_tmol"].values),
+        rtol=2e-2,
         err_msg=err_msg,
     )
