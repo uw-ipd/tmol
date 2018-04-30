4efa045437aed5264466f2581c5660915f47a391
Alex Ford <fordas@uw.edu>
2018-04-29 23:57:45 -0700

    Add enum/categorical interconversion support functions. (#37)
    Add support functions for interconversion between enum classes and pandas categorical values. Intended to support pandas-based analysis of array data using int-enum encoding.

---
+++ tmol/utility/categorical.py
@@ -0,0 +1,57 @@
+"""Support functions for `enum`/`pandas.Categorical` interconversion."""
+import pandas
+import enum
+
+
+def enum_val_catdtype(
+        enum_type: enum.Enum
+) -> pandas.api.types.CategoricalDtype:
+    """Generate categorial dtype convering enumeratation values."""
+    if issubclass(enum_type, enum.Flag):
+        raise NotImplementedError(
+            f"enum.Flag categorical not supported: {enum_type}"
+        )
+
+    return pandas.api.types.CategoricalDtype(
+        categories=list(enum_type.__members__.values())
+    )
+
+
+def enum_name_catdtype(
+        enum_type: enum.Enum
+) -> pandas.api.types.CategoricalDtype:
+    """Generate categorial dtype convering enumeratation member names."""
+    if issubclass(enum_type, enum.Flag):
+        raise NotImplementedError(
+            f"enum.Flag categorical not supported: {enum_type}"
+        )
+
+    return pandas.api.types.CategoricalDtype(
+        categories=list(enum_type.__members__.keys())
+    )
+
+
+def vals_to_val_cat(enum_type: enum.Enum, values) -> pandas.Categorical:
+    """Convert enum values to a categorial."""
+    return pandas.Categorical(values, dtype=enum_val_catdtype(enum_type))
+
+
+def vals_to_name_cat(enum_type: enum.Enum, values) -> pandas.Categorical:
+    """Convert enum values to a categorial of member names."""
+    return (
+        vals_to_val_cat(enum_type, values)
+        .rename_categories(enum_name_catdtype(enum_type).categories)
+    )
+
+
+def names_to_name_cat(enum_type: enum.Enum, values) -> pandas.Categorical:
+    """Convert enum names to a categorial."""
+    return pandas.Categorical(values, dtype=enum_name_catdtype(enum_type))
+
+
+def names_to_val_cat(enum_type: enum.Enum, values) -> pandas.Categorical:
+    """Convert enum names to a categorial of enum values."""
+    return (
+        names_to_name_cat(enum_type, values)
+        .rename_categories(enum_val_catdtype(enum_type).categories)
+    )
