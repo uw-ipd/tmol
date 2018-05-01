aa2bc4de1bd9c5696bcba13a9bd2342cb39c559a
Andrew Leaver-Fay <aleaverfay@gmail.com>
2018-05-01 10:55:46 -0400

    yapfing test_chemical.py

--- tmol/tests/database/test_chemical.py
+++ tmol/tests/database/test_chemical.py
@@ -1,14 +1,16 @@
 import unittest
 from collections import Counter
 
+
 class TestChemicalDatabase(unittest.TestCase):
     def test_residue_defs(self):
         from tmol.database import default
 
         atom_types = set(default.chemical.atom_types)
-        assert len(default.chemical.atom_types) == len(atom_types), "Duplicate atom types."
+        assert len(default.chemical.atom_types
+                   ) == len(atom_types), "Duplicate atom types."
 
-        atypenames = [ x[0] for x in atom_types ]
+        atypenames = [x[0] for x in atom_types]
         for r in default.chemical.residues:
             for a in r.atoms:
                 assert a.atom_type in atypenames, f"Invalid atom type. res: {r.name3} atom: {a}"
