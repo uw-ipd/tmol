75877898eeaea8f1d2001b1ae27865ee3d10f1bd
Alex Ford <fordas@uw.edu>
2018-05-08 15:22:35 -0700

    Add gradcheck test for kinematics torch op.

--- tmol/tests/kinematics/test_torch_op.py
+++ tmol/tests/kinematics/test_torch_op.py
@@ -6,7 +6,7 @@ from tmol.kinematics.metadata import DOFMetadata, DOFTypes
 from tmol.kinematics.torch_op import KinematicOp
 
 
-def test_kinematic_torch_op_smoke(ubq_system):
+def test_kinematic_torch_op_refold(ubq_system):
     tsys = ubq_system
 
     torsion_pairs = numpy.block([
@@ -40,3 +40,39 @@ def test_kinematic_torch_op_smoke(ubq_system):
     refold_coords = kop.apply(kop.src_mobile_dofs)
 
     numpy.testing.assert_allclose(coords, refold_coords)
+
+
+def test_kinematic_torch_op_gradcheck(ubq_system):
+    tsys = ubq_system
+
+    torsion_pairs = numpy.block([
+        [tsys.torsion_metadata["atom_index_b"]],
+        [tsys.torsion_metadata["atom_index_c"]],
+    ]).T
+    torsion_bonds = torsion_pairs[numpy.all(torsion_pairs > 0, axis=-1)]
+
+    kintree = KinematicBuilder().append_connected_component(
+        *KinematicBuilder.component_for_prioritized_bonds(
+            root=0,
+            mandatory_bonds=torsion_bonds,
+            all_bonds=tsys.bonds,
+        )
+    ).kintree
+
+    kinematic_metadata = DOFMetadata.for_kintree(kintree)
+
+    torsion_dofs = kinematic_metadata[
+        (kinematic_metadata.dof_type == DOFTypes.bond_torsion)
+    ]
+
+    coords = torch.from_numpy(tsys.coords)
+
+    kop = KinematicOp.from_src_coords(
+        kintree,
+        torsion_dofs,
+        coords,
+    )
+
+    assert torch.autograd.gradcheck(
+        kop.apply, (kop.src_mobile_dofs, ), raise_exception=True
+    )
