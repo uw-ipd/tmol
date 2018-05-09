eac8ffbe29bff6d726a65346a3a3facc6157fef4
Alex Ford <fordas@uw.edu>
2018-05-09 08:40:08 -0700

    Add dof & cartesian coord components, extract total score from atom graph. (#12)
    Squash rebase of #33, including components of #48.
    
    Extract RealSpaceScoreGraph, managing the existing logic of scoring from
    a set of input cartesian coordinates, from bonded atomic score graph.
    
    Add DofSpaceScoreGraph component, utilizing kinematic operations to
    render system coordinates for scoring. Expand packed residue system
    score parameter generation with `system_torsion_space_graph_params`,
    returning graph parameters needed for torsion-space sampling via
    `DofSpaceScoreGraph`/`BondedAtomScoreGraph` combinations.
    
    Update "default" score graph and tests using custom graphs to add real
    space coords to graph component bases.
    
    Add tmol.score.coordinates submodule, handling generation of coordinates
    for score components.
    
    Move `system_graph_params` utility function into score submodule.
    Convert to "system_real_graph_params", preparing to add dof space score
    graph.
    
    Add real-space to dof-space total score comparison test and grad-check based
    tests of real-space and torsion-space total-score gradients.
    
    Extract TotalScoreComponentsGraph, managing existing logic of
    registration and summation of total score components, from bonded atomic
    score graph. Update potential implementations to include
    TotalScoreComponentsGraph in graph bases.
    
    Update "default" score graph and tests using custom score graphs to add
    TotalScoreComponentsGraph to graph bases.
    
    Expanding hbond score graph smoke tests to cover gradient nans.

---
+++ tmol/score/coordinates.py
@@ -0,0 +1,58 @@
+import torch
+import math
+
+import properties
+from tmol.properties.array import VariableT
+from tmol.properties.reactive import derived_from
+from tmol.kinematics.torch_op import KinematicOp
+
+
+class CartesianAtomicCoordinateProvider(properties.HasProperties):
+    coords = VariableT("source atomic coordinates")
+
+    def step(self):
+        """Recalculate total_score and gradients wrt/ coords. Does not clear coord grads."""
+
+        self._notify(
+            dict(
+                name="coords",
+                prev=getattr(self, "coords"),
+                mode="observe_set"
+            )
+        )
+
+        self.total_score.backward()
+        return self.total_score
+
+
+class KinematicAtomicCoordinateProvider(properties.HasProperties):
+    dofs = VariableT("source atomic coordinates")
+    kinop = properties.Instance("kinematic op", KinematicOp)
+
+    @derived_from(("dofs", "kinop", "system_size"),
+                  VariableT("source atomic coordinates"))
+    def coords(self):
+        kincoords = self.kinop(self.dofs)
+
+        coords = torch.full(
+            (self.system_size, 3),
+            math.nan,
+            dtype=self.dofs.dtype,
+            layout=self.dofs.layout,
+            device=self.dofs.device,
+            requires_grad=False,
+        )
+
+        coords[self.kinop.kintree.id[1:]] = kincoords[1:] # yapf: disable
+
+        return coords.to(torch.float)
+
+    def step(self):
+        """Recalculate total_score and gradients wrt/ dofs. Does not clear dof grads."""
+
+        self._notify(
+            dict(name="dofs", prev=getattr(self, "dofs"), mode="observe_set")
+        )
+
+        self.total_score.backward()
+        return self.total_score
---
+++ tmol/tests/score/test_dof_space.py
@@ -0,0 +1,92 @@
+import torch
+
+from tmol.system.residue.packed import PackedResidueSystem
+
+from tmol.system.residue.score import (
+    system_cartesian_space_graph_params,
+    system_torsion_space_graph_params,
+)
+
+from tmol.score import TotalScoreGraph
+from tmol.score.coordinates import (
+    CartesianAtomicCoordinateProvider,
+    KinematicAtomicCoordinateProvider,
+)
+
+
+class RealSpaceScore(
+        TotalScoreGraph,
+        CartesianAtomicCoordinateProvider,
+):
+    pass
+
+
+class DofSpaceScore(
+        TotalScoreGraph,
+        KinematicAtomicCoordinateProvider,
+):
+    pass
+
+
+def test_torsion_space_by_real_space_total_score(ubq_system):
+
+    real_space = RealSpaceScore(
+        **system_cartesian_space_graph_params(ubq_system)
+    )
+
+    torsion_space = DofSpaceScore(
+        **system_torsion_space_graph_params(ubq_system)
+    )
+
+    real_total = real_space.step()
+    torsion_total = torsion_space.step()
+
+    assert (real_total == torsion_total).all()
+
+
+def test_torsion_space_coord_smoke(ubq_system):
+    tsys = ubq_system
+
+    torsion_space = DofSpaceScore(**system_torsion_space_graph_params(tsys))
+
+    start_dofs = torch.tensor(torsion_space.dofs, requires_grad=True)
+    start_coords = torch.tensor(torsion_space.coords, requires_grad=False)
+    cmask = torch.isnan(start_coords).sum(dim=-1) == 0
+
+    def coord_residuals(dofs):
+        torsion_space.dofs = dofs
+        return (torsion_space.coords[cmask] -
+                start_coords[cmask]).norm(dim=-1).sum()
+
+    torch.random.manual_seed(1663)
+    pdofs = torch.tensor((torch.rand_like(start_dofs) - .5) * 1e-2,
+                         requires_grad=True)
+
+    assert pdofs.requires_grad
+
+    res = coord_residuals(pdofs)
+    assert res.requires_grad
+
+    res.backward(retain_graph=True)
+    assert pdofs.grad is not None
+
+
+def test_torsion_space_to_coordinate_gradcheck(ubq_res):
+    tsys = PackedResidueSystem.from_residues(ubq_res[:6])
+
+    torsion_space = DofSpaceScore(**system_torsion_space_graph_params(tsys))
+
+    start_dofs = torsion_space.dofs.detach().clone().requires_grad_()
+    start_coords = torsion_space.coords.detach().clone()
+
+    cmask = torch.isnan(start_coords).sum(dim=-1) == 0
+
+    def coord_residuals(dofs):
+        torsion_space.dofs = dofs
+        res = (torsion_space.coords[cmask] - (start_coords[cmask])).sum(dim=-1)
+
+        return res
+
+    assert torch.autograd.gradcheck(
+        coord_residuals, (start_dofs, ), eps=5e-3, atol=5e-4, rtol=5e-3
+    )
