b99757081a4b976157041568aee9d55ed3c4e43e
Alex Ford <fordas@uw.edu>
2018-05-09 18:04:52 -0700

    Fix hydrogen bond score with empty acceptor lists.
    Fixup hydrogen bond scoring in cases when *no* acceptors of a given type
    are present in the system by skipping parameter resolution if no
    donor/acceptor pairs are present. Occurs due to lack of torch support
    for zero-sized dimensions in tensors, tracked in https://github.com/pytorch/pytorch/issues/5014.

--- tmol/tests/score/hbond/test_score.py
+++ tmol/tests/score/hbond/test_score.py
@@ -6,14 +6,33 @@ from tmol.score.hbond import HBondScoreGraph
 from tmol.system.residue.score import system_real_graph_params
 
 
+class HBGraph(
+        HBondScoreGraph,
+        RealSpaceScoreGraph,
+):
+    pass
+
+
 def test_hbond_smoke(ubq_system):
-    class HBGraph(
-            HBondScoreGraph,
-            RealSpaceScoreGraph,
-    ):
-        pass
 
     hbond_graph = HBGraph(
+        **system_real_graph_params(ubq_system, requires_grad=False)
+    )
+
+    hbond_scores = numpy.array(hbond_graph.hbond_scores)
+    nan_scores = numpy.flatnonzero(numpy.isnan(hbond_scores))
+
+    assert len(nan_scores) == 0
+
+
+def test_hbond_smoke_bbonly(bb_hbond_database, ubq_system):
+    """Backbone-only score.
+
+    Covers cases missing specific classes of acceptors.
+    """
+
+    hbond_graph = HBGraph(
+        hbond_database=bb_hbond_database,
         **system_real_graph_params(ubq_system, requires_grad=False)
     )
 
