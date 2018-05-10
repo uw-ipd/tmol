Fix interatomic distance gradients with nan coords.
Fix interatomic distance gradient propagation in cases with
nan-coordinates, as in packed residue system. Perform nan-to-zero
conversion at the interatomic coordinate delta, rather than interatomic
coordinate distance, level of compute graph. Resolves issues with
nan-valued gradients in ljlk score graph.

Add deeper smoke-test for nan values in ljlk score graph component,
including gradient values.

tmol/tests/score/test_lklj.py
@@ -1,61 +1,115 @@
-import unittest
+import pytest
+import torch
 import numpy
 
-import tmol.system.residue
-import tmol.score
-
-from tmol.tests.data.pdb import data as test_pdbs
-from tmol.system.residue.io import read_pdb
 from tmol.system.residue.score import system_real_graph_params
 
+from tmol.score.coordinates import RealSpaceScoreGraph
+from tmol.score.ljlk import LJLKScoreGraph
+from tmol.score.interatomic_distance import BlockedInteratomicDistanceGraph
 
-class TestLJLK(unittest.TestCase):
-    @unittest.expectedFailure
-    def test_numpyros_comparison(self):
-        test_pdb = test_pdbs["1ubq"]
-        test_structure = read_pdb(test_pdb)
 
-        test_params = system_real_graph_params(
-            test_structure,
-            drop_missing_atoms=False,
-            requires_grad=False,
-        )
+class LJLKGraph(
+        LJLKScoreGraph,
+        BlockedInteratomicDistanceGraph,
+        RealSpaceScoreGraph,
+):
+    pass
 
-        expected_scores = {
-            'lj_atr': -425.3,
-            'lj_rep': 248.8,
-            'lk': 255.8,
-        }
 
+@pytest.mark.xfail
+def test_ljlk_numpyros_comparison(ubq_system):
+    test_structure = ubq_system
+
+    test_params = system_real_graph_params(
+        test_structure,
+        drop_missing_atoms=False,
+        requires_grad=False,
+    )
+
+    expected_scores = {
+        'lj_atr': -425.3,
+        'lj_rep': 248.8,
+        'lk': 255.8,
+    }
+
+    numpy.testing.assert_allclose(
+        LJLKGraph(**test_params).total_lj.detach(),
+        expected_scores["lj_atr"] + expected_scores["lj_rep"],
+        rtol=5e-3
+    )
+
+    numpy.testing.assert_allclose(
+        LJLKGraph(**test_params).total_lk.detach(),
+        expected_scores["lk"],
+        rtol=5e-3
+    )
+
+
+def test_baseline_comparison(ubq_system):
+    test_structure = ubq_system
+
+    test_params = system_real_graph_params(
+        test_structure,
+        drop_missing_atoms=False,
+        requires_grad=False,
+    )
+
+    test_graph = LJLKGraph(**test_params)
+
+    expected_scores = {
+        'total_lj': -176.5,
+        'total_lk': 249.3,
+    }
+
+    for term, val in expected_scores.items():
         numpy.testing.assert_allclose(
-            tmol.score.ScoreGraph(**test_params).total_lj.detach(),
-            expected_scores["lj_atr"] + expected_scores["lj_rep"],
-            rtol=5e-3
+            getattr(test_graph, term).detach(), val, rtol=5e-3
         )
 
-        numpy.testing.assert_allclose(
-            tmol.score.ScoreGraph(**test_params).total_lk.detach(),
-            expected_scores["lk"],
-            rtol=5e-3
-        )
 
-    def test_baseline_comparison(self):
-        test_structure = read_pdb(test_pdbs["1ubq"])
+def save_intermediate_grad(var):
+    def store_grad(grad):
+        var.grad = grad
 
-        test_params = system_real_graph_params(
-            test_structure,
-            drop_missing_atoms=False,
-            requires_grad=False,
-        )
+    var.register_hook(store_grad)
 
-        test_graph = tmol.score.ScoreGraph(**test_params)
 
-        expected_scores = {
-            'total_lj': -176.5,
-            'total_lk': 249.3,
-        }
+def test_ljlk_smoke(ubq_system):
+    score_graph = LJLKGraph(
+        **system_real_graph_params(ubq_system, requires_grad=True)
+    )
 
-        for term, val in expected_scores.items():
-            numpy.testing.assert_allclose(
-                getattr(test_graph, term).detach(), val, rtol=5e-3
-            )
+    save_intermediate_grad(score_graph.lj)
+    save_intermediate_grad(score_graph.lk)
+    save_intermediate_grad(score_graph.atom_pair_dist)
+    save_intermediate_grad(score_graph.atom_pair_delta)
+
+    score_graph.total_score.backward(retain_graph=True)
+
+    assert (score_graph.total_score != 0).all()
+
+    lj_nan_scores = torch.nonzero(torch.isnan(score_graph.lj))
+    lj_nan_grads = torch.nonzero(torch.isnan(score_graph.lj.grad))
+    assert len(lj_nan_scores) == 0
+    assert len(lj_nan_grads) == 0
+    assert (score_graph.total_lj != 0).all()
+
+    lk_nan_scores = torch.nonzero(torch.isnan(score_graph.lk))
+    lk_nan_grads = torch.nonzero(torch.isnan(score_graph.lk.grad))
+    assert len(lk_nan_scores) == 0
+    assert len(lk_nan_grads) == 0
+    assert (score_graph.total_lk != 0).all()
+
+    nonzero_dist_grads = torch.nonzero(score_graph.atom_pair_dist.grad)
+    assert len(nonzero_dist_grads) != 0
+
+    nonzero_delta_grads = torch.nonzero(score_graph.atom_pair_delta.grad)
+    assert len(nonzero_delta_grads) != 0
+    nan_delta_grads = torch.nonzero(
+        torch.isnan(score_graph.atom_pair_delta.grad)
+    )
+    assert len(nan_delta_grads) == 0
+
+    nan_coord_grads = torch.nonzero(torch.isnan(score_graph.coords.grad))
+    assert len(nan_coord_grads) == 0

tmol/utility/genericnumeric.py
@@ -3,7 +3,7 @@ from functools import singledispatch
 import numpy
 import torch
 
-from torch.autograd import Variable
+from torch import Tensor
 
 
 @singledispatch
@@ -20,4 +20,4 @@ def exp(x, out=None):
     return numpy.exp(x, out=out)
 
 
-exp.register(Variable)(torch.exp)
+exp.register(Tensor)(torch.exp)