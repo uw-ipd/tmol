66a72c181830e52893e888b82ac501c4896dd07d
Alex Ford <fordas@uw.edu>
2018-05-11 10:45:13 -0700

    Kinematic tree construction via spanning trees. (#12)
    Squash commit of changes from #33, merging #48.
    
    Adds kinematic tree builder, providing initialization of kinematic trees
    via spanning graphs of bonded atoms. Provides support for prioritized
    spanning tree construction, allowing specific of bonds that *must* be
    present in the generated kinematic tree, otherwise building via a
    minimum spanning tree of the bond graph.

---
+++ tmol/tests/kinematics/test_builder.py
@@ -0,0 +1,81 @@
+import numpy
+import torch
+
+from tmol.kinematics import (
+    backwardKin,
+    forwardKin,
+)
+
+from tmol.kinematics.builder import KinematicBuilder
+
+
+def test_builder_refold(ubq_system):
+    tsys = ubq_system
+
+    kintree = KinematicBuilder().append_connected_component(
+        *KinematicBuilder.bonds_to_connected_component(0, tsys.bonds)
+    ).kintree
+
+    kincoords = torch.DoubleTensor(tsys.coords[kintree.id])
+    refold_kincoords = forwardKin(
+        kintree,
+        backwardKin(kintree, kincoords).dofs
+    ).coords
+
+    assert numpy.all(refold_kincoords[0] == 0)
+
+    refold_coords = numpy.full_like(tsys.coords, numpy.nan)
+    refold_coords[kintree.id[1:].squeeze()] = refold_kincoords[1:]
+
+    numpy.testing.assert_allclose(tsys.coords, refold_coords)
+
+
+def test_builder_framing(ubq_system):
+    """Test first-three-atom framing logic in kinematic builder."""
+    tsys = ubq_system
+    kintree = KinematicBuilder().append_connected_component(
+        *KinematicBuilder.bonds_to_connected_component(0, tsys.bonds)
+    ).kintree
+
+    # The first entries in the tree should be the global DOF root, self-parented,
+    # followed by the first atom.
+    root_children = kintree[kintree.parent == 0]
+    assert len(root_children) == 2
+    numpy.testing.assert_array_equal(kintree.parent[:2], [0, 0])
+    numpy.testing.assert_array_equal(kintree.id[:2], [-1, 0])
+
+    # The first atom has two children. The first atom and its first child are framed by
+    # [first_child, root, second_child]
+    atom_root_children = numpy.flatnonzero(numpy.array(kintree.parent) == 1)
+    assert len(atom_root_children) == 2
+
+    first_atom = kintree[1]
+    assert int(first_atom.frame_x) == atom_root_children[0]
+    assert int(first_atom.frame_y) == 1
+    assert int(first_atom.frame_z) == atom_root_children[1]
+
+    first_atom_first_child = kintree[atom_root_children[0]]
+    assert int(first_atom_first_child.frame_x) == atom_root_children[0]
+    assert int(first_atom_first_child.frame_y) == 1
+    assert int(first_atom_first_child.frame_z) == atom_root_children[1]
+
+    # The rest of the children are framed by:
+    # [self, root, first_child]
+    for c in atom_root_children[1:]:
+        first_atom_other_child = kintree[c]
+        assert int(first_atom_other_child.frame_x) == c
+        assert int(first_atom_other_child.frame_y) == 1
+        assert int(first_atom_other_child.frame_z) == atom_root_children[0]
+
+    # Other atoms are framed normally, [self, parent, grandparent]
+    normal_atoms = numpy.flatnonzero(numpy.array(kintree.parent > 1))
+    numpy.testing.assert_array_equal(
+        kintree.frame_x[normal_atoms], normal_atoms
+    )
+    numpy.testing.assert_array_equal(
+        kintree.frame_y[normal_atoms], kintree.parent[normal_atoms]
+    )
+    numpy.testing.assert_array_equal(
+        kintree.frame_z[normal_atoms],
+        kintree.parent[kintree.parent[normal_atoms]]
+    )
