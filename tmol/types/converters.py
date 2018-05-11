825dfc8b3972fda28c85e9e911b7eefd7c2f24c4
Alex Ford <fordas@uw.edu>
2018-05-11 01:20:21 -0700

    Add conversion support for Union & Optional. (#46)
    Add type converter for Union types based on per-subtype conversion. Attempts conversion in order specified in Union type. Add support for "Optional" types as [T, NoneType] unions.
    
    Removes print statement from union validator function.

--- tmol/types/converters.py
+++ tmol/types/converters.py
@@ -26,5 +26,27 @@ def validate_convert(type_annotation, value):
     return value
 
 
-get_converter.register(typing._Union)(validate_convert)
+@toolz.curry
+def union_convert(union_annotation, value):
+    for subtype in union_annotation.__args__:
+        try:
+            validators.get_validator(subtype)(value)
+            return value
+        except (TypeError, ValueError):
+            pass
+
+    errors = []
+    for subtype in union_annotation.__args__:
+        try:
+            result = get_converter(subtype)(value)
+            return result
+        except (TypeError, ValueError) as ex:
+            errors.append(ex)
+
+    raise TypeError(
+        "Unable to convert to any union subtype: {union_annotaion} value: {value}"
+    )
+
+
+get_converter.register(typing._Union)(union_convert)
 get_converter.register(typing.TupleMeta)(validate_convert)
