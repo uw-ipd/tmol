6bbb3f968c0972a7b7db84887c46202efe7b23ee
Alex Ford <fordas@uw.edu>
2018-05-11 12:59:35 -0700

    Add torch.autograd support for kinematics operations. (#12)
    Squash rebase of #33, merging #41.
    
    Add torch.autograd component managing dof->coordinate forward kinematics
    and backward derivative propagation. Includes refold-based smoketest of
    forward operation and gradcheck-based verification of backward
    operation.

---
+++ tmol/tests/kinematics/test_torch_op.py
@@ -0,0 +1,176 @@
+import pytest
+import typing
+
+import pandas
+import torch
+import numpy
+
+from torch.autograd.gradcheck import get_numerical_jacobian, get_analytical_jacobian
+
+from tmol.types.torch import Tensor
+
+from tmol.kinematics.datatypes import KinTree
+from tmol.kinematics.metadata import DOFMetadata, DOFTypes
+from tmol.kinematics.torch_op import KinematicOp
+
+from tmol.system.residue.packed import PackedResidueSystem
+from tmol.system.residue.restypes import Residue
+from tmol.system.residue.kinematics import KinematicDescription
+
+
+def test_kinematic_torch_op_refold(ubq_system):
+    tsys = ubq_system
+    tkin = KinematicDescription.for_system(tsys.bonds, tsys.torsion_metadata)
+
+    torsion_dofs = tkin.dof_metadata[
+        (tkin.dof_metadata.dof_type == DOFTypes.bond_torsion)
+    ]
+
+    coords = torch.from_numpy(tsys.coords)
+
+    kincoords = coords[tkin.kintree.id]
+    kincoords[torch.isnan(kincoords)] = 0.0
+
+    kop = KinematicOp.from_coords(
+        tkin.kintree,
+        torsion_dofs,
+        kincoords,
+    )
+
+    refold_kincoords = kop.apply(kop.src_mobile_dofs)
+
+    numpy.testing.assert_allclose(kincoords, refold_kincoords)
+
+
+@pytest.fixture
+def gradcheck_test_system(
+        ubq_res: typing.Sequence[Residue],
+) -> typing.Tuple[KinTree,
+                  DOFMetadata,
+                  Tensor("f8")[:, 3],
+                  ]:
+    tsys = PackedResidueSystem.from_residues(ubq_res[:4])
+    tkin = KinematicDescription.for_system(tsys.bonds, tsys.torsion_metadata)
+
+    coords = torch.from_numpy(tsys.coords)
+    kincoords = coords[tkin.kintree.id]
+    kincoords[torch.isnan(kincoords)] = 0.0
+
+    return (tkin.kintree, tkin.dof_metadata, kincoords)
+
+
+def kop_gradcheck_report(
+        kop, dofs, start_dofs, eps=1e-6, atol=1e-5, rtol=1e-3
+):
+    initial_gradcheck = torch.autograd.gradcheck(
+        kop, (start_dofs, ), raise_exception=False
+    )
+
+    if initial_gradcheck:
+        # Initial exhausive gradcheck succeeded.
+        return
+
+    # Intiial gradcheck failed, generate a more specific failure report.
+
+    result = kop(start_dofs)
+
+    # Extract results from torch/autograd/gradcheck.py
+    (analytical, ), reentrant, correct_grad_sizes = get_analytical_jacobian(
+        (start_dofs, ), result
+    )
+    numerical = get_numerical_jacobian(kop, start_dofs, start_dofs, eps)
+
+    a = analytical
+    n = numerical
+
+    grad_match = ((a - n).abs() <= (atol + rtol * n.abs()))
+    if grad_match.all():
+        return
+
+    failures = torch.nonzero(~grad_match)
+    nfailures = len(failures)
+    failures = pandas.DataFrame({
+        "dof_input": failures[:, 0],
+        "node_idx": failures[:, 1] / 3,
+        "node_coord": numpy.array(list("xyz"))[failures[:, 1] % 3],
+        "analytical": a[~grad_match],
+        "numerical": n[~grad_match],
+    }).drop_duplicates()[[
+        "dof_input", "node_idx", "node_coord", "analytical", "numerical"
+    ]]
+
+    dof_summary = dofs.to_frame()
+    failing_dofs = failures["dof_input"].drop_duplicates().values
+
+    assert nfailures == 0, (
+        f"DOFs failed grad check:\n{failures}\n\n"
+        f"Failed DOF metadata:\n{dof_summary.iloc[failing_dofs]}\n\n"
+    )
+
+    # Rerun gradcheck to w/ exception to return full error.
+    torch.autograd.gradcheck(kop, (start_dofs, ), raise_exception=True)
+
+
+def test_kinematic_torch_op_gradcheck_perturbed(gradcheck_test_system):
+    kintree, dofs, kincoords = gradcheck_test_system
+
+    # Temporary workaround for #45, disable theta for post-jump siblings
+    post_root_siblings = ((dofs.parent_id == 0) &
+                          (dofs.dof_type == DOFTypes.bond_angle))
+    dofs = dofs[~post_root_siblings]
+
+    kop = KinematicOp.from_coords(
+        kintree,
+        dofs,
+        kincoords,
+    )
+
+    torch.random.manual_seed(1663)
+    start_dofs = torch.tensor(
+        kop.src_mobile_dofs +
+        ((torch.rand_like(kop.src_mobile_dofs) - .5) * .01),
+        requires_grad=True
+    )
+
+    kop_gradcheck_report(kop, dofs, start_dofs)
+
+
+def test_kinematic_torch_op_gradcheck(gradcheck_test_system):
+    kintree, dofs, kincoords = gradcheck_test_system
+
+    # Temporary workaround for #45, disable theta for post-jump siblings
+    post_root_siblings = ((dofs.parent_id == 0) &
+                          (dofs.dof_type == DOFTypes.bond_angle))
+    dofs = dofs[~post_root_siblings]
+
+    kop = KinematicOp.from_coords(
+        kintree,
+        dofs,
+        kincoords,
+    )
+
+    start_dofs = torch.tensor(kop.src_mobile_dofs, requires_grad=True)
+
+    kop_gradcheck_report(kop, dofs, start_dofs)
+
+
+def test_kinematic_torch_op_smoke(
+        gradcheck_test_system, pytorch_backward_coverage
+):
+    kintree, dofs, kincoords = gradcheck_test_system
+
+    kop = KinematicOp.from_coords(
+        kintree,
+        dofs,
+        kincoords,
+    )
+
+    start_dofs = torch.tensor(kop.src_mobile_dofs, requires_grad=True)
+
+    coords = kop(start_dofs)
+    coords.register_hook(pytorch_backward_coverage)
+
+    total = coords.sum()
+    total.backward()
+
+    assert start_dofs.grad is not None
