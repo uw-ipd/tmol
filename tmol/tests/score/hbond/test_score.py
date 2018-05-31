3b56a14a81dcaf4eef652adffd0aa5cbef76793e
Alex Ford <fordas@uw.edu>
2018-05-31 16:07:23 -0700

    Add hbond component benchmarks.

--- tmol/tests/score/hbond/test_score.py
+++ tmol/tests/score/hbond/test_score.py
@@ -1,3 +1,4 @@
+import pytest
 import torch
 
 from tmol.score.coordinates import CartesianAtomicCoordinateProvider
@@ -11,6 +12,47 @@ from tmol.utility.reactive import reactive_attrs
 @reactive_attrs
 class HBGraph(CartesianAtomicCoordinateProvider, HBondScoreGraph, TorchDevice):
     pass
+
+
+@pytest.mark.benchmark(
+    group="score_term",
+    min_rounds=10,
+    warmup=True,
+    warmup_iterations=10,
+)
+@pytest.mark.parametrize(
+    "benchmark_pass",
+    ["total", "forward", "backward"],
+)
+def test_hbond_ubq_score(
+        benchmark,
+        benchmark_pass,
+        ubq_system,
+        torch_device,
+):
+    score_graph = HBGraph(
+        **system_cartesian_space_graph_params(
+            ubq_system, requires_grad=True, device=torch_device
+        )
+    )
+
+    if benchmark_pass is "total":
+
+        @benchmark
+        def total():
+            score_graph.coords = score_graph.coords
+            return score_graph.step()
+    elif benchmark_pass is "forward":
+
+        @benchmark
+        def forward():
+            score_graph.coords = score_graph.coords
+            return float(score_graph.total_score)
+    elif benchmark_pass is "backward":
+
+        @benchmark
+        def backward():
+            score_graph.total_score.backward(retain_graph=True)
 
 
 def test_hbond_smoke(ubq_system, test_hbond_database, torch_device):
