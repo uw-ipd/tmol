6a2367d8631a3faff8ad05ad62cb4011f1f3f506
Alex Ford <fordas@uw.edu>
2018-06-07 16:40:21 -0700

    Add `Factory` mixin documentation.

--- tmol/score/factory.py
+++ tmol/score/factory.py
@@ -2,12 +2,32 @@ import tmol.utility.mixins as mixins
 
 
 class Factory:
+    """Mixin managing cooperative score graph factory functions.
+
+    `Factory` manages cooperative evaluation of a set of component-specifc
+    factory functions, defined via `factory_for` class/static methods. Each
+    factory function should extract a set of graph __init__ kwargs from an
+    input `val`, defaulting to implementing a partial clone from `val`
+    attributes.
+
+    Components factory functions *should*, if appropriate, allow for
+    `singledispatch` based overload on the type of `val`, allowing for
+    customization of score graph initialization for new input types. See
+    `tmol.system.score_support` for factory functions providing score graph
+    initialization from residue systems.
+
+    See `tmol.utility.mixins.cooperative_superclass_factory` for details of
+    kwarg-to-parameter resolution.
+    """
+
     @classmethod
     def build_for(cls, val, **kwargs):
+        """Construct score graph for val, defaults to cloning val."""
         return cls(**cls.init_parameters_for(val, **kwargs))
 
     @classmethod
     def init_parameters_for(cls, val, **kwargs):
+        """Get score graph params for val, defaults to cloning."""
         return mixins.cooperative_superclass_factory(
             cls,
             "factory_for",
