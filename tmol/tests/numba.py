054e6449c2ca720bcfdac2bfe1216f679c73f7ce
Alex Ford <fordas@uw.edu>
2018-06-28 17:18:50 -0600

    Fixup numba `with_cudasim` fixture to restore original cuda.

--- tmol/tests/numba.py
+++ tmol/tests/numba.py
@@ -5,18 +5,31 @@ import importlib
 import numba
 import numba.cuda
 
+import contextlib
 
-@pytest.fixture(scope="function")
-def numba_cudasim():
+
+@contextlib.contextmanager
+def with_cudasim():
+    """Rebinds `numba.cuda` to the simulator for the context."""
+    old_numba_dict = dict(numba.cuda.__dict__)
     old_numba_cudasim = numba.config.ENABLE_CUDASIM
 
     numba.config.ENABLE_CUDASIM = 1
     importlib.reload(numba.cuda)
 
-    yield numba.cuda
+    try:
+        yield numba.cuda
+    finally:
+        numba.config.ENABLE_CUDASIM = old_numba_cudasim
+        importlib.reload(numba.cuda)
 
-    numba.config.ENABLE_CUDASIM = old_numba_cudasim
-    importlib.reload(numba.cuda)
+        numba.cuda.__dict__.update(old_numba_dict)
+
+
+@pytest.fixture(scope="function")
+def numba_cudasim():
+    with with_cudasim():
+        yield numba.cuda
 
 
 @pytest.fixture(scope="function")
@@ -24,12 +37,5 @@ def numba_cuda_or_cudasim():
     if numba.cuda.is_available():
         yield numba.cuda
     else:
-        old_numba_cudasim = numba.config.ENABLE_CUDASIM
-
-        numba.config.ENABLE_CUDASIM = 1
-        importlib.reload(numba.cuda)
-
-        yield numba.cuda
-
-        numba.config.ENABLE_CUDASIM = old_numba_cudasim
-        importlib.reload(numba.cuda)
+        with with_cudasim():
+            yield numba.cuda
