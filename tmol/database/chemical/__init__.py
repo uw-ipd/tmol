aa8dffb76c7886542a86029e5250c2e0be06f830
Andrew Leaver-Fay <aleaverfay@gmail.com>
2018-07-11 10:02:03 -0400

    Adding AA enumeration so we can use an index to talk about which amino acid we're working with -- necessary for rama and dunbrack.
    Also adding a potentially useful, potentially unnecessary distinction
    between mainchain and sidechain named dihedrals to the residue-type
    parameter file

--- tmol/database/chemical/__init__.py
+++ tmol/database/chemical/__init__.py
@@ -1,12 +1,84 @@
 from typing import Tuple, Optional, NewType
 from tmol.utility.units import parse_angle, u
 from toolz import curry
+from frozendict import frozendict
 
 import attr
 import cattr
 
 import os
 import yaml
+import enum
+
+
+class AAType(enum.IntEnum):
+    aa_ala = 0
+    aa_cys = enum.auto()
+    aa_asp = enum.auto()
+    aa_glu = enum.auto()
+    aa_phe = enum.auto()
+    aa_gly = enum.auto()
+    aa_his = enum.auto()
+    aa_ile = enum.auto()
+    aa_lys = enum.auto()
+    aa_leu = enum.auto()
+    aa_met = enum.auto()
+    aa_asn = enum.auto()
+    aa_pro = enum.auto()
+    aa_gln = enum.auto()
+    aa_arg = enum.auto()
+    aa_ser = enum.auto()
+    aa_thr = enum.auto()
+    aa_val = enum.auto()
+    aa_trp = enum.auto()
+    aa_tyr = enum.auto()
+
+
+one_letter_to_aatype = frozendict({
+    "A": AAType.aa_ala,
+    "C": AAType.aa_cys,
+    "D": AAType.aa_asp,
+    "E": AAType.aa_glu,
+    "F": AAType.aa_phe,
+    "G": AAType.aa_gly,
+    "H": AAType.aa_his,
+    "I": AAType.aa_ile,
+    "K": AAType.aa_lys,
+    "L": AAType.aa_leu,
+    "M": AAType.aa_met,
+    "N": AAType.aa_asn,
+    "P": AAType.aa_pro,
+    "Q": AAType.aa_gln,
+    "R": AAType.aa_arg,
+    "S": AAType.aa_ser,
+    "T": AAType.aa_thr,
+    "V": AAType.aa_val,
+    "W": AAType.aa_trp,
+    "Y": AAType.aa_tyr
+})
+
+three_letter_to_aatype = frozendict({
+    "ALA": AAType.aa_ala,
+    "CSY": AAType.aa_cys,
+    "ASP": AAType.aa_asp,
+    "GLU": AAType.aa_glu,
+    "PHE": AAType.aa_phe,
+    "GLY": AAType.aa_gly,
+    "HIS": AAType.aa_his,
+    "ILE": AAType.aa_ile,
+    "LYS": AAType.aa_lys,
+    "LEU": AAType.aa_leu,
+    "MET": AAType.aa_met,
+    "ASN": AAType.aa_asn,
+    "PRO": AAType.aa_pro,
+    "GLN": AAType.aa_gln,
+    "ARG": AAType.aa_arg,
+    "SER": AAType.aa_ser,
+    "THR": AAType.aa_thr,
+    "VAL": AAType.aa_val,
+    "TRP": AAType.aa_trp,
+    "TYR": AAType.aa_tyr
+})
 
 
 @attr.s(auto_attribs=True, frozen=True, slots=True)
@@ -54,6 +126,7 @@ class ConnectedAtom:
 @attr.s(auto_attribs=True, frozen=True, slots=True)
 class Torsion:
     name: str
+    mainchain: bool
     a: ConnectedAtom
     b: ConnectedAtom
     c: ConnectedAtom
