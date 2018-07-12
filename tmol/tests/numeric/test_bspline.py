6fee9e6a92e871c18224ecf1b508ff4e16f6c8d5
Andrew Leaver-Fay <aleaverfay@gmail.com>
2018-07-12 09:15:38 -0400

    Adding a test fixture to test the available bspline degrees
    Looks like this causes a lot of the tests to fail!!

--- tmol/tests/numeric/test_bspline.py
+++ tmol/tests/numeric/test_bspline.py
@@ -1,23 +1,29 @@
 import torch
 import numpy
+import pytest
 from tmol.numeric.bspline import compute_coeffs, interpolate
 
 
-def test_2d_bspline():
+@pytest.fixture(params=[2, 3, 4, 5])
+def bspline_degree(request):
+    return request.param
+
+
+def test_2d_bspline(bspline_degree):
     # 2d
     x = torch.arange(-5, 6).unsqueeze(1)
     y = torch.arange(-5, 6).unsqueeze(0)
     z = (1 - x * x - y * y) * torch.exp(-0.5 * (x * x + y * y))
     z = z.type(torch.float)
 
-    zcoeff = compute_coeffs(z, 3)
-    zint = interpolate(zcoeff, 3, torch.Tensor([[2, 5], [3, 4]]))
+    zcoeff = compute_coeffs(z, bspline_degree)
+    zint = interpolate(zcoeff, bspline_degree, torch.Tensor([[2, 5], [3, 4]]))
 
     zgold = torch.Tensor([z[2, 5], z[3, 4]])
     numpy.testing.assert_allclose(zgold.numpy(), zint.numpy(), rtol=1e-6)
 
 
-def test_5x2d_bspline():
+def test_5x2d_bspline(bspline_degree):
     # 2d
     zs = torch.full((5, 11, 11), 0, dtype=torch.float)
     zscoeff = torch.full((5, 11, 11), 0, dtype=torch.float)
@@ -26,7 +32,7 @@ def test_5x2d_bspline():
         y = torch.arange(-5, 6).unsqueeze(0)
         z = (1 - x * x - y * y) * torch.exp(-0.5 * (x * x + y * y)) + i
         z = z.type(torch.float)
-        zcoeff = compute_coeffs(z, 3)
+        zcoeff = compute_coeffs(z, bspline_degree)
         zs[i, :, :] = z
         zscoeff[i, :, :] = zcoeff
 
@@ -39,7 +45,7 @@ def test_5x2d_bspline():
     numpy.testing.assert_allclose(zgold.numpy(), zint.numpy(), rtol=1e-6)
 
 
-def test_3d_bspline():
+def test_3d_bspline(bspline_degree):
     # 3d
     x = torch.arange(-5, 6).reshape(11, 1, 1)
     y = torch.arange(-5, 6).reshape(1, 11, 1)
@@ -47,13 +53,15 @@ def test_3d_bspline():
     w = (1 - x * x - y * y - z * z) * torch.exp(-0.5 * (x * x + y * y + z * z))
     w = w.type(torch.float)
 
-    wcoeff = compute_coeffs(w, 3)
-    wint = interpolate(wcoeff, 3, torch.Tensor([[2, 3, 5], [3, 4, 1]]))
+    wcoeff = compute_coeffs(w, bspline_degree)
+    wint = interpolate(
+        wcoeff, bspline_degree, torch.Tensor([[2, 3, 5], [3, 4, 1]])
+    )
     wgold = torch.Tensor([w[2, 3, 5], w[3, 4, 1]])
     numpy.testing.assert_allclose(wgold.numpy(), wint.numpy(), rtol=1e-6)
 
 
-def test_4d_bspline():
+def test_4d_bspline(bspline_degree):
     # 4d
     x = torch.arange(-5, 6).reshape(11, 1, 1, 1)
     y = torch.arange(-5, 6).reshape(1, 11, 1, 1)
@@ -63,8 +71,10 @@ def test_4d_bspline():
          ) * torch.exp(-0.5 * (x * x + y * y + z * z + w * w))
     u = u.type(torch.float)
 
-    ucoeff = compute_coeffs(u, 3)
-    uint = interpolate(ucoeff, 3, torch.Tensor([[2, 3, 5, 7], [3, 4, 1, 7]]))
+    ucoeff = compute_coeffs(u, bspline_degree)
+    uint = interpolate(
+        ucoeff, bspline_degree, torch.Tensor([[2, 3, 5, 7], [3, 4, 1, 7]])
+    )
 
     ugold = torch.Tensor((u[2, 3, 5, 7], u[3, 4, 1, 7]))
     numpy.testing.assert_allclose(ugold.numpy(), uint.numpy(), rtol=1e-6)
