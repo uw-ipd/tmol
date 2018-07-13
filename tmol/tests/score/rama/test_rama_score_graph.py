cec6b72b16a123d1902bd13cdb74d8d9a18a73aa
Andrew Leaver-Fay <aleaverfay@gmail.com>
2018-07-13 14:42:00 -0400

    Non-functional unit test; I'm getting errors from attrs in trying to instantiate the TRama class

---
+++ tmol/tests/score/rama/test_rama_score_graph.py
@@ -0,0 +1,25 @@
+import pytest
+
+import torch
+import numpy
+
+from tmol.utility.reactive import reactive_attrs
+from tmol.tests.torch import requires_cuda
+from tmol.score import (
+    TotalScoreGraph,
+    CartesianAtomicCoordinateProvider,
+)
+from tmol.score.rama.rama_score_graph import RamaScoreGraph
+
+
+@reactive_attrs(auto_attribs=True)
+class TRama(CartesianAtomicCoordinateProvider, RamaScoreGraph,
+            TotalScoreGraph):
+    """Cart total."""
+    pass
+
+
+def test_create_torsion_provider(ubq_system):
+    src = TRama.build_for(ubq_system)
+    assert src
+    print(src.rama_scores)
