8b54c04dc2ac88ce423d4a31876af8abe9bfda79
Andrew Leaver-Fay <aleaverfay@gmail.com>
2018-07-13 14:41:11 -0400

    ScoreGraph for the Ramachandran term

---
+++ tmol/score/rama/rama_score_graph.py
@@ -0,0 +1,90 @@
+import attr
+from typing import Optional
+
+import torch
+import numpy
+
+from ..database import ParamDB
+from ..device import TorchDevice
+from ..total_score import ScoreComponentAttributes, TotalScoreComponentsGraph
+from ..factory import Factory
+from ..torsions import AlphaAABackboneTorsionProvider
+from ..polymeric_bonds import PolymericBonds
+
+from tmol.database import ParameterDatabase
+from tmol.database.scoring.rama import CompactedRamaDatabase
+
+from tmol.utility.reactive import reactive_attrs, reactive_property
+
+from tmol.types.attrs import ValidateAttrs
+from tmol.types.functional import validate_args
+from tmol.types.array import NDArray
+from tmol.types.torch import Tensor
+
+
+@reactive_attrs(auto_attribs=True)
+class RamaScoreGraph(
+        AlphaAABackboneTorsionProvider,
+        PolymericBonds,
+        ParamDB,
+        TorchDevice,
+        Factory,
+):
+    @staticmethod
+    def factory_for(
+            val, parameter_database: ParamDB, device: torch.device, **_
+    ):
+        """Request the CompoactedRamaDatabase "held" (memoized) in the
+        parameter database. We only want a single copy of the parameter
+        database to live on the CPU or on the device.
+        """
+        rama_db = parameter_database.scoring.get_compacted_rama_db(device)
+        return dict(rama_db=rama_db, )
+
+    rama_db: CompactedRamaDatabase = attr.ib()
+
+    @rama_db.default
+    def _default_rama_database(self):
+        return self.parameter_database.scoring.get_compacted_rama_database(
+            torch.device("cpu")
+        )
+
+    @property
+    def component_total_score_terms(self):
+        """Expose rama score sum as total_score term."""
+        return ScoreComponentAttributes(
+            name="rama",
+            total="total_rama",
+        )
+
+    @reactive_property
+    @validate_args
+    def rama_scores(
+            phi_tor: Tensor(torch.float)[:], psi_tor: Tensor(torch.float)[:]
+    ) -> Tensor(torch.float):
+        has_rama = ~torch.isnan(phi_tor) and ~torch.isnan(psi_tor)
+        phi_psi = torch.cat((
+            phi_tor[has_rama].reshape(-1, 1), psi_tor[has_rama].reshape(-1, 1)
+        ),
+                            dim=1)
+        # shift range of [-pi,pi) to [0,36)
+        phi_psi *= 180 / (10 * numpy.pi)
+        phi_psi += 18
+
+        has_upper = self.upper != -1
+        upper_is_pro = self.res_aa[self.upper[has_upper
+                                              and has_rama]] == AAType.aa_pro
+        rama_inds = torch.cat(
+            ((self.res_aa[has_upper and has_rama]).reshape(-1, 1),
+             upper_is_pro.reshape(-1, 1)),
+            dim=1
+        )
+        return interpolate(
+            rama_db.coefficients, rama_db.bspdeg, phi_psi, rama_inds
+        )
+
+    @reactive_property
+    @validate_args
+    def total_rama(rama_scores: Tensor(torch.float)[:]) -> Tensor(torch.float):
+        """total ramachandran score"""
+        return rama_scores.sum()
