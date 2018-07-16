fb0733601e7935e7c7eeef743ce57befde698a26
Alex Ford <fordas@uw.edu>
2018-07-16 10:05:24 -0700

    Generate gh-pages from sphinx docs.

--- docs/conf.py
+++ docs/conf.py
@@ -45,6 +45,7 @@ extensions = [
     "sphinx.ext.mathjax",
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
+    "sphinx.ext.githubpages",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
