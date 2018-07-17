04d4ea62f3da3c93085e240a79ed20ddb133152e
Alex Ford <fordas@uw.edu>
2018-07-17 22:31:32 +0000

    buildkite/bin/update-gh-pages at 0.0.0-92-gabaa9a8 (HEAD)
    source: abaa9a8f2bf437a66badc11b1a6e4b2c465e74d5

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '',
+    VERSION: '0.0.0-92-gabaa9a8',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
