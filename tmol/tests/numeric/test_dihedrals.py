da2a8c607ed127c54cdff5fc0ee4ddfd450540bc
Andrew Leaver-Fay <aleaverfay@gmail.com>
2018-07-17 11:49:13 -0400

    Fixing the dihedral calculation unit test

--- tmol/tests/numeric/test_dihedrals.py
+++ tmol/tests/numeric/test_dihedrals.py
@@ -1,12 +1,13 @@
 from toolz.curried import compose, map
 import numpy
+import torch
 
 from tmol.numeric.dihedrals import coord_dihedrals
 from tmol.utility.units import parse_angle
 
 
 def test_coord_dihedrals():
-    coords = numpy.array([
+    coords = torch.FloatTensor([
         [24.969, 13.428, 30.692],  # N
         [24.044, 12.661, 29.808],  # CA
         [22.785, 13.482, 29.543],  # C
@@ -18,12 +19,12 @@ def test_coord_dihedrals():
         [numpy.nan, numpy.nan, numpy.nan],
     ])
 
-    dihedral_atoms = numpy.array([
+    dihedral_atoms = torch.LongTensor([
         [0, 1, 2, 3],
         [0, 1, 4, 5],
         [1, 4, 5, 6],
         [1, 4, 5, 7],
-        [-1, 0, 1, 3],
+        [8, 0, 1, 3],
     ])
 
     dihedrals = compose(numpy.array, list, map(parse_angle))([
@@ -35,10 +36,10 @@ def test_coord_dihedrals():
     ])
 
     calc_dihedrals = coord_dihedrals(
-        coords[dihedral_atoms][:, 0],
-        coords[dihedral_atoms][:, 1],
-        coords[dihedral_atoms][:, 2],
-        coords[dihedral_atoms][:, 3],
+        coords.index_select(0, dihedral_atoms[:, 0].squeeze()),
+        coords.index_select(0, dihedral_atoms[:, 1].squeeze()),
+        coords.index_select(0, dihedral_atoms[:, 2].squeeze()),
+        coords.index_select(0, dihedral_atoms[:, 3].squeeze()),
     )
 
-    numpy.testing.assert_allclose(dihedrals, calc_dihedrals, atol=1e-5)
+    numpy.testing.assert_allclose(calc_dihedrals.numpy(), dihedrals, atol=1e-5)
