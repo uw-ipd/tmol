74b816ac9d599a03f70fe16f1af80e60fffe0896
Alex Ford <fordas@uw.edu>
2018-07-24 22:37:16 -0700

    Add reactive_attrs '**kwargs' support. (#92)
    * Add reactive_attrs '**kwargs' support.
    
    Add support for **kwargs to reactive properties. Provides support for a
    "dynamic" definition of a reactive property's input dependencies as
    class definition time and/or generic binding of specific input
    properties into a generic property definition function.
    
    Fixup invocation of function, invoking via kwargs (rather than
    positional parameter) to support kwargs pass through. Expand function
    signature inspection to more effectively detect, and error, on
    positional-only parameters.
    
    Flesh out reactive_attrs test coverage for various signature types.
    Expand tests of reactive_property determination and evaluation. Add test
    coverage for kwargs feature.
    
    * Expand `tmol.utility.reactive` documentation.
    
    Expand `tmol.utility.reactive` module docstring with (ideally)
    clarifying example of property evaluation.

--- tmol/utility/reactive.py
+++ tmol/utility/reactive.py
@@ -1,47 +1,403 @@
-"""
-A prototype implementation of attrs-based "reactive" properties.
+"""Attrs-based "reactive" properties.
 
-A containing class stores state divided into two types: "standard" attributes,
-which represent source data, and "reactive" properties, which are derived from
-other attributes and other properties.
+Overview
+--------
 
-Reactive props are calculated dynamically via a "pull" model, accessing a
-property invokes its definition function to calculate the property value. The
-value is cached within the containing object and is returned, if available, on
-the next property access.
+`reactive` is a framework to describe the composition and execution of
+functions as properties and attributes of a class. By storing function results
+as object properties, rather than named variables, `reactive` allows you to
+organize :doc:`pure functions <toolz:purity>` in a declarative syntax, then
+automatically handles dependency resolution, on-demand execution and result
+caching.
 
-The definition function may depend on a set a attrs/props in the object, bound
-as the input parameters of the value function. This values are bound from the
-object *before* value function execution. As depdencies *may* be themselves
-reactive, resulting in a backward traversal through reactive properties to
-calculate the required value. DAG-ness is not mandated, but infinite recursion
-will occur in the case of mutually interdependent property values. Caveat usor.
+As a motivating example, `reactive` lets you write::
 
-Reative props are invalidated via a "push" model. Deleting or changing a
-property value will invalidate the store value for all declared dependent
-properties. These dependents may themselves have dependents, resulting in a
-forward traversal through reactive properties to invalidate all derived
-property values. This allows recalcuation when the property is next requested.
+    @reactive_attrs(auto_attribs=True)
+    class FactionalAnalysis:
+        source: str
 
-Property invalidation may be controlled via an optional "should_invalidate"
-function, which consumes (a) the current reactive property value, (b) the name
-of the changed input value and (c) the updated input parameter value. This
-function returns True if the reactive property should be invalidated in
-response to the change and False if the reactive value should be preserved.
+        montegue_faction: Sequence[str] = ["Romeo", "Benvolio", "Montegue", "Lady Montegue"]
+        capulet_faction: Sequence[str] = ["Juliet", "Tybalt", "Capulet", "Lady Capulet"]
 
-Subclassing semantics akin to "attr" can be used. A superclass *may* specifiy
-property dependencies that are provided by a subclass. The standard MRO is used
-to resolve the value function for any duplicate properties.
+        @reactive_property
+        def lines(source);
+            '''Convert text of play to spoken lines.'''
+            ...
+
+        @reactive_property
+        def soliloquies(lines):
+            '''Group spoken lines and identify soliloquies.'''
+            ...
+
+        @reactive_property
+        def montegue_verbosity(montegue_faction, soliloquies):
+            '''Calcuate average verbosity of the Montegue faction.'''
+            ...
+
+        @reactive_property
+        def capulet_verbosity(capulet_faction, soliloquies):
+            '''Calcuate average verbosity of the Capulet faction.'''
+            ...
+
+then *does the right thing* when you ask for::
+
+    analysis = FactionalAnalysis(source=act_1)
+    analysis.montegue_verbosity / analysis.capulet_verbosity
+
+Argument Resolution
+~~~~~~~~~~~~~~~~~~~
+
+`reactive` organizes a computation by binding input data and function results
+as `attributes <attrs:overview>` of a "reactive object". Function *inputs* are
+resolved on-demand by keyword argument name via `getattr` from reactive object
+properties.  By eschewing `self`, `reactive` allows direct declaration of
+inter-function dependencies.
+
+For example, ``FactionalAnalysis`` results in the dependency graph:
+
+.. graphviz::
+    :align: center
+
+    digraph {
+        source [shape=box ]
+        capulet_faction [shape=box ]
+        montegue_faction [shape=box ]
+
+        source -> lines
+        lines -> soliloquies
+
+        capulet_faction -> capulet_verbosity
+        soliloquies -> capulet_verbosity
+
+        montegue_faction -> montegue_verbosity
+        soliloquies -> montegue_verbosity
+    }
+
+|
+
+Result Caching
+~~~~~~~~~~~~~~
+
+Given the assumption of :doc:`functional purity <toolz:purity>`, `reactive`
+caches the result of `reactive_property` evaluations from the on-demand
+resolution of property arguments. This provides in a lazy pull-based execution
+model, in which requesting a `reactive_property` value results in the
+evaluation, and caching, of its antecedent dependencies.
+
+In ``FactionalAnalysis``, ``analysis.capulet_verbosity`` results in in the
+evaluations:
+
+.. graphviz::
+    :align: center
+
+    digraph {
+        source [shape=box ]
+        capulet_faction [shape=box ]
+        montegue_faction [shape=box ]
+
+        lines [style=filled]
+        soliloquies [style=filled]
+        capulet_verbosity [style=filled]
+
+        source -> lines [penwidth=5.0]
+        lines -> soliloquies [penwidth=5.0]
+
+        capulet_faction -> capulet_verbosity [penwidth=5.0]
+        soliloquies -> capulet_verbosity [penwidth=5.0]
+
+        montegue_faction -> montegue_verbosity
+        soliloquies -> montegue_verbosity
+    }
+
+|
+
+The subsequent ``analysis.montegue_verbosity`` reuses the cached
+``soliloquies``, resulting in the evaluations:
+
+.. graphviz::
+    :align: center
+
+    digraph {
+        source [shape=box ]
+        capulet_faction [shape=box ]
+        montegue_faction [shape=box ]
+
+        lines [style=filled]
+        soliloquies [style=filled]
+        capulet_verbosity [style=filled]
+        montegue_verbosity [style=filled]
+
+        source -> lines
+        lines -> soliloquies
+
+        capulet_faction -> capulet_verbosity
+        soliloquies -> capulet_verbosity
+
+        montegue_faction -> montegue_verbosity [penwidth=5.0]
+        soliloquies -> montegue_verbosity [penwidth=5.0]
+    }
+
+|
+
+Further access to any property returns the cached value.
+
+Reactive Invalidation
+~~~~~~~~~~~~~~~~~~~~~
+
+`reactive` uses the declared dependency graph to invalidate dependent values if
+an attribute value is changed, and forward-propogates the invalidation of
+reactive properties to all dependents. This provides a lazy, push-based update
+model, in which updating an attribute value results in the selective
+invalidation of its full direct and indirect dependencies.
+
+.. note:: `reactive` only tracks modification of attribute values by `setattr`,
+    and does *not* track internal modification of attribute values. Updates to
+    attributes must be propogated by property reassignment. Eg::
+
+        # Possible, modify and assign
+        analysis.capulet_faction.extend(["Sampson", "Gregory"])
+        analysis.capulet_faction = analysis.capulet_faction
+
+        # Preferred, assign updated value
+        analysis.capulet_faction = analysis.capulet_faction + ["Sampson", "Gregory"]
+
+In ``FactionalAnalysis`` an update of ``capulet_faction`` results in invalidations:
+
+.. graphviz::
+    :align: center
+
+    digraph {
+        source [shape=box ]
+        capulet_faction [shape=tripleoctagon ]
+        montegue_faction [shape=box ]
+
+        lines [style=filled]
+        soliloquies [style=filled]
+        capulet_verbosity [style=dashed]
+        montegue_verbosity [style=filled]
+
+        source -> lines
+        lines -> soliloquies
+
+        capulet_faction -> capulet_verbosity [penwidth=5.0]
+        soliloquies -> capulet_verbosity
+
+        montegue_faction -> montegue_verbosity
+        soliloquies -> montegue_verbosity
+    }
+
+|
+
+A subsequent update of ``source`` results in the forward-invalidations:
+
+.. graphviz::
+    :align: center
+
+    digraph {
+        source [shape=tripleoctagon ]
+        capulet_faction [shape=box ]
+        montegue_faction [shape=box ]
+
+        lines [style=dashed]
+        soliloquies [style=dashed]
+        capulet_verbosity
+        montegue_verbosity [style=dashed]
+
+        source -> lines [penwidth=5.0]
+        lines -> soliloquies [penwidth=5.0]
+
+        capulet_faction -> capulet_verbosity
+        soliloquies -> capulet_verbosity [penwidth=5.0]
+
+        montegue_faction -> montegue_verbosity
+        soliloquies -> montegue_verbosity [penwidth=5.0]
+    }
+
+|
+
+
+Class Composition
+~~~~~~~~~~~~~~~~~
+
+`reactive` classes can be combined via standard inheritance, with cross class
+dependencies are resolved via by the standard :term:`method resolution order
+<python:method resolution order>`. This enables a robust mixin-based
+composition model in which function inputs are passed, by name, between loosely
+coupled components.
+
+For example, consider an extension of ``FactionalAnalysis`` to incorporate an
+additional form of sentiment analysis::
+
+    @reactive_attrs(auto_attribs=True)
+    class Source:
+        source: str
+
+        montegue_faction: Sequence[str] = ["Romeo", "Benvolio", "Montegue", "Lady Montegue"]
+        capulet_faction: Sequence[str] = ["Juliet", "Tybalt", "Capulet", "Lady Capulet"]
+
+        @reactive_property
+        def lines(source);
+            '''Convert text of play to spoken lines.'''
+            ...
+
+    @reactive_attrs
+    class Verbosity:
+        @reactive_property
+        def soliloquies(lines):
+            '''Group spoken lines and identify soliloquies.'''
+            ...
+
+        @reactive_property
+        def montegue_verbosity(montegue_faction, soliloquies):
+            '''Calcuate average verbosity of the Montegue faction.'''
+            ...
+
+        @reactive_property
+        def capulet_verbosity(capulet_faction, soliloquies):
+            '''Calcuate average verbosity of the Capulet faction.'''
+            ...
+
+    @reactive_attrs
+    class Sentiment:
+        @reactive_property
+        def modern_lines(lines):
+            '''Convert from shakespearean to modern english.'''
+            ...
+
+        @reactive_property
+        def line_sentiment(modern_lines):
+            '''Black-box sentiment analysis.'''
+            ...
+
+        @reactive_property
+        def montegue_sentiment(montegue_faction, line_sentiment):
+            '''Calcuate average sentiment of the Montegue faction.'''
+            ...
+
+        @reactive_property
+        def capulet_sentiment(capulet_faction, line_sentiment):
+            '''Calcuate average sentiment of the Capulet faction.'''
+            ...
+
+    @reactive_attrs
+    class GrudgeAnalysis(Sourde, Verbosity, Sentiment):
+        @reactive_property
+        def grudge(
+            montegue_sentiment,
+            montegue_verbosity,
+            capulet_sentiment,
+            capulet_verbosity,
+        ):
+            '''From ancient grudge break to new mutiny?'''
+            pass
+
+Resulting in the combined reactive dependencies:
+
+.. graphviz::
+    :align: center
+
+    digraph {
+        subgraph cluster_Source {
+            label = "Source"
+
+            source [shape=box ]
+            capulet_faction [shape=box ]
+            montegue_faction [shape=box ]
+
+            lines
+            source -> lines
+        }
+
+        subgraph cluster_Verbosity {
+            label = "Verbosity"
+
+            soliloquies
+            capulet_verbosity
+            montegue_verbosity
+
+            lines -> soliloquies
+
+            capulet_faction -> capulet_verbosity
+            soliloquies -> capulet_verbosity
+
+            montegue_faction -> montegue_verbosity
+            soliloquies -> montegue_verbosity
+        }
+
+        subgraph cluster_Sentiment {
+            label = "Sentiment"
+
+            modern_lines
+            line_sentiment
+
+            montegue_sentiment
+            capulet_sentiment
+
+            lines -> modern_lines
+            modern_lines -> line_sentiment
+
+            line_sentiment -> montegue_sentiment
+            montegue_faction -> montegue_sentiment
+
+            line_sentiment -> capulet_sentiment
+            capulet_faction -> capulet_sentiment
+        }
+
+        subgraph cluster_GrudgeAnalysis {
+            label = "GrudgeAnalysis";
+
+            grudge
+
+            capulet_sentiment -> grudge
+            capulet_verbosity -> grudge
+
+            montegue_sentiment -> grudge
+            montegue_verbosity -> grudge
+        }
+
+
+    }
+
+|
+
+
+Custom Invalidation
+~~~~~~~~~~~~~~~~~~~
+
+Forward-invalidation can be customized on a per-property basis via an optional
+`should_invalidate` function. The function is provided (a) the current
+reactive property value, (b) the name of the changed input value and (c) the
+updated input parameter value, returning `True` if the reactive property should
+be invalidated in response to the change and `False` if the reactive value
+should be preserved.
+
+.. note::
+    `should_invalidate` *requires* the value of reevaluation of
+    parameter dependencies to provide an updated input parameter value, causing an
+    "eager" reevaluation of the subgraph preceeding the `should_invalidate`
+    property.
+
+** Parameters
+~~~~~~~~~~~~~
+
+Functions with :term:`**keyword parameters <python:parameter>` *must*
+specify a set of expected argument names when added as a `reactive_property`.
+
+
 """
 
 import inspect
 from collections import defaultdict
-from typing import Callable, Any, Optional, Tuple
+from typing import Callable, Any, Optional, Tuple, Union
 
 import attr
 
 
 def reactive_attrs(maybe_cls=None, **attrs_kwargs):
+    """A class decorator that initializes reactive properties over the target
+    type.
+    """
+
     def wrap(cls):
         return attr.attrs(_setup_reactive(cls), **attrs_kwargs)
 
@@ -51,14 +407,38 @@ def reactive_attrs(maybe_cls=None, **attrs_kwargs):
         return wrap(maybe_cls)
 
 
-def reactive_property(f, should_invalidate=None):
-    prop = ReactiveProperty.from_function(f)
-    prop.should_invalidate(should_invalidate)
-    return prop
+def reactive_property(func=None, *, should_invalidate=None, kwargs=None):
+    """Decorate a function as a reactive property of a class.
+
+    ..  warning::
+        Does *not* do anything unless the class is also decorated with
+        :func:`reactive_attrs`.
+
+    Args:
+        func: The target function. The reactive property name and
+            dependencies will be inferred from the function name and signature.
+
+        should_invalidate: A :func:`callable` controlling selective
+            invalidation of the reactive property in response to dependency
+            changes.
+
+        kwargs: Name of expected keyword parameters iff ``func`` accepts
+            :term:`**keyword parameters <python:parameter>`.
+    """
+
+    def bind(f):
+        prop = _ReactiveProperty.from_function(f, kwargs=kwargs)
+        prop.should_invalidate(should_invalidate)
+        return prop
+
+    if func is not None:
+        return bind(func)
+    else:
+        return bind
 
 
 @attr.s(slots=True, auto_attribs=True)
-class ReactiveProperty:
+class _ReactiveProperty:
     name: str
     parameters: Tuple[str, ...]
 
@@ -71,16 +451,62 @@ class ReactiveProperty:
         return f
 
     @classmethod
-    def from_function(cls, fun):
-        parameters = tuple(inspect.signature(fun).parameters)
-        if "self" in parameters:
+    def from_function(
+            cls,
+            fun: Callable,
+            kwargs: Optional[Union[str, Tuple[str, ...]]] = None
+    ):
+        parameters = inspect.signature(fun).parameters.values()
+
+        param_types = set(p.kind for p in parameters)
+
+        ### Check for any positional-only arguments
+        if any(param_type not in (inspect.Parameter.POSITIONAL_OR_KEYWORD,
+                                  inspect.Parameter.KEYWORD_ONLY,
+                                  inspect.Parameter.VAR_KEYWORD)
+               for param_type in param_types):
+            raise ValueError(
+                f"function signature contains invalid parameter type: {parameters}"
+            )
+
+        ### Get name of all keyword params
+        parameter_names = tuple(
+            p.name for p in parameters if p.kind in (
+                inspect.Parameter.POSITIONAL_OR_KEYWORD,
+                inspect.Parameter.KEYWORD_ONLY
+            )
+        )
+
+        ### Check for "self" parameter
+        if "self" in parameter_names:
             raise ValueError(
                 "Reactive property value function do not bind 'self' parameter."
             )
 
+        ### Check for **kwargs, and add request kwarg names if provided
+        if inspect.Parameter.VAR_KEYWORD in param_types:
+            if isinstance(kwargs, str):
+                kwargs = (kwargs, )
+
+            if kwargs is None:
+                raise ValueError(
+                    "Function binds **kwargs, but no kwarg names provided."
+                )
+            elif set(parameter_names).intersection(kwargs):
+                raise ValueError(
+                    "Specified kwarg is already explict parameter. parameters: {params} kwargs: {kwargs}"
+                )
+
+            parameter_names = parameter_names + tuple(kwargs)
+        else:
+            if kwargs is not None:
+                raise ValueError(
+                    "Function does not bind **kwargs, but kwarg names provided."
+                )
+
         return cls(
             name=fun.__name__,
-            parameters=parameters,
+            parameters=parameter_names,
             f_value=fun,
         )
 
@@ -91,7 +517,7 @@ def _setup_reactive(cls):
     reactive_props = {
         n: v
         for n, v in cd.items()
-        if isinstance(v, ReactiveProperty)
+        if isinstance(v, _ReactiveProperty)
     }
 
     for n in reactive_props:
@@ -151,7 +577,7 @@ def _setup_reactive(cls):
     return cls
 
 
-def invalidate_reactive_deps(obj, n, n_value=attr.NOTHING):
+def _invalidate_reactive_deps(obj, n, n_value=attr.NOTHING):
     if not hasattr(obj, "_reactive_values") or n not in obj.__reactive_deps__:
         return
 
@@ -165,7 +591,7 @@ def invalidate_reactive_deps(obj, n, n_value=attr.NOTHING):
                     continue
 
             delattr(obj._reactive_values, dname)
-            invalidate_reactive_deps(obj, dname)
+            _invalidate_reactive_deps(obj, dname)
 
 
 def __reactive_getattr__(self, n):
@@ -177,7 +603,7 @@ def __reactive_getattr__(self, n):
     if hasattr(self._reactive_values, n):
         return getattr(self._reactive_values, n)
 
-    val = prop.f_value(*(getattr(self, p) for p in prop.parameters))
+    val = prop.f_value(**{p: getattr(self, p) for p in prop.parameters})
 
     setattr(self._reactive_values, n, val)
 
@@ -186,9 +612,9 @@ def __reactive_getattr__(self, n):
 
 def __reactive_setattr__(self, n, v):
     object.__setattr__(self, n, v)
-    invalidate_reactive_deps(self, n, v)
+    _invalidate_reactive_deps(self, n, v)
 
 
 def __reactive_delattr__(self, n):
     object.__delattr__(self, n)
-    invalidate_reactive_deps(self, n)
+    _invalidate_reactive_deps(self, n)
