c82f464f3940a4277e7c69ffb995035046de5abf
Alex Ford <fordas@uw.edu>
2018-08-22 09:09:11 -0700

    Convert score representation to layered sets of atoms.
    Convert score atom representation from a shape (n,) buffer of atoms to
    a shape (l, n) buffer of of layered atoms. Change in data representation
    forms foundation for later addition of intra/inter layer scoring
    semantics.
    
    Add new "stack_system" score graph component describing system depth and
    size. Tidy up implementation of existing score graph components to
    eliminate use of default attribute values, in favor of factory function
    defaults, in order to resolve keyword attribute ordering issues with
    attrs.
    
    Update blocked interatomic distance system to operated on layered atoms,
    and adding testing for proper inter-block threshold distance calculation
    in layered systems. Update blocked distance calculation to decompose
    calculation into per-block "sphere" calculation followed by calculation
    of inter-sphere threshold distance calculation.
    
    Update score term implementations to support layered interface, but only
    support single layer graphs.
    
    Update viewer component and viewer tests to support depth 1 score
    graphs.
    
    Add minor expansion of architectural notes to describe layered system.
    Update docs napoleon configuration to report attribute documentation
    from class docstrings.

--- tmol/score/bonded_atom.py
+++ tmol/score/bonded_atom.py
@@ -3,53 +3,83 @@ from functools import singledispatch
 import torch
 import numpy
 
-import scipy.sparse
+import sparse
+import scipy.sparse.csgraph as csgraph
 
 from tmol.utility.reactive import reactive_attrs, reactive_property
 
 from tmol.types.array import NDArray
 from tmol.types.torch import Tensor
+from tmol.types.functional import validate_args
 
 from .database import ParamDB
 from .factory import Factory
+from .stacked_system import StackedSystem
 
 
 @reactive_attrs(auto_attribs=True)
-class BondedAtomScoreGraph(ParamDB, Factory):
+class BondedAtomScoreGraph(StackedSystem, ParamDB, Factory):
+    """Score graph component describing a system's atom types and bonds.
+
+    Attributes:
+        atom_types: [layer, atom_index] String atom type descriptors.
+            Type descriptions defined in :py:mod:`tmol.database.chemical`.
+
+        bonds:[layer, atom_index, atom_index] Inter-atomic bond indices.
+            Note that bonds are strictly intra-layer, and are defined by a
+            single layer index for both atoms of the bond.
+
+        MAX_BONDED_PATH_LENGTH: Maximum relevant inter-atomic path length.
+            Limits search depth used in ``bonded_path_length``, all longer
+            paths reported as ``inf``.
+
+    """
+
+    MAX_BONDED_PATH_LENGTH = 6
+
     @staticmethod
     @singledispatch
     def factory_for(other, **_):
         """`clone`-factory, extract atom types and bonds from other."""
         return dict(atom_types=other.atom_types, bonds=other.bonds)
 
-    # String atom types
-    atom_types: NDArray(object)[:]
-
-    # Inter-atomic bond indices
-    bonds: NDArray(int)[:, 2]
+    atom_types: NDArray(object)[:, :]
+    bonds: NDArray(int)[:, 3]
 
     @reactive_property
-    def system_size(atom_types) -> int:
-        """Number of atom locations within the system."""
-        return len(atom_types)
-
-    @reactive_property
-    def real_atoms(atom_types: NDArray(object)[:],) -> Tensor(bool)[:]:
-        """Mask of 'real' atomic indices in the system."""
+    @validate_args
+    def real_atoms(atom_types: NDArray(object)[:, :],) -> Tensor(bool)[:, :]:
+        """Mask of non-null atomic indices in the system."""
         return torch.ByteTensor(
             (atom_types != None).astype(numpy.ubyte)
         )  # noqa: E711 - None != is a vectorized check for None.
 
     @reactive_property
+    @validate_args
     def bonded_path_length(
-        bonds: NDArray(int)[:, 2], system_size: int
-    ) -> NDArray("f4")[:, :]:
-        """Dense inter-atomic bonded path length distance matrix."""
-        return scipy.sparse.csgraph.shortest_path(
-            scipy.sparse.coo_matrix(
-                (numpy.ones(bonds.shape[0], dtype=bool), (bonds[:, 0], bonds[:, 1])),
-                shape=(system_size, system_size),
-            ),
-            directed=False,
-            unweighted=True,
-        ).astype("f4")
+        bonds: NDArray(int)[:, 3],
+        stack_depth: int,
+        system_size: int,
+        MAX_BONDED_PATH_LENGTH: int,
+    ) -> NDArray("f4")[:, :, :]:
+        """Dense inter-atomic bonded path length distance tables.
+
+        Returns:
+            [layer, from_atom, to_atom]
+            Per-layer interatomic bonded path length entries.
+        """
+
+        bond_graph = sparse.COO(
+            bonds.T,
+            data=numpy.full(len(bonds), True),
+            shape=(stack_depth, system_size, system_size),
+            cache=True,
+        )
+
+        result = numpy.empty(bond_graph.shape, dtype="f4")
+        for l in range(stack_depth):
+            result[l] = csgraph.dijkstra(
+                bond_graph[l].tocsr(), directed=False, unweighted=True, limit=6
+            )
+
+        return result
---
+++ tmol/score/stacked_system.py
@@ -0,0 +1,31 @@
+from functools import singledispatch
+
+from tmol.utility.reactive import reactive_attrs
+
+from .factory import Factory
+
+
+@reactive_attrs(auto_attribs=True)
+class StackedSystem(Factory):
+    """Score graph component describing stacked system's "depth" and "size".
+
+    A score graph is defined over a set of independent system layers. The
+    number layers defines the stacked "depth", and the maximum number of atoms
+    per layer defines the system "size". Each layer is defined over the same
+    maximum number of atoms, but systems may have varying number of null atoms.
+
+    Atom indices are defined by a layer index, atom index (l, n) tuple.
+
+    Attributes:
+        stack_depth: The system stack depth, ``l``.
+        system_size: The maximum number of atoms per layer, ``n``.
+    """
+
+    @staticmethod
+    @singledispatch
+    def factory_for(val, **_):
+        """Overridable clone-constructor."""
+        return dict(stack_depth=val.stack_depth, system_size=val.system_size)
+
+    stack_depth: int
+    system_size: int
