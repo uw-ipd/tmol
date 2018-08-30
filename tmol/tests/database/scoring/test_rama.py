5ad0fb3debce528f23d93653379cc1174b7c1fae
Andrew Leaver-Fay <aleaverfay@gmail.com>
2018-08-30 09:28:37 -0400

    Commenting out rama-load benchmark for higher coverage; last two bits of tmol/database/scoring/rama.py now tested

--- tmol/tests/database/scoring/test_rama.py
+++ tmol/tests/database/scoring/test_rama.py
@@ -42,7 +42,7 @@ def test_compacted_rama(torch_device):
             interp_vals = compacted.bspline.interpolate(x, y)
             numpy.testing.assert_allclose(
                 interp_vals.cpu().detach().numpy(),
-                original_vals.detach().numpy(),
+                original_vals.cpu().detach().numpy(),
                 atol=1e-5,
                 rtol=1e-7,
             )
@@ -55,28 +55,43 @@ def test_load_compacted_rama_once(torch_device):
     assert crama1 is crama2
 
 
-@pytest.mark.skip(reason="Slow benchmark in yaml case, not functionally relevant.")
-@pytest.mark.benchmark(group="rama_load", min_rounds=1)
-@pytest.mark.parametrize("method", ["json", "yaml-loader", "yaml-cloader"])
-def test_rama_load_benchmark(benchmark, method):
-    path = {
-        "json": "tmol/database/default/scoring/rama.json",
-        "yaml-loader": "tmol/database/default/scoring/rama.yaml",
-        "yaml-cloader": "tmol/database/default/scoring/rama.yaml",
-    }[method]
+# -- code coverage killer -- @pytest.mark.skip(reason="Slow benchmark in yaml case, not functionally relevant.")
+# -- code coverage killer -- @pytest.mark.benchmark(group="rama_load", min_rounds=1)
+# -- code coverage killer -- @pytest.mark.parametrize("method", ["json", "yaml-loader", "yaml-cloader"])
+# -- code coverage killer -- def test_rama_load_benchmark(benchmark, method):
+# -- code coverage killer --     path = {
+# -- code coverage killer --         "json": "tmol/database/default/scoring/rama.json",
+# -- code coverage killer --         "yaml-loader": "tmol/database/default/scoring/rama.yaml",
+# -- code coverage killer --         "yaml-cloader": "tmol/database/default/scoring/rama.yaml",
+# -- code coverage killer --     }[method]
+# -- code coverage killer --
+# -- code coverage killer --     load = {
+# -- code coverage killer --         "json": lambda infile: json.load(infile),
+# -- code coverage killer --         "yaml-loader":
+# -- code coverage killer --         # defaults to yaml.Loader
+# -- code coverage killer --         lambda infile: yaml.load(infile),
+# -- code coverage killer --         "yaml-cloader": lambda infile: yaml.load(infile, yaml.CLoader),
+# -- code coverage killer --     }[method]
+# -- code coverage killer --
+# -- code coverage killer --     @benchmark
+# -- code coverage killer --     def db():
+# -- code coverage killer --         with open(path, "r") as infile:
+# -- code coverage killer --             raw = load(infile)
+# -- code coverage killer --         return cattr.structure(raw, RamaDatabase)
+# -- code coverage killer --
+# -- code coverage killer --     assert len(db.tables) == 40
 
-    load = {
-        "json": lambda infile: json.load(infile),
-        "yaml-loader":
-        # defaults to yaml.Loader
-        lambda infile: yaml.load(infile),
-        "yaml-cloader": lambda infile: yaml.load(infile, yaml.CLoader),
-    }[method]
 
-    @benchmark
-    def db():
-        with open(path, "r") as infile:
-            raw = load(infile)
-        return cattr.structure(raw, RamaDatabase)
+def test_rama_request_absent_aa():
+    db = ParameterDatabase.get_default()
+    rama_table = db.scoring.rama.find("CYX", False)
+    assert rama_table is None
 
-    assert len(db.tables) == 40
+
+def test_rama_repr():
+    db = ParameterDatabase.get_default()
+    rama_repr = repr(db.scoring.rama)
+    parts = rama_repr.partition("(")
+    assert parts[0] == "RamaDatabase"
+    rama_path_parts = parts[2].partition("tmol/database/")
+    assert rama_path_parts[2] == "default/scoring/rama.json)"
