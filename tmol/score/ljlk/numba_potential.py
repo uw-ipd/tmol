f524d7a27b23833d02fcc625bfa790cce33e1613
Alex Ford <fordas@uw.edu>
2018-09-24 10:59:18 -0700

    Update numba potential typing.
    Update numba potential to support full auto-jit of potential/kernel
    implementations, rather than pre-specifying type signatures. Update
    potential implementations to specify single-precision operations, rather
    than implicit double-precision of internal operations.

--- tmol/score/ljlk/numba_potential.py
+++ tmol/score/ljlk/numba_potential.py
@@ -1,21 +1,7 @@
 import math
 
 import numba
-
-_lj_potential_signature = (
-    numba.float32,  # dist
-    numba.uint8,  # bonded_path_length
-    numba.float32,  # lj_sigma
-    numba.float32,  # lj_switch_slope
-    numba.float32,  # lj_switc_intercept
-    numba.float32,  # lj_coeff_sigma12
-    numba.float32,  # lj_coeff_sigma6
-    numba.float32,  # lj_spline_y0
-    numba.float32,  # lj_spline_dy0
-    numba.float32,  # lj_switch_dis1sigma
-    numba.float32,  # spline_start
-    numba.float32,  # max_dis
-)
+from numpy import float32 as f4
 
 
 def _lj_potential(
@@ -34,14 +20,15 @@ def _lj_potential(
     spline_start: float,
     max_dis: float,
 ) -> float:
-    lj = 0.0
+
+    lj = f4(0)
 
     if dist > max_dis:
         # Outside of interaction distance
-        return 0.0
+        return 0
     elif bonded_path_length < 4:
         # Within bonded distance
-        return 0.0
+        return 0
     elif dist > spline_start:
         # lr spline fade
 
@@ -51,16 +38,16 @@ def _lj_potential(
         x = dist
         y0 = lj_spline_y0
         dy0 = lj_spline_dy0
-        u0 = (3.0 / (x1 - x0)) * ((-y0) / (x1 - x0) - dy0)
-        u1 = (3.0 / (x1 - x0)) * (y0 / (x1 - x0))
+        u0 = (f4(3) / (x1 - x0)) * ((-y0) / (x1 - x0) - dy0)
+        u1 = (f4(3) / (x1 - x0)) * (y0 / (x1 - x0))
 
-        lj = ((x - x1) * ((x - x0) * (u1 * (x0 - x) + u0 * (x - x1)) + 3 * y0)) / (
-            3 * (x0 - x1)
+        lj = ((x - x1) * ((x - x0) * (u1 * (x0 - x) + u0 * (x - x1)) + f4(3) * y0)) / (
+            f4(3) * (x0 - x1)
         )
     elif dist > lj_switch_dis2sigma * lj_sigma:
         # analytic 12-6
 
-        invdist2 = 1 / (dist * dist)
+        invdist2 = f4(1) / (dist * dist)
         invdist6 = invdist2 * invdist2 * invdist2
         invdist12 = invdist6 * invdist6
 
@@ -70,38 +57,12 @@ def _lj_potential(
         lj = dist * lj_switch_slope + lj_switch_intercept
 
     if bonded_path_length == 4:
-        lj *= 0.2
+        lj *= f4(0.2)
 
     return lj
 
 
-lj_potential_cpu = numba.njit(_lj_potential_signature)(_lj_potential)
-lj_potential_cuda = numba.cuda.jit(_lj_potential_signature, device=True)(_lj_potential)
-
-_lj_kernel_signature = (
-    # a coords/types
-    numba.float32[:, :],
-    numba.int64[:],
-    # b coords/types
-    numba.float32[:, :],
-    numba.int64[:],
-    # a_b bonded path length
-    numba.uint8[:, :],
-    # output buffer
-    numba.float32[:, :],
-    # pair score params
-    numba.float32[:, :],
-    numba.float32[:, :],
-    numba.float32[:, :],
-    numba.float32[:, :],
-    numba.float32[:, :],
-    numba.float32[:, :],
-    numba.float32[:, :],
-    # global score params
-    numba.float32,
-    numba.float32,
-    numba.float32,
-)
+lj_potential = numba.njit(_lj_potential)
 
 
 def _lj_kernel_cpu(
@@ -141,7 +102,7 @@ def _lj_kernel_cpu(
                 delta[0] * delta[0] + delta[1] * delta[1] + delta[2] * delta[2]
             )
 
-            lj_out[i, j] = lj_potential_cpu(
+            lj_out[i, j] = lj_potential(
                 dist,
                 a_b_bonded_path_length[i, j],
                 lj_sigma[at, bt],
@@ -157,11 +118,11 @@ def _lj_kernel_cpu(
             )
 
 
-lj_kernel_serial_cpu = numba.njit(_lj_kernel_signature, parallel=False)(_lj_kernel_cpu)
-lj_kernel_parallel_cpu = numba.njit(_lj_kernel_signature, parallel=True)(_lj_kernel_cpu)
+lj_kernel_serial_cpu = numba.njit(parallel=False, nogil=True)(_lj_kernel_cpu)
+lj_kernel_parallel_cpu = numba.njit(parallel=True, nogil=True)(_lj_kernel_cpu)
 
 
-@numba.cuda.jit(_lj_kernel_signature)
+@numba.cuda.jit
 def lj_kernel_cuda(
     a_coords,
     a_types,
@@ -199,7 +160,7 @@ def lj_kernel_cuda(
     delta = (a[0] - b[0], a[1] - b[1], a[2] - b[2])
     dist = math.sqrt(delta[0] * delta[0] + delta[1] * delta[1] + delta[2] * delta[2])
 
-    lj_out[i, j] = lj_potential_cuda(
+    lj_out[i, j] = lj_potential(
         dist,
         a_b_bonded_path_length[i, j],
         lj_sigma[at, bt],
@@ -239,24 +200,24 @@ def lj_kernel(
 
     if a_coords.device.type == "cpu":
         (lj_kernel_parallel_cpu if parallel else lj_kernel_serial_cpu)(
-            a_coords.numpy(),
-            a_types.numpy(),
-            b_coords.numpy(),
-            b_types.numpy(),
-            a_b_bonded_path_length.numpy(),
-            result.numpy(),
+            a_coords.__array__(),
+            a_types.__array__(),
+            b_coords.__array__(),
+            b_types.__array__(),
+            a_b_bonded_path_length.__array__(),
+            result.__array__(),
             # Pair score parameters
-            lj_sigma.numpy(),
-            lj_switch_slope.numpy(),
-            lj_switch_intercept.numpy(),
-            lj_coeff_sigma12.numpy(),
-            lj_coeff_sigma6.numpy(),
-            lj_spline_y0.numpy(),
-            lj_spline_dy0.numpy(),
+            lj_sigma.__array__(),
+            lj_switch_slope.__array__(),
+            lj_switch_intercept.__array__(),
+            lj_coeff_sigma12.__array__(),
+            lj_coeff_sigma6.__array__(),
+            lj_spline_y0.__array__(),
+            lj_spline_dy0.__array__(),
             # Global score parameters
-            float(lj_switch_dis2sigma),
-            float(spline_start),
-            float(max_dis),
+            f4(lj_switch_dis2sigma),
+            f4(spline_start),
+            f4(max_dis),
         )
     else:
         assert a_coords.device.type == "cuda"
@@ -279,9 +240,9 @@ def lj_kernel(
             lj_spline_y0,
             lj_spline_dy0,
             # Global score parameters
-            lj_switch_dis2sigma,
-            spline_start,
-            max_dis,
+            f4(lj_switch_dis2sigma),
+            f4(spline_start),
+            f4(max_dis),
         )
 
     return result
