4c4c7aeb935827b70c84dfcfff7bbf6bb32b8e39
Andrew Leaver-Fay <aleaverfay@gmail.com>
2018-10-02 10:20:01 -0400

    Add non-Ramachandran score components that Rama needs (from PR #79)
    This PR introduces three scoring mix-ins needed to evaluate Rama,
    and that will be similarly used for Dunbrack:
     - AlphaAABackboneTorsionProvider
     - PolymericBonds, and
     - ResidueProperties
    
    The AlphaAABackboneTorsionProvider measures the torsions for
    particular residues in the system; the indices of the atoms
    defining those torsions are provided by the PackedSystem
    (or whatever else defines the score graph).
    
    The PolymericBonds class gives the residue-to-residue indices
    for chemical bonds, so that residue i can know the index of
    the residue(s) it is chemically bonded to (which isn't always
    i+1). This is needed by Rama so that it can know what the
    upper residue is for residue i to decide between the normal-
    and pre-proline tables. These indices are provided by the
    PackedSystem.
    
    The ResidueProperties class holds arrays of property lists
    for each of the residues in the ScoreGraph. These properties
    are copied out of the ResidueTypes by the PackedSystem.
    
    Finally, this PR adds access to double-precision representation
    of the system's coordinates. This is needed as there is a
    startling loss of precision when going from dihedrals to
    coordinates then back to dihedrals.
    
    This PR depends on PRs #116 and #117

---
+++ tmol/score/torsions.py
@@ -0,0 +1,83 @@
+from functools import singledispatch
+
+import torch
+import numpy
+import attr
+
+from tmol.utility.reactive import reactive_attrs, reactive_property
+
+from tmol.numeric.dihedrals import coord_dihedrals
+
+from tmol.types.functional import validate_args
+from tmol.types.torch import Tensor
+
+from .factory import Factory
+
+
+@reactive_attrs(auto_attribs=True)
+class AlphaAABackboneTorsionProvider(Factory):
+    @staticmethod
+    @singledispatch
+    def factory_for(other, device: torch.device, **_):
+        """``clone``-factory, extract coords from other."""
+        # if requires_grad is None:
+        #    requires_grad = other.coords.requires_grad
+
+        phi_inds = torch.tensor(other.phi_inds, dtype=torch.long, device=device)
+        psi_inds = torch.tensor(other.psi_inds, dtype=torch.long, device=device)
+        omega_inds = torch.tensor(other.omega_inds, dtype=torch.long, device=device)
+
+        return dict(phi_inds=phi_inds, psi_inds=psi_inds, omega_inds=omega_inds)
+
+    # global indices used to define the torsions
+    # an entry of -1 for any atom means the torsion is undefined
+    # and will produce a NaN in the corresponding _tor Tensor
+    phi_inds: Tensor(torch.long)[:, :, 4] = attr.ib()
+    psi_inds: Tensor(torch.long)[:, :, 4] = attr.ib()
+    omega_inds: Tensor(torch.long)[:, :, 4] = attr.ib()
+
+    @reactive_property
+    def phi_tor(
+        coords64: Tensor(torch.double)[:, :, 3], phi_inds: Tensor(torch.long)[:, :, 4]
+    ) -> Tensor(torch.float)[:, :]:
+        assert coords64.shape[0] == 1
+        assert phi_inds.shape[0] == 1
+        phi_tor = measure_torsions(coords64[0, :], phi_inds[0, :])
+        return phi_tor[None, :]
+
+    @reactive_property
+    def psi_tor(
+        coords64: Tensor(torch.double)[:, :, 3], psi_inds: Tensor(torch.long)[:, :, 4]
+    ) -> Tensor(torch.float)[:, :]:
+        assert coords64.shape[0] == 1
+        assert psi_inds.shape[0] == 1
+        psi_tor = measure_torsions(coords64[0, :], psi_inds[0, :])
+        return psi_tor[None, :]
+
+    @reactive_property
+    def omega_tor(
+        coords64: Tensor(torch.double)[:, :, 3], omega_inds: Tensor(torch.long)[:, :, 4]
+    ) -> Tensor(torch.float)[:, :]:
+        assert coords64.shape[0] == 1
+        assert omega_inds.shape[0] == 1
+        omega_tor = measure_torsions(coords64[0, :], omega_inds[0, :])
+        return omega_tor[None, :]
+
+
+@validate_args
+def measure_torsions(
+    coords: Tensor(torch.double)[:, 3], inds: Tensor(torch.long)[:, 4]
+) -> Tensor(torch.float):
+    bad = torch.sum(inds == -1, 1) > 0
+    tors = torch.full(
+        (inds.shape[0],), numpy.nan, dtype=torch.float, device=coords.device
+    )
+    tors[bad] = numpy.nan
+    p1 = coords[inds[~bad, 0]]
+    p2 = coords[inds[~bad, 1]]
+    p3 = coords[inds[~bad, 2]]
+    p4 = coords[inds[~bad, 3]]
+
+    tors[~bad] = coord_dihedrals(p1, p2, p3, p4)
+
+    return tors
