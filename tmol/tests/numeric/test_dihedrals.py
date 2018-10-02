52a2bfaa3948412db17b1fb471cdc065e643f35b
Andrew Leaver-Fay <aleaverfay@gmail.com>
2018-10-02 09:34:20 -0400

    Change numeric/dihedrals.py to use torch and doubles (from PR #79)
    Instead of computing dihedrals using numpy, use torch operations.

--- tmol/tests/numeric/test_dihedrals.py
+++ tmol/tests/numeric/test_dihedrals.py
@@ -1,12 +1,13 @@
 from toolz.curried import compose, map
 import numpy
+import torch
 
 from tmol.numeric.dihedrals import coord_dihedrals
 from tmol.utility.units import parse_angle
 
 
 def test_coord_dihedrals():
-    coords = numpy.array(
+    coords = torch.tensor(
         [
             [24.969, 13.428, 30.692],  # N
             [24.044, 12.661, 29.808],  # CA
@@ -17,11 +18,12 @@ def test_coord_dihedrals():
             [23.691, 9.935, 28.389],  # CD1
             [22.557, 9.096, 30.459],  # CD2
             [numpy.nan, numpy.nan, numpy.nan],
-        ]
+        ],
+        dtype=torch.float64,
     )
 
-    dihedral_atoms = numpy.array(
-        [[0, 1, 2, 3], [0, 1, 4, 5], [1, 4, 5, 6], [1, 4, 5, 7], [-1, 0, 1, 3]]
+    dihedral_atoms = torch.LongTensor(
+        [[0, 1, 2, 3], [0, 1, 4, 5], [1, 4, 5, 6], [1, 4, 5, 7], [8, 0, 1, 3]]
     )
 
     dihedrals = compose(numpy.array, list, map(parse_angle))(
@@ -29,10 +31,10 @@ def test_coord_dihedrals():
     )
 
     calc_dihedrals = coord_dihedrals(
-        coords[dihedral_atoms][:, 0],
-        coords[dihedral_atoms][:, 1],
-        coords[dihedral_atoms][:, 2],
-        coords[dihedral_atoms][:, 3],
+        coords.index_select(0, dihedral_atoms[:, 0].squeeze()),
+        coords.index_select(0, dihedral_atoms[:, 1].squeeze()),
+        coords.index_select(0, dihedral_atoms[:, 2].squeeze()),
+        coords.index_select(0, dihedral_atoms[:, 3].squeeze()),
     )
 
-    numpy.testing.assert_allclose(dihedrals, calc_dihedrals, atol=1e-5)
+    numpy.testing.assert_allclose(calc_dihedrals.numpy(), dihedrals, atol=1e-5)
