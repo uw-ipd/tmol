c94d3175086ca7f120757568795f07b290a3e493
Alex Ford <fordas@uw.edu>
2018-10-04 14:04:49 -0700

    Add cpp_extension and tensor utils, jit extension test.
    Add `tmol.utility.cpp_extension` module, wrapping
    `torch.utils.cpp_extension` to add include paths for tmol specific
    headers. Support `load` and `load_inline`.
    
    Add `tmol.utility.tensor` utility headers, including a backport of
    torch v1 TensorAccessor features and generic utilities for casting
    tensor data to alternate types.
    
    Add simple jit-compiled cpp extension test, including test of header
    inclusion and tensor utils.

---
+++ tmol/tests/utility/vector_magnitude.cpp
@@ -0,0 +1,50 @@
+#include <tmol/utility/tensor/TensorAccessor.h>
+#include <tmol/utility/tensor/TensorUtil.h>
+#include <torch/torch.h>
+#include <Eigen/Core>
+#include <cmath>
+
+at::Tensor vector_magnitude_aten(at::Tensor input) {
+  return (input * input).sum(-1).sqrt();
+}
+
+at::Tensor vector_magnitude_accessor(at::Tensor input_t) {
+  auto output_t = at::empty(input_t.type(), input_t.size(0));
+
+  auto input = input_t.accessor<float, 2>();
+  auto output = output_t.accessor<float, 1>();
+
+  for (int64_t i = 0; i < input.size(0); ++i) {
+    auto v = input[i];
+    output[i] = std::sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
+  }
+
+  return output_t;
+}
+
+at::Tensor vector_magnitude_eigen(at::Tensor input_t) {
+  auto output_t = at::empty(input_t.type(), input_t.size(0));
+
+  auto input = tmol::reinterpret_tensor<Eigen::Vector3f, float, 2>(input_t);
+  auto output = output_t.accessor<float, 1>();
+
+  for (int64_t i = 0; i < input.size(0); ++i) {
+    auto v = input[i][0];
+    output[i] = std::sqrt(v.dot(v));
+  }
+
+  return output_t;
+}
+
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+  m.def(
+      "aten", &vector_magnitude_aten, "ATen-based vector_magnitude function.");
+  m.def(
+      "accessor",
+      &vector_magnitude_accessor,
+      "TensorAccessor-based vector_magnitude function.");
+  m.def(
+      "eigen",
+      &vector_magnitude_eigen,
+      "Eigen-based vector_magnitude function.");
+}
---
+++ tmol/utility/cpp_extension.py
@@ -0,0 +1,37 @@
+import os
+
+from functools import wraps
+
+from ..extern import include_paths as extern_include_paths
+from .. import include_paths as tmol_include_paths
+
+import torch.utils.cpp_extension
+
+os.environ.update(CXX="g++")
+
+
+_default_include_paths = tmol_include_paths() + extern_include_paths()
+
+
+@wraps(torch.utils.cpp_extension.load)
+def load(*args, **kwargs):
+    """Jit-compile torch cpp_extension with tmol paths."""
+
+    kwargs["extra_cflags"] = kwargs.get("extra_cflags", ["-O3"])
+    kwargs["extra_include_paths"] = (
+        kwargs.get("extra_include_flags", []) + _default_include_paths
+    )
+
+    return torch.utils.cpp_extension.load(*args, **kwargs)
+
+
+@wraps(torch.utils.cpp_extension.load_inline)
+def load_inline(*args, **kwargs):
+    """Jit-compile torch cpp_extension with tmol paths."""
+
+    kwargs["extra_cflags"] = kwargs.get("extra_cflags", ["-O3"])
+    kwargs["extra_include_paths"] = (
+        kwargs.get("extra_include_flags", []) + _default_include_paths
+    )
+
+    return torch.utils.cpp_extension.load_inline(*args, **kwargs)
