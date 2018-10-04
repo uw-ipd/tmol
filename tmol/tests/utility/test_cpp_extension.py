dbac3247bcf5a86546b3e2e236ce36e4461142f4
Alex Ford <fordas@uw.edu>
2018-10-04 14:04:49 -0700

    Add cpp_extension and tensor utils, jit extension test.
    Add `tmol.utility.cpp_extension` module, wrapping
    `torch.utils.cpp_extension` to add include paths for tmol specific
    headers. Support `load` and `load_inline`.
    
    Add `tmol.utility.tensor` utility headers, including a backport of
    torch v1 TensorAccessor features and generic utilities for casting
    tensor data to alternate types.
    
    Add simple jit-compiled cpp extension test, including test of header
    inclusion and tensor utils.

---
+++ tmol/tests/utility/test_cpp_extension.py
@@ -0,0 +1,29 @@
+import pytest
+import os.path
+import torch
+
+import tmol.utility.cpp_extension as cpp_extension
+
+
+@pytest.fixture
+def vector_magnitude():
+    return cpp_extension.load(
+        "vector_magnitude",
+        [os.path.dirname(__file__) + "/vector_magnitude.cpp"],
+        verbose=True,
+    )
+
+
+def test_cpp_extension(vector_magnitude):
+
+    tvec = torch.arange(300, dtype=torch.float).reshape(100, 3)
+    expected = tvec.norm(dim=-1)
+
+    results = {
+        "aten": vector_magnitude.aten(tvec),
+        "accessor": vector_magnitude.accessor(tvec),
+        "eigen": vector_magnitude.eigen(tvec),
+    }
+
+    for _rn, r in results.items():
+        torch.testing.assert_allclose(r, expected)
