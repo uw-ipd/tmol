3f3020094345fb3a14bd934e10d6b55e52e71747
Alex Ford <fordas@uw.edu>
2018-10-07 04:03:47 +0000

    Add macro support for lj parameter tensors.
    Add a set of utility macros to support passing parameter tensors for lj
    potential. Short-term solution to duplicating parameter names throughout
    cpp potential implementation.

--- tmol/score/ljlk/cpp_potential.cpu.cpp
+++ tmol/score/ljlk/cpp_potential.cpu.cpp
@@ -5,41 +5,18 @@
 
 namespace tmol {
 namespace score {
-namespace ljlk {
-at::Tensor lj_intra(
-    at::Tensor coords_t,
-    at::Tensor types_t,
-    at::Tensor bonded_path_length_t,
-    at::Tensor lj_sigma_t,
-    at::Tensor lj_switch_slope_t,
-    at::Tensor lj_switch_intercept_t,
-    at::Tensor lj_coeff_sigma12_t,
-    at::Tensor lj_coeff_sigma6_t,
-    at::Tensor lj_spline_y0_t,
-    at::Tensor lj_spline_dy0_t,
-    at::Tensor lj_switch_dis2sigma_t,
-    at::Tensor spline_start_t,
-    at::Tensor max_dis_t) {
+namespace lj {
+
+template <typename Real, typename AtomType, typename Int>
+at::Tensor lj_intra(at::Tensor coords_t, at::Tensor types_t, LJ_PARAM_ARGS) {
   auto out_t = coords_t.type().zeros({coords_t.size(0), coords_t.size(0)});
-  auto out = out_t.accessor<float, 2>();
 
-  auto coords = tmol::reinterpret_tensor<Eigen::Vector3f, float, 2>(coords_t);
-  auto types = types_t.accessor<int64_t, 1>();
-  auto bonded_path_length = bonded_path_length_t.accessor<uint8_t, 2>();
+  auto coords = tmol::reinterpret_tensor<Eigen::Vector3f, Real, 2>(coords_t);
+  auto types = types_t.accessor<AtomType, 1>();
 
-  auto lj_sigma = lj_sigma_t.accessor<float, 2>();
-  auto lj_switch_slope = lj_switch_slope_t.accessor<float, 2>();
-  auto lj_switch_intercept = lj_switch_intercept_t.accessor<float, 2>();
-  auto lj_coeff_sigma12 = lj_coeff_sigma12_t.accessor<float, 2>();
-  auto lj_coeff_sigma6 = lj_coeff_sigma6_t.accessor<float, 2>();
-  auto lj_spline_y0 = lj_spline_y0_t.accessor<float, 2>();
-  auto lj_spline_dy0 = lj_spline_dy0_t.accessor<float, 2>();
+  auto out = out_t.accessor<Real, 2>();
 
-  // Reshape globals into 1d, accessor cast of 0-d tensors segfaults.
-  auto lj_switch_dis2sigma =
-      lj_switch_dis2sigma_t.reshape({1}).accessor<float, 1>();
-  auto spline_start = spline_start_t.reshape({1}).accessor<float, 1>();
-  auto max_dis = max_dis_t.reshape({1}).accessor<float, 1>();
+  LJ_PARAM_UNPACK
 
   for (int i = 0; i < coords.size(0); ++i) {
     auto a = coords[i][0];
@@ -59,19 +36,19 @@ at::Tensor lj_intra(
       Eigen::Vector3f delta = a - b;
       auto dist = std::sqrt(delta.dot(delta));
 
-      out[i][j] = lj_potential(
-          dist,
-          bonded_path_length[i][j],
-          lj_sigma[at][bt],
-          lj_switch_slope[at][bt],
-          lj_switch_intercept[at][bt],
-          lj_coeff_sigma12[at][bt],
-          lj_coeff_sigma6[at][bt],
-          lj_spline_y0[at][bt],
-          lj_spline_dy0[at][bt],
-          lj_switch_dis2sigma[0],
-          spline_start[0],
-          max_dis[0]);
+      out[i][j] =
+          lj(dist,
+             bonded_path_length[i][j],
+             lj_sigma[at][bt],
+             lj_switch_slope[at][bt],
+             lj_switch_intercept[at][bt],
+             lj_coeff_sigma12[at][bt],
+             lj_coeff_sigma6[at][bt],
+             lj_spline_y0[at][bt],
+             lj_spline_dy0[at][bt],
+             lj_switch_dis2sigma[0],
+             spline_start[0],
+             max_dis[0]);
     }
   }
 
@@ -80,42 +57,17 @@ at::Tensor lj_intra(
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   using namespace pybind11::literals;
-  m.def(
-      "lj_potential",
-      &lj_potential<float, int64_t>,
-      "LJ potential.",
-      "dist"_a,
-      "bonded_path_length"_a,
-      "lj_sigma"_a,
-      "lj_switch_slope"_a,
-      "lj_switch_intercept"_a,
-      "lj_coeff_sigma12"_a,
-      "lj_coeff_sigma6"_a,
-      "lj_spline_y0"_a,
-      "lj_spline_dy0"_a,
-      "lj_switch_dis2sigma"_a,
-      "spline_start"_a,
-      "max_dis"_a);
+  m.def("lj", &lj<float, uint8_t>, "LJ potential.", "dist"_a, LJ_PARAM_PYARGS);
 
   m.def(
       "lj_intra",
-      &lj_intra,
+      &lj_intra<float, int64_t, uint8_t>,
       "LJ intra-coordinate score.",
       "coords"_a,
       "types"_a,
-      "bonded_path_length"_a,
-      "lj_sigma"_a,
-      "lj_switch_slope"_a,
-      "lj_switch_intercept"_a,
-      "lj_coeff_sigma12"_a,
-      "lj_coeff_sigma6"_a,
-      "lj_spline_y0"_a,
-      "lj_spline_dy0"_a,
-      "lj_switch_dis2sigma"_a,
-      "spline_start"_a,
-      "max_dis"_a);
+      LJ_PARAM_PYARGS);
 }
 
-}  // namespace ljlk
+}  // namespace lj
 }  // namespace score
 }  // namespace tmol
--- tmol/score/ljlk/lj.hh
+++ tmol/score/ljlk/lj.hh
@@ -1,68 +1,105 @@
 #include <Eigen/Core>
 
-namespace tmol
-{
-namespace score
-{
-namespace ljlk
-{
+/////Utility macros to support parameter passthrough in operators
+
+// pybind11 parameter arguments
+#define LJ_PARAM_PYARGS                                                   \
+  "bonded_path_length"_a, "lj_sigma"_a, "lj_switch_slope"_a,              \
+      "lj_switch_intercept"_a, "lj_coeff_sigma12"_a, "lj_coeff_sigma6"_a, \
+      "lj_spline_y0"_a, "lj_spline_dy0"_a, "lj_switch_dis2sigma"_a,       \
+      "spline_start"_a, "max_dis"_a
+
+// c++ level parameter tensors
+#define LJ_PARAM_ARGS                                                 \
+  at::Tensor bonded_path_length_t, at::Tensor lj_sigma_t,             \
+      at::Tensor lj_switch_slope_t, at::Tensor lj_switch_intercept_t, \
+      at::Tensor lj_coeff_sigma12_t, at::Tensor lj_coeff_sigma6_t,    \
+      at::Tensor lj_spline_y0_t, at::Tensor lj_spline_dy0_t,          \
+      at::Tensor lj_switch_dis2sigma_t, at::Tensor spline_start_t,    \
+      at::Tensor max_dis_t
+
+// unpack parameter tensors into TensorAccessors
+#define LJ_PARAM_UNPACK                                                        \
+  auto bonded_path_length =                                                    \
+      tmol::reinterpret_tensor<Int, Int, 2>(bonded_path_length_t);             \
+                                                                               \
+  auto lj_sigma = tmol::reinterpret_tensor<Real, Real, 2>(lj_sigma_t);         \
+  auto lj_switch_slope =                                                       \
+      tmol::reinterpret_tensor<Real, Real, 2>(lj_switch_slope_t);              \
+  auto lj_switch_intercept =                                                   \
+      tmol::reinterpret_tensor<Real, Real, 2>(lj_switch_intercept_t);          \
+  auto lj_coeff_sigma12 =                                                      \
+      tmol::reinterpret_tensor<Real, Real, 2>(lj_coeff_sigma12_t);             \
+  auto lj_coeff_sigma6 =                                                       \
+      tmol::reinterpret_tensor<Real, Real, 2>(lj_coeff_sigma6_t);              \
+  auto lj_spline_y0 = tmol::reinterpret_tensor<Real, Real, 2>(lj_spline_y0_t); \
+  auto lj_spline_dy0 =                                                         \
+      tmol::reinterpret_tensor<Real, Real, 2>(lj_spline_dy0_t);                \
+  auto lj_switch_dis2sigma = tmol::reinterpret_tensor<Real, Real, 1>(          \
+      lj_switch_dis2sigma_t.reshape(1));                                       \
+  auto spline_start =                                                          \
+      tmol::reinterpret_tensor<Real, Real, 1>(spline_start_t.reshape(1));      \
+  auto max_dis = tmol::reinterpret_tensor<Real, Real, 1>(max_dis_t.reshape(1));
+
+namespace tmol {
+namespace score {
+namespace lj {
 template <typename Real, typename Int>
-EIGEN_DEVICE_FUNC Real lj_potential(
-    Real& dist,
-    const Int& bonded_path_length,
-    const Real& lj_sigma,
-    const Real& lj_switch_slope,
-    const Real& lj_switch_intercept,
-    const Real& lj_coeff_sigma12,
-    const Real& lj_coeff_sigma6,
-    const Real& lj_spline_y0,
-    const Real& lj_spline_dy0,
-    const Real& lj_switch_dis2sigma,
-    const Real& spline_start,
-    const Real& max_dis)
-{
-    Real lj = 0.0;
+EIGEN_DEVICE_FUNC Real
+lj(Real& dist,
+   const Int& bonded_path_length,
+   const Real& lj_sigma,
+   const Real& lj_switch_slope,
+   const Real& lj_switch_intercept,
+   const Real& lj_coeff_sigma12,
+   const Real& lj_coeff_sigma6,
+   const Real& lj_spline_y0,
+   const Real& lj_spline_dy0,
+   const Real& lj_switch_dis2sigma,
+   const Real& spline_start,
+   const Real& max_dis) {
+  Real lj = 0.0;
 
-    if (dist > max_dis) {
-        // Outside of interaction distance
-        return 0.0;
-    } else if (bonded_path_length < 4) {
-        // Within bonded distance
-        return 0.0;
-    } else if (dist > spline_start) {
-        // lr spline fade
+  if (dist > max_dis) {
+    // Outside of interaction distance
+    return 0.0;
+  } else if (bonded_path_length < 4) {
+    // Within bonded distance
+    return 0.0;
+  } else if (dist > spline_start) {
+    // lr spline fade
 
-        Real x0 = spline_start;
-        Real x1 = max_dis;
+    Real x0 = spline_start;
+    Real x1 = max_dis;
 
-        auto x = dist;
-        auto y0 = lj_spline_y0;
-        auto dy0 = lj_spline_dy0;
-        Real u0 = (3.0 / (x1 - x0)) * ((-y0) / (x1 - x0) - dy0);
-        Real u1 = (3.0 / (x1 - x0)) * (y0 / (x1 - x0));
+    auto x = dist;
+    auto y0 = lj_spline_y0;
+    auto dy0 = lj_spline_dy0;
+    Real u0 = (3.0 / (x1 - x0)) * ((-y0) / (x1 - x0) - dy0);
+    Real u1 = (3.0 / (x1 - x0)) * (y0 / (x1 - x0));
 
-        lj = ((x - x1) * ((x - x0) * (u1 * (x0 - x) + u0 * (x - x1)) + 3.0 * y0))
-             / (3.0 * (x0 - x1));
-    } else if (dist > lj_switch_dis2sigma * lj_sigma) {
-        // analytic 12-6
+    lj = ((x - x1) * ((x - x0) * (u1 * (x0 - x) + u0 * (x - x1)) + 3.0 * y0))
+         / (3.0 * (x0 - x1));
+  } else if (dist > lj_switch_dis2sigma * lj_sigma) {
+    // analytic 12-6
 
-        Real invdist2 = 1.0 / (dist * dist);
-        Real invdist6 = invdist2 * invdist2 * invdist2;
-        Real invdist12 = invdist6 * invdist6;
+    Real invdist2 = 1.0 / (dist * dist);
+    Real invdist6 = invdist2 * invdist2 * invdist2;
+    Real invdist12 = invdist6 * invdist6;
 
-        lj = (lj_coeff_sigma12 * invdist12) + (lj_coeff_sigma6 * invdist6);
-    } else {
-        // linear
-        lj = dist * lj_switch_slope + lj_switch_intercept;
-    }
+    lj = (lj_coeff_sigma12 * invdist12) + (lj_coeff_sigma6 * invdist6);
+  } else {
+    // linear
+    lj = dist * lj_switch_slope + lj_switch_intercept;
+  }
 
-    if (bonded_path_length == 4) {
-        lj *= 0.2;
-    }
+  if (bonded_path_length == 4) {
+    lj *= 0.2;
+  }
 
-    return lj;
+  return lj;
 }
 
-}  // namespace ljlk
+}  // namespace lj
 }  // namespace score
 }  // namespace tmol
