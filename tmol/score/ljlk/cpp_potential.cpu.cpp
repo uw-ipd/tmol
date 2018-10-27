2e0a32b89c8e534b663e93fc2a58f890b567bc8d
Alex Ford <fordas@uw.edu>
2018-10-26 17:19:47 -0700

    Tidy up cpu potential, auto to TView.

--- tmol/score/ljlk/cpp_potential.cpu.cpp
+++ tmol/score/ljlk/cpp_potential.cpu.cpp
@@ -10,80 +10,37 @@ namespace score {
 namespace lj {
 
 template <typename Real, typename Int>
-at::Tensor block_interaction_table(
-    at::Tensor coords_t, Real max_dis, Int block_size) {
-  typedef Eigen::AlignedBox<Real, 3> Box;
-  typedef Eigen::Matrix<Real, 3, 1> Vector;
-
-  auto coords = tmol::view_tensor<Vector, 2>(coords_t);
-
-  AT_ASSERTM(
-      coords_t.size(0) % block_size == 0,
-      "Coordinate size must be even multiple of target block size.");
-  int64_t num_blocks = coords_t.size(0) / block_size;
-
-  auto out_t = at::empty({num_blocks, num_blocks}, coords_t.type());
-  auto out = tmol::view_tensor<Real, 2>(out_t);
-
-  static_assert(sizeof(Box) == sizeof(Real) * 6, "");
-  auto box_t = at::zeros({num_blocks, 6}, coords_t.type());
-  auto boxes = tmol::view_tensor<Box, 2>(box_t);
-
-  for (int bi = 0; bi < num_blocks; ++bi) {
-    int bsi = bi * block_size;
-    Box block_box;
-    for (int i = bsi; i < bsi + block_size; ++i) {
-      block_box.extend(coords[i][0]);
-    }
-
-    boxes[bi][0] = block_box;
-  }
-
-  for (int bi = 0; bi < num_blocks; ++bi) {
-    Box block_box = boxes[bi][0];
-    block_box.extend(block_box.max() + Vector(max_dis, max_dis, max_dis));
-    block_box.extend(block_box.min() - Vector(max_dis, max_dis, max_dis));
-
-    for (int bj = bi; bj < num_blocks; ++bj) {
-      out[bi][bj] = block_box.intersects(boxes[bj][0]);
-    }
-  }
-
-  return out_t;
-}
-
-template <typename Real, typename Int>
 at::Tensor block_interaction_lists(
     at::Tensor coords_t, Real max_dis, Int block_size) {
   typedef Eigen::AlignedBox<Real, 3> Box;
   typedef Eigen::Matrix<Real, 3, 1> Vector;
 
-  auto coords = tmol::view_tensor<Vector, 2>(coords_t);
+  TView<Vector, 2> coords = tmol::view_tensor<Vector, 2>(coords_t);
 
   AT_ASSERTM(
       coords_t.size(0) % block_size == 0,
       "Coordinate size must be even multiple of target block size.");
   int64_t num_blocks = coords_t.size(0) / block_size;
 
+  static_assert(sizeof(Box) == sizeof(Real) * 6, "");
+  at::Tensor boxes_t = at::empty({num_blocks, 6}, at::TensorOptions(coords_t));
+  TView<Box, 2> boxes = tmol::view_tensor<Box, 2>(boxes_t);
+
   at::Tensor block_lists_t = at::empty(
       {num_blocks, num_blocks},
       at::TensorOptions(coords_t).dtype(at::CTypeToScalarType<Int>::to()));
-  auto block_lists = tmol::view_tensor<Int, 2>(block_lists_t);
+  tmol::TView<Int, 2> block_lists = tmol::view_tensor<Int, 2>(block_lists_t);
 
   at::Tensor block_list_lengths_t = at::zeros(
-      {num_blocks + 1},
+      num_blocks + 1,
       at::TensorOptions(coords_t).dtype(at::CTypeToScalarType<Int>::to()));
-  auto block_list_lengths =
+  tmol::TView<Int, 1> block_list_lengths =
       tmol::view_tensor<Int, 1>(block_list_lengths_t.slice(0, 1));
-
-  static_assert(sizeof(Box) == sizeof(Real) * 6, "");
-  auto box_t = at::zeros({num_blocks, 6}, coords_t.type());
-  auto boxes = tmol::view_tensor<Box, 2>(box_t);
 
   for (int bi = 0; bi < num_blocks; ++bi) {
     int bsi = bi * block_size;
-    Box block_box;
-    for (int i = bsi; i < bsi + block_size; ++i) {
+    Box block_box(coords[bsi][0]);
+    for (int i = bsi + 1; i < bsi + block_size; ++i) {
       block_box.extend(coords[i][0]);
     }
 
@@ -94,6 +51,8 @@ at::Tensor block_interaction_lists(
     Box block_box = boxes[bi][0];
     block_box.extend(block_box.max() + Vector(max_dis, max_dis, max_dis));
     block_box.extend(block_box.min() - Vector(max_dis, max_dis, max_dis));
+
+    block_list_lengths[bi] = 0;
 
     for (int bj = 0; bj < num_blocks; ++bj) {
       if (block_box.intersects(boxes[bj][0])) {
@@ -104,11 +63,12 @@ at::Tensor block_interaction_lists(
   }
 
   at::Tensor block_spans_t = block_list_lengths_t.cumsum(0);
-  auto block_spans = tmol::view_tensor<Int, 1>(block_spans_t);
+  at::Tensor result_t = at::empty(
+      {block_spans_t[num_blocks].toCLong(), 2},
+      at::TensorOptions(block_spans_t));
 
-  at::Tensor result_t =
-      at::empty({block_spans[num_blocks], 2}, block_spans_t.type());
-  auto result = tmol::view_tensor<Int, 2>(result_t);
+  TView<Int, 1> block_spans = tmol::view_tensor<Int, 1>(block_spans_t);
+  TView<Int, 2> result = tmol::view_tensor<Int, 2>(result_t);
   for (int bi = 0; bi < num_blocks; ++bi) {
     for (int r = 0; r < block_list_lengths[bi]; ++r) {
       result[block_spans[bi] + r][0] = bi;
@@ -129,9 +89,10 @@ at::Tensor lj_intra_block(
   typedef Eigen::AlignedBox<Real, 3> Box;
   typedef Eigen::Matrix<Real, 3, 1> Vector;
 
-  auto coords = tmol::view_tensor<Vector, 2>(coords_t);
-  auto block_interactions = tmol::view_tensor<Int, 2>(block_interactions_t);
-  auto types = tmol::view_tensor<Int, 1>(types_t);
+  TView<Vector, 2> coords = tmol::view_tensor<Vector, 2>(coords_t);
+  TView<Int, 2> block_interactions =
+      tmol::view_tensor<Int, 2>(block_interactions_t);
+  TView<Int, 1> types = tmol::view_tensor<Int, 1>(types_t);
 
   LJ_PARAM_UNPACK
 
@@ -139,7 +100,7 @@ at::Tensor lj_intra_block(
 
   at::Tensor out_t =
       at::empty({num_blocks, block_size, block_size}, coords_t.type());
-  auto out = tmol::view_tensor<Real, 3>(out_t);
+  TView<Real, 3> out = tmol::view_tensor<Real, 3>(out_t);
 
   for (int bp = 0; bp < num_blocks; ++bp) {
     int bi = block_interactions[bp][0];
@@ -194,14 +155,6 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
       "LJ potential derivative.",
       "dist"_a,
       LJ_PARAM_PYARGS);
-
-  m.def(
-      "block_interaction_table",
-      &block_interaction_table<float, int64_t>,
-      "Calculate coordinate-block interaction table.",
-      "coords"_a,
-      "max_dis"_a,
-      "block_size"_a);
 
   m.def(
       "block_interaction_lists",
