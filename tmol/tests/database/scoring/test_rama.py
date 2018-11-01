2828e857b4365e5d64848cf52dbdd45372bd6eef
Andrew Leaver-Fay <aleaverfay@gmail.com>
2018-11-01 14:43:15 -0400

    This change belongs with the second to last commit

--- tmol/tests/database/scoring/test_rama.py
+++ tmol/tests/database/scoring/test_rama.py
@@ -2,7 +2,7 @@ import numpy
 import torch
 import pytest
 
-from tmol.database.scoring.rama import RamaDatabase, CompactedRamaDatabase
+from tmol.database.scoring.rama import RamaDatabase
 from tmol.database import ParameterDatabase
 
 
@@ -28,41 +28,6 @@ def test_rama_mapper():
         )
         == 15
     )
-
-
-def test_compacted_rama(torch_device):
-    ramadb = RamaDatabase.from_files("tmol/database/default/scoring/rama/")
-    compacted = CompactedRamaDatabase.from_ramadb(ramadb, torch_device)
-    assert compacted.table.shape == (40, 36, 36)
-    phi_vals = (
-        torch.arange(36, device=torch_device)
-        .reshape(-1, 1)
-        .repeat(1, 36)
-        .reshape(-1, 1)
-    )
-    psi_vals = torch.arange(36, device=torch_device).repeat(1, 36).reshape(-1, 1)
-    x = torch.cat((phi_vals, psi_vals), dim=1)
-    y = torch.full((36 * 36, 1), 0, dtype=torch.long, device=torch_device)
-    for i in range(20):
-        for j in range(2):
-            y[:, 0] = i + 20 * j
-            xlong = x.type(torch.long)
-            inds = y[:, 0] * 36 * 36 + xlong[:, 0] * 36 + xlong[:, 1]
-            original_vals = compacted.table.reshape(-1)[inds]
-            interp_vals = compacted.bspline.interpolate(x, y)
-            numpy.testing.assert_allclose(
-                interp_vals.cpu().detach().numpy(),
-                original_vals.cpu().detach().numpy(),
-                atol=1e-5,
-                rtol=1e-7,
-            )
-
-
-def test_load_compacted_rama_once(torch_device):
-    db = ParameterDatabase.get_default()
-    crama1 = CompactedRamaDatabase.from_ramadb(db.scoring.rama, torch_device)
-    crama2 = CompactedRamaDatabase.from_ramadb(db.scoring.rama, torch_device)
-    assert crama1 is crama2
 
 
 @pytest.mark.benchmark(group="rama_load", min_rounds=1)
