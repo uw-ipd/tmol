8ee6a4d7de42e5ffd9be33960ce01986ba29180c
Alex Ford <fordas@uw.edu>
2018-12-17 18:54:29 -0800

    Revert "Add Ramachandran database layer (#118)"
    Revert #118, which is causing issues due to read/write access to lmdb
    database. Will re-integrate with non-lmdb based rama database.
    
    This reverts commit 1ba9299e4da166b204eb812077d251ddbbd72909.

--- tmol/database/scoring/__init__.py
+++ tmol/database/scoring/__init__.py
@@ -3,7 +3,6 @@ import attr
 
 from .hbond import HBondDatabase
 from .ljlk import LJLKDatabase
-from .rama import RamaDatabase
 
 
 @attr.s(auto_attribs=True, slots=True, frozen=True)
@@ -11,12 +10,10 @@ class ScoringDatabase:
 
     ljlk: LJLKDatabase
     hbond: HBondDatabase
-    rama: RamaDatabase
 
     @classmethod
     def from_file(cls, path=os.path.dirname(__file__)):  # noqa
         return cls(
             ljlk=LJLKDatabase.from_file(os.path.join(path, "ljlk.yaml")),
             hbond=HBondDatabase.from_file(os.path.join(path, "hbond.yaml")),
-            rama=RamaDatabase.from_files(os.path.join(path, "rama/")),
         )
--- tmol/tests/support/test_scoring.py
+++ tmol/tests/support/test_scoring.py
@@ -1,12 +1,9 @@
 import os
 import yaml
-import shutil
-import zarr
 
 from .rosetta import requires_rosetta_database
 
 from tmol.support.scoring.hbond_param_import import RosettaHBParams
-from tmol.support.scoring.rewrite_rama_binary import RamaTableImport
 
 
 @requires_rosetta_database
@@ -16,43 +13,3 @@ def test_hbond_param_import(rosetta_database):
     )
 
     yaml.load(params.to_yaml())
-
-
-@requires_rosetta_database
-def test_rama_table_import(rosetta_database):
-
-    out_path = "test_rama_writer.bin"
-    if os.path.isdir(out_path):
-        shutil.rmtree(out_path)
-
-    rama_path = os.path.join(
-        rosetta_database, "scoring/score_functions/rama/fd_beta_nov2016/"
-    )
-    RamaTableImport.zarr_from_db(rama_path, out_path)
-
-    store2 = zarr.LMDBStore(out_path)
-    zgroup = zarr.group(store=store2)
-    assert "LAA_LEU_STANDARD" in zgroup
-    assert "LAA_THR_STANDARD" in zgroup
-    assert "LAA_LEU_PREPRO" in zgroup
-    assert "LAA_THR_PREPRO" in zgroup
-    assert len(zgroup) == 40
-
-    leu_group = zgroup["LAA_LEU_STANDARD"]
-    leu_start = leu_group["bb_start"][:]
-    assert len(leu_start.shape) == 1
-    assert leu_start.shape[0] == 2
-    assert leu_start[0] == -180 and leu_start[1] == -180
-
-    leu_step = leu_group["bb_step"][:]
-    assert len(leu_step.shape) == 1
-    assert leu_step.shape[0] == 2
-    assert leu_step[0] == 10 and leu_step[1] == 10
-
-    leu_probs = leu_group["probabilities"][:]
-    assert len(leu_probs.shape) == 2
-    assert leu_probs.shape[0] == 36
-    assert leu_probs.shape[1] == 36
-
-    store2.close()
-    shutil.rmtree(out_path)
