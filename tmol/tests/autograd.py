62d639bd719221729b1f9cc0622bec4706cdaa82
Alex Ford <fordas@uw.edu>
2018-12-27 15:03:36 -0800

    Add autograd test utility for vector-valued functions.
    Add "VectorizedOp" test utility, providing an autograd harness over
    gufunc vectorized vector-valued functions. Intended to support
    simplified autograd gradchecks for tensor-valued functions calculating
    a value & derivative wrt tensor inputs.

--- tmol/tests/autograd.py
+++ tmol/tests/autograd.py
@@ -176,3 +176,87 @@ def gradcheck(
                 return fail_test("grad is incorrect size")
 
     return True
+
+
+class VectorizedOp:
+    """Torch autograd wrapper for gufunc-vectorized "v and dv" functions.
+
+    Test harness for evaluation of vector-valued functions of the form:
+        f(*inputs_with_grad, *inputs_without_grad) -> (value, *d_value_d_inputs_with_grad)
+
+    Functions are wrapped with a numpy.vectorize gufunc dispatcher, which performs vector dispatch
+    over tensors of input values. An autograd 
+
+    The [(input), ...] -> [(val), (d_val_d_input), ...] function is converted into:
+
+    forward:
+    1. Evaluates the function for val and d_val_d_input.
+    2. Stores d_val_d_input for use in backward.
+    2. Return sum(val).
+
+    backward:
+    1. Returns (d_d_val * d_val_d_input) for each input.
+    """
+
+    def __init__(self, f, signature=None):
+        if isinstance(f, numpy.lib.function_base.vectorize):
+            self.f = f
+            signature = f.signature
+        else:
+            assert signature is not None
+            self.f = numpy.vectorize(f, signature=signature)
+
+        self.n_input_grad = len(signature.split("->")[1].split(",")) - 1
+        self.n_input_non_grad = (
+            len(signature.split("->")[0].split(",")) - self.n_input_grad
+        )
+
+        assert self.n_input_grad > 0
+
+        super().__init__()
+
+    def __call__(self, *args):
+        return self._VectorizedFun(self)(*args)
+
+    class _VectorizedFun(torch.autograd.Function):
+        def __init__(self, op):
+            self.op = op
+            super().__init__()
+
+        def forward(ctx, *args):
+            args = tuple(args)
+            tensor_args = tuple(map(torch.Tensor.detach, args[: ctx.op.n_input_grad]))
+            other_args = tuple(args[ctx.op.n_input_grad :])
+            result = ctx.op.f(*(tensor_args + other_args))
+
+            v, *raw_dv_di = tuple(map(torch.from_numpy, result))
+            assert len(raw_dv_di) == ctx.op.n_input_grad
+
+            def _prep_grad(i, dv_di):
+                if dv_di.shape != i.shape:
+                    if dv_di.shape[1:] == i.shape:
+                        # Broadcast along implicit leading dimension, sum off dimension
+                        return dv_di.sum(dim=0)
+
+                    # Broadcast along dim-1 leading dimension, sum into leading dimension
+                    assert (1,) + dv_di.shape[
+                        1:
+                    ] == i.shape, "Unknown input broadcast pattern."
+                    return dv_di.sum(dim=0, keepdim=True)
+
+                else:
+                    # No broadcast occured
+                    return dv_di
+
+            ctx.save_for_backward(
+                *[_prep_grad(*v) for v in zip(tensor_args, raw_dv_di)]
+            )
+
+            assert v.dim() == 1
+
+            return v.sum()
+
+        def backward(ctx, d_d_v):
+            d_d_i = tuple(d_d_v * d_v_d_i for d_v_d_i in ctx.saved_tensors)
+
+            return d_d_i + (None,) * ctx.op.n_input_non_grad
