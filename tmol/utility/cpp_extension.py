7cca718dfa93198b04f3175e5294d72ea9eae56e
Alex Ford <fordas@uw.edu>
2019-01-02 18:43:32 -0800

    Add c++14 via conda compilers. (#138)
    * Add gcc7 to core dependencies, enable c++14, fixup local docker builds.
    
    Add anaconda gcc compiler to dev dependencies, providing support for
    c++14 features by avoiding system compilers. Update ycm and
    cpp_extension configuration.
    
    Update docker-compose.yml to add "test-cpu" build configuration,
    allowing local test runs for non-cuda configurations.
    
    Add .dockerignore covering local conda environment and jupyter caches.
    Allows local docker build of test environment by reducing context sent
    to docker daemon.
    
    * Add --std=c++14 flags for compiled extensions.
    
    * Tidy cpp_extension flags, convert to debug-mode build.
    
    * Add conda activate to test docker entrypoint.
    
    Add docker-entrypoint.sh and update ENTRYPOINT in test environment to
    performing a full conda environment activation. This augments the
    existing bashrc based activation, ensuring that the base conda
    environment is activated for all test commands. Required to allow
    compiler activation for jit-compiled extensions.

--- tmol/utility/cpp_extension.py
+++ tmol/utility/cpp_extension.py
@@ -10,21 +10,35 @@ import torch.utils.cpp_extension
 # Add warning filter for use of c++ (rather than g++) for extension
 # compilation. c++ is provided by g++ on our platform.
 warnings.filterwarnings(
-    "ignore", message=r"(\n|.)*\(c\+\+\) may be ABI-incompatible with PyTorch"
+    "ignore",
+    message=r"(\n|.)*"
+    r"x86_64-conda_cos6-linux-gnu-c\+\+.*"
+    r"may be ABI-incompatible with PyTorch(\n|.)*",
 )
 
-_default_include_paths = tmol_include_paths() + extern_include_paths()
+_default_include_paths = list(tmol_include_paths() + extern_include_paths())
+
+_required_flags = ["--std=c++14"]
+# _default_flags = ["-O3"]
+_default_flags = ["-g", "-Og"]
+
+
+def _augment_kwargs(kwargs):
+    kwargs["extra_cflags"] = (
+        list(kwargs.get("extra_cflags", _default_flags)) + _required_flags
+    )
+    kwargs["extra_include_paths"] = (
+        list(kwargs.get("extra_include_flags", [])) + _default_include_paths
+    )
+
+    return kwargs
 
 
 @wraps(torch.utils.cpp_extension.load)
 def load(*args, **kwargs):
     """Jit-compile torch cpp_extension with tmol paths."""
 
-    kwargs["extra_cflags"] = kwargs.get("extra_cflags", ["-O3"])
-    kwargs["extra_include_paths"] = (
-        kwargs.get("extra_include_flags", []) + _default_include_paths
-    )
-
+    kwargs = _augment_kwargs(kwargs)
     return torch.utils.cpp_extension.load(*args, **kwargs)
 
 
@@ -32,9 +46,5 @@ def load(*args, **kwargs):
 def load_inline(*args, **kwargs):
     """Jit-compile torch cpp_extension with tmol paths."""
 
-    kwargs["extra_cflags"] = kwargs.get("extra_cflags", ["-O3"])
-    kwargs["extra_include_paths"] = (
-        kwargs.get("extra_include_flags", []) + _default_include_paths
-    )
-
+    kwargs = _augment_kwargs(kwargs)
     return torch.utils.cpp_extension.load_inline(*args, **kwargs)
