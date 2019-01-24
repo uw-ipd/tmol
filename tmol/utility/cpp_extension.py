165de1e1b27877aa4b0f2c55a5ced6988adb626b
Alex Ford <fordas@uw.edu>
2019-01-24 00:09:11 -0800

    Update target cuda to v10. (#150)
    * Add cuda compilation flags to cpp_extension.
    
    * Update conda cuda toolkit components to cuda 10. Update test
      environment image to ubuntu 18.04 & cuda 10.
    
    * Fixup c++ filetype and language version in ycm_extra_conf.
    
    * Add nvcc version dump to buildkite env report.

--- tmol/utility/cpp_extension.py
+++ tmol/utility/cpp_extension.py
@@ -1,3 +1,4 @@
+import pathlib
 from functools import wraps
 import warnings
 
@@ -22,10 +23,17 @@ _required_flags = ["--std=c++14"]
 # _default_flags = ["-O3"]
 _default_flags = ["-g", "-Og"]
 
+_required_cuda_flags = ["--expt-extended-lambda", "--expt-relaxed-constexpr"]
+_default_cuda_flags = []
+
 
 def _augment_kwargs(kwargs):
     kwargs["extra_cflags"] = (
         list(kwargs.get("extra_cflags", _default_flags)) + _required_flags
+    )
+    kwargs["extra_cuda_cflags"] = (
+        list(kwargs.get("extra_cuda_cflags", _default_cuda_flags))
+        + _required_cuda_flags
     )
     kwargs["extra_include_paths"] = (
         list(kwargs.get("extra_include_flags", [])) + _default_include_paths
@@ -48,3 +56,30 @@ def load_inline(*args, **kwargs):
 
     kwargs = _augment_kwargs(kwargs)
     return torch.utils.cpp_extension.load_inline(*args, **kwargs)
+
+
+def relpaths(src_path, paths):
+    """Paths relative to the parent of given src file.
+
+    Used to indiciate paths relative to a module's __file__.
+
+    Example:
+        srcs = relpaths(__file__, ["sibling.cpp", "sibling.cu"])
+    """
+
+    if isinstance(paths, (str, bytes)):
+        paths = [paths]
+
+    return [str(pathlib.Path(src_path).parent / s) for s in paths]
+
+
+def modulename(src_name):
+    """Adapt module name to valid cpp extension name.
+
+    Used to adapt a module __name__ to a valid extension name.
+
+    Example:
+        name = modulename(__name__)
+    """
+
+    return src_name.replace(".", "_")
