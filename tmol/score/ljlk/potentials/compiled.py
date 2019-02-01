5205a2ed092c33310fdf0242197c0c697bdbb4e1
Frank DiMaio <dimaio@uw.edu>
2019-02-01 13:44:04 -0800

    Check if CUDA is available before trying to invoke nvcc

--- tmol/score/ljlk/potentials/compiled.py
+++ tmol/score/ljlk/potentials/compiled.py
@@ -1,8 +1,12 @@
-from tmol.utility.cpp_extension import load, relpaths, modulename
+from tmol.utility.cpp_extension import load, relpaths, modulename, cuda_if_available
 
 _compiled = load(
     modulename(__name__),
-    relpaths(__file__, ["compiled.cuda.cu", "compiled.cpu.cpp", "compiled.pybind.cpp"]),
+    cuda_if_available(
+        relpaths(
+            __file__, ["compiled.pybind.cpp", "compiled.cpu.cpp", "compiled.cuda.cu"]
+        )
+    ),
 )
 
 lk_isotropic = _compiled.lk_isotropic
