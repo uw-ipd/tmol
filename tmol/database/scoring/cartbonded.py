2da6e8150a9a95210ec35279cfe9025cfa6994bf
Frank DiMaio <dimaio@uw.edu>
2019-02-08 14:13:56 -0800

    Add cart_bonded database

---
+++ tmol/database/scoring/cartbonded.py
@@ -0,0 +1,66 @@
+import attr
+import cattr
+import yaml
+
+from typing import Tuple
+
+
+@attr.s(auto_attribs=True, slots=True, frozen=True)
+class LengthGroup:
+    res: str
+    atm1: str
+    atm2: str
+    x0: float
+    K: float
+
+
+@attr.s(auto_attribs=True, slots=True, frozen=True)
+class AngleGroup:
+    res: str
+    atm1: str
+    atm2: str
+    atm3: str
+    x0: float
+    K: float
+
+
+@attr.s(auto_attribs=True, slots=True, frozen=True)
+class TorsionGroup:
+    res: str
+    atm1: str
+    atm2: str
+    atm3: str
+    atm4: str
+    x0: float
+    K: float
+    ncyc: int = 1
+
+
+@attr.s(auto_attribs=True, slots=True, frozen=True)
+class HxlTorsionGroup:
+    res: str
+    atm1: str
+    atm2: str
+    atm3: str
+    atm4: str
+    k1: float = 0.0
+    phi1: float = 0.0
+    k2: float = 0.0
+    phi2: float = 0.0
+    k3: float = 0.0
+    phi3: float = 0.0
+
+
+@attr.s(auto_attribs=True, frozen=True, slots=True)
+class CartBondedDatabase:
+    length_parameters: Tuple[LengthGroup, ...]
+    angle_parameters: Tuple[AngleGroup, ...]
+    torsion_parameters: Tuple[TorsionGroup, ...]
+    improper_parameters: Tuple[TorsionGroup, ...]
+    hxltorsion_parameters: Tuple[HxlTorsionGroup, ...]
+
+    @classmethod
+    def from_file(cls, path):
+        with open(path, "r") as infile:
+            raw = yaml.load(infile)
+        return cattr.structure(raw, cls)
