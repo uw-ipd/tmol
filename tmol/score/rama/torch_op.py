Fix rama derivative errors.  Score graph tests still remaining.

tmol/score/rama/torch_op.py
@@ -74,6 +74,8 @@ class RamaScoreFun(torch.autograd.Function):
 
         phi_indices = phi_indices.transpose(0, 1)  # coo_tensor wants this
         psi_indices = psi_indices.transpose(0, 1)  # coo_tensor wants this
+        dE_dphis = dE_dphis.reshape([-1, 3, 4])
+        dE_dpsis = dE_dpsis.reshape([-1, 3, 4])
         ctx.save_for_backward(phi_indices, psi_indices, dE_dphis, dE_dpsis)
 
         return E
@@ -85,12 +87,12 @@ class RamaScoreFun(torch.autograd.Function):
         for i in range(4):
             dVdA += torch.sparse_coo_tensor(
                 phi_indices[i, None, :],
-                dV_dE[..., None] * dE_dphis[..., 3 * i : (3 * i + 3)],
+                dV_dE[..., None] * dE_dphis[..., i],
                 (ctx.coords_shape),
             ).to_dense()
             dVdA += torch.sparse_coo_tensor(
                 psi_indices[i, None, :],
-                dV_dE[..., None] * dE_dpsis[..., 3 * i : (3 * i + 3)],
+                dV_dE[..., None] * dE_dpsis[..., i],
                 (ctx.coords_shape),
             ).to_dense()
 

tmol/tests/score/rama/test_torch_op.py
@@ -182,7 +182,7 @@ def test_rama_intra(default_database, ubq_system, torch_device):
 
 
 # torch gradcheck
-def test_elec_intra_gradcheck(default_database, ubq_system, torch_device):
+def test_rama_intra_gradcheck(default_database, ubq_system, torch_device):
     s = ScoreSetup.from_fixture(default_database, ubq_system, torch_device)
     func = RamaOp.from_param_resolver(s.param_resolver)
 
@@ -191,7 +191,7 @@ def test_elec_intra_gradcheck(default_database, ubq_system, torch_device):
     t_atm_indices = torch.from_numpy(atm_mask.nonzero()[0]).to(
         device=torch_device, dtype=torch.long
     )
-    t_atm_indices = t_atm_indices[4:9]  # limit runtime
+    t_atm_indices = t_atm_indices[2]  # limit runtime
 
     def eval_rama(coords_subset):
         coords = s.tcoords.clone()
@@ -203,5 +203,5 @@ def test_elec_intra_gradcheck(default_database, ubq_system, torch_device):
 
     masked_coords = s.tcoords[t_atm_indices]
     torch.autograd.gradcheck(
-        eval_rama, (masked_coords.requires_grad_(True),), eps=1e-3, atol=1e-3, rtol=-1
+        eval_rama, (masked_coords.requires_grad_(True),), eps=1e-3, atol=5e-3
     )