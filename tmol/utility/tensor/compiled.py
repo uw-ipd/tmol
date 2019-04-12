0561c45325a74b2ce08331c5e0b5a2492c61be50
Andrew Leaver-Fay <aleaverfay@gmail.com>
2019-04-12 09:55:30 -0400

    Refine TensorCollection creation utility + update Rama

--- tmol/utility/tensor/compiled.py
+++ tmol/utility/tensor/compiled.py
@@ -1,4 +1,6 @@
 from tmol.utility.cpp_extension import load, relpaths, modulename, cuda_if_available
+from typing import List
+from tmol.types.functional import validate_args
 
 _compiled = load(
     modulename(__name__),
@@ -10,25 +12,52 @@ _compiled = load(
 )
 
 
-def create_tensor_collection1(*args, **kwargs):
+@validate_args
+def create_tensor_collection(tensor_list: List):
+    if len(tensor_list) == 0:
+        raise ValueError(
+            "tensor list passed to create_tensor_collection must be non-empty"
+        )
+
+    for tensor in tensor_list:
+        assert len(tensor.shape) == len(
+            tensor_list[0].shape
+        ), "All tensors in tensor_list must be the same dimension"
+
+    if len(tensor_list[0].shape) == 1:
+        return _create_tensor_collection1(tensor_list)
+    elif len(tensor_list[0].shape) == 2:
+        return _create_tensor_collection2(tensor_list)
+    elif len(tensor_list[0].shape) == 3:
+        return _create_tensor_collection3(tensor_list)
+    elif len(tensor_list[0].shape) == 4:
+        return _create_tensor_collection4(tensor_list)
+    else:
+        raise ValueError(
+            "highest supported dimension for TensorCollection is 4; input tensors are of dimension "
+            + str(len(tensor_list[0].shape))
+        )
+
+
+def _create_tensor_collection1(tensor_list: List):
     return _compiled.create_tensor_collection1[
-        (args[0][0].device.type, args[0][0].dtype)
-    ](*args, **kwargs)
+        (tensor_list[0][0].device.type, tensor_list[0][0].dtype)
+    ](tensor_list)
 
 
-def create_tensor_collection2(*args, **kwargs):
+def _create_tensor_collection2(tensor_list: List):
     return _compiled.create_tensor_collection2[
-        (args[0][0].device.type, args[0][0].dtype)
-    ](*args, **kwargs)
+        (tensor_list[0][0].device.type, tensor_list[0][0].dtype)
+    ](tensor_list)
 
 
-def create_tensor_collection3(*args, **kwargs):
+def _create_tensor_collection3(tensor_list: List):
     return _compiled.create_tensor_collection3[
-        (args[0][0].device.type, args[0][0].dtype)
-    ](*args, **kwargs)
+        (tensor_list[0][0].device.type, tensor_list[0][0].dtype)
+    ](tensor_list)
 
 
-def create_tensor_collection4(*args, **kwargs):
+def _create_tensor_collection4(tensor_list: List):
     return _compiled.create_tensor_collection4[
-        (args[0][0].device.type, args[0][0].dtype)
-    ](*args, **kwargs)
+        (tensor_list[0][0].device.type, tensor_list[0][0].dtype)
+    ](tensor_list)
