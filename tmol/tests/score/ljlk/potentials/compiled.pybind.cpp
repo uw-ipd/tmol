15b4777360f45f18b8e1f62b27b756e4a221685e
Andrew Leaver-Fay <aleaverfay@gmail.com>
2019-05-08 11:18:00 -0400

    ok -- finally reverted enough of this branch to master to get ljlk working

--- tmol/tests/score/ljlk/potentials/compiled.pybind.cpp
+++ tmol/tests/score/ljlk/potentials/compiled.pybind.cpp
@@ -22,19 +22,12 @@ void bind_potentials(pybind11::module& m) {
       "j_params"_a,
       "global_params"_a);
 
-  m.def(
-      "vdw_V_dV",
-      [](Real dist, Real sigma, Real epsilon) {
-        return vdw<Real>::V_dV(dist, sigma, epsilon).astuple();
-      },
-      "dist"_a,
-      "sigma"_a,
-      "epsilon"_a);
-  m.def("vdw_V", &vdw<Real>::V, "dist"_a, "sigma"_a, "epsilon"_a);
+  m.def("vdw_V_dV", &vdw_V_dV<Real>, "dist"_a, "sigma"_a, "epsilon"_a);
+  m.def("vdw_V", &vdw_V<Real>, "dist"_a, "sigma"_a, "epsilon"_a);
 
   m.def(
       "lj_score_V",
-      &lj_score<Real>::V,
+      &lj_score_V<Real>,
       "dist"_a,
       "bonded_path_length"_a,
       "i_params"_a,
@@ -43,14 +36,7 @@ void bind_potentials(pybind11::module& m) {
 
   m.def(
       "lj_score_V_dV",
-      [](Real dist,
-         Real bonded_path_length,
-         LJTypeParams<Real> i,
-         LJTypeParams<Real> j,
-         LJGlobalParams<Real> global) {
-        return lj_score<Real>::V_dV(dist, bonded_path_length, i, j, global)
-            .astuple();
-      },
+      &lj_score_V_dV<Real>,
       "dist"_a,
       "bonded_path_length"_a,
       "i_params"_a,
@@ -59,7 +45,7 @@ void bind_potentials(pybind11::module& m) {
 
   m.def(
       "f_desolv_V",
-      &f_desolv<Real>::V,
+      &f_desolv_V<Real>,
       "dist"_a,
       "lj_radius_i"_a,
       "lk_dgfree_i"_a,
@@ -68,17 +54,7 @@ void bind_potentials(pybind11::module& m) {
 
   m.def(
       "f_desolv_V_dV",
-      [](
-
-          Real dist,
-          Real lj_radius_i,
-          Real lk_dgfree_i,
-          Real lk_lambda_i,
-          Real lk_volume_j) {
-        return f_desolv<Real>::V_dV(
-                   dist, lj_radius_i, lk_dgfree_i, lk_lambda_i, lk_volume_j)
-            .astuple();
-      },
+      &f_desolv_V_dV<Real>,
       "dist"_a,
       "lj_radius_i"_a,
       "lk_dgfree_i"_a,
@@ -87,7 +63,7 @@ void bind_potentials(pybind11::module& m) {
 
   m.def(
       "lk_isotropic_score_V",
-      &lk_isotropic_score<Real>::V,
+      &lk_isotropic_score_V<Real>,
       "dist"_a,
       "bonded_path_length"_a,
       "i"_a,
@@ -96,21 +72,13 @@ void bind_potentials(pybind11::module& m) {
 
   m.def(
       "lk_isotropic_score_V_dV",
-      [](Real dist,
-         Real bonded_path_length,
-         LKTypeParams<Real> i,
-         LKTypeParams<Real> j,
-         LJGlobalParams<Real> global) {
-        return lk_isotropic_score<Real>::V_dV(
-                   dist, bonded_path_length, i, j, global)
-            .astuple();
-      },
+      &lk_isotropic_score_V_dV<Real>,
       "dist"_a,
       "bonded_path_length"_a,
       "i"_a,
       "j"_a,
       "global"_a);
-}  // namespace potentials
+}
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) { bind_potentials<double>(m); }
 
--- tmol/tests/score/ljlk/test_torch_op.py
+++ tmol/tests/score/ljlk/test_torch_op.py
@@ -12,7 +12,6 @@ from tmol.score.bonded_atom import bonded_path_length
 import tmol.database
 
 from tmol.utility.args import ignore_unused_kwargs
-from tmol.tests.autograd import gradcheck
 
 
 @attr.s(auto_attribs=True)
@@ -91,7 +90,7 @@ def _dense_potential(potential, coords, atom_type_idx, atom_pair_bpl, param_reso
 
 
 def test_lj_intra_op(default_database, ubq_system, torch_device):
-    """LJOp.intra returns sum of triu entries of the dense lj score matrix."""
+    """LJOp.intra returns triu entries of the dense lj score matrix."""
     s = ScoreSetup.from_fixture(default_database, ubq_system, torch_device)
 
     expected_dense = numpy.triu(
@@ -102,36 +101,30 @@ def test_lj_intra_op(default_database, ubq_system, torch_device):
 
     op = LJOp(s.param_resolver)
 
-    val = op.intra(s.tcoords, s.ttype, s.tbpl)
+    v_inds, v_val = op.intra(s.tcoords, s.ttype, s.tbpl)
 
-    torch.testing.assert_allclose(
-        val, torch.tensor(expected_dense).to(torch_device).sum()
-    )
+    assert not v_inds.requires_grad
+    assert v_val.requires_grad
 
-    subind = torch.arange(0, s.tcoords.shape[0], 100)
-
-    def op_subset(c):
-        fcoords = s.tcoords.clone()
-        fcoords[subind] = c
-
-        return op.intra(fcoords, s.ttype, s.tbpl)
-
-    gradcheck(op_subset, (s.tcoords[subind].requires_grad_(True),), eps=1e-3)
+    op_dense = _todense(v_inds, v_val, (s.coords.shape[0],) * 2)
+    torch.testing.assert_allclose(op_dense, expected_dense, rtol=1e-6, atol=1e-7)
 
 
 def test_lj_inter_op(default_database, torch_device, ubq_system):
-    """LJOp.inter returns sum of the dense lj score matrix."""
+    """LJOp.intra returns entries of the dense lj score matrix."""
     s = ScoreSetup.from_fixture(default_database, ubq_system, torch_device)
 
     part = ubq_system.system_size // 2
 
-    expected_dense = numpy.nan_to_num(
-        _dense_lj(s.coords, s.atom_type_idx, s.atom_pair_bpl, s.param_resolver)
+    expected_dense = numpy.triu(
+        numpy.nan_to_num(
+            _dense_lj(s.coords, s.atom_type_idx, s.atom_pair_bpl, s.param_resolver)
+        )
     )[:part, part:]
 
     op = LJOp(s.param_resolver)
 
-    val = op.inter(
+    v_inds, v_val = op.inter(
         s.tcoords[:part],
         s.ttype[:part],
         s.tcoords[part:],
@@ -139,29 +132,62 @@ def test_lj_inter_op(default_database, torch_device, ubq_system):
         s.tbpl[:part, part:],
     )
 
-    torch.testing.assert_allclose(
-        val, torch.tensor(expected_dense).to(torch_device).sum()
-    )
+    assert not v_inds.requires_grad
+    assert v_val.requires_grad
 
-    subind = torch.arange(0, s.tcoords.shape[0], 100)
+    op_dense = _todense(v_inds, v_val, shape=(part, ubq_system.system_size - part))
 
-    def op_subset(c):
-        fcoords = s.tcoords.clone()
-        fcoords[subind] = c
+    torch.testing.assert_allclose(op_dense, expected_dense, rtol=1e-6, atol=1e-7)
 
-        return op.inter(
-            fcoords[:part],
-            s.ttype[:part],
-            fcoords[part:],
-            s.ttype[part:],
-            s.tbpl[:part, part:],
+
+def test_lj_inter_op_gradcheck(default_database, ubq_system, torch_device):
+    s = ScoreSetup.from_fixture(default_database, ubq_system, torch_device)
+
+    natoms = 16
+    op = LJOp(s.param_resolver)
+
+    coords_a = s.tcoords[0:natoms]
+    coords_b = s.tcoords[natoms : natoms * 2]
+
+    def eval_inter(coords_a, coords_b):
+
+        i, v = op.inter(
+            coords_a,
+            s.ttype[0:natoms],
+            coords_b,
+            s.ttype[natoms : natoms * 2],
+            s.tbpl[0:natoms, natoms : natoms * 2],
         )
 
-    gradcheck(op_subset, (s.tcoords[subind].requires_grad_(True),), eps=1e-3)
+        return v
+
+    torch.autograd.gradcheck(
+        eval_inter,
+        (coords_a.requires_grad_(True), coords_b.requires_grad_(True)),
+        eps=1e-3,
+    )
+
+
+def test_lj_intra_op_gradcheck(default_database, ubq_system, torch_device):
+    s = ScoreSetup.from_fixture(default_database, ubq_system, torch_device)
+
+    natoms = 32
+    op = LJOp(s.param_resolver)
+
+    coords = s.tcoords[:natoms]
+
+    def eval_intra(coords):
+
+        i, v = op.intra(coords, s.ttype[:natoms], s.tbpl[:natoms, :natoms])
+
+        return v
+
+    torch.autograd.gradcheck(eval_intra, (coords.requires_grad_(True),), eps=1e-3)
 
 
 def test_lk_intra_op(default_database, ubq_system, torch_device):
-    """LKOp.intra returns sum of triu entries of the dense lk score matrix."""
+    """LKOp.intra returns triu entries of the dense lk score matrix."""
+
     s = ScoreSetup.from_fixture(default_database, ubq_system, torch_device)
 
     expected_dense = numpy.triu(
@@ -172,36 +198,31 @@ def test_lk_intra_op(default_database, ubq_system, torch_device):
 
     op = LKOp(s.param_resolver)
 
-    val = op.intra(s.tcoords, s.ttype, s.tbpl)
+    v_inds, v_val = op.intra(s.tcoords, s.ttype, s.tbpl)
 
-    torch.testing.assert_allclose(
-        val, torch.tensor(expected_dense).to(torch_device).sum()
-    )
+    assert not v_inds.requires_grad
+    assert v_val.requires_grad
 
-    subind = torch.arange(0, s.tcoords.shape[0], 100)
-
-    def op_subset(c):
-        fcoords = s.tcoords.clone()
-        fcoords[subind] = c
-
-        return op.intra(fcoords, s.ttype, s.tbpl)
-
-    gradcheck(op_subset, (s.tcoords[subind].requires_grad_(True),), eps=1e-3)
+    op_dense = _todense(v_inds, v_val, (s.coords.shape[0],) * 2)
+    torch.testing.assert_allclose(op_dense, expected_dense, rtol=1e-6, atol=1e-7)
 
 
-def test_lk_inter_op(default_database, torch_device, ubq_system):
-    """LKOp.inter returns sum of the dense lk score matrix."""
+def test_lk_inter_op(default_database, ubq_system, torch_device):
+    """LKOp.intra returns entries of the dense lk score matrix."""
+
     s = ScoreSetup.from_fixture(default_database, ubq_system, torch_device)
 
     part = ubq_system.system_size // 2
 
-    expected_dense = numpy.nan_to_num(
-        _dense_lk(s.coords, s.atom_type_idx, s.atom_pair_bpl, s.param_resolver)
+    expected_dense = numpy.triu(
+        numpy.nan_to_num(
+            _dense_lk(s.coords, s.atom_type_idx, s.atom_pair_bpl, s.param_resolver)
+        )
     )[:part, part:]
 
     op = LKOp(s.param_resolver)
 
-    val = op.inter(
+    v_inds, v_val = op.inter(
         s.tcoords[:part],
         s.ttype[:part],
         s.tcoords[part:],
@@ -209,22 +230,54 @@ def test_lk_inter_op(default_database, torch_device, ubq_system):
         s.tbpl[:part, part:],
     )
 
-    torch.testing.assert_allclose(
-        val, torch.tensor(expected_dense).to(torch_device).sum()
-    )
+    assert not v_inds.requires_grad
+    assert v_val.requires_grad
 
-    subind = torch.arange(0, s.tcoords.shape[0], 100)
+    op_dense = _todense(v_inds, v_val, shape=(part, ubq_system.system_size - part))
 
-    def op_subset(c):
-        fcoords = s.tcoords.clone()
-        fcoords[subind] = c
+    torch.testing.assert_allclose(op_dense, expected_dense, rtol=1e-6, atol=1e-7)
 
-        return op.inter(
-            fcoords[:part],
-            s.ttype[:part],
-            fcoords[part:],
-            s.ttype[part:],
-            s.tbpl[:part, part:],
+
+def test_lk_inter_op_gradcheck(default_database, ubq_system, torch_device):
+    s = ScoreSetup.from_fixture(default_database, ubq_system, torch_device)
+
+    natoms = 16
+    op = LKOp(s.param_resolver)
+
+    coords_a = s.tcoords[0:natoms]
+    coords_b = s.tcoords[natoms : natoms * 2]
+
+    def eval_inter(coords_a, coords_b):
+
+        i, v = op.inter(
+            coords_a,
+            s.ttype[0:natoms],
+            coords_b,
+            s.ttype[natoms : natoms * 2],
+            s.tbpl[0:natoms, natoms : natoms * 2],
         )
 
-    gradcheck(op_subset, (s.tcoords[subind].requires_grad_(True),), eps=1e-3)
+        return v
+
+    torch.autograd.gradcheck(
+        eval_inter,
+        (coords_a.requires_grad_(True), coords_b.requires_grad_(True)),
+        eps=1e-3,
+    )
+
+
+def test_lk_intra_op_gradcheck(default_database, ubq_system, torch_device):
+    s = ScoreSetup.from_fixture(default_database, ubq_system, torch_device)
+
+    natoms = 32
+    op = LKOp(s.param_resolver)
+
+    coords = s.tcoords[:natoms]
+
+    def eval_intra(coords):
+
+        i, v = op.intra(coords, s.ttype[:natoms], s.tbpl[:natoms, :natoms])
+
+        return v
+
+    torch.autograd.gradcheck(eval_intra, (coords.requires_grad_(True),), eps=1e-3)
