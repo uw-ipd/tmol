6bf9bc820532a9e26d063a2df6c770cbd797e05b
Frank DiMaio <dimaio@uw.edu>
2019-05-13 18:06:14 -0700

    Updating cartbonded to be a torchscript kernel

--- tmol/score/cartbonded/potentials/compiled.cpu.cpp
+++ tmol/score/cartbonded/potentials/compiled.cpu.cpp
@@ -4,6 +4,7 @@
 #include <tmol/score/common/geom.hh>
 
 #include "potentials.hh"
+#include "params.hh"
 
 namespace tmol {
 namespace score {
@@ -17,14 +18,12 @@ template <tmol::Device D, typename Real, typename Int>
 struct CartBondedLengthDispatch {
   static auto f(
       TView<Vec<Real, 3>, 1, D> coords,
-      TView<Vec<Int, 2>, 1, D> atompair_indices,
-      TView<Int, 1, D> parameter_indices,
-      TView<Real, 1, D> K,
-      TView<Real, 1, D> x0)
+      TView<CartBondedLengthParams<Int>, 1, D> atom_indices,
+      TView<CartBondedHarmonicTypeParams<Real>, 1, D> param_table)
       -> std::tuple<
           TPack<Real, 1, D>,
           TPack<Vec<Real, 3>, 1, D> > {
-    auto num_Vs = parameter_indices.size(0);
+    auto num_Vs = atom_indices.size(0);
 
     auto V_t = TPack<Real, 1, D>::zeros({1});
     auto dV_dx_t = TPack<Vec<Real, 3>, 1, D>::zeros({coords.size(0)});
@@ -33,12 +32,11 @@ struct CartBondedLengthDispatch {
     auto dV_dx = dV_dx_t.view;
 
     auto f_i = ([=] EIGEN_DEVICE_FUNC(int i) {
-      Int ati = atompair_indices[i][0];
-      Int atj = atompair_indices[i][1];
-      Int pari = parameter_indices[i];
-
+      Int ati = atom_indices[i].atom_index_i;
+      Int atj = atom_indices[i].atom_index_j;
+      Int pari = atom_indices[i].param_index;
       auto cblength = cblength_V_dV(
-          coords[ati], coords[atj], K[pari], x0[pari]);
+          coords[ati], coords[atj], param_table[pari].K, param_table[pari].x0);
 
       V[0] += std::get<0>(cblength);
       dV_dx[ati] += std::get<1>(cblength);
@@ -57,14 +55,12 @@ template <tmol::Device D, typename Real, typename Int>
 struct CartBondedAngleDispatch {
   static auto f(
       TView<Vec<Real, 3>, 1, D> coords,
-      TView<Vec<Int, 3>, 1, D> atomtriple_indices,
-      TView<Int, 1, D> parameter_indices,
-      TView<Real, 1, D> K,
-      TView<Real, 1, D> x0)
+      TView<CartBondedAngleParams<Int>, 1, D> atom_indices,
+      TView<CartBondedHarmonicTypeParams<Real>, 1, D> param_table)
       -> std::tuple<
           TPack<Real, 1, D>,
           TPack<Vec<Real, 3>, 1, D> > {
-    auto num_Vs = parameter_indices.size(0);
+    auto num_Vs = atom_indices.size(0);
 
     auto V_t = TPack<Real, 1, D>::zeros({1});
     auto dV_dx_t = TPack<Vec<Real, 3>, 1, D>::zeros({coords.size(0)});
@@ -73,12 +69,12 @@ struct CartBondedAngleDispatch {
     auto dV_dx = dV_dx_t.view;
 
     auto f_i = ([=] EIGEN_DEVICE_FUNC(int i) {
-      Int ati = atomtriple_indices[i][0];
-      Int atj = atomtriple_indices[i][1];
-      Int atk = atomtriple_indices[i][2];
-      Int pari = parameter_indices[i];
+      Int ati = atom_indices[i].atom_index_i;
+      Int atj = atom_indices[i].atom_index_j;
+      Int atk = atom_indices[i].atom_index_k;
+      Int pari = atom_indices[i].param_index;
       auto cbangle = cbangle_V_dV(
-          coords[ati], coords[atj], coords[atk], K[pari], x0[pari]);
+          coords[ati], coords[atj], coords[atk], param_table[pari].K, param_table[pari].x0);
 
       V[0] += std::get<0>(cbangle);
       dV_dx[ati] += std::get<1>(cbangle);
@@ -99,15 +95,12 @@ template <tmol::Device D, typename Real, typename Int>
 struct CartBondedTorsionDispatch {
   static auto f(
       TView<Vec<Real, 3>, 1, D> coords,
-      TView<Vec<Int, 4>, 1, D> atomquad_indices,
-      TView<Int, 1, D> parameter_indices,
-      TView<Real, 1, D> K,
-      TView<Real, 1, D> x0,
-      TView<Int, 1, D> period)
+      TView<CartBondedTorsionParams<Int>, 1, D> atom_indices,
+      TView<CartBondedPeriodicTypeParams<Real>, 1, D> param_table)
       -> std::tuple<
           TPack<Real, 1, D>,
           TPack<Vec<Real, 3>, 1, D> > {
-    auto num_Vs = parameter_indices.size(0);
+    auto num_Vs = atom_indices.size(0);
 
     auto V_t = TPack<Real, 1, D>::zeros({1});
     auto dV_dx_t = TPack<Vec<Real, 3>, 1, D>::zeros({coords.size(0)});
@@ -116,19 +109,19 @@ struct CartBondedTorsionDispatch {
     auto dV_dx = dV_dx_t.view;
 
     auto f_i = ([=] EIGEN_DEVICE_FUNC(int i) {
-      Int ati = atomquad_indices[i][0];
-      Int atj = atomquad_indices[i][1];
-      Int atk = atomquad_indices[i][2];
-      Int atl = atomquad_indices[i][3];
-      Int pari = parameter_indices[i];
+      Int ati = atom_indices[i].atom_index_i;
+      Int atj = atom_indices[i].atom_index_j;
+      Int atk = atom_indices[i].atom_index_k;
+      Int atl = atom_indices[i].atom_index_l;
+      Int pari = atom_indices[i].param_index;
       auto cbtorsion = cbtorsion_V_dV(
           coords[ati],
           coords[atj],
           coords[atk],
           coords[atl],
-          K[pari],
-          x0[pari],
-          period[pari]);
+          param_table[pari].K,
+          param_table[pari].x0,
+          param_table[pari].period);
 
       V[0] += std::get<0>(cbtorsion);
       dV_dx[ati] += std::get<1>(cbtorsion);
@@ -149,18 +142,12 @@ template <tmol::Device D, typename Real, typename Int>
 struct CartBondedHxlTorsionDispatch {
   static auto f(
       TView<Vec<Real, 3>, 1, D> coords,
-      TView<Vec<Int, 4>, 1, D> atomquad_indices,
-      TView<Int, 1, D> parameter_indices,
-      TView<Real, 1, D> K1,
-      TView<Real, 1, D> K2,
-      TView<Real, 1, D> K3,
-      TView<Real, 1, D> phi1,
-      TView<Real, 1, D> phi2,
-      TView<Real, 1, D> phi3)
+      TView<CartBondedTorsionParams<Int>, 1, D> atom_indices,
+      TView<CartBondedSinusoidalTypeParams<Real>, 1, D> param_table)
       -> std::tuple<
           TPack<Real, 1, D>,
           TPack<Vec<Real, 3>, 1, D> > {
-    auto num_Vs = parameter_indices.size(0);
+    auto num_Vs = atom_indices.size(0);
 
     auto V_t = TPack<Real, 1, D>::zeros({1});
     auto dV_dx_t = TPack<Vec<Real, 3>, 1, D>::zeros({coords.size(0)});
@@ -169,23 +156,24 @@ struct CartBondedHxlTorsionDispatch {
     auto dV_dx = dV_dx_t.view;
 
     auto f_i = ([=] EIGEN_DEVICE_FUNC(int i) {
-      Int ati = atomquad_indices[i][0];
-      Int atj = atomquad_indices[i][1];
-      Int atk = atomquad_indices[i][2];
-      Int atl = atomquad_indices[i][3];
-      Int pari = parameter_indices[i];
+      Int ati = atom_indices[i].atom_index_i;
+      Int atj = atom_indices[i].atom_index_j;
+      Int atk = atom_indices[i].atom_index_k;
+      Int atl = atom_indices[i].atom_index_l;
+      Int pari = atom_indices[i].param_index;
       auto cbhxltorsion =
           cbhxltorsion_V_dV(
               coords[ati],
               coords[atj],
               coords[atk],
               coords[atl],
-              K1[pari],
-              K2[pari],
-              K3[pari],
-              phi1[pari],
-              phi2[pari],
-              phi3[pari]);
+              param_table[pari].k1,
+              param_table[pari].k2,
+              param_table[pari].k3,
+              param_table[pari].phi1,
+              param_table[pari].phi2,
+              param_table[pari].phi3);
+
 
       V[0] += std::get<0>(cbhxltorsion);
       dV_dx[ati] += std::get<1>(cbhxltorsion);
@@ -203,23 +191,12 @@ struct CartBondedHxlTorsionDispatch {
   }
 };
 
-template struct CartBondedLengthDispatch<tmol::Device::CPU, float, int32_t>;
-template struct CartBondedLengthDispatch<tmol::Device::CPU, double, int32_t>;
 template struct CartBondedLengthDispatch<tmol::Device::CPU, float, int64_t>;
 template struct CartBondedLengthDispatch<tmol::Device::CPU, double, int64_t>;
-template struct CartBondedAngleDispatch<tmol::Device::CPU, float, int32_t>;
-template struct CartBondedAngleDispatch<tmol::Device::CPU, double, int32_t>;
 template struct CartBondedAngleDispatch<tmol::Device::CPU, float, int64_t>;
 template struct CartBondedAngleDispatch<tmol::Device::CPU, double, int64_t>;
-template struct CartBondedTorsionDispatch<tmol::Device::CPU, float, int32_t>;
-template struct CartBondedTorsionDispatch<tmol::Device::CPU, double, int32_t>;
 template struct CartBondedTorsionDispatch<tmol::Device::CPU, float, int64_t>;
 template struct CartBondedTorsionDispatch<tmol::Device::CPU, double, int64_t>;
-template struct CartBondedHxlTorsionDispatch<tmol::Device::CPU, float, int32_t>;
-template struct CartBondedHxlTorsionDispatch<
-    tmol::Device::CPU,
-    double,
-    int32_t>;
 template struct CartBondedHxlTorsionDispatch<tmol::Device::CPU, float, int64_t>;
 template struct CartBondedHxlTorsionDispatch<
     tmol::Device::CPU,
