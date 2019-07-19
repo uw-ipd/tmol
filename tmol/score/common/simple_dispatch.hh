a7323b93c60689d95ba3809644a27a62bba63b11
Andrew Leaver-Fay <fay@digs>
2019-07-19 14:08:44 -0700

    further progress

--- tmol/score/common/simple_dispatch.hh
+++ tmol/score/common/simple_dispatch.hh
@@ -32,6 +32,15 @@ struct AABBDispatch {
       TView<Int, 1, D> coord_idx_i,
       TView<Int, 1, D> coord_idx_j,
       Func f);
+
+  template <typename Real, typename Int, typename Func>
+  static void forall_stacked_idx_pairs(
+      Real threshold_distance,
+      TView<Eigen::Matrix<Real, 3, 1>, 2, D> coords_i,
+      TView<Eigen::Matrix<Real, 3, 1>, 2, D> coords_j,
+      TView<Int, 2, D> coord_idx_i,
+      TView<Int, 2, D> coord_idx_j,
+      Func f);
 };
 
 template <tmol::Device D>
@@ -57,6 +66,15 @@ struct AABBTriuDispatch {
       TView<Eigen::Matrix<Real, 3, 1>, 1, D> coords_j,
       TView<Int, 1, D> coord_idx_i,
       TView<Int, 1, D> coord_idx_j,
+      Func f);
+
+  template <typename Real, typename Int, typename Func>
+  static void forall_stacked_idx_pairs(
+      Real threshold_distance,
+      TView<Eigen::Matrix<Real, 3, 1>, 2, D> coords_i,
+      TView<Eigen::Matrix<Real, 3, 1>, 2, D> coords_j,
+      TView<Int, 2, D> coord_idx_i,
+      TView<Int, 2, D> coord_idx_j,
       Func f);
 };
 
--- tmol/score/lk_ball/potentials/dispatch.impl.hh
+++ tmol/score/lk_ball/potentials/dispatch.impl.hh
@@ -34,65 +34,66 @@ template <
     typename Int>
 struct LKBallDispatch {
   static auto forward(
-      TView<Vec<Real, 3>, 1, D> coords_i,
-      TView<int64_t, 1, D> polars_i,
-      TView<Int, 1, D> atom_type_i,
-      TView<Vec<Real, 3>, 2, D> waters_i,
+      TView<Vec<Real, 3>, 2, D> coords_i,
+      TView<int64_t, 2, D> polars_i,
+      TView<Int, 2, D> atom_type_i,
+      TView<Vec<Real, 3>, 3, D> waters_i,
 
-      TView<Vec<Real, 3>, 1, D> coords_j,
-      TView<int64_t, 1, D> occluders_j,
-      TView<Int, 1, D> atom_type_j,
-      TView<Vec<Real, 3>, 2, D> waters_j,
+      TView<Vec<Real, 3>, 2, D> coords_j,
+      TView<int64_t, 2, D> occluders_j,
+      TView<Int, 2, D> atom_type_j,
+      TView<Vec<Real, 3>, 3, D> waters_j,
 
-      TView<Real, 2, D> bonded_path_lengths,
+      TView<Real, 3, D> bonded_path_lengths,
 
       TView<LKBallTypeParams<Real>, 1, D> type_params,
       TView<LKBallGlobalParams<Real>, 1, D> global_params)
-      -> TPack<Real, 1, D> {
+      -> TPack<Real, 2, D> {
     NVTXRange _function(__FUNCTION__);
-
+    Int const nstacks = coords_i.size(0);
+    
     nvtx_range_push("dispatch::alloc");
-    auto Vs_t = TPack<Real, 1, D>::zeros({4});
+    auto Vs_t = TPack<Real, 2, D>::zeros({nstacks, 4});
     auto Vs = Vs_t.view;
     nvtx_range_pop();
 
     nvtx_range_push("dispatch::score");
     Real threshold_distance = 6.0;  // fd this should be a global param
-    Dispatch<D>::forall_idx_pairs(
+    Dispatch<D>::forall_stacked_idx_pairs(
         threshold_distance,
         coords_i,
         coords_j,
         polars_i,
         occluders_j,
-        [=] EIGEN_DEVICE_FUNC(int i_idx, int j_idx) {
-          Int i = polars_i[i_idx];
-          Int j = occluders_j[j_idx];
-          Int ati = atom_type_i[i];
-          Int atj = atom_type_j[j];
+        [=] EIGEN_DEVICE_FUNC(int stack, int i_idx, int j_idx) {
+          Int i = polars_i[stack][i_idx];
+          Int j = occluders_j[stack][j_idx];
+          Int ati = atom_type_i[stack][i];
+          Int atj = atom_type_j[stack][j];
 
           // fd: should '4' (#wats) be a templated parameter?
           Eigen::Matrix<Real, 4, 3> wmat_i;
           Eigen::Matrix<Real, 4, 3> wmat_j;
 
           for (int wi = 0; wi < 4; wi++) {
-            wmat_i.row(wi) = waters_i[i][wi];
-            wmat_j.row(wi) = waters_j[j][wi];
+            wmat_i.row(wi) = waters_i[stack][i][wi];
+            wmat_j.row(wi) = waters_j[stack][j][wi];
           }
 
           auto score = lk_ball_score<Real, 4>::V(
-              coords_i[i],
-              coords_j[j],
+              coords_i[stack][i],
+              coords_j[stack][j],
               wmat_i,
               wmat_j,
-              bonded_path_lengths[i][j],
+              bonded_path_lengths[stack][i][j],
               type_params[ati],
               type_params[atj],
               global_params[0]);
 
-          common::accumulate<D, Real>::add(Vs[0], score.lkball_iso);
-          common::accumulate<D, Real>::add(Vs[1], score.lkball);
-          common::accumulate<D, Real>::add(Vs[2], score.lkbridge);
-          common::accumulate<D, Real>::add(Vs[3], score.lkbridge_uncpl);
+          common::accumulate<D, Real>::add(Vs[stack][0], score.lkball_iso);
+          common::accumulate<D, Real>::add(Vs[stack][1], score.lkball);
+          common::accumulate<D, Real>::add(Vs[stack][2], score.lkbridge);
+          common::accumulate<D, Real>::add(Vs[stack][3], score.lkbridge_uncpl);
         });
     nvtx_range_pop();
 
@@ -100,36 +101,38 @@ struct LKBallDispatch {
   }
 
   static auto backward(
-      TView<Real, 1, D> dTdV,
-      TView<Vec<Real, 3>, 1, D> coords_i,
-      TView<int64_t, 1, D> polars_i,
-      TView<Int, 1, D> atom_type_i,
-      TView<Vec<Real, 3>, 2, D> waters_i,
+      TView<Real, 2, D> dTdV,
+      TView<Vec<Real, 3>, 2, D> coords_i,
+      TView<int64_t, 2, D> polars_i,
+      TView<Int, 2, D> atom_type_i,
+      TView<Vec<Real, 3>, 3, D> waters_i,
 
-      TView<Vec<Real, 3>, 1, D> coords_j,
-      TView<int64_t, 1, D> occluders_j,
-      TView<Int, 1, D> atom_type_j,
-      TView<Vec<Real, 3>, 2, D> waters_j,
+      TView<Vec<Real, 3>, 2, D> coords_j,
+      TView<int64_t, 2, D> occluders_j,
+      TView<Int, 2, D> atom_type_j,
+      TView<Vec<Real, 3>, 3, D> waters_j,
 
-      TView<Real, 2, D> bonded_path_lengths,
+      TView<Real, 3, D> bonded_path_lengths,
 
       TView<LKBallTypeParams<Real>, 1, D> type_params,
       TView<LKBallGlobalParams<Real>, 1, D> global_params)
       -> std::tuple<
-          TPack<Vec<Real, 3>, 1, D>,
-          TPack<Vec<Real, 3>, 1, D>,
           TPack<Vec<Real, 3>, 2, D>,
-          TPack<Vec<Real, 3>, 2, D>> {
+          TPack<Vec<Real, 3>, 2, D>,
+          TPack<Vec<Real, 3>, 3, D>,
+          TPack<Vec<Real, 3>, 3, D>> {
     NVTXRange _function(__FUNCTION__);
 
     nvtx_range_push("dispatch::dalloc");
     // deriv w.r.t. heavyatom position
-    auto dV_dI_t = TPack<Vec<Real, 3>, 1, D>::zeros({coords_i.size(0)});
-    auto dV_dJ_t = TPack<Vec<Real, 3>, 1, D>::zeros({coords_j.size(0)});
+    int const n_stacks = coords_i.size(0);
+    
+    auto dV_dI_t = TPack<Vec<Real, 3>, 2, D>::zeros({n_stacks, coords_i.size(1)});
+    auto dV_dJ_t = TPack<Vec<Real, 3>, 2, D>::zeros({n_stacks, coords_j.size(1)});
 
     // deriv w.r.t. water position
-    auto dW_dI_t = TPack<Vec<Real, 3>, 2, D>::zeros({coords_i.size(0), 4});
-    auto dW_dJ_t = TPack<Vec<Real, 3>, 2, D>::zeros({coords_j.size(0), 4});
+    auto dW_dI_t = TPack<Vec<Real, 3>, 3, D>::zeros({n_stacks, coords_i.size(1), 4});
+    auto dW_dJ_t = TPack<Vec<Real, 3>, 3, D>::zeros({n_stacks, coords_j.size(1), 4});
 
     auto dV_dI = dV_dI_t.view;
     auto dV_dJ = dV_dJ_t.view;
@@ -139,71 +142,71 @@ struct LKBallDispatch {
 
     nvtx_range_push("dispatch::dscore");
     Real threshold_distance = 6.0;  // fd this should be a global param
-    Dispatch<D>::forall_idx_pairs(
+    Dispatch<D>::forall_stacked_idx_pairs(
         threshold_distance,
         coords_i,
         coords_j,
         polars_i,
         occluders_j,
-        [=] EIGEN_DEVICE_FUNC(int i_idx, int j_idx) {
-          Int i = polars_i[i_idx];
-          Int j = occluders_j[j_idx];
-          Int ati = atom_type_i[i];
-          Int atj = atom_type_j[j];
+        [=] EIGEN_DEVICE_FUNC(int stack, int i_idx, int j_idx) {
+          Int i = polars_i[stack][i_idx];
+          Int j = occluders_j[stack][j_idx];
+          Int ati = atom_type_i[stack][i];
+          Int atj = atom_type_j[stack][j];
 
           // fd: should '4' (#wats) be a templated parameter?
           Eigen::Matrix<Real, 4, 3> wmat_i;
           Eigen::Matrix<Real, 4, 3> wmat_j;
 
           for (int wi = 0; wi < 4; wi++) {
-            wmat_i.row(wi) = waters_i[i][wi];
-            wmat_j.row(wi) = waters_j[j][wi];
+            wmat_i.row(wi) = waters_i[stack][i][wi];
+            wmat_j.row(wi) = waters_j[stack][j][wi];
           }
 
           auto dV = lk_ball_score<Real, 4>::dV(
-              coords_i[i],
-              coords_j[j],
+              coords_i[stack][i],
+              coords_j[stack][j],
               wmat_i,
               wmat_j,
-              bonded_path_lengths[i][j],
+              bonded_path_lengths[stack][i][j],
               type_params[ati],
               type_params[atj],
               global_params[0]);
 
           common::accumulate<D, Vec<Real, 3>>::add(
-              dV_dI[i], dTdV[0] * dV.dI.d_lkball_iso);
+              dV_dI[stack][i], dTdV[stack][0] * dV.dI.d_lkball_iso);
           common::accumulate<D, Vec<Real, 3>>::add(
-              dV_dJ[j], dTdV[0] * dV.dJ.d_lkball_iso);
+              dV_dJ[stack][j], dTdV[stack][0] * dV.dJ.d_lkball_iso);
           common::accumulate<D, Vec<Real, 3>>::add(
-              dV_dI[i], dTdV[1] * dV.dI.d_lkball);
+              dV_dI[stack][i], dTdV[stack][1] * dV.dI.d_lkball);
           common::accumulate<D, Vec<Real, 3>>::add(
-              dV_dJ[j], dTdV[1] * dV.dJ.d_lkball);
+              dV_dJ[stack][j], dTdV[stack][1] * dV.dJ.d_lkball);
           common::accumulate<D, Vec<Real, 3>>::add(
-              dV_dI[i], dTdV[2] * dV.dI.d_lkbridge);
+              dV_dI[stack][i], dTdV[stack][2] * dV.dI.d_lkbridge);
           common::accumulate<D, Vec<Real, 3>>::add(
-              dV_dJ[j], dTdV[2] * dV.dJ.d_lkbridge);
+              dV_dJ[stack][j], dTdV[stack][2] * dV.dJ.d_lkbridge);
           common::accumulate<D, Vec<Real, 3>>::add(
-              dV_dI[i], dTdV[3] * dV.dI.d_lkbridge_uncpl);
+              dV_dI[stack][i], dTdV[stack][3] * dV.dI.d_lkbridge_uncpl);
           common::accumulate<D, Vec<Real, 3>>::add(
-              dV_dJ[j], dTdV[3] * dV.dJ.d_lkbridge_uncpl);
+              dV_dJ[stack][j], dTdV[stack][3] * dV.dJ.d_lkbridge_uncpl);
 
           for (int wi = 0; wi < 4; wi++) {
             common::accumulate<D, Vec<Real, 3>>::add(
-                dW_dI[i][wi], dTdV[0] * dV.dWI.d_lkball_iso.row(wi));
+                dW_dI[stack][i][wi], dTdV[stack][0] * dV.dWI.d_lkball_iso.row(wi));
             common::accumulate<D, Vec<Real, 3>>::add(
-                dW_dJ[j][wi], dTdV[0] * dV.dWJ.d_lkball_iso.row(wi));
+                dW_dJ[stack][j][wi], dTdV[stack][0] * dV.dWJ.d_lkball_iso.row(wi));
             common::accumulate<D, Vec<Real, 3>>::add(
-                dW_dI[i][wi], dTdV[1] * dV.dWI.d_lkball.row(wi));
+                dW_dI[stack][i][wi], dTdV[stack][1] * dV.dWI.d_lkball.row(wi));
             common::accumulate<D, Vec<Real, 3>>::add(
-                dW_dJ[j][wi], dTdV[1] * dV.dWJ.d_lkball.row(wi));
+                dW_dJ[stack][j][wi], dTdV[stack][1] * dV.dWJ.d_lkball.row(wi));
             common::accumulate<D, Vec<Real, 3>>::add(
-                dW_dI[i][wi], dTdV[2] * dV.dWI.d_lkbridge.row(wi));
+                dW_dI[stack][i][wi], dTdV[stack][2] * dV.dWI.d_lkbridge.row(wi));
             common::accumulate<D, Vec<Real, 3>>::add(
-                dW_dJ[j][wi], dTdV[2] * dV.dWJ.d_lkbridge.row(wi));
+                dW_dJ[stack][j][wi], dTdV[stack][2] * dV.dWJ.d_lkbridge.row(wi));
             common::accumulate<D, Vec<Real, 3>>::add(
-                dW_dI[i][wi], dTdV[3] * dV.dWI.d_lkbridge_uncpl.row(wi));
+                dW_dI[stack][i][wi], dTdV[stack][3] * dV.dWI.d_lkbridge_uncpl.row(wi));
             common::accumulate<D, Vec<Real, 3>>::add(
-                dW_dJ[j][wi], dTdV[3] * dV.dWJ.d_lkbridge_uncpl.row(wi));
+                dW_dJ[stack][j][wi], dTdV[stack][3] * dV.dWJ.d_lkbridge_uncpl.row(wi));
           }
         });
     nvtx_range_pop();
