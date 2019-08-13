d0e9b0eeb9c30f771ba8b83c7f4615895eeb4740
Andrew Leaver-Fay <fay@digs>
2019-08-13 14:19:47 -0700

    More for-loop removal

--- tmol/score/cartbonded/params.py
+++ tmol/score/cartbonded/params.py
@@ -66,31 +66,18 @@ class CartBondedParamResolver(ValidateAttrs):
         assert resnames.shape[1] == atm1s.shape[1]
         assert resnames.shape[1] == atm2s.shape[1]
 
-        nstacks = resnames.shape[0]
+        inds = numpy.full((resnames.shape[0], resnames.shape[1]), -9999, dtype=numpy.int64)
+        real = resnames.astype(bool)
+        inds[real] = self.bondlength_index.get_indexer([resnames[real], atm1s[real], atm2s[real]])
+        inds[inds == -1] = self.bondlength_index.get_indexer([resnames[inds==-1], atm2s[inds==-1], atm1s[inds==-1]])
+        wildcard = numpy.full_like(resnames, "_")
+        inds[inds == -1] = self.bondlength_index.get_indexer(
+            [wildcard[inds == -1], atm1s[inds == -1], atm2s[inds == -1]])
+        inds[inds == -1] = self.bondlength_index.get_indexer(
+            [wildcard[inds == -1], atm2s[inds == -1], atm1s[inds == -1]])
+        inds[inds == -9999] = -1
 
-        i = numpy.full((resnames.shape[0], resnames.shape[1]), -9999, dtype=numpy.int64)
-        for stack in range(nstacks):
-            nreal = sum(resnames[stack].astype(bool))
-            real = (stack, slice(0, nreal))
-            # slice i, resnames, atm1s, and atm2s
-            ist, resnamest, atm1st, atm2st = (
-                i[real],
-                resnames[real],
-                atm1s[real],
-                atm2s[real],
-            )
-            ist[:] = self.bondlength_index.get_indexer([resnamest, atm1st, atm2st])
-            ist[ist == -1] = self.bondlength_index.get_indexer(
-                [resnamest[ist == -1], atm2st[ist == -1], atm1st[ist == -1]]
-            )
-            wildcard = numpy.full_like(resnamest, "_")
-            ist[ist == -1] = self.bondlength_index.get_indexer(
-                [wildcard[ist == -1], atm1st[ist == -1], atm2st[ist == -1]]
-            )
-            ist[ist == -1] = self.bondlength_index.get_indexer(
-                [wildcard[ist == -1], atm2st[ist == -1], atm1st[ist == -1]]
-            )
-        return i
+        return inds
 
     @validate_args
     def resolve_angles(
@@ -101,48 +88,38 @@ class CartBondedParamResolver(ValidateAttrs):
         atm3s: NDArray(object)[:, :],
     ) -> NDArray("i8")[...]:
         """Resolve string quads into integer bondangle parameter indices."""
-        nstacks = resnames.shape[0]
-        i = numpy.full((resnames.shape[0], resnames.shape[1]), -9999, dtype=numpy.int64)
-        for stack in range(nstacks):
-            nreal = sum(resnames[stack].astype(bool))
-            real = (stack, slice(nreal))
-            # slice i, resnames, atm1s, atm2s, and atm3s
-            ist, resnamest, atm1st, atm2st, atm3st = (
-                i[real],
-                resnames[real],
-                atm1s[real],
-                atm2s[real],
-                atm3s[real],
-            )
-            ist[:] = self.bondangle_index.get_indexer(
-                [resnamest, atm1st, atm2st, atm3st]
-            )
-            ist[ist == -1] = self.bondangle_index.get_indexer(
-                [
-                    resnamest[ist == -1],
-                    atm3st[ist == -1],
-                    atm2st[ist == -1],
-                    atm1st[ist == -1],
-                ]
-            )
-            wildcard = numpy.full_like(resnamest, "_")
-            ist[ist == -1] = self.bondangle_index.get_indexer(
-                [
-                    wildcard[ist == -1],
-                    atm1st[ist == -1],
-                    atm2st[ist == -1],
-                    atm3st[ist == -1],
-                ]
-            )
-            ist[ist == -1] = self.bondangle_index.get_indexer(
-                [
-                    wildcard[ist == -1],
-                    atm3st[ist == -1],
-                    atm2st[ist == -1],
-                    atm1st[ist == -1],
-                ]
-            )
-        return i
+
+        inds = numpy.full(resnames.shape[0:2], -9999, dtype=numpy.int64)
+        real = resnames.astype(bool)
+        inds[real] = self.bondangle_index.get_indexer([resnames[real], atm1s[real], atm2s[real], atm3s[real]])
+        inds[inds == -1] = self.bondangle_index.get_indexer(
+            [
+                resnames[inds == -1],
+                atm3s[inds == -1],
+                atm2s[inds == -1],
+                atm1s[inds == -1],
+            ]
+        )
+        wildcard = numpy.full_like(resnames, "_")
+        inds[inds == -1] = self.bondangle_index.get_indexer(
+            [
+                wildcard[inds == -1],
+                atm1s[inds == -1],
+                atm2s[inds == -1],
+                atm3s[inds == -1],
+            ]
+        )
+        inds[inds == -1] = self.bondangle_index.get_indexer(
+            [
+                wildcard[inds == -1],
+                atm3s[inds == -1],
+                atm2s[inds == -1],
+                atm1s[inds == -1],
+            ]
+        )
+
+        inds[inds == -9999] = -1
+        return inds
 
     @validate_args
     def resolve_torsions(
@@ -154,52 +131,43 @@ class CartBondedParamResolver(ValidateAttrs):
         atm4s: NDArray(object)[:, :],
     ) -> NDArray("i8")[...]:
         """Resolve string quints into integer torsion parameter indices."""
-        nstacks = resnames.shape[0]
-        i = numpy.full_like(resnames, -9999, dtype=numpy.int64)
-        for stack in range(nstacks):
-            nreal = numpy.sum(resnames[stack].astype(bool))
-            real = (stack, slice(nreal))
-            ist, resnamest, atm1st, atm2st, atm3st, atm4st = (
-                i[real],
-                resnames[real],
-                atm1s[real],
-                atm2s[real],
-                atm3s[real],
-                atm4s[real],
-            )
 
-            ist[:] = self.torsion_index.get_indexer(
-                [resnames[real], atm1s[real], atm2s[real], atm3s[real], atm4s[real]]
-            )
-            ist[ist == -1] = self.torsion_index.get_indexer(
-                [
-                    resnamest[ist == -1],
-                    atm4st[ist == -1],
-                    atm3st[ist == -1],
-                    atm2st[ist == -1],
-                    atm1st[ist == -1],
-                ]
-            )
-            wildcard = numpy.full_like(resnamest, "_")
-            ist[ist == -1] = self.torsion_index.get_indexer(
-                [
-                    wildcard[ist == -1],
-                    atm1st[ist == -1],
-                    atm2st[ist == -1],
-                    atm3st[ist == -1],
-                    atm4st[ist == -1],
-                ]
-            )
-            ist[ist == -1] = self.torsion_index.get_indexer(
-                [
-                    wildcard[ist == -1],
-                    atm4st[ist == -1],
-                    atm3st[ist == -1],
-                    atm2st[ist == -1],
-                    atm1st[ist == -1],
-                ]
-            )
-        return i
+        inds = numpy.full_like(resnames, -9999, dtype=numpy.int64)
+        real = resnames.astype(bool)
+        inds[real] = self.torsion_index.get_indexer(
+            [resnames[real], atm1s[real], atm2s[real], atm3s[real], atm4s[real]]
+        )
+        inds[inds == -1] = self.torsion_index.get_indexer(
+            [
+                resnames[inds == -1],
+                atm4s[inds == -1],
+                atm3s[inds == -1],
+                atm2s[inds == -1],
+                atm1s[inds == -1],
+            ]
+        )
+        wildcard = numpy.full_like(resnames, "_")
+        inds[inds == -1] = self.torsion_index.get_indexer(
+            [
+                wildcard[inds == -1],
+                atm1s[inds == -1],
+                atm2s[inds == -1],
+                atm3s[inds == -1],
+                atm4s[inds == -1],
+            ]
+        )
+        inds[inds == -1] = self.torsion_index.get_indexer(
+            [
+                wildcard[inds == -1],
+                atm4s[inds == -1],
+                atm3s[inds == -1],
+                atm2s[inds == -1],
+                atm1s[inds == -1],
+            ]
+        )
+
+        inds[inds == -9999] = -1
+        return inds
 
     @validate_args
     def resolve_impropers(
@@ -212,34 +180,26 @@ class CartBondedParamResolver(ValidateAttrs):
     ) -> NDArray("i8")[...]:
         """Resolve string quints into integer improper torsion parameter indices."""
         # impropers have a defined ordering
-        nstacks = resnames.shape[0]
-        i = numpy.full_like(resnames, -9999, dtype=numpy.int64)
-        for stack in range(nstacks):
-            nreal = numpy.sum(resnames[stack].astype(bool))
-            real = (stack, slice(nreal))
-            ist, resnamest, atm1st, atm2st, atm3st, atm4st = (
-                i[real],
-                resnames[real],
-                atm1s[real],
-                atm2s[real],
-                atm3s[real],
-                atm4s[real],
-            )
 
-            ist[:] = self.improper_index.get_indexer(
-                [resnamest, atm1st, atm2st, atm3st, atm4st]
-            )
-            wildcard = numpy.full_like(resnamest, "_")
-            ist[ist == -1] = self.improper_index.get_indexer(
-                [
-                    wildcard[ist == -1],
-                    atm1st[ist == -1],
-                    atm2st[ist == -1],
-                    atm3st[ist == -1],
-                    atm4st[ist == -1],
-                ]
-            )
-        return i
+        inds = numpy.full_like(resnames, -9999, dtype=numpy.int64)
+        real = resnames.astype(bool)
+
+        inds[real] = self.improper_index.get_indexer(
+            [resnames[real], atm1s[real], atm2s[real], atm3s[real], atm4s[real]]
+        )
+        wildcard = numpy.full_like(resnames, "_")
+        inds[inds == -1] = self.improper_index.get_indexer(
+            [
+                wildcard[inds == -1],
+                atm1s[inds == -1],
+                atm2s[inds == -1],
+                atm3s[inds == -1],
+                atm4s[inds == -1],
+            ]
+        )
+
+        inds[inds == -9999] = -1
+        return inds
 
     @validate_args
     def resolve_hxltorsions(
@@ -251,51 +211,42 @@ class CartBondedParamResolver(ValidateAttrs):
         atm4s: NDArray(object),
     ) -> NDArray("i8")[...]:
         """Resolve string quints into integer hydroxyl torsion parameter indices."""
-        nstacks = resnames.shape[0]
-        i = numpy.full_like(resnames, -9999, dtype=numpy.int64)
-        for stack in range(nstacks):
-            nreal = numpy.sum(resnames[stack].astype(bool))
-            real = (stack, slice(nreal))
-            ist, resnamest, atm1st, atm2st, atm3st, atm4st = (
-                i[real],
-                resnames[real],
-                atm1s[real],
-                atm2s[real],
-                atm3s[real],
-                atm4s[real],
-            )
-            ist[:] = self.hxltorsion_index.get_indexer(
-                [resnamest, atm1st, atm2st, atm3st, atm4st]
-            )
-            ist[ist == -1] = self.hxltorsion_index.get_indexer(
-                [
-                    resnamest[ist == -1],
-                    atm4st[ist == -1],
-                    atm3st[ist == -1],
-                    atm2st[ist == -1],
-                    atm1st[ist == -1],
-                ]
-            )
-            wildcard = numpy.full_like(resnamest, "_")
-            ist[ist == -1] = self.hxltorsion_index.get_indexer(
-                [
-                    wildcard[ist == -1],
-                    atm1st[ist == -1],
-                    atm2st[ist == -1],
-                    atm3st[ist == -1],
-                    atm4st[ist == -1],
-                ]
-            )
-            ist[ist == -1] = self.hxltorsion_index.get_indexer(
-                [
-                    wildcard[ist == -1],
-                    atm4st[ist == -1],
-                    atm3st[ist == -1],
-                    atm2st[ist == -1],
-                    atm1st[ist == -1],
-                ]
-            )
-        return i
+        inds = numpy.full_like(resnames, -9999, dtype=numpy.int64)
+        real = resnames.astype(bool)
+        inds[real] = self.hxltorsion_index.get_indexer(
+            [resnames[real], atm1s[real], atm2s[real], atm3s[real], atm4s[real]]
+        )
+        inds[inds == -1] = self.hxltorsion_index.get_indexer(
+            [
+                resnames[inds == -1],
+                atm4s[inds == -1],
+                atm3s[inds == -1],
+                atm2s[inds == -1],
+                atm1s[inds == -1],
+            ]
+        )
+        wildcard = numpy.full_like(resnames, "_")
+        inds[inds == -1] = self.hxltorsion_index.get_indexer(
+            [
+                wildcard[inds == -1],
+                atm1s[inds == -1],
+                atm2s[inds == -1],
+                atm3s[inds == -1],
+                atm4s[inds == -1],
+            ]
+        )
+        inds[inds == -1] = self.hxltorsion_index.get_indexer(
+            [
+                wildcard[inds == -1],
+                atm4s[inds == -1],
+                atm3s[inds == -1],
+                atm2s[inds == -1],
+                atm1s[inds == -1],
+            ]
+        )
+
+        inds[inds == -9999] = -1
+        return inds
 
     @classmethod
     @validate_args
