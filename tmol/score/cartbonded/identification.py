d78cf377b648fcf9441bad936925ed166afb6678
Andrew Leaver-Fay <fay@digs>
2019-08-13 12:06:09 -0700

    replace -9999 as a sentinel for jagged padding with -1
    I could have sworn I had seen Alex use -9999 in a bunch of
    places, so I started adopting it here, but going back through
    the code now, I see it in only one place. Huh.

--- tmol/score/cartbonded/identification.py
+++ tmol/score/cartbonded/identification.py
@@ -17,7 +17,7 @@ def find_angles(bonds, bond_spans):
     nstacks = bonds.shape[0]
     nbonds = bonds.shape[1]
     max_angles = int(nbonds * 3 / 2)  # assumes each node has at most 4 connections
-    angles = numpy.full((nstacks, max_angles, 3), -9999, dtype=numpy.int64)
+    angles = numpy.full((nstacks, max_angles, 3), -1, dtype=numpy.int64)
 
     # use atm1:atm3 ordering to ensure no duplication
     nangles_list = numpy.zeros((nstacks,), dtype=numpy.int64)
@@ -44,7 +44,7 @@ def find_torsions(bonds, bond_spans):
     nstacks = bonds.shape[0]
     nbonds = bonds.shape[1]
     max_torsions = int(nbonds * 9 / 2)  # assumes each node has at most 4 connections
-    torsions = numpy.full((nstacks, max_torsions, 4), -9999, dtype=numpy.int64)
+    torsions = numpy.full((nstacks, max_torsions, 4), -1, dtype=numpy.int64)
 
     # use atm0:atm3 ordering to ensure no duplication
     # note: cycles like A-B-C-A are valid!
@@ -88,7 +88,7 @@ def find_impropers(bonds, bond_spans):
     nstacks = bonds.shape[0]
     nbonds = bonds.shape[1]
     max_impropers = nbonds * 6  # assumes each node has at most 4 connections
-    improper = numpy.full((nstacks, max_impropers, 4), -9999, dtype=numpy.int64)
+    improper = numpy.full((nstacks, max_impropers, 4), -1, dtype=numpy.int64)
 
     # note: ABCD and BACD are considered separate terms
     nimproper_list = numpy.zeros((nstacks,), dtype=numpy.int64)
@@ -138,7 +138,7 @@ class CartBondedIdentification:
             bonds[i, :, 0] < bonds[i, :, 1] for i in range(bonds.shape[0])
         ]
         max_bonds = max(numpy.sum(selected) for selected in selected_bonds)
-        lengths = numpy.full((bonds.shape[0], max_bonds, 2), -9999, dtype=int)
+        lengths = numpy.full((bonds.shape[0], max_bonds, 2), -1, dtype=int)
         for i in range(bonds.shape[0]):
             ireal = numpy.sum(selected_bonds[i])
             lengths[i, :ireal, :] = bonds[i, selected_bonds[i], :]
