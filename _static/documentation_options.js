f816aedbdfbb8196cc10c62febb8a465c7d08e5c
Andrew Leaver-Fay <aleaverfay@gmail.com>
2019-08-15 21:43:07 +0000

    buildkite/bin/update-gh-pages at 0.0.0-184-g8079577 (HEAD)
    source: 807957732a882a19ed24aa612718d9ced1ae33dc

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.0-183-g31b3188a',
+    VERSION: '0.0.0-184-g80795773',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
