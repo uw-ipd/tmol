892634b50578c6af92e5645f6992b6ccd2c8d849
Andrew Leaver-Fay <fay@digs>
2019-08-16 11:19:07 -0700

    remove for loops over stacks from bonded_atom.py

--- tmol/score/bonded_atom.py
+++ tmol/score/bonded_atom.py
@@ -24,9 +24,12 @@ from .device import TorchDevice
 
 @attr.s(auto_attribs=True, frozen=True, slots=True)
 class IndexedBonds:
+    # bonds = [ nstacks x nbonds x 2 ]
+    #   with 2 = (atom1ind atom2ind)
+    # bond_spans = [ nstacks x max-natoms x 2 ]
+    #   with 2 = (bond start ind, bond end ind + 1)
     bonds: Tensor(int)[:, :, 2]
     bond_spans: Tensor(int)[:, :, 2]
-    # src_index: Tensor(int)[:]
 
     @classmethod
     def from_bonds(cls, src_bonds, minlength=None):
@@ -40,30 +43,46 @@ class IndexedBonds:
         if not minlength:
             minlength = numpy.max(uniq_bonds[:, 1:]) + 1
 
-        bond_spans = numpy.empty((nstacks, minlength, 2), dtype=int)
-        max_nbonds = max(
-            numpy.sum(uniq_bonds[:, 0] == stack) for stack in range(nstacks)
+        bond_counts_per_stack = numpy.bincount(
+            uniq_bonds[:, 0], minlength=nstacks
+        ).reshape((nstacks, 1))
+        max_nbonds = numpy.max(bond_counts_per_stack)
+
+        bonds = numpy.full((nstacks, max_nbonds, 2), -1, dtype=int)
+        counts = numpy.arange(max_nbonds, dtype=int).reshape((1, max_nbonds))
+        lowinds = counts < bond_counts_per_stack
+        nz = numpy.nonzero(lowinds)
+        bonds[nz[0], nz[1], :] = uniq_bonds[:, 1:]
+
+        bond_spans = numpy.full((nstacks, minlength, 2), -1, dtype=int)
+
+        # bond spans: the starting and stoping index for every bond for an atom;
+        # created using the cumsum function after counting how many bonds each
+        # atom has, using the bincount function.
+        #
+        # To use the bincount function, we need to ravel the 1st-atom-in-bond
+        # array, and we need to assign atoms in separate stacks to diferent
+        # integers (which is what max_atom_offsets is for). We also need to
+        # set all of the negative bond inds to an out-of-range value (like
+        # (maxats+1)*nstacks -1) and then, after performing the bincount,
+        # slice off the part of the array that carries its counts.
+
+        max_atom_offsets = numpy.arange(nstacks, dtype=int).reshape((nstacks, 1)) * (
+            minlength + 1
         )
-        bonds = numpy.full((nstacks, max_nbonds, 2), -9999, dtype=int)
-
-        for stack in range(nstacks):
-            stack_bonds = uniq_bonds[uniq_bonds[:, 0] == stack]
-            bonds[stack, : stack_bonds.shape[0]] = stack_bonds[:, 1:]
-
-            # Generate [start_idx, end_idx) spans for contiguous [(i, j_n)...]
-            # blocks in the sorted bond table indexed by i
-            num_bonds = numpy.cumsum(
-                numpy.bincount(stack_bonds[:, 1], minlength=minlength)
-            )
-
-            bond_spans[stack, 0, 0] = 0
-            bond_spans[stack, 1 : num_bonds.shape[0], 0] = num_bonds[:-1]
-            bond_spans[stack, 0 : num_bonds.shape[0], 1] = num_bonds
+        first_at_shifted = numpy.copy(bonds[:, :, 0])
+        first_at_shifted[first_at_shifted < 0] = (minlength + 1) * nstacks - 1
+        first_at_shifted = (first_at_shifted + max_atom_offsets).ravel()
+        bincounts = numpy.bincount(
+            first_at_shifted, minlength=((minlength + 1) * nstacks)
+        ).reshape(nstacks, -1)[:, :minlength]
+        bonds_cumsum = numpy.cumsum(bincounts, axis=1)
+        bond_spans[:, 0, 0] = 0
+        bond_spans[:, 1:minlength, 0] = bonds_cumsum[:, :-1]
+        bond_spans[:, 0:minlength, 1] = bonds_cumsum
 
         return cls(
-            bonds=torch.from_numpy(bonds),
-            bond_spans=torch.from_numpy(bond_spans),
-            # src_index=torch.from_numpy(src_index),
+            bonds=torch.from_numpy(bonds), bond_spans=torch.from_numpy(bond_spans)
         )
 
     @classmethod
