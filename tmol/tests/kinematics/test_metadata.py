53772f41a18c52c8c24c92775d69f732cc6af449
Andrew Leaver-Fay <aleaverfay@gmail.com>
2019-11-21 08:06:57 -0800

    Fix a few unit tests + cast PackedResidueSystem datamembers to int in ctor

--- tmol/tests/kinematics/test_metadata.py
+++ tmol/tests/kinematics/test_metadata.py
@@ -9,7 +9,7 @@ from tmol.system.kinematics import KinematicDescription
 
 def test_metadata_dataframe_smoke(ubq_system):
     tsys = ubq_system
-    tkin = KinematicDescription.for_system(tsys.bonds, tsys.torsion_metadata)
+    tkin = KinematicDescription.for_system(tsys.system_size, tsys.bonds, (tsys.torsion_metadata,))
 
     restored = DOFMetadata.from_frame(tkin.dof_metadata.to_frame())
 
