7a19e2b298f1700a4fb5dfb79c9320bc9c1c81a9
Andrew Leaver-Fay <aleaverfay@gmail.com>
2020-01-17 11:26:40 -0800

    compile more parts; cpu runs, but segfault in cuda simA

--- tmol/pack/simulated_annealing.py
+++ tmol/pack/simulated_annealing.py
@@ -11,8 +11,11 @@ def run_simulated_annealing(energy_tables: PackerEnergyTables,):
         energy_tables.nrotamers_for_res,
         energy_tables.oneb_offsets,
         energy_tables.res_for_rot,
-        energy_tables.nenergies,
+        energy_tables.respair_nenergies,
+        energy_tables.chunk_size,
+        energy_tables.chunk_offset_offsets,
         energy_tables.twob_offsets,
+        energy_tables.fine_chunk_offsets,
         energy_tables.energy1b,
         energy_tables.energy2b,
     )
