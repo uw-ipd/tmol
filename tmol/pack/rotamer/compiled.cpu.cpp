8fd230f405f1313253d62388c24be7aa13fd9fed
Andrew Leaver-Fay <aleaverfay@gmail.com>
2020-05-06 10:40:33 -0400

    Changing parameter list for chi sampler

--- tmol/pack/rotamer/compiled.cpu.cpp
+++ tmol/pack/rotamer/compiled.cpu.cpp
@@ -22,14 +22,9 @@ template <tmol::Device D, typename Real, typename Int>
 struct DunbrackChiSampler {
   static auto f(
       TView<Vec<Real, 3>, 1, D> coords,
-      TView<int64_t, 1, D> res_coord_start_ind,
+      //TView<int64_t, 1, D> res_coord_start_ind,
 
       TView<Vec<int64_t, 2>, 1, D> rottable_set_for_res, // pos 0: seqpos, pos 1: table ind
-
-      TView<Int, 1, D> ndihe_for_res,               // nres x 1
-      TView<Int, 1, D> dihedral_offset_for_res,     // nres x 1
-      TView<Vec<Int, 4>, 1, D> dihedral_atom_inds,  // ndihe x 4
-
 
       TView<Real, 3, D> rotameric_prob_tables,
       TView<Vec<int64_t, 2>, 1, D> rotprob_table_sizes,
@@ -38,16 +33,9 @@ struct DunbrackChiSampler {
       TView<Real, 3, D> rotameric_sdev_tables,
       TView<Vec<int64_t, 2>, 1, D> rotmean_table_sizes,
       TView<Vec<int64_t, 2>, 1, D> rotmean_table_strides,
-
-      TView<int64_t, 1, D> n_rotamers_for_tableset,
-      TView<Int, 1, D> n_rotamers_for_tableset_offsets,
-      TView<int64_t, 3, D> sorted_rotamer_2_rotamer,
-
-
       TView<Vec<Real, 2>, 1, D> rotameric_bb_start,        // ntable-set entries
       TView<Vec<Real, 2>, 1, D> rotameric_bb_step,         // ntable-set entries
       TView<Vec<Real, 2>, 1, D> rotameric_bb_periodicity,  // ntable-set entries
-
       TView<Real, 4, D> semirotameric_tables,              // n-semirot-tabset
       TView<Vec<int64_t, 3>, 1, D> semirot_table_sizes,    // n-semirot-tabset
       TView<Vec<int64_t, 3>, 1, D> semirot_table_strides,  // n-semirot-tabset
@@ -57,6 +45,9 @@ struct DunbrackChiSampler {
       TView<Int, 1, D> rotameric_rotind2tableind,
       TView<Int, 1, D> semirotameric_rotind2tableind,
 
+      TView<Int, 1, D> ndihe_for_res,               // nres x 1
+      TView<Int, 1, D> dihedral_offset_for_res,     // nres x 1
+      TView<Vec<Int, 4>, 1, D> dihedral_atom_inds,  // ndihe x 4
 
       TView<Int, 2, D> rottable_set_for_buildable_restype,  // n-buildable-restypes x 2
       TView<Int, 2, D> chi_expansion_for_buildable_restypes,
@@ -64,18 +55,18 @@ struct DunbrackChiSampler {
       TView<Int, 3, D> non_dunbrack_expansion_counts,
       TView<Real, 1, D> prob_cumsum_limit_for_buildable_restype,
 
-      TView<Int, 1, D> nrotameric_chi_for_res,            // nres x 1
-      TView<Int, 1, D> rotres2resid,                      // nres x 1
-      TView<Int, 1, D> prob_table_offset_for_rotresidue,  // n-rotameric-res x 1
-      TView<Int, 1, D> rotind2tableind_offset_for_res,    // n-res x 1
+      // ?? TView<Int, 1, D> nrotameric_chi_for_res,            // nres x 1
+      // ?? TView<Int, 1, D> rotres2resid,                      // nres x 1
+      // ?? TView<Int, 1, D> prob_table_offset_for_rotresidue,  // n-rotameric-res x 1
+      // ?? TView<Int, 1, D> rotind2tableind_offset_for_res,    // n-res x 1
 
-      TView<Int, 1, D> rotmean_table_offset_for_residue,  // n-res x 1
+      // ?? TView<Int, 1, D> rotmean_table_offset_for_residue,  // n-res x 1
 
-      TView<Int, 2, D> rotameric_chi_desc,  // n-rotameric-chi x 2
+      // ?? TView<Int, 2, D> rotameric_chi_desc,  // n-rotameric-chi x 2
       // rotchi_desc[:,0] == residue index for this chi
       // rotchi_desc[:,1] == chi_dihedral_index for res
 
-      TView<Int, 2, D> semirotameric_chi_desc,  // n-semirotameric-residues x 4
+      // ?? TView<Int, 2, D> semirotameric_chi_desc,  // n-semirotameric-residues x 4
       // semirotchi_desc[:,0] == residue index
       // semirotchi_desc[:,1] == semirotchi_dihedral_index res
       // semirotchi_desc[:,2] == semirot_table_offset
@@ -83,19 +74,29 @@ struct DunbrackChiSampler {
 
       // scratch space, perhaps does not belong as an input parameter?
       TView<Real, 1, D> dihedrals,                        // ndihe x 1
-      TView<Eigen::Matrix<Real, 4, 3>, 1, D> ddihe_dxyz,  // ndihe x 3
+      // ?? TView<Eigen::Matrix<Real, 4, 3>, 1, D> ddihe_dxyz,  // ndihe x 3
       // TView<Real, 1, D> rotchi_devpen,                    // n-rotameric-chi
       // x 1 TView<Real, 2, D> ddevpen_dbb,  // Where d chimean/d dbbdihe is
       //                                // stored, nscdihe x 2
-      TView<Int, 1, D> rotameric_rottable_assignment,     // nres x 1
-      TView<Int, 1, D> semirotameric_rottable_assignment  // nres x 1
-      )
+      // ?? TView<Int, 1, D> rotameric_rottable_assignment,     // nres x 1
+      // ?? TView<Int, 1, D> semirotameric_rottable_assignment  // nres x 1
+
+      // ?? TView<int64_t, 1, D> n_rotamers_for_tableset,
+      // ?? TView<Int, 1, D> n_rotamers_for_tableset_offsets,
+      // ?? TView<int64_t, 3, D> sorted_rotamer_2_rotamer,
+
+
+
+  )
       -> std::tuple<
           TPack<Real, 1, D>,
           TPack<Real, 1, D> >  // d(-ln(prob_nonrotameric)) / dtor atoms
   {
     auto rval1 = TPack<Real, 1, D>::zeros({5});
     auto rval2 = TPack<Real, 1, D>::zeros({5});
+
+    std::cout << "made it!" << std::endl;
+
     return {rval1, rval2};
 
     // construct the list of chi for the rotamers that should be built
--- tmol/pack/rotamer/compiled.h
+++ tmol/pack/rotamer/compiled.h
@@ -22,17 +22,12 @@ template <tmol::Device D, typename Real, typename Int>
 struct DunbrackChiSampler {
   static auto f(
       TView<Vec<Real, 3>, 1, D> coords,
-      TView<int64_t, 1, D> res_coord_start_ind,
+      // TView<int64_t, 1, D> res_coord_start_ind,
 
       TView<Vec<int64_t, 2>, 1, D>
           rottable_set_for_res,  // pos 0: seqpos, pos 1: table ind
 
-      TView<Int, 1, D> ndihe_for_res,               // nres x 1
-      TView<Int, 1, D> dihedral_offset_for_res,     // nres x 1
-      TView<Vec<Int, 4>, 1, D> dihedral_atom_inds,  // ndihe x 4
-
       TView<Real, 3, D> rotameric_prob_tables,
-      TView<Real, 3, D> rotameric_neglnprob_tables,
       TView<Vec<int64_t, 2>, 1, D> rotprob_table_sizes,
       TView<Vec<int64_t, 2>, 1, D> rotprob_table_strides,
       TView<Real, 3, D> rotameric_mean_tables,
@@ -51,37 +46,50 @@ struct DunbrackChiSampler {
       TView<Int, 1, D> rotameric_rotind2tableind,
       TView<Int, 1, D> semirotameric_rotind2tableind,
 
-      TView<Int, 1, D> rottable_set_for_res,              // nres x 1
-      TView<Int, 1, D> nchi_for_res,                      // nres x 1
-      TView<Int, 1, D> nrotameric_chi_for_res,            // nres x 1
-      TView<Int, 1, D> rotres2resid,                      // nres x 1
-      TView<Int, 1, D> prob_table_offset_for_rotresidue,  // n-rotameric-res x 1
-      TView<Int, 1, D> rotind2tableind_offset_for_res,    // n-res x 1
+      TView<Int, 1, D> ndihe_for_res,               // nres x 1
+      TView<Int, 1, D> dihedral_offset_for_res,     // nres x 1
+      TView<Vec<Int, 4>, 1, D> dihedral_atom_inds,  // ndihe x 4
 
-      TView<Int, 1, D> rotmean_table_offset_for_residue,  // n-res x 1
+      TView<Int, 2, D>
+          rottable_set_for_buildable_restype,  // n-buildable-restypes x 2
+      TView<Int, 2, D> chi_expansion_for_buildable_restypes,
+      TView<Real, 3, D> non_dunbrack_expansion_for_buildable_restype,
+      TView<Int, 3, D> non_dunbrack_expansion_counts,
+      TView<Real, 1, D> prob_cumsum_limit_for_buildable_restype,
 
-      TView<Int, 2, D> rotameric_chi_desc,  // n-rotameric-chi x 2
+      // ?? TView<Int, 1, D> nrotameric_chi_for_res,            // nres x 1
+      // ?? TView<Int, 1, D> rotres2resid,                      // nres x 1
+      // ?? TView<Int, 1, D> prob_table_offset_for_rotresidue,  //
+      // n-rotameric-res x 1
+      // ?? TView<Int, 1, D> rotind2tableind_offset_for_res,    // n-res x 1
+
+      // ?? TView<Int, 1, D> rotmean_table_offset_for_residue,  // n-res x 1
+
+      // ?? TView<Int, 2, D> rotameric_chi_desc,  // n-rotameric-chi x 2
       // rotchi_desc[:,0] == residue index for this chi
       // rotchi_desc[:,1] == chi_dihedral_index for res
 
-      TView<Int, 2, D> semirotameric_chi_desc,  // n-semirotameric-residues x 4
+      // ?? TView<Int, 2, D> semirotameric_chi_desc,  //
+      // n-semirotameric-residues x 4
       // semirotchi_desc[:,0] == residue index
       // semirotchi_desc[:,1] == semirotchi_dihedral_index res
       // semirotchi_desc[:,2] == semirot_table_offset
       // semirotchi_desc[:,3] == semirot_table_set (e.g. 0-7)
 
       // scratch space, perhaps does not belong as an input parameter?
-      TView<Real, 1, D> dihedrals,                        // ndihe x 1
-      TView<Eigen::Matrix<Real, 4, 3>, 1, D> ddihe_dxyz,  // ndihe x 3
+      TView<Real, 1, D> dihedrals  // ndihe x 1
+      // ?? TView<Eigen::Matrix<Real, 4, 3>, 1, D> ddihe_dxyz,  // ndihe x 3
       // TView<Real, 1, D> rotchi_devpen,                    // n-rotameric-chi
       // x 1 TView<Real, 2, D> ddevpen_dbb,  // Where d chimean/d dbbdihe is
       //                                // stored, nscdihe x 2
-      TView<Int, 1, D> rotameric_rottable_assignment,     // nres x 1
-      TView<Int, 1, D> semirotameric_rottable_assignment  // nres x 1
-      ) -> std::
-      tuple<TPack<Real, 1, D>, TPack<Real, 1, D> >  // d(-ln(prob_nonrotameric))
-                                                    // / dtor atoms
-      ;
+      // ?? TView<Int, 1, D> rotameric_rottable_assignment,     // nres x 1
+      // ?? TView<Int, 1, D> semirotameric_rottable_assignment  // nres x 1
+
+      // ?? TView<int64_t, 1, D> n_rotamers_for_tableset,
+      // ?? TView<Int, 1, D> n_rotamers_for_tableset_offsets,
+      // ?? TView<int64_t, 3, D> sorted_rotamer_2_rotamer,
+
+      ) -> std::tuple<TPack<Real, 1, D>, TPack<Real, 1, D> >;
 };
 
 #undef CoordQuad
