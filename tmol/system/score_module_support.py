10f24058df5475d24662e5fe67c3d788ca504540
Alex <a.sewall.ford@gmail.com>
2020-07-31 11:28:59 -0700

    Add shim for coords for module forward pass.

--- tmol/system/score_module_support.py
+++ tmol/system/score_module_support.py
@@ -1,9 +1,12 @@
 import numpy
+import torch
 from typing import List
 
 from ..score.modules.bases import ScoreSystem
 from ..score.modules.stacked_system import StackedSystem
 from ..score.modules.bonded_atom import BondedAtoms
+from ..score.modules.device import TorchDevice
+from ..score.modules.coords import coords_for
 
 from .packed import PackedResidueSystem, PackedResidueSystemStack
 
@@ -100,3 +103,56 @@ def stacked_bonded_atoms_for_system(
         res_indices=stackem("res_indices"),
         res_names=stackem("res_names"),
     )
+
+
+@coords_for.register(PackedResidueSystem)
+def coords_for_system(
+    system: PackedResidueSystem,
+    score_system: ScoreSystem,
+    *,
+    requires_grad: bool = True,
+):
+
+    stack_params = StackedSystem.get(score_system)
+    device = TorchDevice.get(score_system).device
+
+    assert stack_params.stack_depth == 1
+    assert stack_params.system_size == len(system.coords)
+
+    coords = torch.tensor(
+        system.coords.reshape(1, len(system.coords), 3),
+        dtype=torch.float,
+        device=device,
+    ).requires_grad_(requires_grad)
+
+    return coords
+
+
+@coords_for.register(PackedResidueSystemStack)
+def coords_for_system_stack(
+    stack: PackedResidueSystemStack,
+    score_system: ScoreSystem,
+    *,
+    requires_grad: bool = True,
+):
+    stack_params = StackedSystem.get(score_system)
+    device = TorchDevice.get(score_system).device
+
+    assert stack_params.stack_depth == len(stack.systems)
+    assert stack_params.system_size == max(
+        int(system.system_size) for system in stack.systems
+    )
+
+    coords = torch.full(
+        (stack_params.stack_depth, stack_params.system_size, 3),
+        numpy.nan,
+        dtype=torch.float,
+        device=device,
+    )
+
+    for i, s in enumerate(stack.systems):
+        coords[i, : s.system_size] = torch.tensor(
+            s.coords, dtype=torch.float, device=device
+        )
+
+    return coords.requires_grad_(requires_grad)
