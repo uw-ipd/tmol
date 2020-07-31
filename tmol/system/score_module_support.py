12f5e644eebf55bdd636ebb33da3594333e329e9
Alex <a.sewall.ford@gmail.com>
2020-07-31 10:12:34 -0700

    Implement module interface for bonded atoms.
    Implement components for device, database, stacked system, and
    bonded atoms support in module-oriented framework. Port existing
    graph-oriented tests into module-oriented framework.

---
+++ tmol/system/score_module_support.py
@@ -0,0 +1,102 @@
+import numpy
+from typing import List
+
+from ..score.modules.bases import ScoreSystem
+from ..score.modules.stacked_system import StackedSystem
+from ..score.modules.bonded_atom import BondedAtoms
+
+from .packed import PackedResidueSystem, PackedResidueSystemStack
+
+
+@StackedSystem.build_for.register(PackedResidueSystem)
+def stack_for_system(
+    system: PackedResidueSystem, score_system: ScoreSystem, **_
+) -> StackedSystem:
+    return StackedSystem(
+        system=score_system, stack_depth=1, system_size=int(system.system_size)
+    )
+
+
+@StackedSystem.build_for.register(PackedResidueSystemStack)
+def stack_for_stacked_system(
+    stack: PackedResidueSystemStack, score_system: ScoreSystem, **_
+) -> StackedSystem:
+    return StackedSystem(
+        system=score_system,
+        stack_depth=len(stack.systems),
+        system_size=max(int(system.system_size) for system in stack.systems),
+    )
+
+
+@BondedAtoms.build_for.register(PackedResidueSystem)
+def bonded_atoms_for_system(
+    system: PackedResidueSystem,
+    score_system: ScoreSystem,
+    *,
+    drop_missing_atoms: bool = False,
+    **_,
+) -> BondedAtoms:
+    bonds = numpy.empty((len(system.bonds), 3), dtype=int)
+    bonds[:, 0] = 0
+    bonds[:, 1:] = system.bonds
+
+    atom_types = system.atom_metadata["atom_type"].copy()[None, :]
+    atom_names = system.atom_metadata["atom_name"].copy()[None, :]
+    res_indices = system.atom_metadata["residue_index"].copy()[None, :]
+    res_names = system.atom_metadata["residue_name"].copy()[None, :]
+
+    if drop_missing_atoms:
+        atom_types[0, numpy.any(numpy.isnan(system.coords), axis=-1)] = None
+
+    return BondedAtoms(
+        system=score_system,
+        bonds=bonds,
+        atom_types=atom_types,
+        atom_names=atom_names,
+        res_indices=res_indices,
+        res_names=res_names,
+    )
+
+
+@BondedAtoms.build_for.register(PackedResidueSystemStack)
+def stacked_bonded_atoms_for_system(
+    stack: PackedResidueSystemStack,
+    system: ScoreSystem,
+    *,
+    drop_missing_atoms: bool = False,
+    **_,
+):
+
+    system_size = StackedSystem.get(system).system_size
+
+    bonds_for_systems: List[BondedAtoms] = [
+        BondedAtoms.get(
+            ScoreSystem._build_with_modules(
+                sys, {BondedAtoms}, drop_missing_atoms=drop_missing_atoms
+            )
+        )
+        for sys in stack.systems
+    ]
+
+    for i, d in enumerate(bonds_for_systems):
+        d.bonds[:, 0] = i
+    bonds = numpy.concatenate(tuple(d.bonds for d in bonds_for_systems))
+
+    def expand_atoms(atdat):
+        atdat2 = numpy.full((1, system_size), None, dtype=object)
+        atdat2[0, : atdat.shape[1]] = atdat
+        return atdat2
+
+    def stackem(key):
+        return numpy.concatenate(
+            [expand_atoms(getattr(d, key)) for d in bonds_for_systems]
+        )
+
+    return BondedAtoms(
+        system=system,
+        bonds=bonds,
+        atom_types=stackem("atom_types"),
+        atom_names=stackem("atom_names"),
+        res_indices=stackem("res_indices"),
+        res_names=stackem("res_names"),
+    )
---
+++ tmol/tests/score/modules/test_bonded_atom.py
@@ -0,0 +1,146 @@
+import pytest
+import numpy
+import torch
+import toolz
+import scipy.sparse.csgraph as csgraph
+
+from tmol.score.modules.bonded_atom import BondedAtoms
+from tmol.score.modules.stacked_system import StackedSystem
+
+from tmol.system.packed import PackedResidueSystem, PackedResidueSystemStack
+
+from tmol.score.modules.bases import ScoreSystem
+
+
+def test_bonded_atom_clone_factory(ubq_system: PackedResidueSystem):
+    src: ScoreSystem = ScoreSystem._build_with_modules(ubq_system, {BondedAtoms})
+
+    # Bond graph is referenced
+    clone: ScoreSystem = ScoreSystem._build_with_modules(src, {BondedAtoms})
+
+    assert BondedAtoms.get(clone).bonds is BondedAtoms.get(src).bonds
+    numpy.testing.assert_allclose(
+        BondedAtoms.get(src).bonds, BondedAtoms.get(clone).bonds
+    )
+    numpy.testing.assert_allclose(
+        BondedAtoms.get(src).bonded_path_length,
+        BondedAtoms.get(clone).bonded_path_length,
+    )
+
+    # Bonded atoms is frozen, can't be modified after init
+    with pytest.raises(AttributeError):
+        BondedAtoms.get(clone).bonds = BondedAtoms.get(clone).bonds[
+            : len(BondedAtoms.get(clone).bonds) // 2
+        ]
+
+
+def test_bonded_atom_clone_factory_from_stacked_systems(
+    ubq_system: PackedResidueSystem
+):
+    twoubq = PackedResidueSystemStack((ubq_system, ubq_system))
+    basg: ScoreSystem = ScoreSystem._build_with_modules(twoubq, {BondedAtoms})
+
+    assert BondedAtoms.get(basg).atom_types.shape == (
+        2,
+        StackedSystem.get(basg).system_size,
+    )
+    assert BondedAtoms.get(basg).atom_names.shape == (
+        2,
+        StackedSystem.get(basg).system_size,
+    )
+    assert BondedAtoms.get(basg).res_names.shape == (
+        2,
+        StackedSystem.get(basg).system_size,
+    )
+    assert BondedAtoms.get(basg).res_indices.shape == (
+        2,
+        StackedSystem.get(basg).system_size,
+    )
+
+
+def test_bonded_path_length(ubq_system: PackedResidueSystem):
+    """Bonded path length is evaluated up to MAX_BONDED_PATH_LENGTH."""
+
+    src: ScoreSystem = ScoreSystem._build_with_modules(ubq_system, {BondedAtoms})
+
+    src_bond_table = numpy.zeros(
+        (StackedSystem.get(src).system_size, StackedSystem.get(src).system_size)
+    )
+    src_bond_table[
+        BondedAtoms.get(src).bonds[:, 1], BondedAtoms.get(src).bonds[:, 2]
+    ] = 1
+    bond_graph = csgraph.csgraph_from_dense(src_bond_table)
+    distance_table = torch.from_numpy(
+        csgraph.shortest_path(bond_graph, directed=False, unweighted=True)
+    ).to(torch.float)
+
+    assert BondedAtoms.get(src).bonded_path_length.shape == (
+        1,
+        StackedSystem.get(src).system_size,
+        StackedSystem.get(src).system_size,
+    )
+    assert numpy.all(
+        BondedAtoms.get(src).bonded_path_length[0][
+            distance_table > BondedAtoms.get(src).MAX_BONDED_PATH_LENGTH
+        ]
+        == numpy.inf
+    )
+    assert numpy.all(
+        BondedAtoms.get(src).bonded_path_length[0][
+            distance_table < BondedAtoms.get(src).MAX_BONDED_PATH_LENGTH
+        ]
+        == distance_table[distance_table < BondedAtoms.get(src).MAX_BONDED_PATH_LENGTH]
+    )
+
+    inds = BondedAtoms.get(src).indexed_bonds
+    assert len(inds.bonds.shape) == 3
+    assert inds.bonds.shape[2] == 2
+
+
+def test_real_atoms(ubq_system: PackedResidueSystem):
+    """``real_atoms`` is set for every residue's atom in an packed residue system."""
+    expected_real_indices = list(
+        toolz.concat(
+            range(i, i + len(r.coords))
+            for i, r in zip(ubq_system.res_start_ind, ubq_system.residues)
+        )
+    )
+
+    src: ScoreSystem = ScoreSystem._build_with_modules(ubq_system, {BondedAtoms})
+
+    assert BondedAtoms.get(src).real_atoms.shape == (
+        1,
+        StackedSystem.get(src).system_size,
+    )
+    assert (
+        list(numpy.flatnonzero(numpy.array(BondedAtoms.get(src).real_atoms)))
+        == expected_real_indices
+    )
+
+
+def test_variable_bonded_path_length(ubq_res):
+    ubq4 = PackedResidueSystem.from_residues(ubq_res[:4])
+    ubq6 = PackedResidueSystem.from_residues(ubq_res[:6])
+    twoubq = PackedResidueSystemStack((ubq4, ubq6))
+
+    basg_both = ScoreSystem._build_with_modules(twoubq, {BondedAtoms})
+    basg4 = ScoreSystem._build_with_modules(ubq4, {BondedAtoms})
+    basg6 = ScoreSystem._build_with_modules(ubq6, {BondedAtoms})
+
+    inds_both = BondedAtoms.get(basg_both).indexed_bonds
+    inds4 = BondedAtoms.get(basg4).indexed_bonds
+    inds6 = BondedAtoms.get(basg6).indexed_bonds
+
+    numpy.testing.assert_allclose(
+        inds_both.bonds[0, : inds4.bonds.shape[1]], inds4.bonds[0]
+    )
+    numpy.testing.assert_allclose(
+        inds_both.bonds[1, : inds6.bonds.shape[1]], inds6.bonds[0]
+    )
+
+    numpy.testing.assert_allclose(
+        inds_both.bond_spans[0, : inds4.bond_spans.shape[1]], inds4.bond_spans[0]
+    )
+    torch.testing.assert_allclose(
+        inds_both.bond_spans[1, : inds6.bond_spans.shape[1]], inds6.bond_spans[0]
+    )
