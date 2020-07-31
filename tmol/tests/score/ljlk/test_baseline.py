01c801af55b5c035f45ff6aa1ed833bb218b2156
Alex <a.sewall.ford@gmail.com>
2020-07-31 11:29:41 -0700

    Implement ljlk score modules.

--- tmol/tests/score/ljlk/test_baseline.py
+++ tmol/tests/score/ljlk/test_baseline.py
@@ -6,6 +6,10 @@ from tmol.score.score_graph import score_graph
 from tmol.score.coordinates import CartesianAtomicCoordinateProvider
 from tmol.score.ljlk import LJScoreGraph, LKScoreGraph
 
+from tmol.score.modules.bases import ScoreSystem
+from tmol.score.modules.ljlk import LJScore, LKScore
+from tmol.score.modules.coords import coords_for
+
 
 @score_graph
 class LJGraph(CartesianAtomicCoordinateProvider, LJScoreGraph):
@@ -17,16 +21,21 @@ class LKGraph(CartesianAtomicCoordinateProvider, LKScoreGraph):
     pass
 
 
-comparisons = {
+graph_comparisons = {
     "lj_regression": (LJGraph, {"total_lj": -177.1}),
     "lk_regression": (LKGraph, {"total_lk": 297.3}),
+}
+
+module_comparisons = {
+    "lj_regression": (LJScore, {"lj": -177.1}),
+    "lk_regression": (LKScore, {"lk": 297.3}),
 }
 
 
 @pytest.mark.parametrize(
     "graph_class,expected_scores",
-    list(comparisons.values()),
-    ids=list(comparisons.keys()),
+    list(graph_comparisons.values()),
+    ids=list(graph_comparisons.keys()),
 )
 def test_baseline_comparison(ubq_system, torch_device, graph_class, expected_scores):
     test_graph = graph_class.build_for(
@@ -39,3 +48,34 @@ def test_baseline_comparison(ubq_system, torch_device, graph_class, expected_sco
     }
 
     assert scores == approx(expected_scores, rel=1e-3)
+
+
+@pytest.mark.parametrize(
+    "score_method,expected_scores",
+    list(module_comparisons.values()),
+    ids=list(module_comparisons.keys()),
+)
+def test_baseline_comparison_modules(
+    ubq_system, torch_device, score_method, expected_scores
+):
+    weight_factor = 10.0
+    weights = {t: weight_factor for t in expected_scores}
+
+    score_system: ScoreSystem = ScoreSystem.build_for(
+        ubq_system,
+        {score_method},
+        weights=weights,
+        device=torch_device,
+        drop_missing_atoms=False,
+    )
+
+    coords = coords_for(ubq_system, score_system, requires_grad=False)
+
+    scores = score_system.intra_forward(coords)
+
+    assert {k: float(v) for k, v in scores.items()} == approx(expected_scores, rel=1e-3)
+
+    total_score = score_system.intra_total(coords)
+    assert float(total_score) == approx(
+        sum(expected_scores.values()) * weight_factor, rel=1e-3
+    )
---
+++ tmol/tests/score/modules/test_ljlk.py
@@ -0,0 +1,74 @@
+import copy
+
+import pytest
+from pytest import approx
+import torch
+
+from tmol.database import ParameterDatabase
+
+from tmol.score.modules.bases import ScoreSystem
+from tmol.score.modules.ljlk import LJScore, LKScore, LJLKParameters
+from tmol.score.modules.coords import coords_for
+
+from tmol.system.packed import PackedResidueSystem, PackedResidueSystemStack
+
+
+@pytest.mark.benchmark(group="score_setup")
+def test_lj_score_setup(benchmark, ubq_system, torch_device):
+    @benchmark
+    def score_graph():
+        return ScoreSystem.build_for(ubq_system, {LJScore}, weights={"lj": 1.0})
+
+    # TODO fordas add test assertions
+
+
+def test_ljlk_database_clone_factory(ubq_system):
+    clone_db = copy.copy(ParameterDatabase.get_default().scoring.ljlk)
+
+    src = ScoreSystem._build_with_modules(ubq_system, {LJLKParameters})
+    assert (
+        LJLKParameters.get(src).ljlk_database
+        is ParameterDatabase.get_default().scoring.ljlk
+    )
+
+    # Parameter database is overridden via kwarg
+    src = ScoreSystem._build_with_modules(
+        ubq_system, {LJLKParameters}, ljlk_database=clone_db
+    )
+    assert LJLKParameters.get(src).ljlk_database is clone_db
+
+    # Parameter database is referenced on clone
+    clone = ScoreSystem._build_with_modules(src, {LJLKParameters})
+    assert (
+        LJLKParameters.get(clone).ljlk_database is LJLKParameters.get(src).ljlk_database
+    )
+
+    # Parameter database is overriden on clone via kwarg
+    clone = ScoreSystem._build_with_modules(
+        src,
+        {LJLKParameters},
+        ljlk_database=ParameterDatabase.get_default().scoring.ljlk,
+    )
+    assert (
+        LJLKParameters.get(clone).ljlk_database
+        is not LJLKParameters.get(src).ljlk_database
+    )
+    assert (
+        LJLKParameters.get(clone).ljlk_database
+        is ParameterDatabase.get_default().scoring.ljlk
+    )
+
+
+def test_lj_for_stacked_system(ubq_system: PackedResidueSystem):
+    twoubq = PackedResidueSystemStack((ubq_system, ubq_system))
+
+    stacked_score = ScoreSystem.build_for(twoubq, {LJScore}, weights={"lj": 1.0})
+
+    coords = coords_for(twoubq, stacked_score)
+
+    tot = stacked_score.intra_total(coords)
+    assert tot.shape == (2,)
+    torch.testing.assert_allclose(tot[0], tot[1])
+
+    sumtot = torch.sum(tot)
+    sumtot.backward()
