dfaf3bb38946b740b23f5dcaba3e0d69aa1e42d5
Alex <a.sewall.ford@gmail.com>
2020-08-11 13:13:26 -0700

    Initial dependency update.
    Update core dependencies:
      python -> 3.7
      torch->1.6
      cuda->10.1
    
    Misc numpy/scipy/pandas updates needed for package builds.
    
    Disable OSX package solve due to invalid solve with pandas 0.25.

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     version=git_version(),
     packages=["tmol"],
     setup_requires=pytest_runner,
-    tests_require=[l.strip() for l in open("requirements.tests.txt").readlines()],
-    install_requires=[l.strip() for l in open("requirements.txt").readlines()],
+    # tests_require=[l.strip() for l in open("requirements.tests.txt").readlines()],
+    # install_requires=[l.strip() for l in open("requirements.txt").readlines()],
     zip_safe=False,
 )
