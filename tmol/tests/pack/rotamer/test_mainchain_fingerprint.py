30014741ffef9753866920fa6d587a4983477964
Andrew Leaver-Fay <aleaverfay@gmail.com>
2020-11-02 05:23:58 -0800

    Add comprehensive mc-fingerprint unit test

--- tmol/tests/pack/rotamer/test_mainchain_fingerprint.py
+++ tmol/tests/pack/rotamer/test_mainchain_fingerprint.py
@@ -160,6 +160,7 @@ def test_merge_fingerprints(default_database):
         "PHE",
         "GLY",
         "HIS",
+        "HIS_D",
         "ILE",
         "LYS",
         "LEU",
@@ -175,7 +176,9 @@ def test_merge_fingerprints(default_database):
         "TYR",
     ]
 
-    rt_list = [rts.restype_map[aa][0] for aa in canonical_aas]
+    rt_list = [
+        next(rt for rt in rts.residue_types if rt.name == aa) for aa in canonical_aas
+    ]
 
     for rt in rt_list:
         construct_single_residue_kintree(rt)
@@ -185,3 +188,82 @@ def test_merge_fingerprints(default_database):
 
     pbt = PackedBlockTypes.from_restype_list(rt_list, device=torch_device)
     find_unique_fingerprints(pbt)
+
+    # we should find that the pbt has been annotated
+    # we should find that it has discovered two backbone types
+    # we should see that, when mapping proline onto a leucine,
+    # that proline has a -1 for the HA atom of leucine's mainchain.
+    # we should see that otherwise the standard set of atoms
+    # map to each other, except for glycine which uses 2HA to map
+    # to HA.
+
+    assert hasattr(pbt, "mc_atom_mapping")
+    assert hasattr(pbt, "mc_builder_mapping")
+    assert hasattr(pbt, "mc_max_builder")
+    assert hasattr(pbt, "mc_max_fingerprint")
+
+    standard_mc_atoms = ["N", "H", "CA", "HA", "C", "O"]
+    glycine_mc_atoms = ["N", "H", "CA", "1HA", "C", "O"]
+
+    standard_mc_atoms_w_pro = ["N", "CA", "HA", "C", "O"]
+    glycine_mc_atoms_w_pro = ["N", "CA", "1HA", "C", "O"]
+
+    def which_atoms(orig_rt, target_rt):
+        if orig_rt.name == "PRO":
+            if target_rt.name == "GLY":
+                return glycine_mc_atoms_w_pro
+            else:
+                return standard_mc_atoms_w_pro
+        else:
+            if target_rt.name == "GLY":
+                return glycine_mc_atoms
+            else:
+                return standard_mc_atoms
+
+    assert dun_sampler.sampler_name() in pbt.mc_builder_mapping
+    dun_sampler_ind = pbt.mc_builder_mapping[dun_sampler.sampler_name()]
+
+    assert fixed_sampler.sampler_name() in pbt.mc_builder_mapping
+    fixed_sampler_ind = pbt.mc_builder_mapping[fixed_sampler.sampler_name()]
+
+    # pro_pbt_ind = pbt.restype_index.get_indexer(["PRO"])[0]
+    # gly_pbt_ind = pbt.restype_index.get_indexer(["GLY"])[0]
+    # leu_pbt_ind = pbt.restype_index.get_indexer(["LEU"])[0]
+    #
+    # assert pro_pbt_ind == dun_sampler_ind
+    # assert gly_pbt_ind == fixed_sampler_ind
+    # assert leu_pbt_ind == dun_sampler_ind
+
+    # print(pro_pbt_ind, gly_pbt_ind, leu_pbt_ind)
+
+    assert pbt.mc_atom_mapping.shape == (2, 2, 21, 6)
+
+    for i, rt_orig in enumerate(pbt.active_residues):
+        orig_rt_builder = pbt.mc_max_builder[i]
+        orig_max_fp = pbt.mc_max_fingerprint[i]
+        orig_mc_ats = which_atoms(rt_orig, rt_orig)
+
+        for j, rt_new in enumerate(pbt.active_residues):
+            new_mc_ats = which_atoms(rt_orig, rt_new)
+            if fixed_sampler.defines_rotamers_for_rt(rt_new):
+                new_rt_builder = fixed_sampler_ind
+            else:
+                new_rt_builder = dun_sampler_ind
+
+            # now the atom mapping:
+            for k in range(6):
+                k_orig = pbt.mc_atom_mapping[orig_rt_builder, orig_max_fp, i, k]
+                k_new = pbt.mc_atom_mapping[new_rt_builder, orig_max_fp, j, k]
+
+                if k_orig >= 0 and k_new >= 0:
+                    assert rt_orig.atoms[k_orig].name == orig_mc_ats[k]
+                    assert rt_new.atoms[k_new].name == new_mc_ats[k]
+                elif k_orig >= 0 and k_new == -1:
+                    assert new_mc_ats[k] not in rt_new.atom_to_idx
+                elif k_orig == -1 and k_new == -1:
+                    # there are simply fewer mc atoms for this fingerprint
+                    # and we have come to the end of the list
+                    assert True
+                else:
+                    # this should never happen
+                    assert k_orig >= 0
