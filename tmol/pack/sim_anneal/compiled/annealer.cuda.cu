81161c78f9dbc08c5dd16cc70ad95e2d650ebd0a
Andrew Leaver-Fay <aleaverfay@gmail.com>
2021-05-14 07:45:00 -0700

    Save end-to-end annealer; unclear how effectively it is working

---
+++ tmol/pack/sim_anneal/compiled/annealer.cuda.cu
@@ -0,0 +1,330 @@
+#include <tmol/utility/tensor/TensorCast.h>
+#include <tmol/pack/sim_anneal/compiled/annealer.hh>
+#include <tmol/score/common/forall_dispatch.cuda.impl.cuh>
+#include <tmol/utility/function_dispatch/aten.hh>
+
+#include <moderngpu/kernel_reduce.hxx>
+
+#include <chrono>
+
+namespace tmol {
+namespace pack {
+namespace sim_anneal {
+namespace compiled {
+
+using tmol::score::common::ForallDispatch;
+
+template <
+    template <tmol::Device>
+    class DeviceDispatch,
+    tmol::Device D,
+    typename Real,
+    typename Int>
+class CUDAPickRotamersStep : public PickRotamersStep {
+ public:
+  CUDAPickRotamersStep(
+      TView<Real, 4, D> context_coords,
+      TView<Int, 2, D> context_block_type,
+      TView<Int, 1, D> pose_id_for_context,
+      TView<Int, 1, D> n_rots_for_pose,
+      TView<Int, 1, D> rot_offset_for_pose,
+      TView<Int, 1, D> block_type_ind_for_rot,
+      TView<Int, 1, D> block_ind_for_rot,
+      TView<Real, 3, D> rotamer_coords,
+      TView<Real, 3, D> alternate_coords,
+      TView<Int, 2, D> alternate_id,
+      TView<Int, 1, D> random_rots,
+      TView<int64_t, 1, tmol::Device::CPU> annealer_event)
+      : context_coords_(context_coords),
+        context_block_type_(context_block_type),
+        pose_id_for_context_(pose_id_for_context),
+        n_rots_for_pose_(n_rots_for_pose),
+        rot_offset_for_pose_(rot_offset_for_pose),
+        block_type_ind_for_rot_(block_type_ind_for_rot),
+        block_ind_for_rot_(block_ind_for_rot),
+        rotamer_coords_(rotamer_coords),
+        alternate_coords_(alternate_coords),
+        alternate_id_(alternate_id),
+        random_rots_(random_rots),
+        annealer_event_(annealer_event) {}
+
+  int max_n_rotamers() const override {
+    int const n_poses = n_rots_for_pose_.size(0);
+
+    using namespace mgpu;
+    typedef launch_box_t<
+        arch_20_cta<32, 3>,
+        arch_35_cta<32, 3>,
+        arch_52_cta<32, 3>>
+        launch_t;
+    auto max_n_rots_tp = TPack<Int, 1, D>::zeros({1});
+    auto max_n_rots_tv = max_n_rots_tp.view;
+
+    mgpu::standard_context_t context;
+    mgpu::reduce<launch_t>(
+        &n_rots_for_pose_[0],
+        n_poses,
+        &max_n_rots_tv[0],
+        maximum_t<Int>(),
+        context);
+    Int max_n_rots;
+    cudaMemcpy(
+        &max_n_rots, &max_n_rots_tv[0], sizeof(Int), cudaMemcpyDeviceToHost);
+
+    std::cout << "Max n rots: " << max_n_rots << std::endl;
+    return max_n_rots;
+  }
+
+  void pick_rotamers() override {
+    clear_old_events();
+    create_new_event();
+
+    PickRotamers<DeviceDispatch, D, Real, Int>::f(
+        context_coords_,
+        context_block_type_,
+        pose_id_for_context_,
+        n_rots_for_pose_,
+        rot_offset_for_pose_,
+        block_type_ind_for_rot_,
+        block_ind_for_rot_,
+        rotamer_coords_,
+        alternate_coords_,
+        alternate_id_,
+        random_rots_,
+        annealer_event_);
+  }
+
+  void clear_old_events() {
+    return;
+    for (auto event_iter = previously_created_events_.begin();
+         event_iter != previously_created_events_.end();
+         /*no increment*/) {
+      cudaEvent_t event = *event_iter;
+      cudaError_t status = cudaEventQuery(event);
+      auto event_iter_next = event_iter;
+      ++event_iter_next;
+      if (status == cudaSuccess) {
+        std::cout << "event " << event << " done" << std::endl;
+        cudaEventDestroy(event);
+        previously_created_events_.erase(event_iter);
+      } else {
+        std::cout << "event " << event << " not yet ready" << std::endl;
+      }
+
+      event_iter = event_iter_next;
+    }
+  }
+
+  void create_new_event() {
+    cudaEvent_t event;
+    cudaEventCreate(&event);
+    annealer_event_[0] = reinterpret_cast<int64_t>(event);
+    previously_created_events_.push_back(event);
+    // std::cout << "Creating new event " << event << std::endl;
+  }
+
+ private:
+  TView<Real, 4, D> context_coords_;
+  TView<Int, 2, D> context_block_type_;
+  TView<Int, 1, D> pose_id_for_context_;
+  TView<Int, 1, D> n_rots_for_pose_;
+  TView<Int, 1, D> rot_offset_for_pose_;
+  TView<Int, 1, D> block_type_ind_for_rot_;
+  TView<Int, 1, D> block_ind_for_rot_;
+  TView<Real, 3, D> rotamer_coords_;
+  TView<Real, 3, D> alternate_coords_;
+  TView<Int, 2, D> alternate_id_;
+  TView<Int, 1, D> random_rots_;
+  TView<int64_t, 1, tmol::Device::CPU> annealer_event_;
+  std::list<cudaEvent_t> previously_created_events_;
+};
+
+template <
+    template <tmol::Device>
+    class DeviceDispatch,
+    tmol::Device D,
+    typename Real,
+    typename Int>
+class CUDAMetropolisAcceptRejectStep : public MetropolisAcceptRejectStep {
+ public:
+  CUDAMetropolisAcceptRejectStep(
+      TView<Real, 1, D> temperature,
+      TView<Real, 4, D> context_coords,
+      TView<Int, 2, D> context_block_type,
+      TView<Real, 3, D> alternate_coords,
+      TView<Int, 2, D> alternate_id,
+      TView<Real, 2, D> rotamer_component_energies,
+      TView<Int, 1, D> accept,
+      TView<int64_t, 1, tmol::Device::CPU> score_events)
+      : temperature_(temperature),
+        context_coords_(context_coords),
+        context_block_type_(context_block_type),
+        alternate_coords_(alternate_coords),
+        alternate_id_(alternate_id),
+        rotamer_component_energies_(rotamer_component_energies),
+        accept_(accept),
+        score_events_(score_events),
+        last_outer_iteration_(-1) {}
+
+  void set_temperature_scheduler(
+      std::shared_ptr<TemperatureScheduler> temp_sched) {
+    temp_sched_ = temp_sched;
+  }
+
+  void accept_reject(int outer_iteration) override {
+    assert(temp_sched_);
+
+    if (outer_iteration != last_outer_iteration_) {
+      Real temperature = temp_sched_->temp(outer_iteration);
+      std::cout << "Setting new temperature" << std::endl;
+      last_outer_iteration_ = outer_iteration;
+      cudaMemcpy(
+          &temperature_[0], &temperature, sizeof(Real), cudaMemcpyHostToDevice);
+    }
+
+    MetropolisAcceptReject<ForallDispatch, D, Real, Int>::f(
+        temperature_,
+        context_coords_,
+        context_block_type_,
+        alternate_coords_,
+        alternate_id_,
+        rotamer_component_energies_,
+        accept_,
+        score_events_);
+  }
+
+  void final_op() override { FinalOp<ForallDispatch, D, Real, Int>::f(); }
+
+ private:
+  TView<Real, 1, D> temperature_;
+  TView<Real, 4, D> context_coords_;
+  TView<Int, 2, D> context_block_type_;
+  TView<Real, 3, D> alternate_coords_;
+  TView<Int, 2, D> alternate_id_;
+  TView<Real, 2, D> rotamer_component_energies_;
+  TView<Int, 1, D> accept_;
+  TView<int64_t, 1, tmol::Device::CPU> score_events_;
+  // std::list<cudaEvent_t> events_;
+  int last_outer_iteration_;
+  std::shared_ptr<TemperatureScheduler> temp_sched_;
+};
+
+template <
+    template <tmol::Device>
+    class DeviceDispatch,
+    tmol::Device D,
+    typename Real,
+    typename Int>
+void PickRotamersStepRegistrator<DeviceDispatch, D, Real, Int>::f(
+    TView<Real, 4, D> context_coords,
+    TView<Int, 2, D> context_block_type,
+    TView<Int, 1, D> pose_id_for_context,
+    TView<Int, 1, D> n_rots_for_pose,
+    TView<Int, 1, D> rot_offset_for_pose,
+    TView<Int, 1, D> block_type_ind_for_rot,
+    TView<Int, 1, D> block_ind_for_rot,
+    TView<Real, 3, D> rotamer_coords,
+    TView<Real, 3, D> alternate_coords,
+    TView<Int, 2, D> alternate_id,
+    TView<Int, 1, D> random_rots,
+    TView<int64_t, 1, tmol::Device::CPU> annealer_event,
+    TView<int64_t, 1, tmol::Device::CPU> annealer) {
+  int64_t annealer_uint = annealer[0];
+  SimAnnealer *sim_annealer = reinterpret_cast<SimAnnealer *>(annealer_uint);
+  std::shared_ptr<PickRotamersStep> pick_step =
+      std::make_shared<CUDAPickRotamersStep<DeviceDispatch, D, Real, Int>>(
+          context_coords,
+          context_block_type,
+          pose_id_for_context,
+          n_rots_for_pose,
+          rot_offset_for_pose,
+          block_type_ind_for_rot,
+          block_ind_for_rot,
+          rotamer_coords,
+          alternate_coords,
+          alternate_id,
+          random_rots,
+          annealer_event);
+
+  sim_annealer->set_pick_rotamers_step(pick_step);
+}
+
+template <
+    template <tmol::Device>
+    class DeviceDispatch,
+    tmol::Device D,
+    typename Real,
+    typename Int>
+void MetropolisAcceptRejectStepRegistrator<DeviceDispatch, D, Real, Int>::f(
+    TView<Real, 1, D> temperature,
+    TView<Real, 4, D> context_coords,
+    TView<Int, 2, D> context_block_type,
+    TView<Real, 3, D> alternate_coords,
+    TView<Int, 2, D> alternate_id,
+    TView<Real, 2, D> rotamer_component_energies,
+    TView<Int, 1, D> accept,
+    TView<int64_t, 1, tmol::Device::CPU> score_events,
+    TView<int64_t, 1, tmol::Device::CPU> annealer) {
+  int64_t annealer_uint = annealer[0];
+  SimAnnealer *sim_annealer = reinterpret_cast<SimAnnealer *>(annealer_uint);
+  std::shared_ptr<MetropolisAcceptRejectStep> metropolis_step =
+      std::make_shared<
+          CUDAMetropolisAcceptRejectStep<DeviceDispatch, D, Real, Int>>(
+          temperature,
+          context_coords,
+          context_block_type,
+          alternate_coords,
+          alternate_id,
+          rotamer_component_energies,
+          accept,
+          score_events);
+
+  sim_annealer->set_metropolis_accept_reject_step(metropolis_step);
+}
+
+template struct PickRotamersStepRegistrator<
+    ForallDispatch,
+    tmol::Device::CUDA,
+    float,
+    int>;
+template struct PickRotamersStepRegistrator<
+    ForallDispatch,
+    tmol::Device::CUDA,
+    double,
+    int>;
+// template struct PickRotamersStepRegistrator<
+//     ForallDispatch,
+//     tmol::Device::CUDA,
+//     float,
+//     int>;
+// template struct PickRotamersStepRegistrator<
+//     ForallDispatch,
+//     tmol::Device::CUDA,
+//     double,
+//     int>;
+
+template struct MetropolisAcceptRejectStepRegistrator<
+    ForallDispatch,
+    tmol::Device::CUDA,
+    float,
+    int>;
+template struct MetropolisAcceptRejectStepRegistrator<
+    ForallDispatch,
+    tmol::Device::CUDA,
+    double,
+    int>;
+// template struct MetropolisAcceptRejectStepRegistrator<
+//     ForallDispatch,
+//     tmol::Device::CUDA,
+//     float,
+//     int>;
+// template struct MetropolisAcceptRejectStepRegistrator<
+//     ForallDispatch,
+//     tmol::Device::CUDA,
+//     double,
+//     int>;
+
+}  // namespace compiled
+}  // namespace sim_anneal
+}  // namespace pack
+}  // namespace tmol
---
+++ tmol/tests/pack/sim_anneal/test_sim_anneal_benchmark.py
@@ -0,0 +1,269 @@
+import torch
+import attr
+import numpy
+import pytest
+
+from tmol.utility.tensor.common_operations import stretch
+from tmol.system.restypes import ResidueTypeSet
+from tmol.system.pose import PackedBlockTypes, Pose, Poses
+
+from tmol.pack.packer_task import PackerTask, PackerPalette
+from tmol.pack.rotamer.dunbrack.dunbrack_chi_sampler import DunbrackChiSampler
+from tmol.pack.rotamer.fixed_aa_chi_sampler import FixedAAChiSampler
+from tmol.pack.rotamer.build_rotamers import RotamerSet, build_rotamers
+from tmol.pack.rotamer.bounding_spheres import create_rotamer_bounding_spheres
+
+from tmol.pack.sim_anneal.annealer import MCAcceptRejectModule, SelectRanRotModule
+
+from tmol.score.ljlk.LJLKEnergy import LJLKEnergy
+from tmol.score.ljlk.params import LJLKParamResolver
+from tmol.system.pose import Pose, Poses
+from tmol.score.chemical_database import AtomTypeParamResolver
+
+
+# class SimAEngine(torch.jit.ScriptModule):
+class SimAEngine:
+    def __init__(self, temperature, selector, inter_module, mc_accept_reject):
+
+        super().__init__()
+
+        def _p(t):
+            return torch.nn.Parameter(t, requires_grad=False)
+
+        self.temperature = _p(temperature)
+        self.selector = selector
+        self.inter_module = inter_module
+        self.mc_accept_reject = mc_accept_reject
+        self.temp_lj_energies = _p(
+            torch.zeros(
+                (selector.pose_id_for_context.shape[0] * 2,),
+                dtype=torch.float32,
+                device=selector.pose_id_for_context.device,
+            )
+        )
+        self.temp_alt_coords = _p(
+            torch.zeros(
+                (
+                    selector.pose_id_for_context.shape[0] * 2,
+                    selector.rotamer_coords.shape[1],
+                    3,
+                ),
+                dtype=torch.float32,
+                device=selector.pose_id_for_context.device,
+            )
+        )
+        self.temp_alt_ids = _p(
+            torch.zeros(
+                (selector.pose_id_for_context.shape[0] * 2, 3),
+                dtype=torch.int32,
+                device=selector.pose_id_for_context.device,
+            )
+        )
+
+    # @torch.jit.script_method
+    # def forward(self, context_coords, context_block_type):
+
+    def go(self, context_coords, context_block_type):
+        alt_coords, alt_ids, rr = self.selector.go(context_coords, context_block_type)
+        lj_energies, _ = self.inter_module.go(
+            context_coords, context_block_type, alt_coords, alt_ids
+        )
+        accept = self.mc_accept_reject.go(
+            self.temperature,
+            context_coords,
+            context_block_type,
+            self.temp_alt_coords,
+            self.temp_alt_ids,
+            self.temp_lj_energies.unsqueeze(0),
+        )
+        return accept
+
+
+@pytest.mark.benchmark(group="simulated_annealing")
+# @pytest.mark.parametrize("n_poses", [10, 30, 100, 300, 1000])
+@pytest.mark.parametrize("n_poses", [100, 300])  # 300
+@pytest.mark.parametrize("n_components", [1, 4])
+def test_run_simA_benchmark(
+    benchmark,
+    n_poses,
+    n_components,
+    default_database,
+    fresh_default_restype_set,
+    rts_ubq_res,
+    torch_device,
+    dun_sampler,
+):
+
+    # def _p(t):
+    #     return torch.nn.Parameter(t, requires_grad=False)
+
+    max_n_blocks = len(rts_ubq_res)
+
+    p = Pose.from_residues_one_chain(rts_ubq_res, torch_device)
+    poses = Poses.from_poses([p] * n_poses, torch_device)
+
+    palette = PackerPalette(fresh_default_restype_set)
+    task = PackerTask(poses, palette)
+    task.restrict_to_repacking()
+
+    fixed_sampler = FixedAAChiSampler()
+    task.add_chi_sampler(dun_sampler)
+    task.add_chi_sampler(fixed_sampler)
+
+    poses, rotamer_set = build_rotamers(poses, task, default_database.chemical)
+    # print("n_rotamers", rotamer_set.coords.shape[0] // n_poses)
+
+    bounding_spheres = create_rotamer_bounding_spheres(poses, rotamer_set)
+
+    # come up with the intial rotamer assignment
+    context_coords = poses.coords.clone()
+    rand_rot = torch.floor(
+        torch.rand((n_poses, max_n_blocks), dtype=torch.float, device=torch_device)
+        * rotamer_set.n_rots_for_block.to(torch.float32)
+    ).to(torch.int64)
+    rand_rot_global = rotamer_set.rot_offset_for_block + rand_rot
+    packable_blocks = rotamer_set.n_rots_for_block != 0
+    context_coords[packable_blocks] = rotamer_set.coords[
+        rand_rot_global[packable_blocks]
+    ]
+    context_block_type = poses.block_type_ind.clone()
+    context_block_type[packable_blocks] = rotamer_set.block_type_ind_for_rot[
+        rand_rot_global[packable_blocks]
+    ].to(torch.int32)
+
+    n_poses_arange = torch.arange(n_poses, dtype=torch.int32, device=torch_device)
+
+    annealer = torch.zeros((1,), dtype=torch.int64, device="cpu")
+    torch.ops.tmol.create_sim_annealer(annealer)
+    print("annealer")
+    print(annealer)
+
+    pose_id_for_context = n_poses_arange
+    n_rots_for_pose = rotamer_set.n_rots_for_pose.to(torch.int32)
+    rot_offset_for_pose = rotamer_set.rot_offset_for_pose.to(torch.int32)
+    block_type_ind_for_rot = rotamer_set.block_type_ind_for_rot.to(torch.int32)
+    block_ind_for_rot = rotamer_set.block_ind_for_rot.to(torch.int32)
+    rotamer_coords = rotamer_set.coords
+
+    alternate_coords = torch.zeros(
+        (n_poses * 2, poses.packed_block_types.max_n_atoms, 3),
+        dtype=torch.float32,
+        device=torch_device,
+    )
+    alternate_id = torch.zeros((n_poses * 2, 3), dtype=torch.int32, device=torch_device)
+    random_rotamers = torch.zeros((n_poses,), dtype=torch.int32, device=torch_device)
+    print("alternate_coords initial")
+    print(alternate_coords.shape)
+    print("alternate_id initial")
+    print(alternate_id.shape)
+
+    annealer_event = torch.zeros(1, dtype=torch.int64, device="cpu")
+    score_events = torch.zeros(n_components, dtype=torch.int64, device="cpu")
+
+    torch.ops.tmol.register_standard_random_rotamer_picker(
+        context_coords,
+        context_block_type,
+        pose_id_for_context,
+        n_rots_for_pose,
+        rot_offset_for_pose,
+        block_type_ind_for_rot,
+        block_ind_for_rot,
+        rotamer_coords,
+        alternate_coords,
+        alternate_id,
+        random_rotamers,
+        annealer_event,
+        annealer,
+    )
+
+    temperature = torch.full((1,), 100, dtype=torch.float32, device=torch_device)
+    rotamer_component_energies = torch.zeros(
+        (n_components, 2 * n_poses), dtype=torch.float32, device=torch_device
+    )
+    accepted = torch.zeros((n_poses,), dtype=torch.int32, device=torch_device)
+
+    torch.ops.tmol.register_standard_metropolis_accept_or_rejector(
+        temperature,
+        context_coords,
+        context_block_type,
+        alternate_coords,
+        alternate_id,
+        rotamer_component_energies,
+        accepted,
+        score_events,
+        annealer,
+    )
+
+    # and the score function
+    resolver = AtomTypeParamResolver.from_database(
+        default_database.chemical, torch_device
+    )
+
+    ljlk_params = LJLKParamResolver.from_database(
+        default_database.chemical, default_database.scoring.ljlk, device=torch_device
+    )
+
+    ljlk_energy = LJLKEnergy(
+        atom_type_resolver=resolver,
+        type_params=ljlk_params.type_params,
+        global_params=ljlk_params.global_params,
+        atom_type_index=ljlk_params.atom_type_index,
+        device=torch_device,
+    )
+
+    weights = {"lj": 1.0, "lk": 1.0}
+
+    for bt in poses.packed_block_types.active_block_types:
+        ljlk_energy.setup_block_type(bt)
+    ljlk_energy.setup_packed_block_types(poses.packed_block_types)
+    ljlk_energy.setup_poses(poses)
+
+    inter_module = ljlk_energy.inter_module(
+        poses.packed_block_types, poses, n_poses_arange, bounding_spheres, weights
+    )
+
+    for i in range(n_components):
+        inter_module.register_with_sim_annealer(
+            context_coords,
+            context_block_type,
+            alternate_coords,
+            alternate_id,
+            rotamer_component_energies[i],
+            score_events[i : i + 1],
+            annealer_event,
+            annealer,
+        )
+
+    torch.random.manual_seed(1111)
+    torch.ops.tmol.run_sim_annealing(annealer)
+
+    #
+    # temperature = torch.full((1,), 100, dtype=torch.float32, device=torch_device)
+    #
+    # alt_coords, alt_ids, rr = selector.go(context_coords, context_block_type)
+    # lj_energies, events = inter_module.go(context_coords, context_block_type, alt_coords, alt_ids)
+    # accept = mc_accept_reject.go(
+    #     temperature,
+    #     context_coords,
+    #     context_block_type,
+    #     alt_coords,
+    #     alt_ids,
+    #     lj_energies.unsqueeze(0)
+    # )
+    #
+    #
+    # lj_energies = torch.zeros((n_poses*2,), dtype=torch.float32, device=torch_device)
+    #
+    # tight_loop = SimAEngine(temperature, selector, inter_module, mc_accept_reject)
+    #
+    # @benchmark
+    # def one_step_simA():
+    #     return tight_loop.go(context_coords, context_block_type)
+    #
+    # accept = one_step_simA
+
+    torch.ops.tmol.delete_sim_annealer(annealer)
+    print("annealer")
+    print(annealer)
+
+    torch.arange(100, device=torch_device).sum()
