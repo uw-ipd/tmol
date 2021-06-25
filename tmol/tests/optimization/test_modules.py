39b90f00409c067e9342175ee45f7d0cdee9b1b9
Henry <milkshakeiii@gmail.com>
2021-06-25 11:01:27 -0700

    restore delted dof minimization test

--- tmol/tests/optimization/test_modules.py
+++ tmol/tests/optimization/test_modules.py
@@ -6,8 +6,6 @@ from tmol.system.kinematics import KinematicDescription
 from tmol.score.modules.bases import ScoreSystem
 from tmol.score.modules.coords import coords_for
 
-from tmol.score.device import TorchDevice
-
 from tmol.system.score_support import get_full_score_system_for
 
 
@@ -30,4 +28,28 @@ def test_cart_network_min(ubq_system, torch_device):
     optimizer.step(closure)  # this optimizes coords, the tensor
 
     E1 = score_system.intra_total(coords)
+    assert E1 < E0
+
+
+def test_dof_network_min(ubq_system, torch_device):
+    score_system = get_full_score_system_for(ubq_system)
+    coords = coords_for(ubq_system, score_system)
+
+    model = TorsionalEnergyNetwork(score_system, ubq_system, torch_device)
+
+    # "kincoords" is for each atom, 9 values,
+    # but only 3 for regular atom, 9 for jump
+    optimizer = LBFGS_Armijo(model.parameters(), lr=0.8, max_iter=20)
+
+    E0 = score_system.intra_total(model.coords())
+
+    def closure():
+        optimizer.zero_grad()
+
+        E = model()
+        E.backward()
+        return E
+
+    optimizer.step(closure)
+    E1 = score_system.intra_total(model.coords())
     assert E1 < E0
