140bc4eb845a9c449398f6e57472245fdbf7b92c
Henry <milkshakeiii@gmail.com>
2021-06-28 17:04:10 -0700

    bad test_scoreterm_benchmarks.py

--- tmol/tests/score/test_scoreterm_benchmarks.py
+++ tmol/tests/score/test_scoreterm_benchmarks.py
@@ -5,83 +5,19 @@ from tmol.utility.reactive import reactive_property
 from tmol.score.device import TorchDevice
 from tmol.score.score_components import ScoreComponentClasses, IntraScore
 
-
-@score_graph
-class DummyIntra(IntraScore):
-    @reactive_property
-    def total_dummy(target):
-        return target.coords.sum()
-
-
-@score_graph
-class DofSpaceDummy(
-    KinematicAtomicCoordinateProvider, BondedAtomScoreGraph, TorchDevice
-):
-    total_score_components = [
-        ScoreComponentClasses("dummy", intra_container=DummyIntra, inter_container=None)
-    ]
+from tmol.score.modules.ljlk import LJScore, LKScore
+from tmol.score.modules.lk_ball import LKBallScore
+from tmol.score.modules.elec import ElecScore
+from tmol.score.modules.cartbonded import CartBondedScore
+from tmol.score.modules.dunbrack import DunbrackScore
+from tmol.score.modules.hbond import HBondScore
+from tmol.score.modules.rama import RamaScore
+from tmol.score.modules.omega import OmegaScore
 
 
-@score_graph
-class DofSpaceTotal(KinematicAtomicCoordinateProvider, TotalScoreGraph, TorchDevice):
-    pass
-
-
-@score_graph
-class TotalScore(CartesianAtomicCoordinateProvider, TotalScoreGraph, TorchDevice):
-    pass
-
-
-@score_graph
-class HBondScore(CartesianAtomicCoordinateProvider, HBondScoreGraph, TorchDevice):
-    pass
-
-
-@score_graph
-class ElecScore(CartesianAtomicCoordinateProvider, ElecScoreGraph, TorchDevice):
-    pass
-
-
-@score_graph
-class RamaScore(CartesianAtomicCoordinateProvider, RamaScoreGraph, TorchDevice):
-    pass
-
-
-@score_graph
-class OmegaScore(CartesianAtomicCoordinateProvider, OmegaScoreGraph, TorchDevice):
-    pass
-
-
-@score_graph
-class DunbrackScore(CartesianAtomicCoordinateProvider, DunbrackScoreGraph, TorchDevice):
-    pass
-
-
-@score_graph
-class CartBondedScore(
-    CartesianAtomicCoordinateProvider, CartBondedScoreGraph, TorchDevice
-):
-    pass
-
-
-@score_graph
-class LJScore(CartesianAtomicCoordinateProvider, LJScoreGraph, TorchDevice):
-    pass
-
-
-@score_graph
-class LKScore(CartesianAtomicCoordinateProvider, LKScoreGraph, TorchDevice):
-    pass
-
-
-@score_graph
-class LKBallScore(CartesianAtomicCoordinateProvider, LKBallScoreGraph, TorchDevice):
-    pass
-
-
-def benchmark_score_pass(benchmark, score_graph, benchmark_pass):
+def benchmark_score_pass(benchmark, score_system, benchmark_pass, coords):
     # Score once to prep graph
-    total = score_graph.intra_score().total
+    total = score_system.intra_score().total
 
     if benchmark_pass == "full":
 
@@ -89,31 +25,31 @@ def benchmark_score_pass(benchmark, score_graph, benchmark_pass):
         def run():
             score_graph.reset_coords()
 
-            total = score_graph.intra_score().total
-            total.backward()
+            result = score_graph.intra_total()
+            # total = ?#
+            # total.backward() ?# TODO: what should go here?
 
-            float(total)
+            # float(total)
 
-            return total
+            # return total
+            return
 
     elif benchmark_pass == "forward":
 
         @benchmark
         def run():
-            score_graph.reset_coords()
+            total = score_system.intra_total(coords)
 
-            total = score_graph.intra_score().total
-
-            float(total)
-
-            return total
+            return  # TODO: what should be returned?
+            # intra_score() used to to return an object with a member total that itself had a .device member?
 
     elif benchmark_pass == "backward":
 
         @benchmark
         def run():
-            total.backward(retain_graph=True)
-            return total
+            # total.backward(retain_graph=True) TODO: what should go here?
+            # return total
+            return
 
     else:
         raise NotImplementedError
@@ -122,47 +58,30 @@ def benchmark_score_pass(benchmark, score_graph, benchmark_pass):
 
 
 @pytest.mark.parametrize(
-    "graph_class",
+    "score_system_weight_pair",
     [
-        TotalScore,
-        DofSpaceTotal,
-        HBondScore,
-        ElecScore,
-        RamaScore,
-        OmegaScore,
-        DunbrackScore,
-        CartBondedScore,
-        LJScore,
-        LKScore,
-        LKBallScore,
-        DofSpaceDummy,
-    ],
-    ids=[
-        "total_cart",
-        "total_torsion",
-        "hbond",
-        "elec",
-        "rama",
-        "omega",
-        "dun",
-        "cartbonded",
-        "lj",
-        "lk",
-        "lk_ball",
-        "kinematics",
+        ({LJScore}, {"lj": 1.0}),
+        ({LKScore}, {"lk": 1.0}),
+        ({LKBallScore}, {"lk_ball": 1.0}),
+        ({ElecScore}, {"elec": 1.0}),
+        ({CartBondedScore}, {"cartbonded": 1.0}),
+        ({DunbrackScore}, {"dunbrack": 1.0}),
+        ({HBondScore}, {"hbond": 1.0}),
+        ({RamaScore}, {"rama": 1.0}),
+        ({OmegaScore}, {"omega": 1.0}),
     ],
 )
 @pytest.mark.parametrize("benchmark_pass", ["full", "forward", "backward"])
 @pytest.mark.benchmark(group="score_components")
 def test_end_to_end_score_graph(
-    benchmark, benchmark_pass, graph_class, torch_device, ubq_system
+    benchmark, benchmark_pass, score_system_weight_pair, torch_device, ubq_system
 ):
     target_system = ubq_system
+    score_system_dict = score_system_weight_pair[0]
+    weight_dict = score_system_weight_pair[1]
+    score_system = ScoreSystem.build_for(target_system, score_system_dict, weight_dict)
+    coords = coords_for(target_system, score_system)
 
-    score_graph = graph_class.build_for(
-        target_system, requires_grad=True, device=torch_device
-    )
-
-    run = benchmark_score_pass(benchmark, score_graph, benchmark_pass)
+    run = benchmark_score_pass(benchmark, score_system, benchmark_pass, coords)
 
     assert run.device == torch_device
