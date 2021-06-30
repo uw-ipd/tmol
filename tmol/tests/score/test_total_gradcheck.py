2311c9835ab6e5ab2160b0ef51f98152cb98032c
Henry <milkshakeiii@gmail.com>
2021-06-30 11:48:42 -0700

    modify test_total_gradcheck.py

--- tmol/tests/score/test_total_gradcheck.py
+++ tmol/tests/score/test_total_gradcheck.py
@@ -1,48 +1,24 @@
 import torch
 
 from tmol.system.packed import PackedResidueSystem
+from tmol.system.score_support import get_full_score_system_for
 
 from tmol.tests.autograd import gradcheck
 
 
-@score_graph
-class RealSpaceScore(CartesianAtomicCoordinateProvider, TotalScoreGraph):
-    pass
-
-
-@score_graph
-class DofSpaceScore(KinematicAtomicCoordinateProvider, TotalScoreGraph):
-    pass
-
-
-def test_torsion_space_gradcheck(ubq_res):
-    test_system = PackedResidueSystem.from_residues(ubq_res[:6])
-
-    torsion_space = DofSpaceScore.build_for(test_system)
-
-    start_dofs = torsion_space.dofs.requires_grad_()
-
-    def total_score(minimizable_dofs):
-        torsion_space.dofs[:, :6] = minimizable_dofs
-        return torsion_space.intra_score().total
-
-    # fd this test needs work...
-    assert gradcheck(total_score, (start_dofs[:, :6],), eps=1e-2, atol=5e-2, nfail=0)
-
-
 def test_real_space_gradcheck(ubq_res):
     test_system = PackedResidueSystem.from_residues(ubq_res[:6])
-    real_space = RealSpaceScore.build_for(test_system)
 
     coord_mask = torch.isnan(real_space.coords).sum(dim=-1) == 0
     start_coords = real_space.coords[coord_mask]
+
+    real_space = get_full_score_system_for(test_system)
 
     def total_score(coords):
         state_coords = real_space.coords.detach().clone()
         state_coords[coord_mask] = coords
 
-        real_space.coords = state_coords
-        return real_space.intra_score().total
+        return real_space.intra_score_only(state_coords)
 
     # fd this test needs work...
     assert gradcheck(total_score, (start_coords,), eps=1e-2, atol=5e-2, nfail=0)
