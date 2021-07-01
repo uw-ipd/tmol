b0cddbf09defd0077d94529dea396601ccec7192
Henry <milkshakeiii@gmail.com>
2021-07-01 16:03:44 -0700

    update omega/test_baseline.py

--- tmol/tests/score/omega/test_baseline.py
+++ tmol/tests/score/omega/test_baseline.py
@@ -5,15 +5,8 @@ from tmol.score.modules.bases import ScoreSystem
 from tmol.score.modules.omega import OmegaScore
 
 
-@score_graph
-class OmegaGraph(CartesianAtomicCoordinateProvider, OmegaScoreGraph):
-    pass
-
-
 def test_omega_baseline_comparison(ubq_system, torch_device):
-    test_graph = OmegaGraph.build_for(
-        ubq_system, drop_missing_atoms=False, requires_grad=False, device=torch_device
-    )
+    score_system = ScoreSystem.build_for(ubq_system, {OmegaScore}, {"omega": 1.0})
 
-    intra_container = test_graph.intra_score()
+    intra_container = score_system.intra_subscore()
     assert float(intra_container.total_omega) == approx(6.741275, rel=1e-3)
