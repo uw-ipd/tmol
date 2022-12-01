f4686e7d07743fe28105a95c291a1f70c1e0c733
Andrew Leaver-Fay <aleaverfay@gmail.com>
2022-12-01 09:55:37 -0500

    Enable forward and backward benchmarking of ScoreFunction

--- tmol/tests/score/test_score_function_benchmarks.py
+++ tmol/tests/score/test_score_function_benchmarks.py
@@ -1,5 +1,6 @@
 import pytest
 import torch
+from tmol.tests.torch import zero_padded_counts
 
 from tmol.score.score_function import ScoreFunction
 
@@ -10,11 +11,12 @@ from tmol.score.ljlk.ljlk_energy_term import LJLKEnergyTerm
 
 
 @pytest.mark.parametrize("energy_term", [LJLKEnergyTerm], ids=["ljlk"])
-@pytest.mark.parametrize("n_poses", [1, 3, 10, 30, 100])
+@pytest.mark.parametrize("n_poses", zero_padded_counts([1, 3, 10, 30, 100]))
 @pytest.mark.benchmark(group="setup_res_centric_scoring")
-def test_res_centric_score_benchmark_setup(
+def dont_test_res_centric_score_benchmark_setup(
     benchmark, energy_term, n_poses, rts_ubq_res, default_database, torch_device
 ):
+    n_poses = int(n_poses)
     pose_stack1 = PoseStackBuilder.one_structure_from_polymeric_residues(
         rts_ubq_res, torch_device
     )
@@ -34,8 +36,8 @@ def test_res_centric_score_benchmark_setup(
 
 
 @pytest.mark.parametrize("energy_term", [LJLKEnergyTerm], ids=["ljlk"])
-@pytest.mark.parametrize("benchmark_pass", ["forward"])
-@pytest.mark.parametrize("n_poses", [1, 3, 10, 30, 100])
+@pytest.mark.parametrize("benchmark_pass", ["forward", "full", "backward"])
+@pytest.mark.parametrize("n_poses", zero_padded_counts([1, 3, 10, 30, 100]))
 @pytest.mark.benchmark(group="res_centric_score_components")
 def test_res_centric_score_benchmark(
     benchmark,
@@ -46,7 +48,7 @@ def test_res_centric_score_benchmark(
     default_database,
     torch_device,
 ):
-    print("n_poses", n_poses)
+    n_poses = int(n_poses)
     pose_stack1 = PoseStackBuilder.one_structure_from_polymeric_residues(
         rts_ubq_res, torch_device
     )
@@ -60,12 +62,13 @@ def test_res_centric_score_benchmark(
     scorer = sfxn.render_whole_pose_scoring_module(pose_stack_n)
 
     if benchmark_pass == "full":
+        pose_stack_n.coords.requires_grad_(True)
 
         @benchmark
         def score_pass():
             scores = torch.sum(scorer(pose_stack_n.coords))
-            scores.backward(retain_graph=True).cpu()
-            return scores
+            scores.backward(retain_graph=True)
+            return scores.cpu()
 
     elif benchmark_pass == "forward":
 
@@ -76,12 +79,13 @@ def test_res_centric_score_benchmark(
             return scores
 
     elif benchmark_pass == "backward":
+        pose_stack_n.coords.requires_grad_(True)
         scores = torch.sum(scorer(pose_stack_n.coords))
 
         @benchmark
         def score_pass():
-            scores.backward(retain_graph=True).cpu()
-            return scores
+            scores.backward(retain_graph=True)
+            return scores.cpu()
 
     else:
         raise NotImplementedError
