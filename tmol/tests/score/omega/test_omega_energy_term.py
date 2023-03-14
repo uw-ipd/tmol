f6ea73f60a7247beb013029d92e894c7f65d05ff
Jeff Flatten <jflat06@cs.washington.edu>
2023-03-14 14:31:00 -0700

    Add single structure test to omega.

--- tmol/tests/score/omega/test_omega_energy_term.py
+++ tmol/tests/score/omega/test_omega_energy_term.py
@@ -52,6 +52,32 @@ def test_whole_pose_scoring_module_gradcheck_whole_pose(
     gradcheck(score, (p1.coords.requires_grad_(True),), eps=1e-3, atol=1e-2, rtol=5e-3)
 
 
+def test_whole_pose_scoring_module_single(
+    rts_ubq_res, default_database, torch_device: torch.device
+):
+    gold_vals = numpy.array([[6.741275]], dtype=numpy.float32)
+    omega_energy = OmegaEnergyTerm(param_db=default_database, device=torch_device)
+    p1 = PoseStackBuilder.one_structure_from_polymeric_residues(
+        res=rts_ubq_res, device=torch_device
+    )
+    for bt in p1.packed_block_types.active_block_types:
+        omega_energy.setup_block_type(bt)
+    omega_energy.setup_packed_block_types(p1.packed_block_types)
+    omega_energy.setup_poses(p1)
+
+    omega_pose_scorer = omega_energy.render_whole_pose_scoring_module(p1)
+
+    coords = torch.nn.Parameter(p1.coords.clone())
+    scores = omega_pose_scorer(coords)
+
+    # make sure we're still good
+    torch.arange(100, device=torch_device)
+
+    numpy.testing.assert_allclose(
+        gold_vals, scores.cpu().detach().numpy(), atol=1e-5, rtol=1e-5
+    )
+
+
 def test_whole_pose_scoring_module_10(
     rts_ubq_res, default_database, torch_device: torch.device
 ):
