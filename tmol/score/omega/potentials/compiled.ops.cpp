adc88013383e4e0298f48a610f897fa7df5e61d0
Jeff Flatten <jflat06@cs.washington.edu>
2023-03-14 12:16:05 -0700

    Fix indentation.

--- tmol/score/omega/potentials/compiled.ops.cpp
+++ tmol/score/omega/potentials/compiled.ops.cpp
@@ -133,15 +133,14 @@ class OmegaPoseScoreOp
     int i = 0;
     auto dscore_dcoords = result[i++];
 
-    return {
-        dscore_dcoords,
+    return {dscore_dcoords,
 
-        torch::Tensor(),
-        torch::Tensor(),
-        torch::Tensor(),
-        torch::Tensor(),
-        torch::Tensor(),
-        torch::Tensor()};
+            torch::Tensor(),
+            torch::Tensor(),
+            torch::Tensor(),
+            torch::Tensor(),
+            torch::Tensor(),
+            torch::Tensor()};
   }
 };
 
