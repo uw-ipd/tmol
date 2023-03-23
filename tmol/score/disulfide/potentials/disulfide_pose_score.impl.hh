7532fcd5cdb4d762356d2e07cc288d13d441dc20
Jeff Flatten <jflat06@cs.washington.edu>
2023-03-23 13:24:59 -0700

    Simplify kernel code.

--- tmol/score/disulfide/potentials/disulfide_pose_score.impl.hh
+++ tmol/score/disulfide/potentials/disulfide_pose_score.impl.hh
@@ -77,6 +77,13 @@ auto DisulfidePoseScoreDispatch<DeviceDispatch, D, Real, Int>::f(
 
   auto func = ([=] TMOL_DEVICE_FUNC(int pose_index, int block_index) {
     const auto& params = global_params[0];
+    const auto& inter_block_connections =
+        pose_stack_inter_block_connections[pose_index];
+    const auto& block_coord_offset = pose_stack_block_coord_offset[pose_index];
+    const auto& block_types = pose_stack_block_type[pose_index];
+
+    auto& pose_V = V[0][pose_index];
+    auto& pose_dV_dx = dV_dx[0][pose_index];
 
     int block_type_index = pose_stack_block_type[pose_index][block_index];
     int block_atom_offset =
@@ -85,13 +92,10 @@ auto DisulfidePoseScoreDispatch<DeviceDispatch, D, Real, Int>::f(
     for (int conn_index = 0; conn_index < max_n_conns; conn_index++) {
       if (disulfide_conns[block_type_index][conn_index]) {
         int other_block_index =
-            pose_stack_inter_block_connections[pose_index][block_index]
-                                              [conn_index][0];
+            inter_block_connections[block_index][conn_index][0];
         int other_conn_index =
-            pose_stack_inter_block_connections[pose_index][block_index]
-                                              [conn_index][1];
-        int other_block_atom_offset =
-            pose_stack_block_coord_offset[pose_index][other_block_index];
+            inter_block_connections[block_index][conn_index][1];
+        int other_block_atom_offset = block_coord_offset[other_block_index];
 
         // Make sure we only calculate the disulfide once per pair of blocks
         if (other_block_index < block_index) continue;
@@ -99,8 +103,7 @@ auto DisulfidePoseScoreDispatch<DeviceDispatch, D, Real, Int>::f(
         // Skip this disulfide if the other block doesn't exist
         if (other_block_index == -1) continue;
 
-        int other_block_type_index =
-            pose_stack_block_type[pose_index][other_block_index];
+        int other_block_type_index = block_types[other_block_index];
 
         // Skip this disulfide if the other end isn't capable of a disulfide
         // bond
@@ -154,187 +157,177 @@ auto DisulfidePoseScoreDispatch<DeviceDispatch, D, Real, Int>::f(
             block2_CA, block2_CB, block2_S, block1_S);
 
         const Real MEST = exp(-20.0);
-        const Real wt_dihSS_(0.1);
-        const Real wt_dihCS_(0.1);
-        const Real wt_ang_(0.1);
-        const Real wt_len_(0.1);
-        const Real shift_(2.0);
+        const Real WT_DIH_SS(0.1);
+        const Real WT_DIH_CS(0.1);
+        const Real WT_ANG(0.1);
+        const Real WT_LEN(0.1);
+        const Real SHIFT(2.0);
 
-        Real const res1_d_multiplier(1.0);
-        Real const res2_d_multiplier(1.0);
+        const Real res1_d_multiplier(1.0);
+        const Real res2_d_multiplier(1.0);
 
-        Real score = -shift_;
+        Real score = -SHIFT;
 
         {  // Calculate Distance
-          Real z = (ssdist.V - global_params[0].d_location)
-                   / global_params[0].d_scale;
+          Real z = (ssdist.V - params.d_location) / params.d_scale;
           Real score_d =
               z * z / 2
-              - log(std::erfc(-global_params[0].d_shape * z / sqrt(2.0))
-                    + MEST);
-          score += wt_len_ * score_d;
+              - log(std::erfc(-params.d_shape * z / sqrt(2.0)) + MEST);
+          score += WT_LEN * score_d;
 
           Real dscore_d =
-              z / global_params[0].d_scale
-              - (exp(-0.5 * z * z * global_params[0].d_shape
-                     * global_params[0].d_shape)
-                 * sqrt(2.0 / M_PI) * global_params[0].d_shape)
-                    / (global_params[0].d_scale
-                           * std::erfc(
-                                 -global_params[0].d_shape * z / sqrt(2.0))
+              z / params.d_scale
+              - (exp(-0.5 * z * z * params.d_shape * params.d_shape)
+                 * sqrt(2.0 / M_PI) * params.d_shape)
+                    / (params.d_scale
+                           * std::erfc(-params.d_shape * z / sqrt(2.0))
                        + 1.e-12);
-          dscore_d *= wt_len_;
+          dscore_d *= WT_LEN;
           accumulate<D, Vec<Real, 3>>::add(
-              dV_dx[0][pose_index][block1_S_ind], dscore_d * ssdist.dV_dA);
+              pose_dV_dx[block1_S_ind], dscore_d * ssdist.dV_dA);
           accumulate<D, Vec<Real, 3>>::add(
-              dV_dx[0][pose_index][block2_S_ind], dscore_d * ssdist.dV_dB);
+              pose_dV_dx[block2_S_ind], dscore_d * ssdist.dV_dB);
         }
 
         {  // Calculate Angles
           Real ang_score(0);
           Real angle1(csang_1.V), angle2(csang_2.V);
-          ang_score += wt_ang_
-                       * (-global_params[0].a_logA
-                          - global_params[0].a_kappa
-                                * cos(angle1 - global_params[0].a_mu));
-          ang_score += wt_ang_
-                       * (-global_params[0].a_logA
-                          - global_params[0].a_kappa
-                                * cos(angle2 - global_params[0].a_mu));
+          ang_score +=
+              WT_ANG
+              * (-params.a_logA - params.a_kappa * cos(angle1 - params.a_mu));
+          ang_score +=
+              WT_ANG
+              * (-params.a_logA - params.a_kappa * cos(angle2 - params.a_mu));
           score += ang_score;
 
-          Real dscore_a = global_params[0].a_kappa
-                          * sin(angle1 - global_params[0].a_mu) * wt_ang_;
-          Real dscore_b = global_params[0].a_kappa
-                          * sin(angle2 - global_params[0].a_mu) * wt_ang_;
+          Real dscore_a = params.a_kappa * sin(angle1 - params.a_mu) * WT_ANG;
+          Real dscore_b = params.a_kappa * sin(angle2 - params.a_mu) * WT_ANG;
           accumulate<D, Vec<Real, 3>>::add(
-              dV_dx[0][pose_index][block1_CB_ind], dscore_a * csang_1.dV_dA);
+              pose_dV_dx[block1_CB_ind], dscore_a * csang_1.dV_dA);
           accumulate<D, Vec<Real, 3>>::add(
-              dV_dx[0][pose_index][block1_S_ind], dscore_a * csang_1.dV_dB);
+              pose_dV_dx[block1_S_ind], dscore_a * csang_1.dV_dB);
           accumulate<D, Vec<Real, 3>>::add(
-              dV_dx[0][pose_index][block2_S_ind], dscore_a * csang_1.dV_dC);
+              pose_dV_dx[block2_S_ind], dscore_a * csang_1.dV_dC);
           accumulate<D, Vec<Real, 3>>::add(
-              dV_dx[0][pose_index][block2_CB_ind], dscore_b * csang_2.dV_dA);
+              pose_dV_dx[block2_CB_ind], dscore_b * csang_2.dV_dA);
           accumulate<D, Vec<Real, 3>>::add(
-              dV_dx[0][pose_index][block2_S_ind], dscore_b * csang_2.dV_dB);
+              pose_dV_dx[block2_S_ind], dscore_b * csang_2.dV_dB);
           accumulate<D, Vec<Real, 3>>::add(
-              dV_dx[0][pose_index][block1_S_ind], dscore_b * csang_2.dV_dC);
+              pose_dV_dx[block1_S_ind], dscore_b * csang_2.dV_dC);
         }
 
         {  // SS dihed
           Real ang_ss(dihed.V), exp_score1(0.0), exp_score2(0.0);
-          exp_score1 = exp(global_params[0].dss_logA1)
-                       * exp(global_params[0].dss_kappa1
-                             * cos(res1_d_multiplier * ang_ss
-                                   - global_params[0].dss_mu1));
-          exp_score2 = exp(global_params[0].dss_logA2)
-                       * exp(global_params[0].dss_kappa2
-                             * cos(res1_d_multiplier * ang_ss
-                                   - global_params[0].dss_mu2));
+          exp_score1 =
+              exp(params.dss_logA1)
+              * exp(params.dss_kappa1
+                    * cos(res1_d_multiplier * ang_ss - params.dss_mu1));
+          exp_score2 =
+              exp(params.dss_logA2)
+              * exp(params.dss_kappa2
+                    * cos(res1_d_multiplier * ang_ss - params.dss_mu2));
           Real score_ss = -log(exp_score1 + exp_score2 + MEST);
-          score += wt_dihSS_ * score_ss;
+          score += WT_DIH_SS * score_ss;
 
           Real dscore_ss(0.0);
-          dscore_ss +=
-              exp_score1 * global_params[0].dss_kappa1
-              * sin(res1_d_multiplier * dihed.V - global_params[0].dss_mu1);
-          dscore_ss +=
-              exp_score2 * global_params[0].dss_kappa2
-              * sin(res1_d_multiplier * dihed.V - global_params[0].dss_mu2);
+          dscore_ss += exp_score1 * params.dss_kappa1
+                       * sin(res1_d_multiplier * dihed.V - params.dss_mu1);
+          dscore_ss += exp_score2 * params.dss_kappa2
+                       * sin(res1_d_multiplier * dihed.V - params.dss_mu2);
           dscore_ss /= (exp_score1 + exp_score2 + MEST);
-          dscore_ss *= wt_dihSS_;
+          dscore_ss *= WT_DIH_SS;
 
           accumulate<D, Vec<Real, 3>>::add(
-              dV_dx[0][pose_index][block1_CB_ind], dscore_ss * dihed.dV_dI);
+              pose_dV_dx[block1_CB_ind], dscore_ss * dihed.dV_dI);
           accumulate<D, Vec<Real, 3>>::add(
-              dV_dx[0][pose_index][block1_S_ind], dscore_ss * dihed.dV_dJ);
+              pose_dV_dx[block1_S_ind], dscore_ss * dihed.dV_dJ);
           accumulate<D, Vec<Real, 3>>::add(
-              dV_dx[0][pose_index][block2_S_ind], dscore_ss * dihed.dV_dK);
+              pose_dV_dx[block2_S_ind], dscore_ss * dihed.dV_dK);
           accumulate<D, Vec<Real, 3>>::add(
-              dV_dx[0][pose_index][block2_CB_ind], dscore_ss * dihed.dV_dL);
+              pose_dV_dx[block2_CB_ind], dscore_ss * dihed.dV_dL);
         }
 
         {  // CB-S dihed
           Real angle1(disulf_ca_dihedral_angle_1.V),
               angle2(disulf_ca_dihedral_angle_2.V);
-          Real exp_score1 = exp(global_params[0].dcs_logA1)
-                            * exp(global_params[0].dcs_kappa1
-                                  * cos(res1_d_multiplier * angle1
-                                        - global_params[0].dcs_mu1));
-          Real exp_score2 = exp(global_params[0].dcs_logA2)
-                            * exp(global_params[0].dcs_kappa2
-                                  * cos(res1_d_multiplier * angle1
-                                        - global_params[0].dcs_mu2));
-          Real exp_score3 = exp(global_params[0].dcs_logA3)
-                            * exp(global_params[0].dcs_kappa3
-                                  * cos(res1_d_multiplier * angle1
-                                        - global_params[0].dcs_mu3));
+          Real exp_score1 =
+              exp(params.dcs_logA1)
+              * exp(params.dcs_kappa1
+                    * cos(res1_d_multiplier * angle1 - params.dcs_mu1));
+          Real exp_score2 =
+              exp(params.dcs_logA2)
+              * exp(params.dcs_kappa2
+                    * cos(res1_d_multiplier * angle1 - params.dcs_mu2));
+          Real exp_score3 =
+              exp(params.dcs_logA3)
+              * exp(params.dcs_kappa3
+                    * cos(res1_d_multiplier * angle1 - params.dcs_mu3));
           score +=
-              wt_dihCS_ * (-log(exp_score1 + exp_score2 + exp_score3 + MEST));
+              WT_DIH_CS * (-log(exp_score1 + exp_score2 + exp_score3 + MEST));
 
           Real dscore_cs = 0.0;
-          dscore_cs += exp_score1 * global_params[0].dcs_kappa1
-                       * sin(angle1 - global_params[0].dcs_mu1);
-          dscore_cs += exp_score2 * global_params[0].dcs_kappa2
-                       * sin(angle1 - global_params[0].dcs_mu2);
-          dscore_cs += exp_score3 * global_params[0].dcs_kappa3
-                       * sin(angle1 - global_params[0].dcs_mu3);
+          dscore_cs +=
+              exp_score1 * params.dcs_kappa1 * sin(angle1 - params.dcs_mu1);
+          dscore_cs +=
+              exp_score2 * params.dcs_kappa2 * sin(angle1 - params.dcs_mu2);
+          dscore_cs +=
+              exp_score3 * params.dcs_kappa3 * sin(angle1 - params.dcs_mu3);
           dscore_cs /= (exp_score1 + exp_score2 + exp_score3 + MEST);
-          dscore_cs *= wt_dihCS_;
+          dscore_cs *= WT_DIH_CS;
 
           accumulate<D, Vec<Real, 3>>::add(
-              dV_dx[0][pose_index][block1_CA_ind],
+              pose_dV_dx[block1_CA_ind],
               dscore_cs * disulf_ca_dihedral_angle_1.dV_dI);
           accumulate<D, Vec<Real, 3>>::add(
-              dV_dx[0][pose_index][block1_CB_ind],
+              pose_dV_dx[block1_CB_ind],
               dscore_cs * disulf_ca_dihedral_angle_1.dV_dJ);
           accumulate<D, Vec<Real, 3>>::add(
-              dV_dx[0][pose_index][block1_S_ind],
+              pose_dV_dx[block1_S_ind],
               dscore_cs * disulf_ca_dihedral_angle_1.dV_dK);
           accumulate<D, Vec<Real, 3>>::add(
-              dV_dx[0][pose_index][block2_S_ind],
+              pose_dV_dx[block2_S_ind],
               dscore_cs * disulf_ca_dihedral_angle_1.dV_dL);
 
-          exp_score1 = exp(global_params[0].dcs_logA1)
-                       * exp(global_params[0].dcs_kappa1
-                             * cos(res2_d_multiplier * angle2
-                                   - global_params[0].dcs_mu1));
-          exp_score2 = exp(global_params[0].dcs_logA2)
-                       * exp(global_params[0].dcs_kappa2
-                             * cos(res2_d_multiplier * angle2
-                                   - global_params[0].dcs_mu2));
-          exp_score3 = exp(global_params[0].dcs_logA3)
-                       * exp(global_params[0].dcs_kappa3
-                             * cos(res2_d_multiplier * angle2
-                                   - global_params[0].dcs_mu3));
+          exp_score1 =
+              exp(params.dcs_logA1)
+              * exp(params.dcs_kappa1
+                    * cos(res2_d_multiplier * angle2 - params.dcs_mu1));
+          exp_score2 =
+              exp(params.dcs_logA2)
+              * exp(params.dcs_kappa2
+                    * cos(res2_d_multiplier * angle2 - params.dcs_mu2));
+          exp_score3 =
+              exp(params.dcs_logA3)
+              * exp(params.dcs_kappa3
+                    * cos(res2_d_multiplier * angle2 - params.dcs_mu3));
           score +=
-              wt_dihCS_ * (-log(exp_score1 + exp_score2 + exp_score3 + MEST));
+              WT_DIH_CS * (-log(exp_score1 + exp_score2 + exp_score3 + MEST));
 
           dscore_cs = 0.0;
-          dscore_cs += exp_score1 * global_params[0].dcs_kappa1
-                       * sin(angle2 - global_params[0].dcs_mu1);
-          dscore_cs += exp_score2 * global_params[0].dcs_kappa2
-                       * sin(angle2 - global_params[0].dcs_mu2);
-          dscore_cs += exp_score3 * global_params[0].dcs_kappa3
-                       * sin(angle2 - global_params[0].dcs_mu3);
+          dscore_cs +=
+              exp_score1 * params.dcs_kappa1 * sin(angle2 - params.dcs_mu1);
+          dscore_cs +=
+              exp_score2 * params.dcs_kappa2 * sin(angle2 - params.dcs_mu2);
+          dscore_cs +=
+              exp_score3 * params.dcs_kappa3 * sin(angle2 - params.dcs_mu3);
           dscore_cs /= (exp_score1 + exp_score2 + exp_score3 + MEST);
-          dscore_cs *= wt_dihCS_;
+          dscore_cs *= WT_DIH_CS;
 
           accumulate<D, Vec<Real, 3>>::add(
-              dV_dx[0][pose_index][block2_CA_ind],
+              pose_dV_dx[block2_CA_ind],
               dscore_cs * disulf_ca_dihedral_angle_2.dV_dI);
           accumulate<D, Vec<Real, 3>>::add(
-              dV_dx[0][pose_index][block2_CB_ind],
+              pose_dV_dx[block2_CB_ind],
               dscore_cs * disulf_ca_dihedral_angle_2.dV_dJ);
           accumulate<D, Vec<Real, 3>>::add(
-              dV_dx[0][pose_index][block2_S_ind],
+              pose_dV_dx[block2_S_ind],
               dscore_cs * disulf_ca_dihedral_angle_2.dV_dK);
           accumulate<D, Vec<Real, 3>>::add(
-              dV_dx[0][pose_index][block1_S_ind],
+              pose_dV_dx[block1_S_ind],
               dscore_cs * disulf_ca_dihedral_angle_2.dV_dL);
         }
 
-        accumulate<D, Real>::add(V[0][pose_index], score);
+        accumulate<D, Real>::add(pose_V, score);
       }
     }
   });
