aecc1a3502726efff9638c1978b82830d96a71aa
Jeff Flatten <jflat06@cs.washington.edu>
2023-05-05 11:40:11 -0700

    Remove unnecessary param class from disulfide potentials.

--- tmol/score/disulfide/potentials/params.hh
+++ tmol/score/disulfide/potentials/params.hh
@@ -47,49 +47,6 @@ struct DisulfideGlobalParams {
   Real shift;
 };
 
-template <typename Real, tmol::Device D>
-struct DisulfideGlobalParamTensors {
-  TView<Real, 1, D> d_location;
-  TView<Real, 1, D> d_scale;
-  TView<Real, 1, D> d_shape;
-
-  TView<Real, 1, D> a_logA;
-  TView<Real, 1, D> a_kappa;
-  TView<Real, 1, D> a_mu;
-
-  TView<Real, 1, D> dss_logA1;
-  TView<Real, 1, D> dss_kappa1;
-  TView<Real, 1, D> dss_mu1;
-  TView<Real, 1, D> dss_logA2;
-  TView<Real, 1, D> dss_kappa2;
-  TView<Real, 1, D> dss_mu2;
-
-  TView<Real, 1, D> dcs_logA1;
-  TView<Real, 1, D> dcs_mu1;
-  TView<Real, 1, D> dcs_kappa1;
-  TView<Real, 1, D> dcs_logA2;
-  TView<Real, 1, D> dcs_mu2;
-  TView<Real, 1, D> dcs_kappa2;
-  TView<Real, 1, D> dcs_logA3;
-  TView<Real, 1, D> dcs_mu3;
-  TView<Real, 1, D> dcs_kappa3;
-
-  template <typename Idx>
-  auto operator[](Idx i) const {
-    return DisulfideGlobalParams<Real>{
-        d_location[i], d_scale[i],    d_shape[i],
-
-        a_logA[i],     a_kappa[i],    a_mu[i],
-
-        dss_logA1[i],  dss_kappa1[i], dss_mu1[i],
-        dss_logA2[i],  dss_kappa2[i], dss_mu2[i],
-
-        dcs_logA1[i],  dcs_mu1[i],    dcs_kappa1[i],
-        dcs_logA2[i],  dcs_mu2[i],    dcs_kappa2[i],
-        dcs_logA3[i],  dcs_mu3[i],    dcs_kappa3[i]};
-  }
-};
-
 }  // namespace potentials
 }  // namespace disulfide
 }  // namespace score
