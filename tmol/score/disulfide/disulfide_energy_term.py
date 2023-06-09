2d128a70cbe05392b26b56074450354abe35d184
Jeff Flatten <jflat06@cs.washington.edu>
2023-06-09 12:45:35 -0700

    Revert "Move dslf parameter stacking into packed block types to avoid running every time we score."
    This reverts commit 99023c48306a08c616d2f38c0071aae5c5ff2d37.

--- tmol/score/disulfide/disulfide_energy_term.py
+++ tmol/score/disulfide/disulfide_energy_term.py
@@ -52,7 +52,6 @@ class DisulfideEnergyTerm(EnergyTerm):
         super(DisulfideEnergyTerm, self).setup_packed_block_types(packed_block_types)
 
         if hasattr(packed_block_types, "disulfide_conns"):
-            assert hasattr(packed_block_types, "global_params")
             return
 
         disulfide_conns = torch.full(
@@ -62,49 +61,11 @@ class DisulfideEnergyTerm(EnergyTerm):
             device=self.device,
         )
 
-        def _t(ts):
-            return tuple(map(lambda t: t.to(torch.float), ts))
-
-        global_params = torch.stack(
-            _t(
-                [
-                    self.global_params.d_location,
-                    self.global_params.d_scale,
-                    self.global_params.d_shape,
-                    self.global_params.a_logA,
-                    self.global_params.a_kappa,
-                    self.global_params.a_mu,
-                    self.global_params.dss_logA1,
-                    self.global_params.dss_kappa1,
-                    self.global_params.dss_mu1,
-                    self.global_params.dss_logA2,
-                    self.global_params.dss_kappa2,
-                    self.global_params.dss_mu2,
-                    self.global_params.dcs_logA1,
-                    self.global_params.dcs_mu1,
-                    self.global_params.dcs_kappa1,
-                    self.global_params.dcs_logA2,
-                    self.global_params.dcs_mu2,
-                    self.global_params.dcs_kappa2,
-                    self.global_params.dcs_logA3,
-                    self.global_params.dcs_mu3,
-                    self.global_params.dcs_kappa3,
-                    self.global_params.wt_dih_ss,
-                    self.global_params.wt_dih_cs,
-                    self.global_params.wt_ang,
-                    self.global_params.wt_len,
-                    self.global_params.shift,
-                ]
-            ),
-            dim=1,
-        )
-
         for i, bt in enumerate(packed_block_types.active_block_types):
             for conn in bt.disulfide_connections:
                 disulfide_conns[i, conn] = True
 
         setattr(packed_block_types, "disulfide_conns", disulfide_conns)
-        setattr(packed_block_types, "global_params", global_params)
 
     def setup_poses(self, poses: PoseStack):
         super(DisulfideEnergyTerm, self).setup_poses(poses)
@@ -118,5 +79,5 @@ class DisulfideEnergyTerm(EnergyTerm):
             pose_stack_inter_block_connections=pose_stack.inter_residue_connections,
             bt_disulfide_conns=pbt.disulfide_conns,
             bt_atom_downstream_of_conn=pbt.atom_downstream_of_conn,
-            global_params=pbt.global_params,
+            global_params=self.global_params,
         )
