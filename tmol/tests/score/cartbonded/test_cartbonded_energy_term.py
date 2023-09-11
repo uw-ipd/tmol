74367b4589a48bfa7bfe61a164d765e3cd255d17
Jeff Flatten <jflat06@cs.washington.edu>
2023-09-11 12:53:38 -0700

    Add test for jagged poses.

--- tmol/tests/score/cartbonded/test_cartbonded_energy_term.py
+++ tmol/tests/score/cartbonded/test_cartbonded_energy_term.py
@@ -132,3 +132,51 @@ def test_whole_pose_scoring_module_10(
     numpy.testing.assert_allclose(
         gold_vals, scores.cpu().detach().numpy(), atol=1e-5, rtol=1e-5
     )
+
+
+def test_whole_pose_scoring_module_jagged(
+    rts_ubq_res, default_database, torch_device: torch.device
+):
+    rts_ubq_60 = rts_ubq_res[:60]
+    rts_ubq_40 = rts_ubq_res[:40]
+    gold_vals = numpy.array(
+        [
+            [37.7848, 30.0712, 19.7317],
+            [183.5777, 149.7569, 107.8989],
+            [50.5842, 38.3034, 24.6760],
+            [9.4305, 6.9274, 5.4583],
+            [47.4197, 38.3253, 29.3032],
+        ],
+        dtype=numpy.float32,
+    )
+    cartbonded_energy = CartBondedEnergyTerm(
+        param_db=default_database, device=torch_device
+    )
+    p1 = PoseStackBuilder.one_structure_from_polymeric_residues(
+        res=rts_ubq_res, device=torch_device
+    )
+    p2 = PoseStackBuilder.one_structure_from_polymeric_residues(
+        res=rts_ubq_60, device=torch_device
+    )
+    p3 = PoseStackBuilder.one_structure_from_polymeric_residues(
+        res=rts_ubq_40, device=torch_device
+    )
+    pn = PoseStackBuilder.from_poses([p1, p2, p3], device=torch_device)
+
+    for bt in pn.packed_block_types.active_block_types:
+        cartbonded_energy.setup_block_type(bt)
+    cartbonded_energy.setup_packed_block_types(pn.packed_block_types)
+    cartbonded_energy.setup_poses(pn)
+
+    cartbonded_pose_scorer = cartbonded_energy.render_whole_pose_scoring_module(pn)
+
+    coords = torch.nn.Parameter(pn.coords.clone())
+    scores = cartbonded_pose_scorer(coords)
+
+    # make sure we're still good
+    torch.arange(100, device=torch_device)
+    print(scores.cpu().detach().numpy())
+
+    numpy.testing.assert_allclose(
+        gold_vals, scores.cpu().detach().numpy(), atol=1e-5, rtol=1e-5
+    )
