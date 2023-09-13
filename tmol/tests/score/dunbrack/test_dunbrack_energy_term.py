c09d5fa96a2970d3ee6f3665288cdfc19551f067
Jeff Flatten <jflat06@cs.washington.edu>
2023-09-12 17:39:54 -0700

    Expand on tests, add jagged test. jagged test currently fails on cuda.

--- tmol/tests/score/dunbrack/test_dunbrack_energy_term.py
+++ tmol/tests/score/dunbrack/test_dunbrack_energy_term.py
@@ -15,7 +15,6 @@ def test_smoke(default_database, torch_device: torch.device):
     dunbrack_energy = DunbrackEnergyTerm(param_db=default_database, device=torch_device)
 
     assert dunbrack_energy.device == torch_device
-    assert dunbrack_energy.global_params.K.device == torch_device
 
 
 def test_annotate_block_types(ubq_res, default_database, torch_device: torch.device):
@@ -26,10 +25,15 @@ def test_annotate_block_types(ubq_res, default_database, torch_device: torch.dev
 
     for bt in bt_list:
         dunbrack_energy.setup_block_type(bt)
-        # assert hasattr(bt, "dunbrack_quad_uaids")
     dunbrack_energy.setup_packed_block_types(pbt)
-    # assert hasattr(pbt, "dunbrack_quad_uaids")
-    # assert pbt.dunbrack_quad_uaids.device == torch_device
+
+    assert hasattr(pbt, "dunbrack_packed_block_data")
+
+    first_tensor = pbt.dunbrack_packed_block_data[0]
+
+    assert first_tensor.device == torch_device
+    dunbrack_energy.setup_packed_block_types(pbt)
+    assert first_tensor is pbt.dunbrack_packed_block_data[0]
 
 
 def test_whole_pose_scoring_module_gradcheck(
@@ -129,6 +133,50 @@ def test_whole_pose_scoring_module_10(
 
     # make sure we're still good
     torch.arange(100, device=torch_device)
+
+    numpy.testing.assert_allclose(
+        gold_vals, scores.cpu().detach().numpy(), atol=1e-5, rtol=1e-5
+    )
+
+
+def test_whole_pose_scoring_module_jagged(
+    rts_ubq_res, default_database, torch_device: torch.device
+):
+    rts_ubq_60 = rts_ubq_res[:60]
+    rts_ubq_40 = rts_ubq_res[:40]
+    gold_vals = numpy.array(
+        [
+            [70.64967, 65.00988, 71.17494],
+            [240.31003, 166.33463, 134.12521],
+            [99.6609, 135.41754, 165.65302],
+        ],
+        dtype=numpy.float32,
+    )
+    dunbrack_energy = DunbrackEnergyTerm(param_db=default_database, device=torch_device)
+    p1 = PoseStackBuilder.one_structure_from_polymeric_residues(
+        res=rts_ubq_res, device=torch_device
+    )
+    p2 = PoseStackBuilder.one_structure_from_polymeric_residues(
+        res=rts_ubq_60, device=torch_device
+    )
+    p3 = PoseStackBuilder.one_structure_from_polymeric_residues(
+        res=rts_ubq_40, device=torch_device
+    )
+    pn = PoseStackBuilder.from_poses([p1, p2, p3], device=torch_device)
+
+    for bt in pn.packed_block_types.active_block_types:
+        dunbrack_energy.setup_block_type(bt)
+    dunbrack_energy.setup_packed_block_types(pn.packed_block_types)
+    dunbrack_energy.setup_poses(pn)
+
+    dunbrack_pose_scorer = dunbrack_energy.render_whole_pose_scoring_module(pn)
+
+    coords = torch.nn.Parameter(pn.coords.clone())
+    scores = dunbrack_pose_scorer(coords)
+
+    # make sure we're still good
+    torch.arange(100, device=torch_device)
+    print(scores.cpu().detach().numpy())
 
     numpy.testing.assert_allclose(
         gold_vals, scores.cpu().detach().numpy(), atol=1e-5, rtol=1e-5
