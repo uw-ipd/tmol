d338f6f5965e1ff728e214888ac83746bce03368
Jeff Flatten <jflat06@cs.washington.edu>
2023-09-13 10:59:19 -0700

    Remove rogue import.

--- tmol/score/dunbrack/dunbrack_energy_term.py
+++ tmol/score/dunbrack/dunbrack_energy_term.py
@@ -15,7 +15,6 @@ from tmol.score.dunbrack.params import ScoringDunbrackDatabaseView
 from tmol.chemical.restypes import RefinedResidueType
 from tmol.pose.packed_block_types import PackedBlockTypes
 from tmol.pose.pose_stack import PoseStack
-from tmol.types.torch import Tensor
 
 from itertools import count
 from functools import partial
