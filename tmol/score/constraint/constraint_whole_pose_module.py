c668ffe35be82f17bb6bb6a9ba7cd7627810291b
Jeff Flatten <jflat06@cs.washington.edu>
2023-09-20 11:56:07 -0700

    Initial commit for constraints

---
+++ tmol/score/constraint/constraint_whole_pose_module.py
@@ -0,0 +1,85 @@
+import torch
+import numpy
+
+
+class ConstraintWholePoseScoringModule(torch.nn.Module):
+    def __init__(
+        self,
+        pose_stack_block_coord_offset,
+        pose_stack_block_types,
+        pose_stack_inter_block_connections,
+        bt_atom_downstream_of_conn,
+    ):
+        super(ConstraintWholePoseScoringModule, self).__init__()
+
+        def _p(t):
+            return torch.nn.Parameter(t, requires_grad=False)
+
+        def _t(ts):
+            return tuple(map(lambda t: t.to(torch.float), ts))
+
+        self.pose_stack_block_coord_offset = _p(pose_stack_block_coord_offset)
+        self.pose_stack_block_types = _p(pose_stack_block_types)
+        self.pose_stack_inter_block_connections = _p(pose_stack_inter_block_connections)
+        self.bt_atom_downstream_of_conn = _p(bt_atom_downstream_of_conn)
+
+    def forward(self, coords):
+        print("HELLO")
+
+        n_cnstrs = 3
+        cnstrs = torch.full(
+            (n_cnstrs, 2, 2), 0, dtype=torch.int32, device=coords.device
+        )
+        for i in range(n_cnstrs):
+            cnstrs[i, 0, 0] = i
+            cnstrs[i, 0, 1] = i
+            cnstrs[i, 1, 0] = i + 1
+            cnstrs[i, 1, 1] = i + 1
+
+        atom_residue_indices = cnstrs[:, :, 0].flatten()
+        atom_atom_indices = cnstrs[:, :, 1].flatten()
+
+        atom_global_atom_indices = (
+            self.pose_stack_block_coord_offset.index_select(1, atom_residue_indices)[0]
+            + atom_atom_indices
+        )
+        print(atom_residue_indices)
+        print(atom_atom_indices)
+        print(atom_global_atom_indices)
+
+        atom1_residue_indices = cnstrs[:, 0, 0]
+        atom1_atom_indices = cnstrs[:, 0, 1]
+
+        atom2_residue_indices = cnstrs[:, 1, 0]
+        atom2_atom_indices = cnstrs[:, 1, 1]
+
+        atom1_global_atom_indices = (
+            self.pose_stack_block_coord_offset.index_select(1, atom1_residue_indices)[0]
+            + atom1_atom_indices
+        )
+        atom2_global_atom_indices = (
+            self.pose_stack_block_coord_offset.index_select(1, atom2_residue_indices)[0]
+            + atom2_atom_indices
+        )
+
+        print(atom1_global_atom_indices)
+        print(atom2_global_atom_indices)
+
+        # print(coords[0, global_atom_indices])
+        print("ATOM1")
+        print(coords[0].index_select(0, atom1_global_atom_indices))
+        print("ATOM2")
+        print(coords[0].index_select(0, atom2_global_atom_indices))
+        c = coords[0]
+        diff = c.index_select(0, atom1_global_atom_indices) - c.index_select(
+            0, atom2_global_atom_indices
+        )
+        dist = diff.pow(2).sum(1).sqrt()
+
+        print("DIST")
+        print(dist)
+
+        ret = torch.unsqueeze(torch.unsqueeze(torch.sum(dist - 4, 0), 0), 0)
+        print(ret)
+        return ret
+        pass
---
+++ tmol/tests/score/constraint/test_constraint_energy_term.py
@@ -0,0 +1,124 @@
+import numpy
+import torch
+
+from tmol.score.constraint.constraint_energy_term import ConstraintEnergyTerm
+from tmol.pose.packed_block_types import residue_types_from_residues, PackedBlockTypes
+from tmol.pose.pose_stack_builder import PoseStackBuilder
+
+from tmol.tests.autograd import gradcheck
+
+
+def test_smoke(default_database, torch_device: torch.device):
+    constraint_energy = ConstraintEnergyTerm(
+        param_db=default_database, device=torch_device
+    )
+
+    assert constraint_energy.device == torch_device
+    assert constraint_energy.global_params.a_mu.device == torch_device
+
+
+def test_annotate_constraint_conns(
+    rts_ubq_res, default_database, torch_device: torch.device
+):
+    constraint_energy = ConstraintEnergyTerm(
+        param_db=default_database, device=torch_device
+    )
+
+    bt_list = residue_types_from_residues(rts_ubq_res)
+    pbt = PackedBlockTypes.from_restype_list(bt_list, torch_device)
+
+    for bt in bt_list:
+        constraint_energy.setup_block_type(bt)
+        assert hasattr(bt, "constraint_connections")
+    constraint_energy.setup_packed_block_types(pbt)
+    assert hasattr(pbt, "constraint_conns")
+    constraint_conns = pbt.constraint_conns
+    constraint_energy.setup_packed_block_types(pbt)
+
+    assert pbt.constraint_conns.device == torch_device
+    assert (
+        pbt.constraint_conns is constraint_conns
+    )  # Test to make sure the parameters remain the same instance
+
+
+def test_whole_pose_scoring_module_gradcheck_whole_pose(
+    rts_ubq_res, default_database, torch_device: torch.device
+):
+    constraint_energy = ConstraintEnergyTerm(
+        param_db=default_database, device=torch_device
+    )
+    p1 = PoseStackBuilder.one_structure_from_polymeric_residues(
+        res=rts_ubq_res, device=torch_device
+    )
+    for bt in p1.packed_block_types.active_block_types:
+        constraint_energy.setup_block_type(bt)
+    constraint_energy.setup_packed_block_types(p1.packed_block_types)
+    constraint_energy.setup_poses(p1)
+
+    constraint_pose_scorer = constraint_energy.render_whole_pose_scoring_module(p1)
+
+    def score(coords):
+        scores = constraint_pose_scorer(coords)
+        return torch.sum(scores)
+
+    gradcheck(score, (p1.coords.requires_grad_(True),), eps=1e-3, atol=1e-2, rtol=5e-3)
+
+
+def test_whole_pose_scoring_module_single(
+    rts_ubq_res, default_database, torch_device: torch.device
+):
+    gold_vals = numpy.array([[-3.25716]], dtype=numpy.float32)
+    constraint_energy = ConstraintEnergyTerm(
+        param_db=default_database, device=torch_device
+    )
+    p1 = PoseStackBuilder.one_structure_from_polymeric_residues(
+        res=rts_ubq_res, device=torch_device
+    )
+    for bt in p1.packed_block_types.active_block_types:
+        constraint_energy.setup_block_type(bt)
+    constraint_energy.setup_packed_block_types(p1.packed_block_types)
+    constraint_energy.setup_poses(p1)
+
+    constraint_pose_scorer = constraint_energy.render_whole_pose_scoring_module(p1)
+
+    coords = torch.nn.Parameter(p1.coords.clone())
+    scores = constraint_pose_scorer(coords)
+
+    # make sure we're still good
+    torch.arange(100, device=torch_device)
+
+    numpy.testing.assert_allclose(
+        gold_vals, scores.cpu().detach().numpy(), atol=1e-5, rtol=1e-5
+    )
+
+
+def test_whole_pose_scoring_module_10(
+    rts_ubq_res, default_database, torch_device: torch.device
+):
+    n_poses = 10
+    gold_vals = numpy.tile(numpy.array([[-3.25716]], dtype=numpy.float32), (n_poses))
+    constraint_energy = ConstraintEnergyTerm(
+        param_db=default_database, device=torch_device
+    )
+    p1 = PoseStackBuilder.one_structure_from_polymeric_residues(
+        res=rts_ubq_res, device=torch_device
+    )
+    pn = PoseStackBuilder.from_poses([p1] * n_poses, device=torch_device)
+
+    for bt in pn.packed_block_types.active_block_types:
+        constraint_energy.setup_block_type(bt)
+    constraint_energy.setup_packed_block_types(pn.packed_block_types)
+
+    constraint_energy.setup_poses(pn)
+
+    constraint_pose_scorer = constraint_energy.render_whole_pose_scoring_module(pn)
+
+    coords = torch.nn.Parameter(pn.coords.clone())
+    scores = constraint_pose_scorer(coords)
+
+    # make sure we're still good
+    torch.arange(100, device=torch_device)
+
+    numpy.testing.assert_allclose(
+        gold_vals, scores.cpu().detach().numpy(), atol=1e-5, rtol=1e-5
+    )
