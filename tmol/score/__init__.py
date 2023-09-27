5a929e3f08198a27845923379cfa5b3430e0b4e3
Andrew Leaver-Fay <aleaverfay@gmail.com>
2023-09-27 12:46:40 -0400

    Enable the disulfide term in beta2016_score_function

--- tmol/score/__init__.py
+++ tmol/score/__init__.py
@@ -21,16 +21,16 @@ def beta2016_score_function(device: torch.device):
     sfxn.set_weight(ScoreType.lk_bridge_uncpl, -0.33)
     sfxn.set_weight(ScoreType.omega, 0.48)
     sfxn.set_weight(ScoreType.rama, 0.50)
-
-    # When these terms come online, here are there weights
-    # sfxn.set_weight(ScoreType.dslf_fa13, 1.25)
-    # sfxn.set_weight(ScoreType.fa_dun_rot, 0.76)
-    # sfxn.set_weight(ScoreType.fa_dun_dev, 0.69)
-    # sfxn.set_weight(ScoreType.fa_dun_semi, 0.78)
+    sfxn.set_weight(ScoreType.disulfide, 1.25)
     sfxn.set_weight(ScoreType.cart_lengths, 0.5)
     sfxn.set_weight(ScoreType.cart_angles, 0.5)
     sfxn.set_weight(ScoreType.cart_torsions, 0.5)
     sfxn.set_weight(ScoreType.cart_impropers, 0.5)
     sfxn.set_weight(ScoreType.cart_hxltorsions, 0.5)
+
+    # When these terms come online, here are there weights
+    # sfxn.set_weight(ScoreType.fa_dun_rot, 0.76)
+    # sfxn.set_weight(ScoreType.fa_dun_dev, 0.69)
+    # sfxn.set_weight(ScoreType.fa_dun_semi, 0.78)
 
     return sfxn
