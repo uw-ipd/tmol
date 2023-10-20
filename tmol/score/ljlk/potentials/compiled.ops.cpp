efa2c61918ffdac54600179ff3092ca6fa5a7bc1
fdimaio <dimaio@uw.edu>
2023-10-20 09:58:28 -0700

    Remove old lk, lj, and lk_ball code

--- tmol/score/ljlk/potentials/compiled.ops.cpp
+++ tmol/score/ljlk/potentials/compiled.ops.cpp
@@ -8,8 +8,6 @@
 #include <tmol/score/common/forall_dispatch.hh>
 #include <tmol/score/common/device_operations.hh>
 
-#include "lj.dispatch.hh"
-#include "lk_isotropic.dispatch.hh"
 #include "ljlk_pose_score.hh"
 #include "rotamer_pair_energy_lj.hh"
 // #include "rotamer_pair_energy_lk.hh"
@@ -23,174 +21,6 @@ using torch::Tensor;
 using torch::autograd::AutogradContext;
 using torch::autograd::Function;
 using torch::autograd::tensor_list;
-
-template <
-    template <
-        template <tmol::Device>
-        class Dispatch,
-        tmol::Device D,
-        typename Real,
-        typename Int>
-    class ScoreDispatch,
-    template <tmol::Device>
-    class DispatchMethod>
-class LJScoreOp : public torch::autograd::Function<
-                      LJScoreOp<ScoreDispatch, DispatchMethod>> {
- public:
-  static Tensor forward(
-      AutogradContext* ctx,
-      Tensor I,
-      Tensor atom_type_I,
-      Tensor J,
-      Tensor atom_type_J,
-      Tensor bonded_path_lengths,
-      Tensor type_params,
-      Tensor global_params) {
-    at::Tensor score;
-    at::Tensor dScore_dI;
-    at::Tensor dScore_dJ;
-
-    using Int = int64_t;
-
-    TMOL_DISPATCH_FLOATING_DEVICE(
-        I.type(), "score_op", ([&] {
-          using Real = scalar_t;
-          constexpr tmol::Device Dev = device_t;
-
-          auto result = ScoreDispatch<DispatchMethod, Dev, Real, Int>::f(
-              TCAST(I),
-              TCAST(atom_type_I),
-              TCAST(J),
-              TCAST(atom_type_J),
-              TCAST(bonded_path_lengths),
-              TCAST(type_params),
-              TCAST(global_params));
-
-          score = std::get<0>(result).tensor;
-          dScore_dI = std::get<1>(result).tensor;
-          dScore_dJ = std::get<2>(result).tensor;
-        }));
-
-    ctx->save_for_backward({dScore_dI, dScore_dJ});
-    return score;
-  }
-
-  static tensor_list backward(AutogradContext* ctx, tensor_list grad_outputs) {
-    auto saved_grads = ctx->get_saved_variables();
-
-    tensor_list result;
-
-    for (auto& saved_grad : saved_grads) {
-      auto ingrad = grad_outputs[0];
-      while (ingrad.dim() < saved_grad.dim()) {
-        ingrad = ingrad.unsqueeze(-1);
-      }
-
-      result.emplace_back(saved_grad * ingrad);
-    }
-
-    int i = 0;
-    auto dI = result[i++];
-    auto dJ = result[i++];
-
-    return {
-        dI,
-        torch::Tensor(),
-        dJ,
-        torch::Tensor(),
-        torch::Tensor(),
-        torch::Tensor(),
-        torch::Tensor(),
-    };
-  }
-};
-
-template <
-    template <
-        template <tmol::Device>
-        class Dispatch,
-        tmol::Device D,
-        typename Real,
-        typename Int>
-    class ScoreDispatch,
-    template <tmol::Device>
-    class DispatchMethod>
-class LKScoreOp : public torch::autograd::Function<
-                      LKScoreOp<ScoreDispatch, DispatchMethod>> {
- public:
-  static Tensor forward(
-      AutogradContext* ctx,
-      Tensor I,
-      Tensor atom_type_I,
-      Tensor heavyatom_inds_I,
-      Tensor J,
-      Tensor atom_type_J,
-      Tensor heavyatom_inds_J,
-      Tensor bonded_path_lengths,
-      Tensor type_params,
-      Tensor global_params) {
-    at::Tensor score;
-    at::Tensor dScore_dI;
-    at::Tensor dScore_dJ;
-
-    using Int = int64_t;
-
-    TMOL_DISPATCH_FLOATING_DEVICE(
-        I.type(), "score_op", ([&] {
-          using Real = scalar_t;
-          constexpr tmol::Device Dev = device_t;
-
-          auto result = ScoreDispatch<DispatchMethod, Dev, Real, Int>::f(
-              TCAST(I),
-              TCAST(atom_type_I),
-              TCAST(heavyatom_inds_I),
-              TCAST(J),
-              TCAST(atom_type_J),
-              TCAST(heavyatom_inds_J),
-              TCAST(bonded_path_lengths),
-              TCAST(type_params),
-              TCAST(global_params));
-
-          score = std::get<0>(result).tensor;
-          dScore_dI = std::get<1>(result).tensor;
-          dScore_dJ = std::get<2>(result).tensor;
-        }));
-
-    ctx->save_for_backward({dScore_dI, dScore_dJ});
-    return score;
-  }
-
-  static tensor_list backward(AutogradContext* ctx, tensor_list grad_outputs) {
-    auto saved_grads = ctx->get_saved_variables();
-
-    tensor_list result;
-
-    for (auto& saved_grad : saved_grads) {
-      auto ingrad = grad_outputs[0];
-      while (ingrad.dim() < saved_grad.dim()) {
-        ingrad = ingrad.unsqueeze(-1);
-      }
-
-      result.emplace_back(saved_grad * ingrad);
-    }
-
-    int i = 0;
-    auto dI = result[i++];
-    auto dJ = result[i++];
-
-    return {
-        dI,
-        torch::Tensor(),
-        torch::Tensor(),
-        dJ,
-        torch::Tensor(),
-        torch::Tensor(),
-        torch::Tensor(),
-        torch::Tensor(),
-        torch::Tensor(),
-    };
-  }
-};
 
 template <template <tmol::Device> class DispatchMethod>
 class LJLKPoseScoreOp
@@ -390,66 +220,6 @@ class LJLKPoseScoreOp
   }
 };
 
-template <
-    template <
-        template <tmol::Device>
-        class Dispatch,
-        tmol::Device D,
-        typename Real,
-        typename Int>
-    class ScoreDispatch,
-    template <tmol::Device>
-    class DispatchMethod>
-Tensor lj_score_op(
-    Tensor I,
-    Tensor atom_type_I,
-    Tensor J,
-    Tensor atom_type_J,
-    Tensor bonded_path_lengths,
-    Tensor type_params,
-    Tensor global_params) {
-  return LJScoreOp<ScoreDispatch, DispatchMethod>::apply(
-      I,
-      atom_type_I,
-      J,
-      atom_type_J,
-      bonded_path_lengths,
-      type_params,
-      global_params);
-}
-
-template <
-    template <
-        template <tmol::Device>
-        class Dispatch,
-        tmol::Device D,
-        typename Real,
-        typename Int>
-    class ScoreDispatch,
-    template <tmol::Device>
-    class DispatchMethod>
-Tensor lk_score_op(
-    Tensor I,
-    Tensor atom_type_I,
-    Tensor heavyatom_inds_I,
-    Tensor J,
-    Tensor atom_type_J,
-    Tensor heavyatom_inds_J,
-    Tensor bonded_path_lengths,
-    Tensor type_params,
-    Tensor global_params) {
-  return LKScoreOp<ScoreDispatch, DispatchMethod>::apply(
-      I,
-      atom_type_I,
-      heavyatom_inds_I,
-      J,
-      atom_type_J,
-      heavyatom_inds_J,
-      bonded_path_lengths,
-      type_params,
-      global_params);
-}
-
 template <template <tmol::Device> class DispatchMethod>
 Tensor ljlk_pose_scores_op(
     Tensor coords,
@@ -630,14 +400,6 @@ Tensor register_lj_lk_rotamer_pair_energy_eval(
 // See https://stackoverflow.com/a/3221914
 #define TORCH_LIBRARY_(ns, m) TORCH_LIBRARY(ns, m)
 TORCH_LIBRARY_(TORCH_EXTENSION_NAME, m) {
-  m.def("score_ljlk_lj", &lj_score_op<LJDispatch, AABBDispatch>);
-  m.def("score_ljlk_lj_triu", &lj_score_op<LJDispatch, AABBTriuDispatch>);
-  m.def(
-      "score_ljlk_lk_isotropic",
-      &lk_score_op<LKIsotropicDispatch, AABBDispatch>);
-  m.def(
-      "score_ljlk_lk_isotropic_triu",
-      &lk_score_op<LKIsotropicDispatch, AABBTriuDispatch>);
   m.def("ljlk_pose_scores", &ljlk_pose_scores_op<DeviceOperations>);
   m.def("score_ljlk_inter_system_scores", &rotamer_pair_energies_op);
   m.def(
