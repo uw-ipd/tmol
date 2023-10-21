0bcfae71ccc90fe71995a27e67b593231f93c80b
fdimaio <dimaio@uw.edu>
2023-10-20 17:54:39 -0700

    linting

--- tmol/tests/score/lk_ball/potentials/test_compiled_lk_ball.py
+++ tmol/tests/score/lk_ball/potentials/test_compiled_lk_ball.py
@@ -176,7 +176,6 @@ def lkball_score_and_gradcheck(
 ):
     from .compiled import LKBallScore, LKFraction, LKBridgeFraction
 
-    aidx_i = ljlk_params.atom_type_index.get_loc(at_i)
     aidx_j = ljlk_params.atom_type_index.get_loc(at_j)
 
     op = LKBallScore(ljlk_params, atype_params)
