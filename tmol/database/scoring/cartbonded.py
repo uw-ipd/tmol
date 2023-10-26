c6e7414786c69173ae68bb7c366c3785d786b8d3
fdimaio <dimaio@uw.edu>
2023-10-26 13:52:28 -0700

    Remove more unneeded code

--- tmol/database/scoring/cartbonded.py
+++ tmol/database/scoring/cartbonded.py
@@ -5,69 +5,6 @@ import yaml
 from typing import Tuple
 
 
-# TODO: remove all the 'Old' versions when we remove the non-res-centric version of cartbonded.
-@attr.s(auto_attribs=True, slots=True, frozen=True)
-class LengthGroupOld:
-    res: str
-    atm1: str
-    atm2: str
-    x0: float
-    K: float
-
-
-@attr.s(auto_attribs=True, slots=True, frozen=True)
-class AngleGroupOld:
-    res: str
-    atm1: str
-    atm2: str
-    atm3: str
-    x0: float
-    K: float
-
-
-@attr.s(auto_attribs=True, slots=True, frozen=True)
-class TorsionGroupOld:
-    res: str
-    atm1: str
-    atm2: str
-    atm3: str
-    atm4: str
-    x0: float
-    K: float
-    period: int = 1
-
-
-@attr.s(auto_attribs=True, slots=True, frozen=True)
-class HxlTorsionGroupOld:
-    res: str
-    atm1: str
-    atm2: str
-    atm3: str
-    atm4: str
-    k1: float = 0.0
-    phi1: float = 0.0
-    k2: float = 0.0
-    phi2: float = 0.0
-    k3: float = 0.0
-    phi3: float = 0.0
-    type: int = 0
-
-
-@attr.s(auto_attribs=True, frozen=True, slots=True)
-class CartBondedDatabaseOld:
-    length_parameters: Tuple[LengthGroupOld, ...]
-    angle_parameters: Tuple[AngleGroupOld, ...]
-    torsion_parameters: Tuple[TorsionGroupOld, ...]
-    improper_parameters: Tuple[TorsionGroupOld, ...]
-    hxltorsion_parameters: Tuple[HxlTorsionGroupOld, ...]
-
-    @classmethod
-    def from_file(cls, path):
-        with open(path, "r") as infile:
-            raw = yaml.safe_load(infile)
-        return cattr.structure(raw, cls)
-
-
 @attr.s(auto_attribs=True, slots=True, frozen=True)
 class LengthGroup:
     atm1: str
