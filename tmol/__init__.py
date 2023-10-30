ca0ad2c19d82ccd433f7ab52ee330c8b2527eb7c
Andrew Leaver-Fay <aleaverfay@gmail.com>
2023-10-30 13:21:06 -0400

    Add method to API

--- tmol/__init__.py
+++ tmol/__init__.py
@@ -4,6 +4,9 @@ from tmol.io.canonical_ordering import (  # noqa: F401
     canonical_form_from_pdb_lines,
 )
 from tmol.io.pose_stack_construction import pose_stack_from_canonical_form  # noqa: F401
+from tmol.io.details.canonical_packed_block_types import (  # noqa: F401
+    default_canonical_packed_block_types,
+)
 from tmol.score import beta2016_score_function  # noqa: F401
 
 
