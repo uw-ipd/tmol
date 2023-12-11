644d48a70d101d98dc91373451c2430906c0c94d
Andrew Leaver-Fay <aleaverfay@gmail.com>
2023-12-11 17:14:17 -0500

    Add another test to make sure the openfold canonical form remains unchanged over time
    This is essentially a regression test.
    
    Also put the tensors in the saved .pt files onto the CPU before serialization
    so that they can be used in the CPU-only unit tests

--- tmol/tests/io/test_pose_stack_from_openfold.py
+++ tmol/tests/io/test_pose_stack_from_openfold.py
@@ -1,5 +1,10 @@
+import os
+import torch
+
+
 from tmol.io.pose_stack_from_openfold import (
     pose_stack_from_openfold,
+    canonical_form_from_openfold,
     _paramdb_for_openfold,
     canonical_ordering_for_openfold,
     packed_block_types_for_openfold,
@@ -15,6 +20,21 @@ def test_create_pose_stack_from_openfold_result(
     assert ps.coords.device == torch_device
     pbt = packed_block_types_for_openfold(torch_device)
     assert ps.packed_block_types is pbt
+
+
+def test_create_canonical_form_from_openfold_stability(
+    openfold_ubq_and_sumo_pred, torch_device
+):
+    cf = canonical_form_from_openfold(openfold_ubq_and_sumo_pred)
+    gold_cf_path = os.path.join(
+        __file__.rpartition("/")[0], "gold_ubq_and_sumo_openfold_canform.pt"
+    )
+    # if torch_device == torch.device("cpu"):
+    #     torch.save(cf, gold_cf_path)
+    gold_cf = torch.load(gold_cf_path)
+    for n, t in gold_cf.items():
+        assert n in cf
+        torch.testing.assert_close(t, cf[n].cpu(), equal_nan=True, atol=1e-5, rtol=1e-5)
 
 
 def test_memoization_of_openfold_paramdb():
