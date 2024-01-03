cf3865494d1d32856e7aae5cd556dcf5e05c7e54
Andrew Leaver-Fay <aleaverfay@gmail.com>
2024-01-03 10:56:37 -0500

    Deleting no-longer-needed file

--- tmol/io/details/canonical_packed_block_types.py
+++
@@ -1,7 +0,0 @@
-# import cattr
-# import torch
-#
-# from tmol.database import ParameterDatabase
-# from tmol.pose.packed_block_types import PackedBlockTypes
-# from tmol.chemical.restypes import RefinedResidueType
-# import toolz.functoolz
