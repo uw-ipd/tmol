44765b4831391248639104bed2034e5a30cb167c
fdimaio <dimaio@uw.edu>
2025-02-28 13:32:44 -0800

    Fix bug in device checking.

--- tmol/utility/function_dispatch/aten.hh
+++ tmol/utility/function_dispatch/aten.hh
@@ -2,16 +2,17 @@
 
 #include <ATen/Dispatch.h>
 #include <tmol/utility/tensor/TensorAccessor.h>
+#include <iostream>
 
 #ifdef WITH_CUDA
 
 #define TMOL_DISPATCH_FLOATING_DEVICE(TYPE, NAME, ...)                 \
   [&] {                                                                \
-    if (TYPE.device() == at::DeviceType::CPU) {                        \
+    if (TYPE.device().is_cpu()) {                                      \
       constexpr tmol::Device device_t = tmol::Device::CPU;             \
       AT_DISPATCH_FLOATING_TYPES(                                      \
           c10::typeMetaToScalarType(TYPE.dtype()), NAME, __VA_ARGS__); \
-    } else if (TYPE.device() == at::DeviceType::CUDA) {                \
+    } else if (TYPE.device().is_cuda()) {                              \
       constexpr tmol::Device device_t = tmol::Device::CUDA;            \
       AT_DISPATCH_FLOATING_TYPES(                                      \
           c10::typeMetaToScalarType(TYPE.dtype()), NAME, __VA_ARGS__); \
@@ -24,11 +25,11 @@
 
 #define TMOL_DISPATCH_FLOATING_DEVICE(TYPE, NAME, ...)                 \
   [&] {                                                                \
-    if (TYPE.device() == at::DeviceType::CPU) {                        \
+    if (TYPE.device().is_cpu()) {                                      \
       constexpr tmol::Device device_t = tmol::Device::CPU;             \
       AT_DISPATCH_FLOATING_TYPES(                                      \
           c10::typeMetaToScalarType(TYPE.dtype()), NAME, __VA_ARGS__); \
-    } else if (TYPE.device() == at::DeviceType::CUDA) {                \
+    } else if (TYPE.device().is_cuda()) {                              \
       AT_ERROR("Unsupported cuda tensor, non-cuda build.");            \
     } else {                                                           \
       AT_ERROR("Unsupported device type.");                            \
@@ -41,11 +42,11 @@
 
 #define TMOL_DISPATCH_INDEX_DEVICE(TYPE, NAME, ...)                    \
   [&] {                                                                \
-    if (TYPE.device() == at::DeviceType::CPU) {                        \
+    if (TYPE.device().is_cpu()) {                                      \
       constexpr tmol::Device device_t = tmol::Device::CPU;             \
       AT_DISPATCH_INDEX_TYPES(                                         \
           c10::typeMetaToScalarType(TYPE.dtype()), NAME, __VA_ARGS__); \
-    } else if (TYPE.device() == at::DeviceType::CUDA) {                \
+    } else if (TYPE.device().is_cuda()) {                              \
       constexpr tmol::Device device_t = tmol::Device::CUDA;            \
       AT_DISPATCH_INDEX_TYPES(                                         \
           c10::typeMetaToScalarType(TYPE.dtype()), NAME, __VA_ARGS__); \
@@ -58,11 +59,11 @@
 
 #define TMOL_DISPATCH_INDEX_DEVICE(TYPE, NAME, ...)                    \
   [&] {                                                                \
-    if (TYPE.device() == at::DeviceType::CPU) {                        \
+    if (TYPE.device().is_cpu()) {                                      \
       constexpr tmol::Device device_t = tmol::Device::CPU;             \
       AT_DISPATCH_INDEX_TYPES(                                         \
           c10::typeMetaToScalarType(TYPE.dtype()), NAME, __VA_ARGS__); \
-    } else if (TYPE.device() == at::DeviceType::CUDA) {                \
+    } else if (TYPE.device().is_cuda()) {                              \
       AT_ERROR("Unsupported cuda tensor, non-cuda build.");            \
     } else {                                                           \
       AT_ERROR("Unsupported device type.");                            \
