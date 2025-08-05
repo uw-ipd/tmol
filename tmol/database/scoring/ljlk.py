3eb27ae05ae348db83f636bfac8e31a1f7fffe58
Andrew Leaver-Fay <aleaverfay@gmail.com>
2025-08-05 20:12:11 +0000

    Add controllable parameter dlin_sigma_scale for Lennard-Jones
    This parameter is the fraction of the Lennard-Jones distance, sigma,
    at which the potential shifts from its 1/d^12 form to a linear
    repulsive energy. The closer this parameter is to 1, the softer the
    repulsive forces

--- tmol/database/scoring/ljlk.py
+++ tmol/database/scoring/ljlk.py
@@ -16,6 +16,7 @@ class LJLKGlobalParameters:
     lj_hbond_dis: float
     lj_hbond_hdis: float
     lj_switch_dis2sigma: float
+    lj_dlin_sigma_factor: float
     lk_min_dis2sigma: float
     lkb_water_dist: float
     lkb_water_angle_sp2: Angle
