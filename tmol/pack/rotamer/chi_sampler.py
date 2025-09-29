Fix off-by-one error in IncludeCurrentSampler due to presence of virtual root in KTO

tmol/pack/rotamer/chi_sampler.py
@@ -418,9 +418,9 @@ def assign_chi_dofs_from_samples(
         dtype=torch.int64,
         device=pbt.device,
     )
+
     # increment with the atom offsets for the source rotamer and by
     # one to include the virtual root
-
     rot_chi_atoms_kto += (
         n_dof_atoms_offset_for_rot[global_rot_ind_for_real_atom].to(torch.int64) + 1
     )

tmol/tests/pack/rotamer/test_build_rotamers.py
@@ -21,9 +21,18 @@ from tmol.pack.rotamer.build_rotamers import (
     get_rotamer_origin_data,
 )
 from tmol.pack.rotamer.chi_sampler import (
-    # copy_dofs_from_orig_to_rotamers_for_sampler
     create_dof_inds_to_copy_from_orig_to_rotamers_for_sampler,
-    # assign_chi_dofs_from_samples
+    # copy_dofs_from_orig_to_rotamers_for_sampler
+)
+from tmol.pack.rotamer.dunbrack.dunbrack_chi_sampler import (
+    DunbrackChiSampler,
+)
+from tmol.pack.rotamer.fixed_aa_chi_sampler import (
+    FixedAAChiSampler,
+)
+from tmol.pack.rotamer.include_current_sampler import (
+    IncludeCurrentSampler,
+    create_full_dof_inds_to_copy_from_orig_to_rotamers_for_include_current_sampler,
 )
 
 
@@ -1663,7 +1672,33 @@ def test_new_rotamer_building_logic3(
     task = PackerTask(poses, palette)
     task.restrict_to_repacking()
 
-    residues_to_fix = [(0, 0), (0, 12), (0, 24), (1, 18), (1, 31), (1, 52)]
+    residues_to_fix = [
+        (0, 0),
+        (0, 1),
+        (0, 2),
+        (0, 3),
+        (0, 7),
+        (0, 8),
+        (0, 9),
+        (0, 10),
+        (0, 11),
+        (0, 12),
+        (0, 15),
+        (0, 16),
+        (0, 17),
+        (0, 18),
+        (0, 20),
+        (0, 21),
+        (0, 22),
+        (0, 23),
+        (0, 24),
+        (0, 25),
+        (0, 26),
+        (0, 27),
+        (1, 18),
+        (1, 31),
+        (1, 52),
+    ]
     for pose, res in residues_to_fix:
         task.blts[pose][res].disable_packing()
 
@@ -1674,7 +1709,6 @@ def test_new_rotamer_building_logic3(
     ###########################################
 
     poses, rotamer_set = build_rotamers(poses, task, chem_db)
-    print("rotamer_set.block_ind_for_rot", rotamer_set.block_ind_for_rot.shape)
 
     for p, r in residues_to_fix:
         # first rotamer for block will also be the block's only rotamer
@@ -1687,24 +1721,33 @@ def test_new_rotamer_building_logic3(
         assert current_rot_block == r
         current_rot_n_atoms = poses.packed_block_types.n_atoms[current_rot_blocktype]
         assert current_rot_blocktype == poses.block_type_ind[p, r]
-        print(
-            f"current_rot_coords {p}, {r}, rot {current_rot} xyz offset {current_rot_offset}"
-        )
-        print(
-            rotamer_set.coords[
-                (current_rot_offset) : (current_rot_offset + current_rot_n_atoms), :
-            ]
-        )
+        bt = poses.packed_block_types.active_block_types[current_rot_blocktype]
+
+        # torch.testing.assert_close(
+        #     poses.coords[
+        #         p,
+        #         pose_stack_coord_offset : (
+        #             pose_stack_coord_offset + current_rot_n_atoms
+        #         ),
+        #     ],
+        #     rotamer_set.coords[
+        #         (current_rot_offset) : (current_rot_offset + current_rot_n_atoms), :
+        #     ],
+        #     atol=1e-3,
+        #     rtol=1e-5,
+        # )
+
         pose_stack_coord_offset = poses.block_coord_offset64[p, r]
-        print("pose coords")
-        print(
-            poses.coords[
-                p,
-                pose_stack_coord_offset : (
-                    pose_stack_coord_offset + current_rot_n_atoms
-                ),
-            ]
-        )
+        # dst = torch.linalg.norm(
+        #     poses.coords[
+        #         p,
+        #         pose_stack_coord_offset : (
+        #             pose_stack_coord_offset + current_rot_n_atoms
+        #         ),
+        #     ] - rotamer_set.coords[
+        #         (current_rot_offset) : (current_rot_offset + current_rot_n_atoms), :
+        #     ]
+        # )
 
         torch.testing.assert_close(
             poses.coords[

tmol/tests/pack/test_load_ig.py
@@ -69,6 +69,7 @@ def construct_faux_rotamer_set_and_sparse_energies_table_from_ig(ig, pdb_fname, 
         pose_for_rot, block_ind_for_rot
     ]
 
+    coord_offset_for_rotamer = torch.zeros((n_rots_total,), dtype=torch.int32)
     coords = torch.zeros((pose_stack.max_n_block_atoms, 3), dtype=torch.float32)
 
     rotamer_set = RotamerSet(
@@ -79,6 +80,7 @@ def construct_faux_rotamer_set_and_sparse_energies_table_from_ig(ig, pdb_fname, 
         pose_for_rot=_d(pose_for_rot),
         block_type_ind_for_rot=_d(block_type_ind_for_rot),
         block_ind_for_rot=_d(block_ind_for_rot32),
+        coord_offset_for_rot=_d(coord_offset_for_rotamer),
         coords=_d(coords),
     )
 
@@ -164,10 +166,12 @@ def construct_stacked_faux_rotamer_set_and_sparse_energies_table_from_ig(
     block_ind_for_rot32 = block_ind_for_rot.to(torch.int32)
     block_type_ind_for_rot = pose_stack.block_type_ind64[0, block_ind_for_rot]
 
-    # obviously a bogus size for this tensor
+    # a bogus size for the coords tensor and bogus values for its offsets
+    coord_offset_for_rotamer = torch.zeros((n_rots_total,), dtype=torch.int32)
     coords = torch.zeros((pose_stack.max_n_block_atoms, 3), dtype=torch.float32)
 
     rotamer_set = RotamerSet(
+        max_n_rots_per_pose=torch.max(n_rots_for_pose).item(),
         n_rots_for_pose=_d(n_rots_for_pose),
         rot_offset_for_pose=_d(rot_offset_for_pose),
         n_rots_for_block=_d(n_rots_for_block),
@@ -175,6 +179,7 @@ def construct_stacked_faux_rotamer_set_and_sparse_energies_table_from_ig(
         pose_for_rot=_d(pose_for_rot),
         block_type_ind_for_rot=_d(block_type_ind_for_rot),
         block_ind_for_rot=_d(block_ind_for_rot32),
+        coord_offset_for_rot=_d(coord_offset_for_rotamer),
         coords=_d(coords),
     )
 