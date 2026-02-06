ab57c7aacf42053411ad0ce391608776faef8327
Jeff Flatten <jflat06@cs.washington.edu>
2026-02-05 17:59:54 -0800

    adding some additional tests and fixing file locations

--- tmol/tests/conftest.py
+++ tmol/tests/conftest.py
@@ -22,6 +22,7 @@ from .data import (  # noqa: F401
     openfold_ubq_and_sumo_pred,
     rosettafold2_ubq_pred,
     rosettafold2_sumo_pred,
+    biotite_1ubq,
     biotite_1r21,
     biotite_1bl8,
 )
