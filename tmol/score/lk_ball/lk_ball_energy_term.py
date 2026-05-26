052eaa62c0a80455c00f7419870d0656512b9817
Andrew Leaver-Fay <aleaverfay@gmail.com>
2026-05-26 18:12:32 +0000

    Fixing rebase mistake

--- tmol/score/lk_ball/lk_ball_energy_term.py
+++ tmol/score/lk_ball/lk_ball_energy_term.py
@@ -273,7 +273,7 @@ class LKBallEnergyTerm(AtomTypeDependentTerm, HBondDependentTerm):
             pose_stack.packed_block_types.lk_ball_params.tile_pol_occ_inds,
             pose_stack.packed_block_types.lk_ball_params.tile_lk_ball_params,
             pose_stack.packed_block_types.bond_separation,
-            self.stack_lk_ball_global_params(),
+            self._lk_ball_global_params,
             # max_dis as host scalar for detect-neighbors call
             float(self.ljlk_param_resolver.global_params.max_dis.item()),
             water_coords,
@@ -335,7 +335,7 @@ class LKBallEnergyTerm(AtomTypeDependentTerm, HBondDependentTerm):
             pose_stack.packed_block_types.lk_ball_params.tile_pol_occ_inds,
             pose_stack.packed_block_types.lk_ball_params.tile_lk_ball_params,
             pose_stack.packed_block_types.bond_separation,
-            self.stack_lk_ball_global_params(),
+            self._lk_ball_global_params,
             # max_dis as host scalar for detect-neighbors call
             float(self.ljlk_param_resolver.global_params.max_dis.item()),
             water_coords,
