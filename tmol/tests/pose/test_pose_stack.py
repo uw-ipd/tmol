19e35289fb2891006040e86af33ca9ceaa22b46b
Andrew Leaver-Fay <aleaverfay@gmail.com>
2026-05-29 19:05:45 -0400

    Add code to split out a single Pose from a PoseStack + PDBInfo + ConstraintSet object (#379)
    Add tests with it to make sure that we can split PoseStacks and preserve their content.
    
    Also fix several issues with concatenating ConstraintSets!

--- tmol/tests/pose/test_pose_stack.py
+++ tmol/tests/pose/test_pose_stack.py
@@ -1,5 +1,7 @@
 import numpy
 import torch
+from tmol.io import pose_stack_from_pdb
+from tmol.pose.pose_stack_builder import PoseStackBuilder
 
 
 def test_n_poses(ubq_40_60_pose_stack):
@@ -92,3 +94,69 @@ def test_expand_coords(ubq_40_60_pose_stack, torch_device):
     numpy.testing.assert_equal(
         real_expanded_coords.cpu().numpy(), real_expanded_coords.cpu().numpy()
     )
+
+
+def test_round_trip_irregular_pose_stack_and_split(
+    default_database, ubq_pdb, torch_device
+):
+    n_poses = 4
+    poses = [
+        pose_stack_from_pdb(ubq_pdb, torch_device, residue_start=0, residue_end=20 + i)
+        for i in range(n_poses)
+    ]
+    pose_stack = PoseStackBuilder.from_poses(poses, torch_device)
+    for i in range(n_poses):
+        split_pose_stack = pose_stack.split(i)
+        assert split_pose_stack.n_poses == 1
+        assert split_pose_stack.max_n_blocks == pose_stack.max_n_blocks
+        assert split_pose_stack.max_n_atoms == pose_stack.max_n_atoms
+        assert split_pose_stack.max_n_block_atoms == pose_stack.max_n_block_atoms
+        assert split_pose_stack.max_n_pose_atoms == pose_stack.max_n_pose_atoms
+        i_pose = poses[i]
+        torch.testing.assert_close(
+            split_pose_stack.coords[:, : i_pose.max_n_pose_atoms], i_pose.coords
+        )
+        torch.testing.assert_close(
+            split_pose_stack.block_coord_offset[:, : i_pose.max_n_blocks],
+            i_pose.block_coord_offset,
+        )
+        torch.testing.assert_close(
+            split_pose_stack.block_coord_offset64[:, : i_pose.max_n_blocks],
+            i_pose.block_coord_offset64,
+        )
+
+        torch.testing.assert_close(
+            split_pose_stack.inter_residue_connections[:, : i_pose.max_n_blocks],
+            i_pose.inter_residue_connections,
+        )
+        torch.testing.assert_close(
+            split_pose_stack.inter_residue_connections64[:, : i_pose.max_n_blocks],
+            i_pose.inter_residue_connections64,
+        )
+
+        torch.testing.assert_close(
+            split_pose_stack.inter_block_bondsep[
+                :, : i_pose.max_n_blocks, : i_pose.max_n_blocks
+            ],
+            i_pose.inter_block_bondsep,
+        )
+        torch.testing.assert_close(
+            split_pose_stack.inter_block_bondsep64[
+                :, : i_pose.max_n_blocks, : i_pose.max_n_blocks
+            ],
+            i_pose.inter_block_bondsep64,
+        )
+        torch.testing.assert_close(
+            split_pose_stack.block_type_ind[:, : i_pose.max_n_blocks],
+            i_pose.block_type_ind,
+        )
+        torch.testing.assert_close(
+            split_pose_stack.block_type_ind64[:, : i_pose.max_n_blocks],
+            i_pose.block_type_ind64,
+        )
+        torch.testing.assert_close(
+            split_pose_stack.chain_id[:, : i_pose.max_n_blocks], i_pose.chain_id
+        )
+        torch.testing.assert_close(
+            split_pose_stack.chain_id64[:, : i_pose.max_n_blocks], i_pose.chain_id64
+        )
