83d16496707753d3860199803b8c75700e762b7b
Jeff Flatten <jflat06@cs.washington.edu>
2026-06-12 16:58:03 -0700

    encode weights as yaml, add soft weights, add soft ref weights

--- tmol/score/__init__.py
+++ tmol/score/__init__.py
@@ -2,8 +2,11 @@ from __future__ import annotations
 
 import toolz.functoolz
 import torch
-from typing import Optional, TYPE_CHECKING
+import os
+
 from tmol.database import ParameterDatabase
+
+from typing import Optional, TYPE_CHECKING
 
 if TYPE_CHECKING:
     from .score_function import ScoreFunction
@@ -15,36 +18,18 @@ def _non_memoized_beta2016(
     """Build a beta_nov2016 score function without memoization."""
     from tmol.database import ParameterDatabase
     from .score_function import ScoreFunction
-    from .score_types import ScoreType
 
     if param_db is None:
         param_db = ParameterDatabase.get_default()
 
-    sfxn = ScoreFunction(param_db, device)
-    sfxn.set_weight(ScoreType.fa_ljatr, 1.0)
-    sfxn.set_weight(ScoreType.fa_ljrep, 0.55)
-    sfxn.set_weight(ScoreType.fa_lk, 1.0)
-    sfxn.set_weight(ScoreType.fa_elec, 1.0)
-    sfxn.set_weight(ScoreType.hbond, 1.0)
-    sfxn.set_weight(ScoreType.lk_ball_iso, -0.38)
-    sfxn.set_weight(ScoreType.lk_ball, 0.92)
-    sfxn.set_weight(ScoreType.lk_bridge, -0.33)
-    sfxn.set_weight(ScoreType.lk_bridge_uncpl, -0.33)
-    sfxn.set_weight(ScoreType.omega, 0.48)
-    sfxn.set_weight(ScoreType.rama, 0.50)
-    sfxn.set_weight(ScoreType.disulfide, 1.25)
-    sfxn.set_weight(ScoreType.cart_lengths, 0.5)
-    sfxn.set_weight(ScoreType.cart_angles, 0.5)
-    sfxn.set_weight(ScoreType.cart_torsions, 0.5)
-    sfxn.set_weight(ScoreType.cart_impropers, 0.5)
-    sfxn.set_weight(ScoreType.cart_hxltorsions, 0.5)
-    sfxn.set_weight(ScoreType.dunbrack_rot, 0.76)
-    sfxn.set_weight(ScoreType.dunbrack_rotdev, 0.69)
-    sfxn.set_weight(ScoreType.dunbrack_semirot, 0.78)
-    sfxn.set_weight(ScoreType.ref, 1.0)
-    sfxn.set_weight(ScoreType.gen_torsions, 1.0)
-
-    return sfxn
+    _weights_path = os.path.join(
+        os.path.dirname(__file__),
+        "..",
+        "database",
+        "score_functions",
+        "beta2016.yaml",
+    )
+    return ScoreFunction.from_weights_file(_weights_path, param_db, device)
 
 
 @toolz.functoolz.memoize
