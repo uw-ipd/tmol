20aeae7f503648aa90e39f2a2279011dba3d4fe9
Kieran Didi <kieran.didi@gmail.com>
2026-08-25 18:31:45 +0000

    Exclude virtual leaving atoms from scoring

--- tmol/score/cartbonded/_cartbonded_energy_term.py
+++ tmol/score/cartbonded/_cartbonded_energy_term.py
@@ -124,11 +124,20 @@ class CartBondedEnergyTerm(AtomTypeDependentTerm):
                     for atom1, atom2, atom4 in comb:
                         improper.append((atom1, atom2, atom3, atom4))
 
-        return (
-            lengths,
-            angles,
-            torsions,
-            improper,
+        virtual = {
+            block_type.atom_to_idx[name] for name in block_type.properties.virtual
+        }
+
+        def without_virtual(subgraphs):
+            return [
+                graph
+                for graph in subgraphs
+                if virtual.isdisjoint(atom for atom in graph if atom >= 0)
+            ]
+
+        return tuple(
+            without_virtual(subgraphs)
+            for subgraphs in (lengths, angles, torsions, improper)
         )
 
     def get_raw_params_for_res(self, res: str):
@@ -326,7 +335,6 @@ class CartBondedEnergyTerm(AtomTypeDependentTerm):
         # Fill the hash table
         cur_val = 0
         for bt in packed_block_types.active_block_types:
-
             bt_params = bt.cartbonded_annotations[self.hash]
             for key_w_str, value in bt_params.cartbonded_params.items():
                 key = tuple(cbet_atom_unique_id_index[at] for at in key_w_str)
