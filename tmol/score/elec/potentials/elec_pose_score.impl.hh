967cc7bc028554f434975a457e51b8ee23317e18
Kieran Didi <kieran.didi@gmail.com>
2026-08-25 23:45:13 +0000

    Specialize electrostatics energy evaluation

--- tmol/score/elec/potentials/elec_pose_score.impl.hh
+++ tmol/score/elec/potentials/elec_pose_score.impl.hh
@@ -701,20 +701,14 @@ auto ElecPoseScoreDispatch<DeviceDispatch, D, Real, Int>::forward(
              int start_atom2,
              ElecScoringData<Real> const& score_dat,
              int cp_separation) {
-          if (compute_derivs) {
-            auto val = elec_atom_energy_and_derivs_full(
-                atom_tile_ind1,
-                atom_tile_ind2,
-                start_atom1,
-                start_atom2,
-                score_dat,
-                cp_separation,
-                dV_dcoords);
-            return val;
-          } else {
-            return elec_atom_energy(
-                atom_tile_ind1, atom_tile_ind2, score_dat, cp_separation);
-          }
+          return elec_atom_energy_and_derivs_full(
+              atom_tile_ind1,
+              atom_tile_ind2,
+              start_atom1,
+              start_atom2,
+              score_dat,
+              cp_separation,
+              dV_dcoords);
         });
 
     auto score_inter_elec_atom_pair = ([=] SCORE_INTER_ELEC_ATOM_PAIR);
@@ -847,7 +841,7 @@ auto ElecPoseScoreDispatch<DeviceDispatch, D, Real, Int>::forward(
   // mgpu::standard_context_t context(wrapped_stream.stream());
 
   // 3
-  if (output_block_pair_energies) {
+  if (output_block_pair_energies || !compute_derivs) {
     DeviceDispatch<D>::template foreach_workgroup<launch_t>(
         mgr, n_poses * max_n_upper_triangle_inds, eval_energies_by_block);
   } else {
