fcedd494dfc4f5035a6768b7cc2bde55ccd53fd8
Kieran Didi <kieran.didi@gmail.com>
2026-08-28 14:39:08 +0000

    Apply block-pair weights in place

--- tmol/score/_score_function.py
+++ tmol/score/_score_function.py
@@ -527,7 +527,7 @@ class BlockPairScoringModule:
         if not torch.is_grad_enabled() and coords.requires_grad:
             coords = coords.detach()
         unweighted = self.unweighted_scores(coords)
-        weighted = self.weights * unweighted if apply_weights else unweighted
+        weighted = unweighted.mul_(self.weights) if apply_weights else unweighted
         summed = torch.sum(weighted, dim=0) if sum_terms else weighted
 
         return summed
