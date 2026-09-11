ed6aa9508026533412c1f12be46f9282ed59476b
Kieran Didi <kieran.didi@gmail.com>
2026-09-11 23:13:44 +0000

    Broadcast pose atom masks without repeated index arrays

--- tmol/pose/_pose_stack.py
+++ tmol/pose/_pose_stack.py
@@ -169,13 +169,11 @@ class PoseStack:
     def real_atoms(self) -> Tensor[torch.bool][:, :]:
         """Return the mask of real, non-padding atoms in ``coords``."""
         # get the list of real atoms to read out of pose coords
-        n_ats_per_pose_arange_expanded = (
-            torch.arange(self.max_n_pose_atoms, dtype=torch.int64, device=self.device)
-            .repeat(self.n_poses)
-            .view(self.n_poses, self.max_n_pose_atoms)
+        atom_inds = torch.arange(
+            self.max_n_pose_atoms, dtype=torch.int64, device=self.device
         )
         n_ats_per_pose = torch.sum(self.n_ats_per_block, dim=1).unsqueeze(1)
-        return n_ats_per_pose_arange_expanded < n_ats_per_pose
+        return atom_inds < n_ats_per_pose
 
     def clone(self) -> "PoseStack":
         """Deep-copy clone of this PoseStack"""
@@ -255,14 +253,10 @@ class PoseStack:
         """
 
         # get the list of real atoms that we will be writing to in the 4D tensor
-        n_ats_per_block_arange_expanded = (
-            torch.arange(self.max_n_block_atoms, dtype=torch.int64, device=self.device)
-            .repeat(self.n_poses * self.max_n_blocks)
-            .view(self.n_poses, self.max_n_blocks, self.max_n_block_atoms)
+        atom_inds = torch.arange(
+            self.max_n_block_atoms, dtype=torch.int64, device=self.device
         )
-        real_expanded_pose_ats = (
-            n_ats_per_block_arange_expanded < self.n_ats_per_block.unsqueeze(2)
-        )
+        real_expanded_pose_ats = atom_inds < self.n_ats_per_block.unsqueeze(2)
 
         # now perform the actual copy
         expanded_coords = torch.zeros(
